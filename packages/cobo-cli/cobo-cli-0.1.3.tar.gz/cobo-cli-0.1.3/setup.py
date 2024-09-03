878be5882b5954149d2f0de2e2126d9056204dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 15:04:38 +0000

    Add package cobo-cli/0.1.3/cobo-cli-0.1.3.tar.gz

---
+++ packages/cobo-cli/cobo-cli-0.1.3.tar.gz/cobo-cli-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt", "r") as f:
+    requires = [x.strip() for x in f if x.strip()]
+
+setup(
+    name="cobo-cli",
+    version="0.1.3",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    entry_points={
+        "console_scripts": [
+            "cobo=cobo_cli.commands:cli",
+        ],
+    },
+)
