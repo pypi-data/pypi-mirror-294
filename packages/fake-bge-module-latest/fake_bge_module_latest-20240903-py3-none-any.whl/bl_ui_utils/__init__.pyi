3a99513196236e4ab25936477445b4ca381f0917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 15:04:53 +0000

    Add package fake-bge-module-latest/20240903/fake_bge_module_latest-20240903-py3-none-any.whl

---
+++ packages/fake-bge-module-latest/fake_bge_module_latest-20240903-py3-none-any.whl/bl_ui_utils/__init__.pyi
@@ -0,0 +1,8 @@
+import typing
+import collections.abc
+import typing_extensions
+from . import bug_report_url
+from . import layout
+
+GenericType1 = typing.TypeVar("GenericType1")
+GenericType2 = typing.TypeVar("GenericType2")
