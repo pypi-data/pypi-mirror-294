a3c6029b57d8430d31adfc4c3943f00f11cde1b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 15:03:12 +0000

    Add package fintechff-indicator/0.3.0/fintechff_indicator-0.3.0.tar.gz

---
+++ packages/fintechff-indicator/fintechff_indicator-0.3.0.tar.gz/fintechff_indicator-0.3.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fintechff_indicator",  # 你的包的名称
+    version="0.3.0",  # 包的版本
+    description="A python package for providing fintech indicators",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Jonathan Lee",
+    author_email="lihuapinghust@gmail.com",
+    url="https://github.com/lihuapinghust/fintechff_indicator",  # 项目的URL
+    packages=find_packages(),  # 自动发现包
+    install_requires=[  # 你的包依赖的库
+        "backtrader",
+        "matplotlib",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
