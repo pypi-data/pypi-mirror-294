de148195eddb4a6e9aeb52bbb7e75cf5e123e778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 14:03:26 +0000

    Add package libp2p-kit-py/0.0.5/libp2p_kit_py-0.0.5.tar.gz

---
+++ packages/libp2p-kit-py/libp2p_kit_py-0.0.5.tar.gz/libp2p_kit_py-0.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+	name='lihp2p_kit_py',
+	version='0.0.5',
+	packages=[
+		'libp2p_kit_py',
+	],
+	install_requires=[
+		'datasets',
+		'urllib3',
+		'requests',
+		'boto3',
+        'toml',
+	],
+    package_data={
+        'orbitdb_kit': [
+            'orbitv3-slave-swarm.js',
+        	'package.json',
+            'yarn.lock'
+        ]
+    },
+	include_package_data=True,
+)
