8ffa228baa53f07fbd8ac0367da8de9be3f008e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 03:03:49 +0000

    Add package ylogging/1.2.0/ylogging-1.2.0.tar.gz

---
+++ packages/ylogging/ylogging-1.2.0.tar.gz/ylogging-1.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ylogging',
+    version='1.2.0',
+    packages=find_packages(),
+    install_requires=[
+        "google-cloud-error-reporting",
+        "google-cloud-logging",
+        "pytz"
+    ],
+    author='yactouat',
+    author_email='yactouat@yactouat.com',
+    description='logging helper for python that I always use in my projects',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yactouat/ylogging',
+    license='MIT',
+)
