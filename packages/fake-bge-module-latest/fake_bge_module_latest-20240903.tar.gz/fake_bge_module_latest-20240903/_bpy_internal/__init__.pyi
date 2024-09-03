8c0fc98a88144af6843a2852e2cf23f3705229fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 15:04:53 +0000

    Add package fake-bge-module-latest/20240903/fake_bge_module_latest-20240903.tar.gz

---
+++ packages/fake-bge-module-latest/fake_bge_module_latest-20240903.tar.gz/fake_bge_module_latest-20240903/_bpy_internal/__init__.pyi
@@ -0,0 +1,10 @@
+import typing
+import collections.abc
+import typing_extensions
+from . import addons
+from . import extensions
+from . import freedesktop
+from . import grease_pencil
+
+GenericType1 = typing.TypeVar("GenericType1")
+GenericType2 = typing.TypeVar("GenericType2")
