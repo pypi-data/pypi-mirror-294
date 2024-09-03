77886e1ca5905c799a003606bed83c80f87dda21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 14:03:16 +0000

    Add package pqwidgets/1.0.0/pqwidgets-1.0.0.tar.gz

---
+++ packages/pqwidgets/pqwidgets-1.0.0.tar.gz/pqwidgets-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pqwidgets',
+    version='1.0.0',
+    description='A custom PyQt5 widgets package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='majunggil',
+    author_email='majunggil.work@gmailc.om',
+    url='https://github.com/majunggil/pqwidgets',
+    packages=find_packages(),
+    install_requires=[
+        'PyQt5>=5.15.0',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
