20f4df304bab01d0c44bfd5d36000d775d14613a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 14:03:21 +0000

    Add package datoso-seed-sfc-enhancedcolors/1.0.1/datoso_seed_sfc_enhancedcolors-1.0.1-py3-none-any.whl

---
+++ packages/datoso-seed-sfc-enhancedcolors/datoso_seed_sfc_enhancedcolors-1.0.1-py3-none-any.whl/datoso_seed_sfc_enhancedcolors/__init__.py
@@ -0,0 +1,6 @@
+"""Initialize seed variables."""
+__all__ = ['__version__', '__author__', '__description__']
+__version__ = '1.0.1'
+__author__ = 'Lacides Miranda'
+__description__ = 'Super Famicom - Enhanced Colors'
+__prefix__ = 'sfc_ec'
