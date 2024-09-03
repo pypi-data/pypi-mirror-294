74720023310425cd7b4acf55ffc5d38b3a85bd56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 15:03:50 +0000

    Add package neoaria-commons/0.2.27/neoaria_commons-0.2.27-py3-none-any.whl

---
+++ packages/neoaria-commons/neoaria_commons-0.2.27-py3-none-any.whl/neoaria/microsoft.py
@@ -0,0 +1,25 @@
+import openpyxl
+
+class Excel:
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.workbook = openpyxl.load_workbook(file_path)
+        self.active_work_sheet = self.wb.active
+
+    def getWorkbook(self):
+        return self.workbook
+
+    def getWorkSheet(self, sheet_name:str)->openpyxl.worksheet.worksheet.Worksheet:
+        return self.workbook[sheet_name]
+
+class PowerPoint:
+
+    
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.presentation = openpyxl.load_workbook(file_path)
+        self.active_slide = self.presentation.active
+
+    def createSlide(self):
+        pass
