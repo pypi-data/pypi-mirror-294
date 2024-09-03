7d7093fc3d2bf9837f4c228b6c74b975f63e01f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 15:04:56 +0000

    Add package dsw-config/4.10.0/dsw_config-4.10.0.tar.gz

---
+++ packages/dsw-config/dsw_config-4.10.0.tar.gz/dsw_config-4.10.0/dsw/config/build_info.py
@@ -0,0 +1,17 @@
+# Generated file
+# - do not overwrite
+# - do not include in git
+from collections import namedtuple
+
+BuildInfo = namedtuple(
+    'BuildInfo',
+    ['version', 'built_at', 'sha', 'branch', 'tag'],
+)
+
+BUILD_INFO = BuildInfo(
+    version='v4.10.0~ed867ed',
+    built_at='2024-09-03 06:47:14Z',
+    sha='ed867eddd9f00958893ec14928ab160e2168e230',
+    branch='HEAD',
+    tag='v4.10.0',
+)
