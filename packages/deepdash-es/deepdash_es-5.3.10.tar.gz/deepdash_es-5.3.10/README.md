5eb6555a6bc836e4f31dd5dd84f854ae17231870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 14:03:14 +0000

    Add package deepdash-es/5.3.10/deepdash_es-5.3.10.tar.gz

---
+++ packages/deepdash-es/deepdash_es-5.3.10.tar.gz/deepdash_es-5.3.10/README.md
@@ -0,0 +1,3 @@
+# My Package
+
+This is a simple example Python package.
---
+++ packages/deepdash-es/deepdash_es-5.3.10.tar.gz/deepdash_es-5.3.10/setup.py
@@ -0,0 +1,17 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='deepdash-es',
+    version='5.3.10',
+    packages=find_packages(),
+    description='A simple example package',
+    author='sachin',
+    author_email='sachin1337hacker@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
