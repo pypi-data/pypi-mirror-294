81cb5c4f79b8293fab38b7062d2d299b6c64c10c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 15:04:40 +0000

    Add package MeUtils/2024.9.3.14.28.45/meutils-2024.9.3.14.28.45.tar.gz

---
+++ packages/MeUtils/meutils-2024.9.3.14.28.45.tar.gz/meutils-2024.9.3.14.28.45/meutils/apis/minicpm/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : __init__.py
+# @Time         : 2024/8/20 10:38
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.pipe import *
