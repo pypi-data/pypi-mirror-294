08c606926d48cddffd7b70c37054af481de1a685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 14:03:21 +0000

    Add package flwr-monitoring/0.2.0/flwr_monitoring-0.2.0-py3-none-any.whl

---
+++ packages/flwr-monitoring/flwr_monitoring-0.2.0-py3-none-any.whl/flwr_monitoring/monitoring_factory.py
@@ -0,0 +1,28 @@
+from .prometheus_monitoring import PrometheusMonitoring
+from .wandb_monitoring import WandBMonitoring  # Import the WandB monitoring class
+import logging
+
+def create_monitoring_tool(tool_name, metrics, config={}):
+    """
+    Creates a monitoring tool based on the given tool_name and metrics.
+
+    Parameters:
+    - tool_name (str): The name of the monitoring tool.
+    - metrics (list): A list of metrics to be monitored.
+
+    Returns:
+    - monitoring_tool: An instance of the monitoring tool.
+
+    Raises:
+    - ValueError: If the given tool_name is unknown.
+    """
+    tool_config = config.get(tool_name, config)
+    # print("Creating monitoring tool", tool_name, tool_config)
+    if tool_name == "prometheus":
+        logging.info("Creating Prometheus monitoring tool", tool_config)
+        return PrometheusMonitoring(metrics, tool_config)
+    elif tool_name == "wandb":
+        logging.info("Creating WandB monitoring tool", tool_config)
+        return WandBMonitoring(metrics, tool_config)
+    else:
+        raise ValueError(f"Unknown monitoring tool: {tool_name}")
