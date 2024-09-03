41d8f6e97d4dc785f9500dd7e4bd992187c8388f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 03:04:09 +0000

    Add package picofun/0.2.1/picofun-0.2.1.tar.gz

---
+++ packages/picofun/picofun-0.2.1.tar.gz/picofun-0.2.1/picofun/template.py
@@ -0,0 +1,39 @@
+"""Template loader for Jinja2 templates."""
+
+import os
+import typing
+
+import jinja2
+
+
+class Template:
+    """Manages Jinja2 templates."""
+
+    def __init__(self, base_path: str | None = "") -> None:
+        """Initialise TemplateLoader."""
+        if not os.path.isabs(base_path):
+            base_path = os.path.join(os.getcwd(), base_path)
+
+        path = os.path.realpath(base_path)
+
+        self._environment = (
+            jinja2.Environment(  # noqa: S701 We're not generating HTML # nosec
+                loader=jinja2.FileSystemLoader(path)
+            )
+        )
+
+        self.templates = {}
+
+    def get(self, filename: str) -> jinja2.Template:
+        """Get a template by filename."""
+        if filename not in self.templates:
+            try:
+                self.templates[filename] = self._environment.get_template(filename)
+            except jinja2.exceptions.TemplateNotFound as e:
+                raise FileNotFoundError(e.name) from e
+
+        return self.templates[filename]
+
+    def render(self, filename: str, **kwargs: dict[str : typing.Any]) -> str:
+        """Render a template by filename."""
+        return self.get(filename).render(**kwargs)
