f3638d4bd4841fbb5f6b8094d98d1c662149df82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 16:03:40 +0000

    Add package CFAPyX/2024.9.2/cfapyx-2024.9.2.tar.gz

---
+++ packages/CFAPyX/cfapyx-2024.9.2.tar.gz/cfapyx-2024.9.2/README.md
@@ -0,0 +1,25 @@
+![CFAPyX long logo: Blue, Green and White squares arranged in Diamond formation](https://github.com/cedadev/CFAPyX/blob/main/docs/source/_images/CFAPyX_long.jpg)
+
+CFA python Xarray module for using CFA files with xarray.
+
+See the [Documentation](https://cedadev.github.io/CFAPyX/) for more details.
+CFAPyX on [Github](https://github.com/cedadev/CFAPyX)
+
+For use with the Xarray module as an additional backend.
+
+# Installation
+
+```
+pip install xarray==2024.6.0
+pip install CFAPyX
+```
+
+# Usage
+
+```
+import xarray as xr
+
+ds = xr.open_dataset('cfa_file.nca', engine='CFA')
+# Continue as normal
+
+```
---
+++ packages/CFAPyX/cfapyx-2024.9.2.tar.gz/cfapyx-2024.9.2/tests/test_cfa.py
@@ -0,0 +1,75 @@
+# All routines for testing CFA general methods.
+import xarray as xr
+
+def test_cfa_pure(active=False):
+
+    # Local testing: Add CFAPyX before tests
+    ds = xr.open_dataset('tests/rain/rainmaker.nca', engine='CFA',
+                         cfa_options={
+                             'substitutions':"/home/users/dwest77/Documents/cfa_python_dw/testfiles/:tests/",
+                             'use_active':active
+                             })
+    
+    ## Test global dataset
+    assert not hasattr(ds,'address')
+    assert not hasattr(ds,'shape')
+    assert not hasattr(ds,'location')
+    
+    assert 'p' in ds
+    assert ds['p'].shape == (20, 180, 360)
+
+    # Using selection with lat/lon values NOT index values
+    p_sel = ds['p'].isel(time=slice(0,3),latitude=slice(140,145), longitude=slice(90,100))
+
+    assert p_sel.shape == (3, 5, 10)
+    assert not hasattr(p_sel, 'aggregated_data')
+    assert not hasattr(p_sel, 'aggregated_dimensions')
+
+    p_mean = p_sel.mean(dim='time')
+
+    assert p_mean.shape == (5, 10)
+    assert (p_mean[0][0].to_numpy() - 0.635366) < 0.01
+
+    p_value = p_sel.mean()
+
+    assert p_value.shape == ()
+    assert (p_value.to_numpy() - 0.511954) < 0.01
+    print('All tests passed!')
+
+def test_cfa_chunks():
+
+    ds = xr.open_dataset('tests/rain/rainmaker.nca', engine='CFA',
+                         cfa_options={
+                             'substitutions':"/home/users/dwest77/Documents/cfa_python_dw/testfiles/:tests/",
+                             'chunks': {'longitude':180},
+                             'chunk_limits':False})
+    
+    ## Test global dataset
+    assert not hasattr(ds,'address')
+    assert not hasattr(ds,'shape')
+    assert not hasattr(ds,'location')
+    
+    assert 'p' in ds
+    assert ds['p'].shape == (20, 180, 360)
+
+    # Using selection with lat/lon values NOT index values
+    p_sel = ds['p'].isel(time=slice(0,3),latitude=slice(140,145), longitude=slice(100,300))
+
+    assert p_sel.shape == (3, 5, 200)
+    assert not hasattr(p_sel, 'aggregated_data')
+    assert not hasattr(p_sel, 'aggregated_dimensions')
+
+    p_mean = p_sel.mean(dim='time')
+
+    assert p_mean.shape == (5, 200)
+    assert (p_mean[0][0].to_numpy() - 0.664414) < 0.01
+
+    p_value = p_sel.mean()
+
+    assert p_value.shape == ()
+    assert (p_value.to_numpy() - 0.490389) < 0.01
+
+if __name__ == '__main__':
+    test_cfa_pure(active=False)
+    test_cfa_pure(active=True)
+    test_cfa_chunks()
