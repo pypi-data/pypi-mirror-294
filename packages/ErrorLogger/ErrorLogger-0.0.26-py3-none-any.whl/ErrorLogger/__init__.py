faadd64b1ebb4cedcb87f974d3d396ac63794ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 14:03:15 +0000

    Add package ErrorLogger/0.0.26/ErrorLogger-0.0.26-py3-none-any.whl

---
+++ packages/ErrorLogger/ErrorLogger-0.0.26-py3-none-any.whl/ErrorLogger/__init__.py
@@ -0,0 +1,5 @@
+# Import the main components of the ErrorLogger library
+from .logger import ErrorLogger
+
+# Specify what should be available when someone imports the package
+__all__ = ['ErrorLogger']
