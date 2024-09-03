da94837a2f93c833e793af2ab7fd5d3276cc0e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 03:03:56 +0000

    Add package adaptix/3.0.0b8/adaptix-3.0.0b8-py3-none-any.whl

---
+++ packages/adaptix/adaptix-3.0.0b8-py3-none-any.whl/adaptix/__init__.py
@@ -0,0 +1,107 @@
+from ._internal.common import Dumper, Loader, TypeHint
+from ._internal.definitions import DebugTrail
+from ._internal.morphing.facade.func import dump, load
+from ._internal.morphing.facade.provider import (
+    as_is_dumper,
+    as_is_loader,
+    constructor,
+    date_by_timestamp,
+    datetime_by_format,
+    datetime_by_timestamp,
+    default_dict,
+    dumper,
+    enum_by_exact_value,
+    enum_by_name,
+    enum_by_value,
+    flag_by_exact_value,
+    flag_by_member_names,
+    loader,
+    name_mapping,
+    validator,
+    with_property,
+)
+from ._internal.morphing.facade.retort import AdornedRetort, FilledRetort, Retort
+from ._internal.morphing.model.crown_definitions import (
+    ExtraCollect,
+    Extractor,
+    ExtraForbid,
+    ExtraKwargs,
+    ExtraSkip,
+    Saturator,
+)
+from ._internal.morphing.name_layout.base import ExtraIn, ExtraOut
+from ._internal.name_style import NameStyle
+from ._internal.provider.facade.provider import bound
+from ._internal.retort.searching_retort import ProviderNotFoundError
+from ._internal.utils import Omittable, Omitted, create_deprecated_alias_getter
+from .provider import (
+    AggregateCannotProvide,
+    CannotProvide,
+    Chain,
+    LocStackPattern,
+    Mediator,
+    P,
+    Provider,
+    Request,
+    create_loc_stack_checker,
+)
+
+__all__ = (
+    "Dumper",
+    "Loader",
+    "TypeHint",
+    "DebugTrail",
+    "loader",
+    "dumper",
+    "as_is_dumper",
+    "as_is_loader",
+    "constructor",
+    "with_property",
+    "validator",
+    "bound",
+    "enum_by_exact_value",
+    "enum_by_name",
+    "enum_by_value",
+    "flag_by_exact_value",
+    "flag_by_member_names",
+    "name_mapping",
+    "default_dict",
+    "datetime_by_format",
+    "date_by_timestamp",
+    "datetime_by_timestamp",
+    "AdornedRetort",
+    "FilledRetort",
+    "Retort",
+    "Omittable",
+    "Omitted",
+    "provider",
+    "CannotProvide",
+    "AggregateCannotProvide",
+    "Chain",
+    "ExtraCollect",
+    "Extractor",
+    "ExtraForbid",
+    "ExtraIn",
+    "ExtraKwargs",
+    "ExtraOut",
+    "ExtraSkip",
+    "Mediator",
+    "NameStyle",
+    "LocStackPattern",
+    "P",
+    "Saturator",
+    "create_loc_stack_checker",
+    "retort",
+    "Provider",
+    "ProviderNotFoundError",
+    "Request",
+    "load",
+    "dump",
+)
+
+__getattr__ = create_deprecated_alias_getter(
+    __name__,
+    {
+        "NoSuitableProvider": "ProviderNotFoundError",
+    },
+)
---
+++ packages/adaptix/adaptix-3.0.0b8-py3-none-any.whl/adaptix/load_error.py
@@ -0,0 +1,57 @@
+from adaptix._internal.morphing.load_error import (
+    AggregateLoadError,
+    BadVariantLoadError,
+    DuplicatedValuesLoadError,
+    ExcludedTypeLoadError,
+    ExtraFieldsLoadError,
+    ExtraItemsLoadError,
+    FormatMismatchLoadError,
+    LoadError,
+    LoadExceptionGroup,
+    MsgLoadError,
+    MultipleBadVariantLoadError,
+    NoRequiredFieldsLoadError,
+    NoRequiredItemsLoadError,
+    OutOfRangeLoadError,
+    TypeLoadError,
+    UnionLoadError,
+    ValidationLoadError,
+    ValueLoadError,
+)
+from adaptix._internal.utils import create_deprecated_alias_getter
+
+__all__ = (
+    "LoadError",
+    "LoadExceptionGroup",
+    "AggregateLoadError",
+    "UnionLoadError",
+    "MsgLoadError",
+    "ExtraFieldsLoadError",
+    "ExtraItemsLoadError",
+    "NoRequiredFieldsLoadError",
+    "NoRequiredItemsLoadError",
+    "TypeLoadError",
+    "ExcludedTypeLoadError",
+    "ValueLoadError",
+    "ValidationLoadError",
+    "BadVariantLoadError",
+    "FormatMismatchLoadError",
+    "DuplicatedValuesLoadError",
+    "OutOfRangeLoadError",
+    "MultipleBadVariantLoadError",
+)
+
+
+__getattr__ = create_deprecated_alias_getter(
+    __name__,
+    {
+        "MsgError": "MsgLoadError",
+        "ExtraFieldsError": "ExtraFieldsLoadError",
+        "ExtraItemsError": "ExtraItemsLoadError",
+        "NoRequiredFieldsError": "NoRequiredFieldsLoadError",
+        "NoRequiredItemsError": "NoRequiredItemsLoadError",
+        "ValidationError": "ValidationLoadError",
+        "BadVariantError": "BadVariantLoadError",
+        "DatetimeFormatMismatch": "FormatMismatchLoadError",
+    },
+)
