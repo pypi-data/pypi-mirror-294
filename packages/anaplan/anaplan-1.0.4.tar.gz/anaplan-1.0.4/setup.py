a727a3787de4c2a46a907086781bc93b945826a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 03:03:57 +0000

    Add package anaplan/1.0.4/anaplan-1.0.4.tar.gz

---
+++ packages/anaplan/anaplan-1.0.4.tar.gz/anaplan-1.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "anaplan",
+      version = "1.0.4",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "Voice information added(expr.)",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'potentiation learning artficial neural networks'],
+
+      
+      )
