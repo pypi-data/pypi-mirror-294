99f6a4fdff5f457e12ac6cb5d4e86809a874d84f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 16:03:40 +0000

    Add package assume-framework/0.4.0.post1/assume_framework-0.4.0.post1-py3-none-any.whl

---
+++ packages/assume-framework/assume_framework-0.4.0.post1-py3-none-any.whl/assume/units/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: ASSUME Developers
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from assume.common.base import BaseUnit
+from assume.units.demand import Demand
+from assume.units.powerplant import PowerPlant
+from assume.units.steel_plant import SteelPlant
+from assume.units.storage import Storage
