4950b1a72c4185dad0e9e0c8e19affd2700cb2ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 14:03:05 +0000

    Add package tracepy/0.2.0/tracepy-0.2.0-py3-none-any.whl

---
+++ packages/tracepy/tracepy-0.2.0-py3-none-any.whl/tracepy/exceptions.py
@@ -0,0 +1,8 @@
+class NormalizationError(Exception):
+    """ Custom exception for unnormalized input. """
+
+class NotOnSurfaceError(Exception):
+    """ Error for rays that do not intersect with a surface. """
+
+class TraceError(Exception):
+    """ Custom error for lens systems where no rays survive being traced. """
