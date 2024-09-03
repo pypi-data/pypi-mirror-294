2db4d3db6f2644a9e8101760c6136b7db533e981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 14:02:55 +0000

    Add package homeassistant-stubs/2024.9.0b3/homeassistant_stubs-2024.9.0b3.tar.gz

---
+++ packages/homeassistant-stubs/homeassistant_stubs-2024.9.0b3.tar.gz/homeassistant_stubs-2024.9.0b3/homeassistant-stubs/components/tplink/const.pyi
@@ -0,0 +1,17 @@
+from _typeshed import Incomplete
+from homeassistant.const import Platform as Platform, UnitOfTemperature as UnitOfTemperature
+from typing import Final
+
+DOMAIN: str
+DISCOVERY_TIMEOUT: int
+CONNECT_TIMEOUT: int
+PRIMARY_STATE_ID: str
+ATTR_CURRENT_A: Final[str]
+ATTR_CURRENT_POWER_W: Final[str]
+ATTR_TODAY_ENERGY_KWH: Final[str]
+ATTR_TOTAL_ENERGY_KWH: Final[str]
+CONF_DEVICE_CONFIG: Final[str]
+CONF_CREDENTIALS_HASH: Final[str]
+CONF_CONNECTION_TYPE: Final[str]
+PLATFORMS: Final[Incomplete]
+UNIT_MAPPING: Incomplete
