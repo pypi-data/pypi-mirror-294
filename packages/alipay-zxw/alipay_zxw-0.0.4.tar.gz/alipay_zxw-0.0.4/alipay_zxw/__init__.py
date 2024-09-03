4c0f473651908fe69807da29d4c19e1995c3e52f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 03:03:57 +0000

    Add package alipay-zxw/0.0.4/alipay_zxw-0.0.4.tar.gz

---
+++ packages/alipay-zxw/alipay_zxw-0.0.4.tar.gz/alipay_zxw-0.0.4/alipay_zxw/__init__.py
@@ -0,0 +1,7 @@
+"""
+update based on third party: python-alipay-sdk
+"""
+from .pay_async import AliPay, DCAliPay, ISVAliPay
+# DCAliPay provides the ability to decrypt the response data from Alipay
+# ISVAliPay provides the ability to verify the response data from Alipay
+
