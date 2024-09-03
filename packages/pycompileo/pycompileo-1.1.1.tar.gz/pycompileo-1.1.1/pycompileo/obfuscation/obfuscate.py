b44f04cc05f8a4e02aacefd3b0afb3b18c60e833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 16:03:47 +0000

    Add package pycompileo/1.1.1/pycompileo-1.1.1.tar.gz

---
+++ packages/pycompileo/pycompileo-1.1.1.tar.gz/pycompileo-1.1.1/pycompileo/obfuscation/obfuscate.py
@@ -0,0 +1,6 @@
+from .obfuscate_package import obfuscate_package
+from .obfuscate_module import obfuscate_module
+
+
+def obfuscate(path: str, output_path=None, syspath: str | None = None):
+    pass
