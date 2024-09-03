bf86a55042a992a799c75c4ee16a80f0953c40ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 16:03:30 +0000

    Add package uri-pathlib-factory/0.1.7/uri_pathlib_factory-0.1.7-py3-none-any.whl

---
+++ packages/uri-pathlib-factory/uri_pathlib_factory-0.1.7-py3-none-any.whl/uri_pathlib_factory/__init__.py
@@ -0,0 +1,19 @@
+import os
+
+from .main import PathFactory, PurePathFactory, load_pathlib_monkey_patch
+
+__version__ = "0.1.7"
+
+__all__ = [
+    "load_pathlib_monkey_patch",
+    "PurePathFactory",
+    "PathFactory",
+]
+
+
+if os.environ.get("URI_PATHLIB_FACTORY_LOAD_PATHLIB_PATCH", "").lower() in [
+    "1",
+    "true",
+    "on",
+]:
+    load_pathlib_monkey_patch()
