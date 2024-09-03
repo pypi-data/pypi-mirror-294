5058c8b4545dffec1fa2822fb3b6e3b7fb533fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 14:03:03 +0000

    Add package plover-q-and-a/0.3.12/plover_q_and_a-0.3.12.tar.gz

---
+++ packages/plover-q-and-a/plover_q_and_a-0.3.12.tar.gz/plover_q_and_a-0.3.12/plover_q_and_a/sign/answer.py
@@ -0,0 +1,51 @@
+"""
+Answer module to handle commands that look like:
+
+    - {:Q_AND_A:ANSWER:INTERRUPTING}
+    - {:Q_AND_A:ANSWER:FOLLOWING_QUESTION}
+    - {:Q_AND_A:ANSWER:FOLLOWING_STATEMENT}
+    - {:Q_AND_A:ANSWER:FOLLOWING_QUESTION:ELABORATE_AFTER:Uh-huh}
+    - {:Q_AND_A:ANSWER:FOLLOWING_QUESTION:YIELD_AFTER:Correct}
+
+ELABORATE_AFTER and YIELD_AFTER are considered follow on arguments, and are
+handled by the `follow_on` module.
+"""
+from typing import Any
+
+from .arguments import (
+    FOLLOWING_QUESTION,
+    FOLLOWING_STATEMENT,
+    INTERRUPTING,
+)
+from .follow_on import handle_follow_on
+
+
+def sign(args: list[str], config: dict[str, Any]) -> str:
+    """
+    Returns the text for an answer type.
+
+    Raises an error if the answer type is blank or not recognised.
+    """
+    if not args:
+        raise ValueError("No answer args provided")
+
+    answer_type: str
+    follow_on_args: list[str]
+    answer_type, *follow_on_args = args
+    answer_type = answer_type.strip().upper()
+
+    if not answer_type:
+        raise ValueError("No answer type provided")
+
+    answer: str
+    if answer_type == INTERRUPTING:
+        answer = config["ANSWER_FOLLOWING_INTERRUPT"]
+    elif answer_type in (FOLLOWING_STATEMENT, FOLLOWING_QUESTION):
+        answer = config[f"ANSWER_{answer_type}"]
+        answer = handle_follow_on(
+            follow_on_args, answer, config, "QUESTION_FOLLOWING_STATEMENT"
+        )
+    else:
+        raise ValueError(f"Unknown answer type provided: {answer_type}")
+
+    return answer
---
+++ packages/plover-q-and-a/plover_q_and_a-0.3.12.tar.gz/plover_q_and_a-0.3.12/plover_q_and_a/sign/question.py
@@ -0,0 +1,55 @@
+"""
+Question module to handle commands that look like:
+
+    - {:Q_AND_A:QUESTION:INITIAL}
+    - {:Q_AND_A:QUESTION:INTERRUPTING}
+    - {:Q_AND_A:QUESTION:FOLLOWING_QUESTION}
+    - {:Q_AND_A:QUESTION:FOLLOWING_STATEMENT}
+    - {:Q_AND_A:QUESTION:FOLLOWING_STATEMENT:ELABORATE_AFTER:Okay}
+    - {:Q_AND_A:QUESTION:FOLLOWING_STATEMENT:YIELD_AFTER:All right}
+
+ELABORATE_AFTER and YIELD_AFTER are considered follow on arguments, and are
+handled by the `follow_on` module.
+"""
+from typing import Any
+
+from .arguments import (
+    FOLLOWING_QUESTION,
+    FOLLOWING_STATEMENT,
+    INITIAL,
+    INTERRUPTING,
+)
+from .follow_on import handle_follow_on
+
+
+def sign(args: list[str], config: dict[str, Any]) -> str:
+    """
+    Assigns the text for a question type.
+
+    Raises an error if the question type is blank or not recognised.
+    """
+    if not args:
+        raise ValueError("No question args provided")
+
+    question_type: str
+    follow_on_args: list[str]
+    question_type, *follow_on_args = args
+    question_type = question_type.strip().upper()
+
+    if not question_type:
+        raise ValueError("No question type provided")
+
+    question: str
+    if question_type == INITIAL:
+        question = config["QUESTION"]
+    elif question_type == INTERRUPTING:
+        question = config["QUESTION_FOLLOWING_INTERRUPT"]
+    elif question_type in (FOLLOWING_STATEMENT, FOLLOWING_QUESTION):
+        question = config[f"QUESTION_{question_type}"]
+        question = handle_follow_on(
+            follow_on_args, question, config, "ANSWER_FOLLOWING_QUESTION"
+        )
+    else:
+        raise ValueError(f"Unknown question type provided: {question_type}")
+
+    return question
