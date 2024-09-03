d96edaa9400af63e417a2d24f189254fb3a34d97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 15:04:35 +0000

    Add package FDASST/0.2/fdasst-0.2.tar.gz

---
+++ packages/FDASST/fdasst-0.2.tar.gz/fdasst-0.2/README.md
@@ -0,0 +1,3 @@
+# Python-Disk-Usage
+ Get disk usage information like how much space is available, free, and used.
+ A Python package to read disk usage also known as File Disk And System Stats Tool
---
+++ packages/FDASST/fdasst-0.2.tar.gz/fdasst-0.2/setup.py
@@ -0,0 +1,27 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="FDASST",  # The name of your package
+    version="0.2",  # The initial release version
+    packages=find_packages(),  # Automatically find package directories
+    description="A Python package to read disk usage also known as File Disk And System Stats Tool",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Shourya Wadhwa",
+    author_email="sourceboxtv@gmail.com",
+    url="https://github.com/CoderLogy/Python-Disk-Usage",  
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "psutil","resource"
+    ],
+    include_package_data=True,
+    license="GPL-3.0-or-later",
+    license_files=('LICENSE',),
+)
