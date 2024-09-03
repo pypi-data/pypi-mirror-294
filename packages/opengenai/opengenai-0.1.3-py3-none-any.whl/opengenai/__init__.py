5a84d72baebdd50c3a7d9cef89b0a8956aeb2d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 16:03:42 +0000

    Add package opengenai/0.1.3/opengenai-0.1.3-py3-none-any.whl

---
+++ packages/opengenai/opengenai-0.1.3-py3-none-any.whl/opengenai/__init__.py
@@ -0,0 +1,6 @@
+from .search_tool import WebFetcher
+from .search import GoogleSearcher
+from .html_parser import FastHTMLParserV3
+from .embed_search import fast_embedding_search
+
+__all__ = ['WebFetcher', 'GoogleSearcher', 'FastHTMLParserV3','fast_embedding_search']
