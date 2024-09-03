612b1b6fb90b93e1954fcf36eca300f030b5f2a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 16:03:35 +0000

    Add package metatensor-torch/0.5.5/metatensor_torch-0.5.5-py3-none-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/metatensor-torch/metatensor_torch-0.5.5-py3-none-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/metatensor/torch/atomistic/__init__.py
@@ -0,0 +1,39 @@
+import torch
+import os
+
+if os.environ.get("METATENSOR_IMPORT_FOR_SPHINX", "0") != "0":
+    from .documentation import System, NeighborListOptions
+    from .documentation import (
+        ModelOutput,
+        ModelEvaluationOptions,
+        ModelCapabilities,
+        ModelMetadata,
+    )
+
+    from .documentation import (
+        read_model_metadata,
+        check_atomistic_model,
+        load_model_extensions,
+        register_autograd_neighbors,
+        unit_conversion_factor,
+    )
+
+else:
+    System = torch.classes.metatensor.System
+    NeighborListOptions = torch.classes.metatensor.NeighborListOptions
+
+    ModelOutput = torch.classes.metatensor.ModelOutput
+    ModelEvaluationOptions = torch.classes.metatensor.ModelEvaluationOptions
+    ModelCapabilities = torch.classes.metatensor.ModelCapabilities
+    ModelMetadata = torch.classes.metatensor.ModelMetadata
+
+    read_model_metadata = torch.ops.metatensor.read_model_metadata
+    load_model_extensions = torch.ops.metatensor.load_model_extensions
+    check_atomistic_model = torch.ops.metatensor.check_atomistic_model
+
+    register_autograd_neighbors = torch.ops.metatensor.register_autograd_neighbors
+    unit_conversion_factor = torch.ops.metatensor.unit_conversion_factor
+
+from .model import MetatensorAtomisticModel, ModelInterface  # noqa: F401
+from .model import load_atomistic_model  # noqa: F401
+from .systems_to_torch import systems_to_torch  # noqa: F401
