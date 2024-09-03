7378cdb702000595207b04f2a3bacdffb1a99c36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 16:03:38 +0000

    Add package aphrodite-engine/0.6.0/aphrodite_engine-0.6.0-cp38-abi3-manylinux1_x86_64.whl

---
+++ packages/aphrodite-engine/aphrodite_engine-0.6.0-cp38-abi3-manylinux1_x86_64.whl/aphrodite/commit_id.py
@@ -0,0 +1,2 @@
+__commit__ = "75122b20ef202b6d0461ffff82743c229962f710"
+__short_commit__ = "75122b2"
---
+++ packages/aphrodite-engine/aphrodite_engine-0.6.0-cp38-abi3-manylinux1_x86_64.whl/aphrodite/modeling/__init__.py
@@ -0,0 +1,7 @@
+from aphrodite.modeling.sampling_metadata import SamplingMetadata
+from aphrodite.modeling.utils import set_random_seed
+
+__all__ = [
+    "SamplingMetadata",
+    "set_random_seed",
+]
