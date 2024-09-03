8881782ffadc83f24a964f306667f339b200c721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 14:03:07 +0000

    Add package dfk-commons/0.1.4/dfk_commons-0.1.4-py3-none-any.whl

---
+++ packages/dfk-commons/dfk_commons-0.1.4-py3-none-any.whl/dfk_commons/functions/get_account.py
@@ -0,0 +1,13 @@
+from cryptography.fernet import Fernet
+from dfk_commons.classes.Account import Account
+
+def get_account(tablesManager, cypher_key, address, rpcProvider):
+    f = Fernet(cypher_key.encode())
+    account_data = tablesManager.accounts.query(
+            KeyConditionExpression="address_ = :address_",
+            ExpressionAttributeValues={
+                ":address_": address,
+            })["Items"][0]
+    key = account_data["key_"]
+    decrypted_key = f.decrypt(key.encode()).decode()
+    return Account(rpcProvider.w3.eth.account.from_key(decrypted_key), rpcProvider, account_data)
