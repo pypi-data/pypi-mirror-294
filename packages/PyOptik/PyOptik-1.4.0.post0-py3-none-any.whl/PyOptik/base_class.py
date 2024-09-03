8d56ecc555ebfe6f4ff34872725c09c558f4b767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 03:04:03 +0000

    Add package PyOptik/1.4.0.post0/PyOptik-1.4.0.post0-py3-none-any.whl

---
+++ packages/PyOptik/PyOptik-1.4.0.post0-py3-none-any.whl/PyOptik/base_class.py
@@ -0,0 +1,19 @@
+
+class Material:
+    def __str__(self) -> str:
+        """
+        Provides an informal string representation of the Material object.
+
+        Returns:
+            str: Informal representation of the Material object.
+        """
+        return f"Material: {self.filename}"
+
+    def __repr__(self) -> str:
+        """
+        Provides a formal string representation of the Material object, including key attributes.
+
+        Returns:
+            str: Formal representation of the Material object.
+        """
+        return self.__str__()
