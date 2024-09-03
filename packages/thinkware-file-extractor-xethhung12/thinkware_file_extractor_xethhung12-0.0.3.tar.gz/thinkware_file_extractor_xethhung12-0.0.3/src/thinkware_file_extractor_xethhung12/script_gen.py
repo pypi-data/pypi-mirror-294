d7435213e06dd11372108cfe2d79310e92208312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 15:04:27 +0000

    Add package thinkware-file-extractor-xethhung12/0.0.3/thinkware_file_extractor_xethhung12-0.0.3.tar.gz

---
+++ packages/thinkware-file-extractor-xethhung12/thinkware_file_extractor_xethhung12-0.0.3.tar.gz/thinkware_file_extractor_xethhung12-0.0.3/src/thinkware_file_extractor_xethhung12/script_gen.py
@@ -0,0 +1,20 @@
+import os
+
+
+def gen_merge_script(source, dest, prefix, suffix):
+    source = os.path.abspath(source)
+    source = source[0:-1] if source.endswith("/") else source
+    dest = os.path.abspath(dest)
+    dest = dest[0:-1] if dest.endswith("/") else dest
+    dest_file=f"{dest}/merge_file"
+    dest_mp4_file=f"{dest}/merge.mp4"
+
+    replaced_source=source.replace("/","\\/")+"\\/"
+    return f"""
+
+
+##### Generate merge script 
+rm -f {dest_file} {dest_mp4_file}
+ls -al {source} | grep -o {prefix}_.*_{suffix}.MP4 | sed 's/^/file {replaced_source}/' > {dest_file}
+ffmpeg -f concat -i {dest_file} -vcodec copy -acodec copy {dest_mp4_file}
+"""
