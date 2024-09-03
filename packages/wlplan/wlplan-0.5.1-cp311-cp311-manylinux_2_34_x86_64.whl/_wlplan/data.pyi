856f1c159f360a08b3bcf90ba4effbe834f9c2d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 16:03:39 +0000

    Add package wlplan/0.5.1/wlplan-0.5.1-cp311-cp311-manylinux_2_34_x86_64.whl

---
+++ packages/wlplan/wlplan-0.5.1-cp311-cp311-manylinux_2_34_x86_64.whl/_wlplan/data.pyi
@@ -0,0 +1,35 @@
+from __future__ import annotations
+import _wlplan.planning
+__all__ = ['Dataset', 'ProblemStates']
+class Dataset:
+    """
+    WLPlan dataset object.
+    
+    Datasets contain a domain and a list of problem states.
+    
+    Parameters
+    ----------
+        domain : Domain
+            Domain object.
+    
+        data : List[ProblemStates]
+            List of problem states.
+    """
+    def __init__(self, domain: _wlplan.planning.Domain, data: list[ProblemStates]) -> None:
+        ...
+class ProblemStates:
+    """
+    Stores a problem and training states for the problem.
+    
+    Upon initialisation, the problem and states are checked for consistency.
+    
+    Parameters
+    ----------
+        problem : Problem
+            Problem object.
+    
+        states : List[State]
+            List of training states.
+    """
+    def __init__(self, problem: _wlplan.planning.Problem, states: list[list[_wlplan.planning.Atom]]) -> None:
+        ...
