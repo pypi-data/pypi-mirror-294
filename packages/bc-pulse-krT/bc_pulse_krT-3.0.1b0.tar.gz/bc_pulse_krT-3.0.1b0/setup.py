e7c5a23f31db5fb8172fa41ea00780dd66be8573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 16:03:32 +0000

    Add package bc-pulse-krT/3.0.1b0/bc_pulse_krT-3.0.1b0.tar.gz

---
+++ packages/bc-pulse-krT/bc_pulse_krT-3.0.1b0.tar.gz/bc_pulse_krT-3.0.1b0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bc_pulse_krT",
+    version="3.0.1b",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "bc_pulse=bc_pulse.__main__:main",
+        ],
+    },
+)
