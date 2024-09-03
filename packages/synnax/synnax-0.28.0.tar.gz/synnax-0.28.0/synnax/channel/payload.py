c34a0230908e5e939fdd4d782798d2cbcd449262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 14:03:04 +0000

    Add package synnax/0.28.0/synnax-0.28.0.tar.gz

---
+++ packages/synnax/synnax-0.28.0.tar.gz/synnax-0.28.0/synnax/channel/payload.py
@@ -0,0 +1,93 @@
+#  Copyright 2023 Synnax Labs, Inc.
+#
+#  Use of this software is governed by the Business Source License included in the file
+#  licenses/BSL.txt.
+#
+#  As of the Change Date specified in that file, in accordance with the Business Source
+#  License, use of this software will be governed by the Apache License, Version 2.0,
+#  included in the file licenses/APL.txt.
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Literal, cast
+
+from freighter import Payload
+
+from synnax.telem import DataType, Rate
+from synnax.util.normalize import normalize
+
+ChannelKey = int
+ChannelName = str
+ChannelKeys = list[int]
+ChannelNames = list[str]
+ChannelParams = ChannelKeys | ChannelNames | ChannelKey | ChannelName
+
+
+class ChannelPayload(Payload):
+    """A payload container that represent the properties of a channel exchanged to and
+    from the Synnax server.
+    """
+
+    key: ChannelKey = 0
+    data_type: DataType
+    rate: Rate = Rate(0)
+    name: str = ""
+    leaseholder: int = 0
+    is_index: bool = False
+    index: ChannelKey = 0
+    internal: bool = False
+    virtual: bool = False
+
+    def __str__(self):
+        return f"Channel(name={self.name}, key={self.key})"
+
+    def __hash__(self) -> int:
+        return hash(self.key)
+
+
+@dataclass
+class NormalizedChannelKeyResult:
+    single: bool
+    variant: Literal["keys"]
+    params: ChannelKeys
+
+
+@dataclass
+class NormalizedChannelNameResult:
+    single: bool
+    variant: Literal["names"]
+    params: ChannelNames
+
+
+def normalize_channel_params(
+    params: ChannelParams,
+) -> NormalizedChannelKeyResult | NormalizedChannelNameResult:
+    """Determine if a list of keys or names is a single key or name."""
+    normalized = normalize(params)
+    if len(normalized) == 0:
+        return NormalizedChannelKeyResult(single=False, variant="keys", params=[])
+    single = isinstance(params, (ChannelKey, ChannelName))
+    if isinstance(normalized[0], str):
+        try:
+            numeric_strings = [ChannelKey(s) for s in normalized]
+            return NormalizedChannelKeyResult(
+                single=single, variant="keys", params=cast(ChannelKeys, numeric_strings)
+            )
+        except ValueError:
+            return NormalizedChannelNameResult(
+                single=single,
+                variant="names",
+                params=cast(ChannelNames, normalized),
+            )
+    if isinstance(normalized[0], ChannelPayload):
+        return NormalizedChannelNameResult(
+            single=single,
+            variant="keys",
+            params=cast(ChannelNames, [c.key for c in normalized]),
+        )
+    return NormalizedChannelKeyResult(
+        single=single,
+        variant="keys",
+        params=cast(ChannelKeys, normalized),
+    )
---
+++ packages/synnax/synnax-0.28.0.tar.gz/synnax-0.28.0/synnax/framer/streamer.py
@@ -0,0 +1,168 @@
+#  Copyright 2023 Synnax Labs, Inc.
+#
+#  Use of this software is governed by the Business Source License included in the file
+#  licenses/BSL.txt.
+#
+#  As of the Change Date specified in that file, in accordance with the Business Source
+#  License, use of this software will be governed by the Apache License, Version 2.0,
+#  included in the file licenses/APL.txt.
+
+from freighter import (
+    EOF,
+    AsyncStream,
+    AsyncStreamClient,
+    ExceptionPayload,
+    Payload,
+    Stream,
+    StreamClient,
+)
+
+from synnax.channel.payload import ChannelKeys
+from synnax.exceptions import UnexpectedError
+from synnax.framer.adapter import ReadFrameAdapter
+from synnax.framer.frame import Frame, FramePayload
+from synnax.telem import CrudeTimeStamp, TimeStamp
+from typing import overload
+
+
+class _Request(Payload):
+    start: TimeStamp
+    keys: ChannelKeys
+
+
+class _Response(Payload):
+    frame: FramePayload
+    error: ExceptionPayload | None
+
+
+_ENDPOINT = "/frame/stream"
+
+
+class Streamer:
+    __stream: Stream[_Request, _Response]
+    __adapter: ReadFrameAdapter
+    from_: CrudeTimeStamp
+
+    def __init__(
+        self,
+        client: StreamClient,
+        adapter: ReadFrameAdapter,
+        from_: CrudeTimeStamp | None = None,
+    ) -> None:
+        self.from_ = from_ or TimeStamp.now()
+        self.__stream = client.stream(_ENDPOINT, _Request, _Response)
+        self.__adapter = adapter
+        self.__open()
+
+    def __open(self):
+        self.__stream.send(_Request(keys=self.__adapter.keys, start=self.from_))
+
+    @overload
+    def read(self, timeout: float) -> Frame | None:
+        ...
+
+    @overload
+    def read(self) -> Frame:
+        ...
+
+    def read(self, timeout: float | None = None) -> Frame | None:
+        try:
+            res, err = self.__stream.receive(timeout)
+            if err is not None:
+                raise err
+            return self.__adapter.adapt(Frame(res.frame))
+        except TimeoutError:
+            return None
+
+    def close(self, timeout: float | None = None):
+        exc = self.__stream.close_send()
+        if exc is not None:
+            raise exc
+        while True:
+            r, exc = self.__stream.receive(timeout)
+            if r is not None:
+                continue
+            if exc is None:
+                raise UnexpectedError(
+                    f"""Unexpected missing close acknowledgement from server.
+                    Please report this issue to the Synnax team.
+                    Response: {r}
+                    """
+                )
+            elif not isinstance(exc, EOF):
+                raise exc
+            break
+
+    def __iter__(self):
+        return self
+
+    def __enter__(self):
+        return self
+
+    def __next__(self):
+        return self.read()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+
+class AsyncStreamer:
+    __stream: AsyncStream[_Request, _Response]
+    __client: AsyncStreamClient
+    __adapter: ReadFrameAdapter
+    from_: CrudeTimeStamp
+
+    def __init__(
+        self,
+        client: AsyncStreamClient,
+        adapter: ReadFrameAdapter,
+        from_: CrudeTimeStamp | None = None,
+    ) -> None:
+        self.from_ = from_ or TimeStamp.now()
+        self.__client = client
+        self.__adapter = adapter
+
+    async def open(self):
+        self.__stream = await self.__client.stream(_ENDPOINT, _Request, _Response)
+        await self.__stream.send(_Request(keys=self.__adapter.keys, start=self.from_))
+
+    @property
+    def received(self) -> bool:
+        return self.__stream.received()
+
+    async def read(self) -> Frame:
+        res, err = await self.__stream.receive()
+        if err is not None:
+            raise err
+        return self.__adapter.adapt(Frame(res.frame))
+
+    async def close_loop(self):
+        await self.__stream.close_send()
+
+    async def close(self):
+        exc = await self.__stream.close_send()
+        if exc is not None:
+            raise exc
+        _, exc = await self.__stream.receive()
+        if exc is None:
+            raise UnexpectedError(
+                """Unexpected missing close acknowledgement from server.
+                Please report this issue to the Synnax team."""
+            )
+        elif not isinstance(exc, EOF):
+            raise exc
+
+    async def __aenter__(self):
+        return self
+
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        try:
+            return await self.read()
+        except EOF:
+            raise StopAsyncIteration
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.close()
