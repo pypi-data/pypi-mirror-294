bb1d9a0ddf02de7ef6f719c7e85245262ccf333d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 03:04:22 +0000

    Add package eolymp/0.10.54/eolymp-0.10.54-py3-none-any.whl

---
+++ packages/eolymp/eolymp-0.10.54-py3-none-any.whl/eolymp/annotations/__init__.py
@@ -0,0 +1,4 @@
+from .scope_pb2 import *
+from .ratelimit_pb2 import *
+from .http_pb2 import *
+from .endpoint_pb2 import *
---
+++ packages/eolymp/eolymp-0.10.54-py3-none-any.whl/eolymp/executor/checker_pb2.pyi
@@ -0,0 +1,38 @@
+from eolymp.executor import file_pb2 as _file_pb2
+from google.protobuf.internal import containers as _containers
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class Checker(_message.Message):
+    __slots__ = ["case_sensitive", "files", "lang", "order_sensitive", "precision", "secret", "source", "source_url", "type"]
+    class Type(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+        __slots__ = []
+    CASE_SENSITIVE_FIELD_NUMBER: _ClassVar[int]
+    FILES_FIELD_NUMBER: _ClassVar[int]
+    LANG_FIELD_NUMBER: _ClassVar[int]
+    LEGACY_PROGRAM: Checker.Type
+    LINES: Checker.Type
+    NONE: Checker.Type
+    ORDER_SENSITIVE_FIELD_NUMBER: _ClassVar[int]
+    PRECISION_FIELD_NUMBER: _ClassVar[int]
+    PROGRAM: Checker.Type
+    QUERY_RESULTS: Checker.Type
+    SECRET_FIELD_NUMBER: _ClassVar[int]
+    SOURCE_FIELD_NUMBER: _ClassVar[int]
+    SOURCE_URL_FIELD_NUMBER: _ClassVar[int]
+    TOKENS: Checker.Type
+    TYPE_FIELD_NUMBER: _ClassVar[int]
+    case_sensitive: bool
+    files: _containers.RepeatedCompositeFieldContainer[_file_pb2.File]
+    lang: str
+    order_sensitive: bool
+    precision: int
+    secret: bool
+    source: str
+    source_url: str
+    type: Checker.Type
+    def __init__(self, type: _Optional[_Union[Checker.Type, str]] = ..., lang: _Optional[str] = ..., source: _Optional[str] = ..., source_url: _Optional[str] = ..., precision: _Optional[int] = ..., case_sensitive: bool = ..., order_sensitive: bool = ..., secret: bool = ..., files: _Optional[_Iterable[_Union[_file_pb2.File, _Mapping]]] = ...) -> None: ...
