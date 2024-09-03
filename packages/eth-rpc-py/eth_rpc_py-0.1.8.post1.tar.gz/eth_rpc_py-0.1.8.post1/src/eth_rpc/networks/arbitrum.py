4ace65e02257333063a8012da1c04e414c3e42e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 16:03:35 +0000

    Add package eth-rpc-py/0.1.8.post1/eth_rpc_py-0.1.8.post1.tar.gz

---
+++ packages/eth-rpc-py/eth_rpc_py-0.1.8.post1.tar.gz/eth_rpc_py-0.1.8.post1/src/eth_rpc/networks/arbitrum.py
@@ -0,0 +1,24 @@
+from typing import ClassVar
+
+from eth_rpc.types import BlockExplorer, Network, Rpcs, RpcUrl
+from pydantic.networks import Url
+
+
+class Arbitrum(Network):
+    chain_id: ClassVar[int] = 42161
+    name: ClassVar[str] = "Arbitrum"
+    native_currency: ClassVar[str] = "ETH"
+    rpc: ClassVar[Rpcs] = Rpcs(
+        default=RpcUrl(
+            http=Url("https://arb1.arbitrum.io/rpc"),
+            wss=None,
+            # wss=Url("wss://test.com"),
+        )
+    )
+    block_explorer: ClassVar[BlockExplorer] = BlockExplorer(
+        name="Arbiscan",
+        url="https://arbiscan.io",
+        api_url="https://api.arbiscan.io/api",
+    )
+    alchemy_str: ClassVar[str | None] = "arb-mainnet"
+    apprx_block_time: ClassVar[float] = 0.26
