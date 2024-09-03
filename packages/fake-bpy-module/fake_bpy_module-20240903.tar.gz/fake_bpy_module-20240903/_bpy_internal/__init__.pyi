57334c93e2ea55903e035af7a5c3999c76707b48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 15:04:36 +0000

    Add package fake-bpy-module/20240903/fake_bpy_module-20240903.tar.gz

---
+++ packages/fake-bpy-module/fake_bpy_module-20240903.tar.gz/fake_bpy_module-20240903/_bpy_internal/__init__.pyi
@@ -0,0 +1,10 @@
+import typing
+import collections.abc
+import typing_extensions
+from . import addons as addons
+from . import extensions as extensions
+from . import freedesktop as freedesktop
+from . import grease_pencil as grease_pencil
+
+GenericType1 = typing.TypeVar("GenericType1")
+GenericType2 = typing.TypeVar("GenericType2")
