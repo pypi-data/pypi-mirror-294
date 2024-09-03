51440b6a8b1f0a9f512ca48ea79f1700cb7f9c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 03:04:18 +0000

    Add package ensembl_tui/0.1a3/ensembl_tui-0.1a3.tar.gz

---
+++ packages/ensembl_tui/ensembl_tui-0.1a3.tar.gz/ensembl_tui-0.1a3/noxfile.py
@@ -0,0 +1,15 @@
+import nox
+
+_py_versions = range(10, 13)
+
+
+@nox.session(python=[f"3.{v}" for v in _py_versions])
+def test(session):
+    session.install(".[test]")
+    session.chdir("tests")
+    session.run(
+        "pytest",
+        "-s",
+        "-x",
+        *session.posargs,
+    )
