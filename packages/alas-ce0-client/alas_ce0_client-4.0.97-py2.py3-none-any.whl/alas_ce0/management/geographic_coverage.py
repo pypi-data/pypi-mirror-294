69421f6c6d26086ef23e7ec32318e3682cba0ea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 14:04:41 +0000

    Add package alas-ce0-client/4.0.97/alas_ce0_client-4.0.97-py2.py3-none-any.whl

---
+++ packages/alas-ce0-client/alas_ce0_client-4.0.97-py2.py3-none-any.whl/alas_ce0/management/geographic_coverage.py
@@ -0,0 +1,12 @@
+from alas_ce0.common.client_base import EntityClientBase
+
+
+class GeographicCoverageClient(EntityClientBase):
+    entity_endpoint_base_url = '/management/geographic-coverages/'
+
+    def __init__(self, country_code='cl', **kwargs):
+        super(GeographicCoverageClient, self).__init__(**kwargs)
+        self.entity_endpoint_base_url += country_code + '/'
+
+    def get_structure_id(self, params):
+        return self.http_post_json(self.entity_endpoint_base_url + "_get-structure-id", params)
