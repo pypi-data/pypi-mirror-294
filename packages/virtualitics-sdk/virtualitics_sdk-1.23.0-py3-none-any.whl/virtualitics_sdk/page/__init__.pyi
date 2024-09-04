226243a9d1f9cf6162da6927321d806b323fcfe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 06:04:03 +0000

    Add package virtualitics-sdk/1.23.0/virtualitics_sdk-1.23.0-py3-none-any.whl

---
+++ packages/virtualitics-sdk/virtualitics_sdk-1.23.0-py3-none-any.whl/virtualitics_sdk/page/__init__.pyi
@@ -0,0 +1,3 @@
+from virtualitics_sdk.page.card import Card as Card
+from virtualitics_sdk.page.page import Page as Page, PageType as PageType
+from virtualitics_sdk.page.section import Section as Section
