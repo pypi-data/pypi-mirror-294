ae16373c1a8c32ae91f0ef9278748e79ae233ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 05:03:30 +0000

    Add package hashstash/0.2.0/hashstash-0.2.0-py3-none-any.whl

---
+++ packages/hashstash/hashstash-0.2.0-py3-none-any.whl/hashstash/engines/sqlite.py
@@ -0,0 +1,16 @@
+from . import *
+import sqlite3
+
+class SqliteHashStash(BaseHashStash):
+    engine = "sqlite"
+
+    @log.debug
+    def get_db(self):
+        log.debug(f'Path exists: {os.path.exists(self.path)}\nPath: {self.path}')
+        log.debug(f'Directory exists: {os.path.exists(self.path_dirname)}\nDirectory: {self.path_dirname}')
+        
+        # Ensure the directory exists
+        os.makedirs(self.path_dirname, exist_ok=True)
+        
+        from sqlitedict import SqliteDict
+        return SqliteDict(self.path, flag='c', autocommit=True)
