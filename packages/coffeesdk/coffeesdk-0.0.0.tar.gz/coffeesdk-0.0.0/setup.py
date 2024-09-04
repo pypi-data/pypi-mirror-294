8f3c005f5632a73a5e01b5ed0d09553ec327a74a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 05:03:23 +0000

    Add package coffeesdk/0.0.0/coffeesdk-0.0.0.tar.gz

---
+++ packages/coffeesdk/coffeesdk-0.0.0.tar.gz/coffeesdk-0.0.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="coffeesdk",
+    author="limooor",
+    author_email="coffeetrader1@hotmail.com",
+    description="A short description of your package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/coffeetrader/coffeeapi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        # 列出您的包的依赖项
+        'requests',
+        'beautifulsoup4',
+    ],
+)
