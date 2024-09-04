8370b7bbec8d7f52e7fb62f1c5c8906058bfd0d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 05:03:50 +0000

    Add package rstatspy/0.1.0/rstatspy-0.1.0-cp38-cp38-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/rstatspy/rstatspy-0.1.0-cp38-cp38-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/lib64/cmake/rstats/rstatsConfig-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "rstats" for configuration "Release"
+set_property(TARGET rstats APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(rstats PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/librstats.so"
+  IMPORTED_SONAME_RELEASE "librstats.so"
+  )
+
+list(APPEND _cmake_import_check_targets rstats )
+list(APPEND _cmake_import_check_files_for_rstats "${_IMPORT_PREFIX}/lib64/librstats.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
