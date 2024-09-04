81871d95317a8e32d5a0955636d09a9e6d3e81e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 03:03:18 +0000

    Add package NewSimpleSQL/0.6.0.9/newsimplesql-0.6.0.9.tar.gz

---
+++ packages/NewSimpleSQL/newsimplesql-0.6.0.9.tar.gz/newsimplesql-0.6.0.9/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="NewSimpleSQL",
+    version='0.6.0.9',
+    packages= [
+        'NewSimpleSQL'
+    ],
+    description="This is a library to simplify the use of a SQLite3 database, easy to use, easy to understand.",
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author="Greem3",
+    author_email="ianpichardo575@gmail.com",
+    url="https://github.com/Greem3/NewSimpleSQL",
+)
