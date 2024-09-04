36e1adfde3b5d877ef56d8559ec009f0cc4e3f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 14:04:10 +0000

    Add package polarscore/0.4.0/polarscore-0.4.0-cp38-abi3-win_amd64.whl

---
+++ packages/polarscore/polarscore-0.4.0-cp38-abi3-win_amd64.whl/polarscore/__init__.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+import polars as pl
+from polars._typing import IntoExpr
+from polars.plugins import register_plugin_function
+
+LIB = Path(__file__).parent
+
+
+def iv(x: IntoExpr, y: IntoExpr) -> pl.Expr:  # noqa: D103
+    output = register_plugin_function(
+        args=[x, y],
+        plugin_path=LIB,
+        function_name="pl_iv",
+        is_elementwise=False,
+        changes_length=True,
+        returns_scalar=True,
+    )
+
+    return output
