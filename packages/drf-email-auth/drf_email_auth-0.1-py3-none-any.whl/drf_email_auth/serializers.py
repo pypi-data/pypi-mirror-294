97c2d97c8970f74a123b4c8b52cd7e71c6f4f34a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 03:03:35 +0000

    Add package drf-email-auth/0.1/drf_email_auth-0.1-py3-none-any.whl

---
+++ packages/drf-email-auth/drf_email_auth-0.1-py3-none-any.whl/drf_email_auth/serializers.py
@@ -0,0 +1,12 @@
+from django.contrib.auth import authenticate
+from rest_framework import serializers
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(email=data['email'], password=data['password'])
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid login credentials")
