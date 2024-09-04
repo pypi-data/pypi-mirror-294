58c48a1f7ba2c2fdde404594c27ac5e8d3eb6687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 06:03:33 +0000

    Add package ibis-framework/9.4.0/ibis_framework-9.4.0.tar.gz

---
+++ packages/ibis-framework/ibis_framework-9.4.0.tar.gz/ibis_framework-9.4.0/ibis/backends/impala/tests/snapshots/test_string_builtins/test_string_builtins/lstrip/out.sql
@@ -0,0 +1,3 @@
+SELECT
+  LTRIM(`t0`.`string_col`) AS `LStrip(string_col)`
+FROM `functional_alltypes` AS `t0`
---
+++ packages/ibis-framework/ibis_framework-9.4.0.tar.gz/ibis_framework-9.4.0/ibis/backends/impala/tests/snapshots/test_string_builtins/test_string_builtins/rstrip/out.sql
@@ -0,0 +1,3 @@
+SELECT
+  RTRIM(`t0`.`string_col`) AS `RStrip(string_col)`
+FROM `functional_alltypes` AS `t0`
