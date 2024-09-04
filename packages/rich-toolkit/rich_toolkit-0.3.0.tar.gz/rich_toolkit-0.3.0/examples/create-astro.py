c2129c78295a4e56eceba5882ee4a1141753f601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 17:04:28 +0000

    Add package rich-toolkit/0.3.0/rich_toolkit-0.3.0.tar.gz

---
+++ packages/rich-toolkit/rich_toolkit-0.3.0.tar.gz/rich_toolkit-0.3.0/examples/create-astro.py
@@ -0,0 +1,61 @@
+import random
+import time
+
+from rich_toolkit import App
+from rich_toolkit.app_style import FancyAppStyle, TaggedAppStyle
+
+from rich.theme import Theme
+
+astro_theme = Theme(
+    {
+        "tag.title": "black on #A7E3A2",
+        "tag": "white on #893AE3",
+        "placeholder": "grey85",
+        "text": "white",
+        "selected": "green",
+        "result": "grey85",
+        "progress": "on #893AE3",
+    }
+)
+
+
+def random_name_generator() -> str:
+    return f"{random.choice(['fancy', 'cool', 'awesome'])}-{random.choice(['banana', 'apple', 'strawberry'])}"
+
+
+app_style = TaggedAppStyle(tag_width=7)
+app_style_fancy = FancyAppStyle()
+
+for style in [app_style, app_style_fancy]:
+    with App(style=style, theme=astro_theme) as app:
+        app.print_title("Launch sequence initiated.", tag="astro")
+
+        app.print_line()
+
+        app_name = app.input(
+            "Where should we create your new project?",
+            tag="dir",
+            default=f"./{random_name_generator()}",
+        )
+
+        app.print_line()
+
+        template = app.ask(
+            "How would you like to start your new project?",
+            tag="tmpl",
+            options=[
+                {"value": "with-samples", "name": "Include sample files"},
+                {"value": "blog", "name": "Use blog template"},
+                {"value": "empty", "name": "Empty"},
+            ],
+        )
+
+        app.print_line()
+
+        ts = app.confirm("Do you plan to write TypeScript?", tag="ts")
+
+        app.print_line()
+
+        with app.progress("Some demo here") as progress:
+            for x in range(3):
+                time.sleep(1)
