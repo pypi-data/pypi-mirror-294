bf030f47176711a68b3e36c4d0d1c9b369e6cd4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 05:03:42 +0000

    Add package RahebPDF/0.2/rahebpdf-0.2.tar.gz

---
+++ packages/RahebPDF/rahebpdf-0.2.tar.gz/rahebpdf-0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RahebPDF',
+    version='0.2',  # Increment the version number (here to 0.2)
+    packages=find_packages(),
+    install_requires=[
+        'PyMuPDF',
+        'Pillow',
+        'fitz',
+        'python-dotenv',
+    ],
+    url='https://github.com/rahebsaeed/RahebPDF',
+    author='Raheb Saeed',
+    author_email='raheebareef@gmail.com',
+    description='A library for converting PDF to HTML and vice versa',
+)
