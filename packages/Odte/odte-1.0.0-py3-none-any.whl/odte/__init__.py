18ca7d561b8a70253aec55c137f2b4bd5216b196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 05:03:26 +0000

    Add package Odte/1.0.0/odte-1.0.0-py3-none-any.whl

---
+++ packages/Odte/odte-1.0.0-py3-none-any.whl/odte/__init__.py
@@ -0,0 +1,9 @@
+from ._version import __version__
+from .Odte import Odte
+
+__author__ = "Ricardo Montañana Gómez"
+__copyright__ = "Copyright 2020-2021, Ricardo Montañana Gómez"
+__license__ = "MIT License"
+__author_email__ = "ricardo.montanana@alu.uclm.es"
+
+__all__ = ["__version__", "Odte"]
