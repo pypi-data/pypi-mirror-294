fc2b094feae06a6fcd9c4979941370a852bc9977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 03:03:24 +0000

    Add package lakeriders-alert/0.1.2/lakeriders_alert-0.1.2-py3-none-any.whl

---
+++ packages/lakeriders-alert/lakeriders_alert-0.1.2-py3-none-any.whl/lakeridersalert/constants/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+POLLING_INTERVAL = 120.0
+TIMEOUT_GET_REQUEST = 10.0
+CANCELLED = "CANCELLED"
+FREE = "FREE"
+EMPTY = "EMPTY"
+DB_NAME = "lakeriders_alert.db"
+
+RUN_WITH_LOGS = bool(os.environ.get("RUN_WITH_LOGS"))
