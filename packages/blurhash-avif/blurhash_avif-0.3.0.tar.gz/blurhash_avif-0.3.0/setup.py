3fb3eeb4a25b3830796c7486ff217d2631c09040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 05:03:31 +0000

    Add package blurhash-avif/0.3.0/blurhash_avif-0.3.0.tar.gz

---
+++ packages/blurhash-avif/blurhash_avif-0.3.0.tar.gz/blurhash_avif-0.3.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:  # noqa
+    long_description = f.read()
+
+setup(
+    name="blurhash-avif",
+    version="0.3.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=["numpy", "Pillow[avif]", "blurhash", "pathlib", "typing"],
+    author="ZuidVolt",
+    description="A library to generate BlurHash and PNG data URLs for AVIF images",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ZuidVolt/blurhash-avif",
+    project_urls={
+        "Issue Tracker": "https://github.com/ZuidVolt/blurhash-avif/issues",
+        "Changelog": "https://github.com/ZuidVolt/blurhash-avif/blob/main/CHANGELOG.md",
+    },
+    license="Apache Software License",
+    keywords="blurhash avif image processing",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    package_data={"": ["*.txt", "*.md"]},  # Include README and other text files
+    include_package_data=True,
+    test_suite="tests",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "blurhash-avif=blurhash_avif:generate_blurhash_and_data_url_from_avif",
+        ],
+    },
+)
