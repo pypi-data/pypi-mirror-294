96f90bc7234cfa526d12be0b9cafebe4c0a8e385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 06:04:04 +0000

    Add package pyfluent-iterables/1.2.0/pyfluent_iterables-1.2.0-py3-none-any.whl

---
+++ packages/pyfluent-iterables/pyfluent_iterables-1.2.0-py3-none-any.whl/pyfluent_iterables/__init__.py
@@ -0,0 +1,8 @@
+"""Fluent API wrapper for Python collections.
+See https://github.com/mifeet/pyfluent-iterables/blob/main/README.md for more information.
+"""
+
+# Version of the package
+__version__ = "1.2.0"
+
+from .fluent import fluent_dict, fluent_of, fluent, FluentIterable, FluentMapping  # noqa
