690f8dfb5fdc6bf54edb808c20d23c31ae5db4ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 04:03:37 +0000

    Add package esi-ui/0.1.0/esi_ui-0.1.0.tar.gz

---
+++ packages/esi-ui/esi_ui-0.1.0.tar.gz/esi_ui-0.1.0/esi_ui/static/dashboard/project/esi/esi.module.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('horizon.dashboard.project.esi', [
+      'horizon.dashboard.project.esi.nodes',
+      'horizon.dashboard.project.esi.offers',
+    ]);
+
+})();
