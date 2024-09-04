c7f4034d01646a7c8faf8f35f059727dea4607af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 14:04:15 +0000

    Add package cyber-201-suspicious-library/0.5/cyber_201_suspicious_library-0.5.tar.gz

---
+++ packages/cyber-201-suspicious-library/cyber_201_suspicious_library-0.5.tar.gz/cyber_201_suspicious_library-0.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cyber_201_suspicious_library",
+    version="0.5",
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    description="An innocent-looking utility library for educational purposes",
+    author="Mr Speak",
+    author_email="peter.speak@ascte.com",
+    url="",
+)
