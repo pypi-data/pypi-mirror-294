57fdbe929f9879ca62f0ccdbb95e0010a38946af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 16:04:28 +0000

    Add package duckdb/1.0.1.dev5143/duckdb-1.0.1.dev5143.tar.gz

---
+++ packages/duckdb/duckdb-1.0.1.dev5143.tar.gz/duckdb-1.0.1.dev5143/duckdb_build/src/planner/operator/logical_any_join.cpp
@@ -0,0 +1,14 @@
+#include "duckdb/planner/operator/logical_any_join.hpp"
+
+namespace duckdb {
+
+LogicalAnyJoin::LogicalAnyJoin(JoinType type) : LogicalJoin(type, LogicalOperatorType::LOGICAL_ANY_JOIN) {
+}
+
+InsertionOrderPreservingMap<string> LogicalAnyJoin::ParamsToString() const {
+	InsertionOrderPreservingMap<string> result;
+	result["Condition"] = condition->ToString();
+	return result;
+}
+
+} // namespace duckdb
