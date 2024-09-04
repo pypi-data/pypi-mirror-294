0b0b7714cfc1d9b3f283095f80142639aff8ce70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 06:03:33 +0000

    Add package hudu-api-ctek/0.0.34/hudu_api_ctek-0.0.34-py3-none-any.whl

---
+++ packages/hudu-api-ctek/hudu_api_ctek-0.0.34-py3-none-any.whl/hudu_api_ctek/assets.py
@@ -0,0 +1,30 @@
+import requests
+
+def get_assets_by_layout_id(api_baseurl, api_key, asset_layout_id):
+    page = 1
+    page_size = 25
+    headers = {'x-api-key': api_key}
+    all_assets = []
+    
+    while True:
+        url = f'{api_baseurl}/assets?asset_layout_id={asset_layout_id}&page={page}'
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()  # Raise an HTTPError for bad responses
+            data = response.json()
+            
+            if not data:  # If the response is empty, break the loop
+                break
+            
+            all_assets.extend(data)
+            
+            # Check if we've received fewer assets than expected; this indicates the last page
+            if len(data) < page_size:  # PAGE_SIZE should be the number of items per page
+                break
+            
+            page += 1
+        
+        except requests.exceptions.RequestException as e:
+            return {"assets": []}, f"Error fetching assets: {e}"
+    
+    return {"assets": all_assets}, None
