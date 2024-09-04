e60cbdebe57fa4616ae44de1dfed06782a11ddfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 14:04:40 +0000

    Add package fused/1.9.5/fused-1.9.5-py3-none-any.whl

---
+++ packages/fused/fused-1.9.5-py3-none-any.whl/fused/_optional_deps.py
@@ -0,0 +1,26 @@
+try:
+    import geopandas as gpd
+
+    HAS_GEOPANDAS = True
+    GPD_GEODATAFRAME = gpd.GeoDataFrame
+except ImportError:
+    HAS_GEOPANDAS = False
+    GPD_GEODATAFRAME = None
+
+try:
+    import mercantile
+
+    HAS_MERCANTILE = True
+    MERCANTILE_TILE = mercantile.Tile
+except ImportError:
+    HAS_MERCANTILE = False
+    MERCANTILE_TILE = None
+
+try:
+    import shapely
+
+    HAS_SHAPELY = True
+    SHAPELY_POLYGON = shapely.geometry.polygon.Polygon
+except ImportError:
+    HAS_SHAPELY = False
+    SHAPELY_POLYGON = None
