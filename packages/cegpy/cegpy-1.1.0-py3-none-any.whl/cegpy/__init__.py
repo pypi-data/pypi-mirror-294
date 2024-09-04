ad5a5d5c538883456b65fcea1c7c9b87642fb4a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 06:03:51 +0000

    Add package cegpy/1.1.0/cegpy-1.1.0-py3-none-any.whl

---
+++ packages/cegpy/cegpy-1.1.0-py3-none-any.whl/cegpy/__init__.py
@@ -0,0 +1,17 @@
+"""
+CEGpy
+=====
+
+CEGpy is a python package for the creation, manipulation, and study of
+Chain Event Graphs.
+
+See documentation of more information.
+
+"""
+
+from cegpy.graphs._ceg import ChainEventGraph
+from cegpy.graphs._ceg_reducer import ChainEventGraphReducer
+from cegpy.trees._event import EventTree
+from cegpy.trees._staged import StagedTree
+
+__version__ = "1.1.0"
