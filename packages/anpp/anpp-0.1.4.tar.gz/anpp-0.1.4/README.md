24870178642a24c0e0dee883e295d3e935e8d49d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 03:03:14 +0000

    Add package anpp/0.1.4/anpp-0.1.4.tar.gz

---
+++ packages/anpp/anpp-0.1.4.tar.gz/anpp-0.1.4/README.md
@@ -0,0 +1,183 @@
+# README
+
+在多个Android项目自由跳转脚本，包含自动补全，可以直接修改template或者修改json配置进行安装，支持自定义命令
+
+# 目录
+
+* [一、安装及使用](#一安装及使用)
+* [二、配置方法](#二配置方法)
+  * [2.1 修改androiddir.bash.template](#21-修改androiddirbashtemplate)
+  * [2.2 修改config.json](#22-修改configjson)
+* [三、安装anpp shell脚本](#三安装anpp-shell脚本)
+  * [3.1 直接安装androiddir.bash.template修改](#31-直接安装androiddirbashtemplate修改)
+  * [3.2 安装config.json模版](#32-安装configjson模版)
+* [四、clean](#四clean)
+* [五、anpp使用方法](#五anpp使用方法)
+  * [5.1 自动补全使用](#51-自动补全使用)
+  * [5.2 项目源代码跳转](#52-项目源代码跳转)
+  * [5.3 其他跳转命令](#53-其他跳转命令)
+* [六、自定义命令](#六自定义命令)
+* [七、自定义别名](#七自定义别名)
+* [八、自动扫描生成配置](#八自动扫描生成配置)
+
+# 一、安装及使用
+
+* pip3 install anpp
+* anpp-build new
+  ```
+  .
+  ├── LICENSE
+  ├── Makefile
+  ├── androiddir.bash.template
+  ├── config.json
+  ├── generator.py
+  └── tools
+      ├── custom.sh
+      └── custom.py
+  ```
+
+# 二、配置方法
+
+## 2.1 修改androiddir.bash.template
+
+修改文件数组：[androiddir.bash.template](androiddir.bash.template)
+
+* defaultPath：所有项目的根目录
+* projects: 项目文件夹名
+* products：项目对应的产品名
+* kernels：内核相对路径，相对于projects
+* dtss：dts相对路径，相对kernels
+* bootloaderStage1s：第一阶段bootloader
+* bootloaderStage2s：第二阶段bootloader
+* outs：out目录，不含product名
+* efuses：签名工具目录
+* components：跳转命令别命（alias）
+
+## 2.2 修改config.json
+
+修改json数据：[config.json](config.json)
+
+* defaultPath：所有项目的根目录
+* project_keys：用于定义project支持哪些属性，定义了但是在project中没有赋值的会使用`.`（路径）替代
+  * project: 项目文件夹名
+  * product：项目对应的产品名
+  * kernel：内核相对路径，相对于projects
+  * dts：dts相对路径，相对kernels
+  * bootloaderStage1s：第一阶段bootloader
+  * bootloaderStage2s：第二阶段bootloader
+  * out：out目录，不含product名
+  * efuse：签名工具目录
+* components：用于合成路径，以及shell命令的alias
+  * cmd：shell命令alias
+  * combine：使用project_keys中的属性，完成cmd命令的路径组合
+
+# 三、安装anpp shell脚本
+
+## 3.1 直接安装androiddir.bash.template修改
+
+* `make template`
+  ```
+  cp androiddir.bash.template ~/.androiddir.sh
+  check if source .androiddir.sh path in /home/pi/.bashrc:
+  .androiddir.sh path not source in /home/pi/.bashrc
+  tail ~/.bashrc last 2 line for terminal check
+  ~/.bashrc content
+  ...
+  # add anpp(https://github.com/ZengjfOS/anpp) function to bash env
+  source ~/.androiddir.sh
+  ```
+* `source ~/.bashrc`
+
+## 3.2 安装config.json模版
+
+* 以下三条命令执行的内容是一致的
+  * `make`
+  * `make json`
+  * `make install`
+* make log
+  ```
+  python3 ./generator.py ~/.androiddir.sh
+  defaultPath: ~/zengjf/
+  projects: ['M0-project', 'A00-project', 'M8-project', 'L00-project']
+  products: ['M0', 'k61v1_64_bsp', 'k62v1_64', 's138']
+  kernels: ['kernel-4.9', 'kernel-4.9', 'kernel-4.19', 'android/kernel/msm-4.14']
+  bootloaderStage1s: ['vendor/mediatek/proprietary/bootable/bootloader/preloader', 'vendor/mediatek/proprietary/bootable/bootloader/preloader', 'vendor/mediatek/proprietary/bootable/bootloader/preloader', 'android/fibo/bp_code/boot_images']
+  bootloaderStage2s: ['vendor/mediatek/proprietary/bootable/bootloader/lk', 'vendor/mediatek/proprietary/bootable/bootloader/lk', 'vendor/mediatek/proprietary/bootable/bootloader/lk', 'android/bootable/bootloader/edk2']
+  outs: ['out/target/product', 'out/target/product', 'out/target/product', 'android/out/target/product']
+  efuses: ['vendor/mediatek/proprietary/scripts/sign-image_v2', 'vendor/mediatek/proprietary/scripts/sign-image_v2', 'vendor/mediatek/proprietary/scripts/sign-image_v2', 'sc13x_download_images_v2/qcm6125-la-2-0/common/sectools']
+  check if source .androiddir.sh path in /home/pi/.bashrc:
+  .androiddir.sh path not source in /home/pi/.bashrc
+  tail ~/.bashrc last 2 line for terminal check
+  ~/.bashrc content
+  ...
+  # add anpp(https://github.com/ZengjfOS/anpp) function to bash env
+  source ~/.androiddir.sh
+  ```
+* `source ~/.bashrc`
+
+# 四、clean
+
+`make clean`
+
+```
+rm ~/.androiddir.sh
+sed -i "/.androiddir.sh/d" ~/.bashrc
+sed -i "/ZengjfOS\/anpp/d" ~/.bashrc
+```
+
+# 五、anpp使用方法
+
+## 5.1 自动补全使用
+
+* `anpp <tab><tab>`
+  ```
+  a00   l00  m0    m8
+  ```
+  * list project
+* `anpp m0 <tab><tab>`
+  ```
+  android  bs1      bs2      dts      kernel   out
+  ```
+  * list component
+* `m0 <tab><tab>`
+  ```
+  android  bs1      bs2      dts      kernel   out
+  ```
+  * list component
+
+## 5.2 项目源代码跳转
+
+* projects中的名字去除`-project`后缀，小写名字可以直接跳转到对应的目录
+* 譬如M0-project，去除名字为M0，小写为m0，所以直接在终端输入m0，可以直接调转到其源代码根目录
+
+## 5.3 其他跳转命令
+
+NO. | 命令名 | 说明
+:--:|:------:|:-----
+1 | android  | 跳转到当前project的根目录
+2 | bs1      | 跳转到当前project的bootloader第一阶段目录
+3 | bs2      | 跳转到当前project的bootloader第二阶段目录
+4 | dts      | 跳转到当前project的设备树目录
+5 | kernel   | 跳转到当前project的内核目录
+6 | out      | 跳转到当前project的out目录
+7 | efuse    | 跳转到当前project的签名工具目录
+
+
+# 六、自定义命令
+
+* 以上的命令都是相对通用的命令，如果需要自定义其他的命令，在[custom.sh](custom.sh)中进行处理
+* `project_product_custom()`会被传入完整的项目参数，以供所有的数据处理，参数顺序参考[config.json](config.json)中的`project_keys`数组顺序
+* 自定义命令依赖project名字调用，例如：`m0 test`命令，调用M0-project的test自定义功能。本质是调用[custom.sh](custom.sh)中`project_product_custom()`，需要自行完成针对参数判断处理
+* 支持anppc（android project product custom）直接调用project_product_custom()函数处理
+
+# 七、自定义别名
+
+[config.json](config.json)中的`alias`字段用于自定义shell alias
+
+# 八、自动扫描生成配置
+
+* 目前只支持QCOM、MTK代码架构
+* [config.json](config.json)中增加`scan`字段
+  * `true`: 表是开启自动扫描，不使用config.json中的项目配置
+  * `false`: 表是关闭自动扫描，使用config.json中的项目配置
+
---
+++ packages/anpp/anpp-0.1.4.tar.gz/anpp-0.1.4/src/anpp/template/generator.py
@@ -0,0 +1,340 @@
+#!/bin/python3
+
+import json
+import sys
+import subprocess
+import os
+
+if len(sys.argv) != 2:
+	out_file_path = "androiddir.bash"
+else:
+	out_file_path = sys.argv[1]
+
+def shell(cmd):
+	print("cmd: " + cmd)
+
+	result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+	print("return code:", result.returncode)
+
+	if result.returncode == 0:
+		return result.stdout.strip()
+	else:
+		return 0
+
+# 1. How to parse json file with c-style comments?
+#   https://stackoverflow.com/questions/29959191/how-to-parse-json-file-with-c-style-comments
+def GetJsonFromFile(filePath):
+    contents = ""
+
+    fh = open(filePath, encoding="utf-8")
+    for line in fh:
+        cleanedLine = line.split("//", 1)[0]
+        if len(cleanedLine) > 0 and line.endswith("\n") and "\n" not in cleanedLine:
+            cleanedLine += "\n"
+        contents += cleanedLine
+    fh.close
+
+    while "/*" in contents:
+        preComment, postComment = contents.split("/*", 1)
+        contents = preComment + postComment.split("*/", 1)[1]
+
+    return contents
+
+# get config
+config = json.loads(GetJsonFromFile('config.json'))
+# print(json.dumps(config, indent=4))
+
+# get project key-value keys and init key variable array
+project_keys = []
+for key in config["project_keys"]:
+	project_keys.append(key)
+	globals()["%ss" % (key)] = []
+
+# get data
+if config["scan"]:
+	defaultPath = config["defaultPath"]
+	SoCDirs = [
+			'mssi/device/mediatek',
+			'qssi/device/qcom'
+		]
+	scanSubProject = []
+	for SoCDir in SoCDirs:
+		SoCType = ""
+		if "qcom" in SoCDir:
+			SoCType = "qcom"
+		elif "mediatek" in SoCDir:
+			SoCType = "mediatek"
+
+		if SoCDir == "":
+			continue
+
+		cmd = 'find ' + defaultPath + ' -maxdepth 5 -type d -path "*/' + SoCDir + '"'
+		result = shell(cmd)
+
+		subProjects = result.split("\n")
+		for sub in subProjects:
+			if sub.strip() != "":
+				projectHome = sub.replace("/" + SoCDir, "")
+				project = os.path.basename(projectHome)
+				print("scaned project:" + project)
+				projectInfo = {}
+
+				cmd = 'find ' + defaultPath + "/" + project + ' -maxdepth 2 -type d -path "*/vendor" | grep -v -E "qssi|mssi"'
+				result = shell(cmd)
+				vendorDir = os.path.dirname(result)
+
+				if len(vendorDir) > 0:
+					vendorDirName = vendorDir.replace(projectHome + "/", "")
+					if vendorDirName.strip() == "":
+						vendorDirName = "."
+
+					cmd = 'find ' + defaultPath + "/" + project + ' -maxdepth 2 -type d -path "*/vendor" | grep -E "qssi|mssi"'
+					result = shell(cmd)
+					xssiDirName = os.path.basename(os.path.dirname(result))
+					projectInfo["xssi"]             = xssiDirName
+
+					if SoCType == "qcom":
+						projectInfo["project"]          = project
+						projectInfo["kernel"]           = vendorDirName + "/kernel_platform/msm-kernel"
+						projectInfo["vendor"]           = vendorDirName
+						projectInfo["out"]              = vendorDirName + "/out/target/product"
+
+						# get product name
+						if os.path.exists(vendorDir + "/out/target/product"):
+							cmd = 'ls ' + vendorDir + "/out/target/product" + ' -1 | grep -v -E "common|configs"'
+							result = shell(cmd)
+							projectInfo["product"]      = result
+						else:
+							projectInfo["product"]      = "unknow"
+
+						projectInfo["dts"]              = "arch/arm64/boot/dts/vendor/qcom"
+
+						cmd = 'find ' + defaultPath + "/" + project + ' -maxdepth 4 -type d -path "*/boot_images"'
+						result = shell(cmd)
+						projectInfo["bootloaderStage1"] = result.split(project + "/")[1]
+
+						projectInfo["bootloaderStage2"] = os.path.dirname(projectInfo["kernel"]) + "/bootable/bootloader/edk2"
+
+						cmd = 'find ' + defaultPath + "/" + project + ' -maxdepth 4 -type f -path "*/contents.xml" | grep -E "\.[0-9]+\.[0-9]+"'
+						result = shell(cmd)
+						projectInfo["efuse"]            = os.path.dirname(result).split(project + "/")[1] + "/common/sectools"
+
+					if SoCType == "mediatek":
+						'''
+						{
+							"project": "M0-project",
+							"product": "M0",
+							"kernel": "kernel-4.9",
+							"dts": "arch/arm64/boot/dts/mediatek/",
+							"bootloaderStage1": "vendor/mediatek/proprietary/bootable/bootloader/preloader",
+							"bootloaderStage2": "vendor/mediatek/proprietary/bootable/bootloader/lk2",
+							"out": "out/target/product",
+							"efuse": "vendor/mediatek/proprietary/scripts/sign-image_v2"
+						},
+						'''
+
+						projectInfo["project"]          = project
+						# get kernel name
+						cmd = 'find ' + vendorDir + "/kernel" + ' -maxdepth 2 -type l -path "*/kernel-*"'
+						result = shell(cmd)
+						projectInfo["kernel"]           = vendorDirName + "/" + os.path.basename(result)
+						projectInfo["vendor"]           = vendorDirName
+						projectInfo["out"]              = vendorDirName + "/out/target/product"
+
+						# get product name
+						if os.path.exists(vendorDir + "/out/target/product"):
+							cmd = 'ls ' + vendorDir + "/out/target/product" + ' -1 | grep -v -E "common|configs"'
+							result = shell(cmd)
+							projectInfo["product"]      = result
+						else:
+							projectInfo["product"]      = "unknow"
+
+						projectInfo["dts"]              = "arch/arm64/boot/dts/mediatek/"
+						projectInfo["bootloaderStage1"] = vendorDirName + "/" + "vendor/mediatek/proprietary/bootable/bootloader/preloader"
+						projectInfo["bootloaderStage2"] = vendorDirName + "/" + "vendor/mediatek/proprietary/bootable/bootloader/lk2"
+						projectInfo["efuse"]            = vendorDirName + "/" + "vendor/mediatek/proprietary/scripts/sign-image_v2"
+
+					print(projectInfo)
+
+					for key in project_keys:
+						if key in projectInfo.keys():
+							globals()["%ss" % (key)].append(projectInfo[key])
+						else:
+							globals()["%ss" % (key)].append(".")
+else:
+	defaultPath = config["defaultPath"]
+	for project in config["projects"]:
+		for key in project_keys:
+			if key in project.keys():
+				globals()["%ss" % (key)].append(project[key])
+			else:
+				globals()["%ss" % (key)].append(".")
+
+# show data
+print("defaultPath: " + defaultPath)
+for key in project_keys:
+	print(key + "s: " + str(globals()["%ss" % (key)]))
+
+# get component key
+component_keys = []
+for component in config["components"]:
+	component_keys.append(component["cmd"])
+
+# generator file
+anpp_config_start = "##### ANPP CONFIG START #####"
+anpp_config_end = "##### ANPP CONFIG END #####"
+anpp_custom_start = "##### ANPP CUSTOM START #####"
+anpp_custom_end = "##### ANPP CUSTOM END #####"
+anpp_component_start = "##### ANPP COMPONENT START #####"
+anpp_component_end = "##### ANPP COMPONENT END #####"
+anpp_command_start = "##### ANPP COMMAND START #####"
+anpp_command_end = "##### ANPP COMMAND END #####"
+anpp_alias_start = "##### ANPP ALIAS START #####"
+anpp_alias_end = "##### ANPP ALIAS END #####"
+anpp_template_skip_line = False
+with open(out_file_path, 'w', encoding = 'utf-8') as f_out:
+	with open("androiddir.bash.template", 'r', encoding = 'utf-8') as f_in:
+		for line in f_in:
+			if line.strip() == anpp_config_start:
+				anpp_template_skip_line = True
+				f_out.write(line)
+
+				f_out.write("# default Workspace dir, all projects are here\n")
+				f_out.write("export defaultPath=" + defaultPath + "\n")
+				f_out.write("\n")
+
+				for key in project_keys:
+					f_out.write("# " + key + " dir info \n")
+					f_out.write(key + "s=(\n")
+					for value_data in globals()["%ss" % (key)]:
+						f_out.write("    '" + value_data + "'\n")
+					f_out.write(")\n")
+					f_out.write("\n")
+
+			elif line.strip() == anpp_config_end:
+				anpp_template_skip_line = False
+			elif line.strip() == anpp_custom_start:
+				anpp_template_skip_line = True
+				f_out.write(line)
+
+				with open("tools/custom.sh", 'r', encoding = 'utf-8') as f_custom:
+					custom_function = False
+					index = 1
+
+					f_out.write("# 1. argv: refer to config.json \"project_keys\" array order except ${1}\n")
+					f_out.write("#     ${1}: cmd\n")
+					for key in config["project_keys"]:
+						index += 1
+						f_out.write("#     ${" + str(index) + "}: " + key + "\n")
+					f_out.write("# 2. return:\n")
+					f_out.write("#     0: function run success\n")
+
+					for line in f_custom:
+						if "project_product_custom" in line:
+							custom_function = True
+						if custom_function:
+							f_out.write(line)
+
+			elif line.strip() == anpp_custom_end:
+				anpp_template_skip_line = False
+			elif line.strip() == anpp_component_start:
+				anpp_template_skip_line = True
+				f_out.write(line)
+
+				f_out.write("# components for shell alias cmd\n")
+				f_out.write("components=(\n")
+				for key in component_keys:
+					f_out.write("    " + key+ "\n")
+				f_out.write(")\n")
+				f_out.write("\n")
+
+			elif line.strip() == anpp_component_end:
+				anpp_template_skip_line = False
+			elif line.strip() == anpp_command_start:
+				anpp_template_skip_line = True
+				f_out.write(line)
+
+				# generate shell variable
+				custom_args = "$1 ${defaultPath} "
+				for key in project_keys:
+					f_out.write("                " + key + "=${" + key + "s[i]}\n")
+					custom_args += "\"${" + key + "}\" "
+
+				# generate for custom python script
+				if custom_args.endswith(" "):
+					custom_args = custom_args[:-1]
+				f_out.write("\n")
+				f_out.write("                returnData=0\n")
+				f_out.write("                if [ -f \"$HOME/.anpp/custom.py\" ]; then\n")
+				f_out.write("                    python3 $HOME/.anpp/custom.py " + custom_args + "\n")
+				f_out.write("                    returnData=$?\n")
+				f_out.write("                fi\n")
+
+				# generate header for if
+				f_out.write("\n")
+				f_out.write("                if [ ${returnData} -ne 0 ]; then\n")
+				f_out.write("                    if [ $1 == \"None\" ]; then\n")
+				f_out.write("                        cd .\n")
+				
+				# generate if body for cmd
+				for key in component_keys:
+					f_out.write("                    elif [ $1 == \"" + key + "\" ]; then\n")
+					cmd_path = ""
+					key_index = component_keys.index(key)
+					for conbine in config["components"][key_index]["combine"]:
+						cmd_path += "${" + conbine +"}/"
+					if cmd_path.endswith("/"):
+						cmd_path = cmd_path[:-1]
+
+					cmd_type = config["components"][key_index]["type"]
+					# dir just for cd
+					if cmd_type == "dir":
+						f_out.write("                        cd " + cmd_path + "\n")
+					# file for copy
+					elif cmd_type == "file":
+						if "file" in config["components"][key_index] and len(config["components"][key_index]["file"]) > 0:
+							output_path = ""
+							for output in config["components"][key_index]["output"]:
+								output_path += "${" + output +"}/"
+							if output_path.endswith("/"):
+								output_path = output_path[:-1]
+
+							f_out.write("                        echo copy file from " + cmd_path + " to " + output_path + "\n")
+							f_out.write("                        mkdir -p " + output_path + "\n")
+							f_out.write("                        cd " + cmd_path + "\n")
+							f_out.write("                        cp -v " + "${" + config["components"][key_index]["file"] + "} " + output_path + "\n")
+							f_out.write("                        cd -\n")
+							f_out.write("                        return\n")
+
+				f_out.write("                    else\n")
+
+				# generate else for if
+				if custom_args.endswith(" "):
+					custom_args = custom_args[:-1]
+				f_out.write("                        project_product_custom " + custom_args + "\n")
+				f_out.write("                        returnData=$?\n")
+				f_out.write("                        if [ ${returnData} -ne 0 ]; then\n")
+				f_out.write("                            echo \"error: $1 returned with value: ${returnData}\"\n")
+				f_out.write("                            return\n")
+				f_out.write("                        fi\n")
+				f_out.write("                    fi \n")
+				f_out.write("                fi \n")
+
+			elif line.strip() == anpp_command_end:
+				anpp_template_skip_line = False
+			elif line.strip() == anpp_alias_start:
+				anpp_template_skip_line = True
+				f_out.write(line)
+
+				f_out.write("alias anpp=\"project_product\"           # just for project_product function alias\n")
+				f_out.write("alias anppc=\"project_product_custom\"   # just for project_product_custom function alias")
+				f_out.write("# custom shell alias cmds\n")
+				for alias in config["alias"]:
+					alias_cmd = "alias " + alias["cmd"] + "='" + alias["shell"] + "'\n"
+					f_out.write(alias_cmd)
+
+			elif line.strip() == anpp_alias_end:
+				anpp_template_skip_line = False
+
+			if not anpp_template_skip_line:
+				f_out.write(line)
