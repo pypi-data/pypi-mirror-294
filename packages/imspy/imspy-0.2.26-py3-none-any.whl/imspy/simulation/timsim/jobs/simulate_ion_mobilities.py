efc4693a3b90a98c963bc910e11cb30e35c471d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 16:04:21 +0000

    Add package imspy/0.2.26/imspy-0.2.26-py3-none-any.whl

---
+++ packages/imspy/imspy-0.2.26-py3-none-any.whl/imspy/simulation/timsim/jobs/simulate_ion_mobilities.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+from imspy.algorithm import DeepPeptideIonMobilityApex, load_deep_ccs_predictor, load_tokenizer_from_resources
+
+
+def simulate_ion_mobilities(
+        ions: pd.DataFrame,
+        im_lower: float,
+        im_upper: float,
+        verbose: bool = False
+) -> pd.DataFrame:
+
+    if verbose:
+        print("Simulating ion mobilities...")
+
+        # load IM predictor
+    IMS = DeepPeptideIonMobilityApex(
+        model=load_deep_ccs_predictor(),
+        tokenizer=load_tokenizer_from_resources('tokenizer-ptm'),
+        verbose=verbose
+    )
+
+    # simulate mobilities
+    dp = IMS.simulate_ion_mobilities_pandas(
+        ions
+    )
+
+    # filter by mobility range
+    ions = dp[
+        (dp.inv_mobility_gru_predictor >= im_lower) &
+        (dp.inv_mobility_gru_predictor <= im_upper)
+        ]
+
+    return ions
