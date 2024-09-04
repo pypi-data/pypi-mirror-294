f03aa2c1551c44eeee67fc263182130b6d661e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 04:04:26 +0000

    Add package pyhud/0.1.2/pyhud-0.1.2-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/pyhud/pyhud-0.1.2-cp310-cp310-macosx_10_9_x86_64.whl/pyhud/exception_handler.py
@@ -0,0 +1,53 @@
+import sys
+import traceback
+from types import TracebackType
+from typing import Any, Optional, Type
+
+from .client import get_client
+from .logging import internal_logger
+
+
+class FatalErrorData:
+    def __init__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_value: Optional[BaseException],
+        exc_traceback: Optional[TracebackType],
+    ):
+        self.error_message = str(exc_value) if exc_value else ""
+        self.error_name = exc_type.__name__ if exc_type else ""
+        self.error_stack = (
+            "".join(traceback.format_tb(exc_traceback)) if exc_traceback else ""
+        )
+
+
+EXCEPTIONS_TO_IGNORE = [KeyboardInterrupt]
+
+
+def install_exception_handler() -> None:
+    ORIGINAL_EXCEPTHOOK = sys.excepthook
+
+    def exception_handler(
+        exc_type: Type[BaseException],
+        exc_value: BaseException,
+        exc_traceback: Optional[TracebackType],
+    ) -> Any:
+        try:
+            if exc_type not in EXCEPTIONS_TO_IGNORE:
+                internal_logger.error(
+                    "Uncaught exception",
+                    exc_info=(exc_type, exc_value, exc_traceback),
+                )
+
+                fatal_error = FatalErrorData(exc_type, exc_value, exc_traceback)
+                client = get_client()
+                client.send_fatal_error(fatal_error)
+        except Exception as e:
+            internal_logger.error(
+                "Failed to send fatal error: {}".format(e),
+                exc_info=(exc_type, exc_value, exc_traceback),
+            )
+        finally:
+            return ORIGINAL_EXCEPTHOOK(exc_type, exc_value, exc_traceback)
+
+    sys.excepthook = exception_handler
