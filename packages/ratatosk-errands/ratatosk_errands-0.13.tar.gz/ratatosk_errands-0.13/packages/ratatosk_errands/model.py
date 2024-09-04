aeed16f956e30e3abce02113fab679b4c88347ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 15:04:30 +0000

    Add package ratatosk-errands/0.13/ratatosk_errands-0.13.tar.gz

---
+++ packages/ratatosk-errands/ratatosk_errands-0.13.tar.gz/ratatosk_errands-0.13/packages/ratatosk_errands/model.py
@@ -0,0 +1,60 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+class ModelInstructions(BaseModel):
+    pass
+
+
+class DiffusionInstructions(ModelInstructions):
+    prompt: str
+    negative_prompt: str | None = None
+    num_inference_steps: int | None = None
+    guidance_scale: float | None = None
+
+
+class ImageToImageInstructions(DiffusionInstructions):
+    base_image_identifier: str
+
+
+class TextToImageInstructions(DiffusionInstructions):
+    width: int | None = None
+    height: int | None = None
+
+
+class ChatInstructions(ModelInstructions):
+    prompt: str
+    history: List[str] = []
+    system_instructions: str | None = None
+    max_new_tokens: int | None = None
+    temperature: float | None = None
+    repetition_penalty: float | None = None
+
+
+class Errand(BaseModel):
+    instructions: ModelInstructions
+    origin: str
+    destination: str
+    identifier: str
+    timestamp: float
+
+
+class ModelResult(BaseModel):
+    pass
+
+
+class DiffusionResult(ModelResult):
+    image_identifier: str
+
+
+class ChatResult(ModelResult):
+    message: str
+
+
+class Reply(BaseModel):
+    result: ModelResult
+    origin: str
+    destination: str
+    identifier: str
+    timestamp: float
