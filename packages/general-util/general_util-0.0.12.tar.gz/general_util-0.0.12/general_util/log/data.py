9040b318ca61697a7b81410876150c892a640f80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 16:04:23 +0000

    Add package general-util/0.0.12/general_util-0.0.12.tar.gz

---
+++ packages/general-util/general_util-0.0.12.tar.gz/general_util-0.0.12/general_util/log/data.py
@@ -0,0 +1,10 @@
+url_module_mapping = {
+    "/docs": ("custom", "document", "docs"),
+    "/openapi.json": ("custom", "document", "docs2")
+}
+
+status_code_rules = {
+    "401": {"message": "Unauthorized", "message_code": "401"},
+    "422": {"message": "Unprocessable Entity", "message_code": "422"},
+    "500": {"message": "Internal Server Error", "message_code": "500"},
+}
