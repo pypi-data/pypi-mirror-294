1afe3494573904a67e07167bb6f3fde8f01f36b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 05:03:44 +0000

    Add package Flask-Admin/2.0.0a0/flask_admin-2.0.0a0-py3-none-any.whl

---
+++ packages/Flask-Admin/flask_admin-2.0.0a0-py3-none-any.whl/flask_admin/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '2.0.0a0'
+__author__ = 'Flask-Admin team'
+__email__ = 'contact@palletsproject.com'
+
+
+from .base import expose, expose_plugview, Admin, BaseView, AdminIndexView  # noqa: F401
---
+++ packages/Flask-Admin/flask_admin-2.0.0a0-py3-none-any.whl/flask_admin/_compat.py
@@ -0,0 +1,39 @@
+# flake8: noqa
+"""
+    flask_admin._compat
+    ~~~~~~~~~~~~~~~~~~~~~~~
+
+    Some py2/py3 compatibility support based on a stripped down
+    version of six so we don't have to depend on a specific version
+    of it.
+
+    :copyright: (c) 2013 by Armin Ronacher.
+    :license: BSD, see LICENSE for more details.
+"""
+from typing import Callable
+
+text_type = str
+string_types = (str,)
+
+
+def itervalues(d: dict):
+    return iter(d.values())
+
+
+def iteritems(d: dict):
+    return iter(d.items())
+
+
+def filter_list(f: Callable, l: list):
+    return list(filter(f, l))
+
+
+def as_unicode(s):
+    if isinstance(s, bytes):
+        return s.decode('utf-8')
+
+    return str(s)
+
+def csv_encode(s):
+    ''' Returns unicode string expected by Python 3's csv module '''
+    return as_unicode(s)
