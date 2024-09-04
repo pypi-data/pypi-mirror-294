532651d8927f5642e8f65e5f217185b40fb75e05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 06:03:32 +0000

    Add package spakky-core/1.5.5/spakky_core-1.5.5-py3-none-any.whl

---
+++ packages/spakky-core/spakky_core-1.5.5-py3-none-any.whl/spakky/domain/ports/event/event_consumer.py
@@ -0,0 +1,36 @@
+from abc import abstractmethod
+from typing import TypeVar, Callable, Protocol, Awaitable, TypeAlias, runtime_checkable
+from asyncio import Event as AsyncEvent
+from threading import Event as ThreadEvent
+
+from spakky.domain.models.event import DomainEvent
+
+DomainEventT = TypeVar("DomainEventT", bound=DomainEvent)
+IEventHandlerCallback: TypeAlias = Callable[[DomainEventT], None]
+IAsyncEventHandlerCallback: TypeAlias = Callable[[DomainEventT], Awaitable[None]]
+
+
+@runtime_checkable
+class IEventConsumer(Protocol):
+    @abstractmethod
+    def register(
+        self,
+        event: type[DomainEventT],
+        handler: IEventHandlerCallback[DomainEventT],
+    ) -> None: ...
+
+    @abstractmethod
+    def start(self, cancellation_token: ThreadEvent) -> None: ...
+
+
+@runtime_checkable
+class IAsyncEventConsumer(Protocol):
+    @abstractmethod
+    async def register(
+        self,
+        event: type[DomainEventT],
+        handler: IAsyncEventHandlerCallback[DomainEventT],
+    ) -> None: ...
+
+    @abstractmethod
+    async def start(self, cancellation_token: AsyncEvent) -> None: ...
