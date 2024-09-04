b2405300616e0b27fe27225df13fcc79495187d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 03:03:17 +0000

    Add package ase2sprkkr/3.0.9b0/ase2sprkkr-3.0.9b0.tar.gz

---
+++ packages/ase2sprkkr/ase2sprkkr-3.0.9b0.tar.gz/ase2sprkkr-3.0.9b0/src/ase2sprkkr/input_parameters/definitions/dos.py
@@ -0,0 +1,28 @@
+""" DOS task input parameters definition"""
+from ...common.grammar_types  import *
+from .sections import *
+from ..input_parameters_definitions import \
+    InputParametersDefinition as InputParameters, \
+    InputValueDefinition as V \
+
+input_parameters = InputParameters(
+      'dos', [
+      CONTROL('DOS'),
+      TAU,
+      ENERGY.copy([
+          V('EMAX', 1.0)
+      ], defaults= {
+          'GRID' : 3,
+          'NE' : 300,
+          'ImE' : 0.01,
+      }),
+      TASK('DOS'),
+      SITES
+  ],
+  executable='kkrgen',
+  mpi=True
+)
+""" DOS - density of states input parameters definition"""
+
+from ...common.doc import process_input_parameters_definition
+process_input_parameters_definition(__name__)
