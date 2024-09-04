c29a19dd892111f13881e34f1e676a6d65566c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 15:05:20 +0000

    Add package pyhcx/0.9.8/pyhcx-0.9.8-py3-none-any.whl

---
+++ packages/pyhcx/pyhcx-0.9.8-py3-none-any.whl/pyhcx/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+# import apis into api package
+from pyhcx.api.chat_api import ChatApi
+from pyhcx.api.completions_api import CompletionsApi
+from pyhcx.api.embeddings_api import EmbeddingsApi
+from pyhcx.api.fine_tuning_api import FineTuningApi
+from pyhcx.api.segmentation_api import SegmentationApi
+
