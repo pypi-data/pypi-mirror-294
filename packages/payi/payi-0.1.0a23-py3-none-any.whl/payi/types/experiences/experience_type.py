536f30d88141c1d1a57c1660e161dd1ba0bfa4d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 05:04:00 +0000

    Add package payi/0.1.0a23/payi-0.1.0a23-py3-none-any.whl

---
+++ packages/payi/payi-0.1.0a23-py3-none-any.whl/payi/types/experiences/experience_type.py
@@ -0,0 +1,17 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+
+
+from ..._models import BaseModel
+
+__all__ = ["ExperienceType"]
+
+
+class ExperienceType(BaseModel):
+    description: str
+
+    experience_type_id: str
+
+    name: str
+
+    request_id: str
