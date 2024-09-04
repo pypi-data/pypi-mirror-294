4d347289f7b42f6f6e101253b152335d2735c9a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 04:04:03 +0000

    Add package simplepgsql/0.1.10/simplepgsql-0.1.10-py2.py3-none-any.whl

---
+++ packages/simplepgsql/simplepgsql-0.1.10-py2.py3-none-any.whl/simplepgsql/__init__.py
@@ -0,0 +1,5 @@
+from .src.depr_simplepgsql import DBConnect
+from .src.simplepgsql import SimplePgSQL
+from .src.params import Params, Query, StandardKeywords
+
+__all__ = ["SimplePgSQL", "Params", "Query", "StandardKeywords"]
