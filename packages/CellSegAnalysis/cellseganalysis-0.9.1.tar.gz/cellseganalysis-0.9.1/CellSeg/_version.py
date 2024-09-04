3c16a8eefa3335f9c419d486db89a2c9ebc653af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 03:03:27 +0000

    Add package CellSegAnalysis/0.9.1/cellseganalysis-0.9.1.tar.gz

---
+++ packages/CellSegAnalysis/cellseganalysis-0.9.1.tar.gz/cellseganalysis-0.9.1/CellSeg/_version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CellSeg SETUP.PY
+#
+short_version = '0.9.1'
+version = '0.9.1'
+full_version = '0.9.1.dev0+5b51c7f'
+git_revision = '5b51c7f77a97b0a0da6c7d49cbffe7a78b617e5a'
+release = False
+if not release:
+    version = full_version
