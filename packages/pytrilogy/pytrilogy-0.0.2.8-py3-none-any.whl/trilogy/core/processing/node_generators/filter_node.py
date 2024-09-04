ca3570c1a1e84eabb1f59558346d2c02886208ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 04:03:28 +0000

    Add package pytrilogy/0.0.2.8/pytrilogy-0.0.2.8-py3-none-any.whl

---
+++ packages/pytrilogy/pytrilogy-0.0.2.8-py3-none-any.whl/trilogy/core/processing/node_generators/filter_node.py
@@ -0,0 +1,167 @@
+from typing import List
+
+
+from trilogy.core.enums import JoinType
+from trilogy.core.models import Concept, Environment, FilterItem, Grain
+from trilogy.core.processing.nodes import (
+    FilterNode,
+    MergeNode,
+    NodeJoin,
+    History,
+    StrategyNode,
+)
+from trilogy.core.processing.node_generators.common import (
+    resolve_filter_parent_concepts,
+)
+from trilogy.constants import logger
+from trilogy.core.processing.utility import padding, unique
+from trilogy.core.processing.node_generators.common import concept_to_relevant_joins
+
+LOGGER_PREFIX = "[GEN_FILTER_NODE]"
+
+
+def gen_filter_node(
+    concept: Concept,
+    local_optional: List[Concept],
+    environment: Environment,
+    g,
+    depth: int,
+    source_concepts,
+    history: History | None = None,
+) -> StrategyNode | None:
+    immediate_parent, parent_row_concepts, parent_existence_concepts = (
+        resolve_filter_parent_concepts(concept)
+    )
+    if not isinstance(concept.lineage, FilterItem):
+        raise SyntaxError('Filter node must have a lineage of type "FilterItem"')
+    where = concept.lineage.where
+
+    logger.info(
+        f"{padding(depth)}{LOGGER_PREFIX} fetching filter node row parents {[x.address for x in parent_row_concepts]}"
+    )
+    core_parents = []
+    parent: StrategyNode = source_concepts(
+        mandatory_list=parent_row_concepts,
+        environment=environment,
+        g=g,
+        depth=depth + 1,
+        history=history,
+    )
+
+    if not parent:
+        logger.info(
+            f"{padding(depth)}{LOGGER_PREFIX} filter node row parents {[x.address for x in parent_row_concepts]} could not be found"
+        )
+        return None
+
+    if not local_optional and not parent_existence_concepts:
+        optimized_pushdown = True
+    else:
+        optimized_pushdown = False
+
+    if optimized_pushdown:
+        if parent.conditions:
+            parent.conditions = parent.conditions + where.conditional
+        else:
+            parent.conditions = where.conditional
+        parent.output_concepts = [concept]
+        parent.grain = Grain(components=[concept])
+        parent.rebuild_cache()
+
+        logger.info(
+            f"{padding(depth)}{LOGGER_PREFIX} returning optimized filter node with pushdown to parent with condition {where.conditional}"
+        )
+        return parent
+
+    core_parents.append(parent)
+
+    if parent_existence_concepts:
+        for existence_tuple in parent_existence_concepts:
+            if not existence_tuple:
+                continue
+            logger.info(
+                f"{padding(depth)}{LOGGER_PREFIX} fetching filter node existence parents {[x.address for x in existence_tuple]}"
+            )
+            parent_existence = source_concepts(
+                mandatory_list=list(existence_tuple),
+                environment=environment,
+                g=g,
+                depth=depth + 1,
+                history=history,
+            )
+            if not parent_existence:
+                logger.info(
+                    f"{padding(depth)}{LOGGER_PREFIX} filter existence node parents could not be found"
+                )
+                return None
+            core_parents.append(parent_existence)
+    flattened_existence = [x for y in parent_existence_concepts for x in y]
+    filter_node = FilterNode(
+        input_concepts=unique(
+            [immediate_parent] + parent_row_concepts + flattened_existence,
+            "address",
+        ),
+        output_concepts=[concept, immediate_parent] + parent_row_concepts,
+        environment=environment,
+        g=g,
+        parents=core_parents,
+        grain=Grain(
+            components=[immediate_parent] + parent_row_concepts,
+        ),
+    )
+
+    assert filter_node.resolve().grain == Grain(
+        components=[immediate_parent] + parent_row_concepts,
+    )
+    if not local_optional or all(
+        [x.address in [y.address for y in parent_row_concepts] for x in local_optional]
+    ):
+        outputs = [
+            x
+            for x in filter_node.output_concepts
+            if x.address in [y.address for y in local_optional]
+        ]
+        logger.info(
+            f"{padding(depth)}{LOGGER_PREFIX} no extra enrichment needed for filter node"
+        )
+        filter_node.output_concepts = [
+            concept,
+        ] + outputs
+        filter_node.rebuild_cache()
+        return filter_node
+    enrich_node = source_concepts(  # this fetches the parent + join keys
+        # to then connect to the rest of the query
+        mandatory_list=[immediate_parent] + parent_row_concepts + local_optional,
+        environment=environment,
+        g=g,
+        depth=depth + 1,
+        history=history,
+    )
+    if not enrich_node:
+        return filter_node
+    x = MergeNode(
+        input_concepts=[concept, immediate_parent] + local_optional,
+        output_concepts=[
+            concept,
+        ]
+        + local_optional,
+        environment=environment,
+        g=g,
+        parents=[
+            # this node fetches only what we need to filter
+            filter_node,
+            enrich_node,
+        ],
+        node_joins=[
+            NodeJoin(
+                left_node=enrich_node,
+                right_node=filter_node,
+                concepts=concept_to_relevant_joins(
+                    [immediate_parent] + parent_row_concepts
+                ),
+                join_type=JoinType.LEFT_OUTER,
+                filter_to_mutual=False,
+            )
+        ],
+    )
+    return x
---
+++ packages/pytrilogy/pytrilogy-0.0.2.8-py3-none-any.whl/trilogy/core/processing/node_generators/rowset_node.py
@@ -0,0 +1,172 @@
+from trilogy.core.models import (
+    Concept,
+    Environment,
+    SelectStatement,
+    RowsetDerivationStatement,
+    RowsetItem,
+    MultiSelectStatement,
+)
+from trilogy.core.processing.nodes import MergeNode, NodeJoin, History, StrategyNode
+from trilogy.core.processing.nodes.base_node import concept_list_to_grain
+from typing import List
+
+from trilogy.core.enums import JoinType, PurposeLineage
+from trilogy.constants import logger
+from trilogy.core.processing.utility import padding, unique
+from trilogy.core.processing.node_generators.common import concept_to_relevant_joins
+
+
+LOGGER_PREFIX = "[GEN_ROWSET_NODE]"
+
+
+def gen_rowset_node(
+    concept: Concept,
+    local_optional: List[Concept],
+    environment: Environment,
+    g,
+    depth: int,
+    source_concepts,
+    history: History | None = None,
+) -> StrategyNode | None:
+    if not isinstance(concept.lineage, RowsetItem):
+        raise SyntaxError(
+            f"Invalid lineage passed into rowset fetch, got {type(concept.lineage)}, expected {RowsetItem}"
+        )
+    lineage: RowsetItem = concept.lineage
+    rowset: RowsetDerivationStatement = lineage.rowset
+    select: SelectStatement | MultiSelectStatement = lineage.rowset.select
+    existence_parents: List[StrategyNode] = []
+    if where := select.where_clause:
+        targets = select.output_components + where.conditional.row_arguments
+        for sub_select in where.conditional.existence_arguments:
+            logger.info(
+                f"{padding(depth)}{LOGGER_PREFIX} generating parent existence node with {[x.address for x in sub_select]}"
+            )
+            parent_check = source_concepts(
+                mandatory_list=sub_select,
+                environment=environment,
+                g=g,
+                depth=depth + 1,
+                history=history,
+            )
+            if not parent_check:
+                logger.info(
+                    f"{padding(depth)}{LOGGER_PREFIX} Cannot generate parent existence node for rowset node for {concept}"
+                )
+                return None
+            existence_parents.append(parent_check)
+    else:
+        targets = select.output_components
+    node: StrategyNode = source_concepts(
+        mandatory_list=unique(targets, "address"),
+        environment=environment,
+        g=g,
+        depth=depth + 1,
+        history=history,
+    )
+
+    if not node:
+        logger.info(
+            f"{padding(depth)}{LOGGER_PREFIX} Cannot generate rowset node for {concept}"
+        )
+        return None
+    # add our existence concepts in
+    if existence_parents:
+        node.parents += existence_parents
+        # we don't need to join to any existence parents
+        # if isinstance(node, MergeNode) and node.node_joins is None:
+        #     # set it explicitly to empty to avoid inference
+        #     node.node_joins = []
+        for parent in existence_parents:
+            for x in parent.output_concepts:
+                if x.address not in node.output_lcl:
+                    node.existence_concepts.append(x)
+
+    node.conditions = select.where_clause.conditional if select.where_clause else None
+    enrichment = set([x.address for x in local_optional])
+    rowset_relevant = [x for x in rowset.derived_concepts]
+    select_hidden = set([x.address for x in select.hidden_components])
+    rowset_hidden = [
+        x
+        for x in rowset.derived_concepts
+        if isinstance(x.lineage, RowsetItem)
+        and x.lineage.content.address in select_hidden
+    ]
+    additional_relevant = [
+        x for x in select.output_components if x.address in enrichment
+    ]
+    # add in other other concepts
+
+    node.add_output_concepts(rowset_relevant + additional_relevant)
+    if select.where_clause:
+        for item in additional_relevant:
+            node.partial_concepts.append(item)
+
+    final_hidden = rowset_hidden + [
+        x
+        for x in node.output_concepts
+        if x.address not in [y.address for y in local_optional + [concept]]
+        and x.derivation != PurposeLineage.ROWSET
+    ]
+    node.hide_output_concepts(final_hidden)
+    # assume grain to be output of select
+    # but don't include anything aggregate at this point
+    assert node.resolution_cache
+
+    node.resolution_cache.grain = concept_list_to_grain(
+        node.output_concepts, parent_sources=node.resolution_cache.datasources
+    )
+
+    possible_joins = concept_to_relevant_joins(additional_relevant)
+    if not local_optional:
+        logger.info(
+            f"{padding(depth)}{LOGGER_PREFIX} no enriched required for rowset node; exiting early"
+        )
+        return node
+    if not possible_joins:
+        logger.info(
+            f"{padding(depth)}{LOGGER_PREFIX} no possible joins for rowset node; exiting early"
+        )
+        return node
+    if all(
+        [x.address in [y.address for y in node.output_concepts] for x in local_optional]
+    ):
+        logger.info(
+            f"{padding(depth)}{LOGGER_PREFIX} all enriched concepts returned from base rowset node; exiting early"
+        )
+        return node
+    enrich_node: MergeNode = source_concepts(  # this fetches the parent + join keys
+        # to then connect to the rest of the query
+        mandatory_list=additional_relevant + local_optional,
+        environment=environment,
+        g=g,
+        depth=depth + 1,
+    )
+    if not enrich_node:
+        logger.info(
+            f"{padding(depth)}{LOGGER_PREFIX} Cannot generate rowset enrichment node for {concept} with optional {local_optional}, returning just rowset node"
+        )
+        return node
+    return MergeNode(
+        input_concepts=enrich_node.output_concepts + node.output_concepts,
+        output_concepts=node.output_concepts + local_optional,
+        environment=environment,
+        g=g,
+        depth=depth,
+        parents=[
+            # this node gets the window
+            node,
+            # this node gets enrichment
+            enrich_node,
+        ],
+        node_joins=[
+            NodeJoin(
+                left_node=enrich_node,
+                right_node=node,
+                concepts=concept_to_relevant_joins(additional_relevant),
+                filter_to_mutual=False,
+                join_type=JoinType.LEFT_OUTER,
+            )
+        ],
+        partial_concepts=node.partial_concepts,
+    )
