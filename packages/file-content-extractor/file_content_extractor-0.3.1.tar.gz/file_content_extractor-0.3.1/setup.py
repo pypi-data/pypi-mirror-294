fff84c8ba61ecd48a4c5272e20fea077b090bc53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 03:03:26 +0000

    Add package file-content-extractor/0.3.1/file_content_extractor-0.3.1.tar.gz

---
+++ packages/file-content-extractor/file_content_extractor-0.3.1.tar.gz/file_content_extractor-0.3.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="file-content-extractor",
+    version="0.3.1",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'fce=main:main', 
+        ],
+    },
+    install_requires=[],
+    author="Daniel",
+    author_email="daniel@udit.one",
+    description="A tool to extract content from text files in a directory, with options to ignore certain files and directories.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/joonheeu/file-content-extractor",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
