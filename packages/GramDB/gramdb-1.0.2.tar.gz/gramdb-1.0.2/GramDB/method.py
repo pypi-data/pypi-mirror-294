7a8ba56b6f4ea1b542969fdd6a150c9f41787787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 15:04:56 +0000

    Add package GramDB/1.0.2/gramdb-1.0.2.tar.gz

---
+++ packages/GramDB/gramdb-1.0.2.tar.gz/gramdb-1.0.2/GramDB/method.py
@@ -0,0 +1,146 @@
+# methods
+import requests
+import json
+import copy
+
+
+async def fetch_func(session, base_url, token, data_id):
+    url = base_url + "/fetch"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": token
+    }
+    payload = {
+        "data_id": data_id
+    }
+    async with session.post(url, headers=headers, json=payload) as response:
+        if response.status == 200:
+            result = await response.json()
+            return True, result
+        else:
+            return False, response.status
+
+
+async def insert_func(session, base_url, token, data, table_name):
+    url = base_url + "/insert"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": token
+    }
+    data["_table_"] = table_name
+    dataa = json.dumps(data)
+    payload = {
+        "data": str(dataa)
+    }
+    async with session.post(url, headers=headers, json=payload) as response:
+        if response.status == 201:
+            result = await response.json()
+            return True, result
+        else:
+            result = await response.json()
+            return False, result
+
+
+async def delete_func(session, base_url, token, data_id):
+    url = base_url + "/delete"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": token
+    }
+    payload = {
+        "data_id": data_id
+    }
+    async with await session.post(url, headers=headers, json=payload) as response:
+        if response.status == 200:
+            result = await response.json()
+            return True, result
+        else:
+            return False, response.status
+
+
+async def update_func(session, base_url, token, data_id, data, table_name):
+    dataa = copy.deepcopy(data)
+    dataa["_table_"] = table_name
+    del dataa["_m_id"]
+    dataa = json.dumps(dataa)
+    
+    url = base_url + "/update"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": token
+    }
+    payload = {
+        "data_id": data_id,
+        "data": dataa
+    }
+    async with session.post(url, headers=headers, json=payload) as response:
+        if response.status == 200:
+            result = await response.json()
+            print(result)
+            return True, result
+        else:
+            print(response.text)
+            return False, response.status
+
+
+async def git_func(session, base_url, token, data):
+    url = base_url + "/git"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": token
+    }
+    payload = {
+        "data" : data
+    }
+    async with session.post(url, headers=headers, json=payload) as response:
+        if response.status == 200:
+            result = await response.json()
+            print(result)
+            return True, result
+        else:
+            print(">>>", response.text)
+            return False, response.status
+
+
+def extract_func(base_url, token):
+    url = base_url + f"/extract?token={token}"
+    headers = {
+        "Content-Type": "application/json",
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        result = response.json()
+        return True, result
+    else:
+        return False, response.status_code
+
+
+async def async_extract_func(base_url, token):
+    url = base_url + f"/extract?token={token}"
+    headers = {
+        "Content-Type": "application/json",
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        result = response.json()
+        return True, result
+    else:
+        return False, response.status_code
+    
+
+def fetchall_func(base_url, token, data_ids):
+    url = base_url + "/fetchall"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": token
+    }
+    payload = {
+        "data_id": data_ids
+    }
+    response = requests.post(url, headers=headers, json=payload)
+    if response.status_code == 200:
+        result = response.json()
+        return True, result
+    else:
+        return False, response.status_code
+        
