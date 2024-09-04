d41aee6fe3eae47dd81d736c42c81958af74ab31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 06:03:34 +0000

    Add package viur-shop/0.1.0.dev16/viur_shop-0.1.0.dev16-py3-none-any.whl

---
+++ packages/viur-shop/viur_shop-0.1.0.dev16-py3-none-any.whl/viur/shop/modules/shipping_config.py
@@ -0,0 +1,15 @@
+from viur.core.prototypes import List
+from .abstract import ShopModuleAbstract
+
+from ..globals import SHOP_LOGGER
+
+logger = SHOP_LOGGER.getChild(__name__)
+
+
+class ShippingConfig(ShopModuleAbstract, List):
+    kindName = "{{viur_shop_modulename}}_shipping_config"
+
+    def adminInfo(self) -> dict:
+        admin_info = super().adminInfo()
+        admin_info["icon"] = "truck-flatbed"
+        return admin_info
---
+++ packages/viur-shop/viur_shop-0.1.0.dev16-py3-none-any.whl/viur/shop/skeletons/shipping_config.py
@@ -0,0 +1,23 @@
+import typing as t  # noqa
+
+from viur.core.bones import *
+from viur.core.skeleton import Skeleton
+from .shipping_precondition import ShippingPreconditionRelSkel
+from ..globals import SHOP_LOGGER
+
+logger = SHOP_LOGGER.getChild(__name__)
+
+
+class ShippingConfigSkel(Skeleton):  # STATE: Complete (as in model)
+    kindName = "{{viur_shop_modulename}}_shipping_config"
+
+    name = StringBone(
+    )
+
+    shipping_skel = RelationalBone(
+        kind="{{viur_shop_modulename}}_shipping",
+        module="{{viur_shop_modulename}}/shipping",
+        using=ShippingPreconditionRelSkel,
+        consistency=RelationalConsistency.PreventDeletion,
+        multiple=True,
+    )
