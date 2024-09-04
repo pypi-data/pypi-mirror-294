640e52ca338a67a98fa66c0a7f4da7fe6f4e7d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 15:05:15 +0000

    Add package dosmaster/1.8.16/dosmaster-1.8.16.tar.gz

---
+++ packages/dosmaster/dosmaster-1.8.16.tar.gz/dosmaster-1.8.16/setup.py
@@ -0,0 +1,51 @@
+from setuptools import find_packages, setup
+
+from dosmaster.main import __version__
+
+with open("README.md") as file:
+    long_description = file.read()
+
+setup(
+    name="dosmaster",
+    version=__version__,
+    description="DOS(Density Of States) Plot Smartly in Terminal",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pyj6767/DOSMaster",
+    author="Youngjun Park, Jaeson Kim in CNMD",
+    author_email="yjpark29@postech.ac.kr",
+    #long_description=long_description,
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Scientific/Engineering :: Physics",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords="vasp dos",
+    packages=find_packages(),
+    python_requires=">=3.7.13",
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "pandas>=1.3.5",
+        "ase>=3.22.1",
+        "colorama>=0.4.6",
+        "PyYAML",
+    ],
+    package_data={
+        "dosmaster": [
+            "dosmaster/subplotter/colors.csv",
+        ]
+    },
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "dosmaster = dosmaster.main.dosmaster:main",
+        ]
+    },
+)
