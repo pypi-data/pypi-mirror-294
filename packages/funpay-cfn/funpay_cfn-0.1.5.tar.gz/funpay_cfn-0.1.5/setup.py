93a10eba937ea6942981897c33cdcbba5678a88b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 05:04:10 +0000

    Add package funpay-cfn/0.1.5/funpay_cfn-0.1.5.tar.gz

---
+++ packages/funpay-cfn/funpay_cfn-0.1.5.tar.gz/funpay_cfn-0.1.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+   name='funpay-cfn',  # Вставьте уникальное имя вашей библиотеки
+   version='0.1.5',
+   author='cfn',
+   author_email='cfn@cfn.com',
+   description='cfn',
+   long_description=open('README.md').read(),
+   long_description_content_type='text/markdown',
+   packages=find_packages(where="."),
+   install_requires=[
+       'requests_toolbelt',
+       'beautifulsoup4',
+       'requests',
+   ],
+   classifiers=[
+       'Programming Language :: Python :: 3',
+       'License :: OSI Approved :: MIT License',
+       'Operating System :: OS Independent',
+   ],
+   python_requires='>=3.6',
+)
