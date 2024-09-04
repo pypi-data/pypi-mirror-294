8b1517048bde1f713f44065828a4e7b051ac724d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 16:03:48 +0000

    Add package torchkeras/4.0.0/torchkeras-4.0.0.tar.gz

---
+++ packages/torchkeras/torchkeras-4.0.0.tar.gz/torchkeras-4.0.0/torchkeras/tabular/models/__init__.py
@@ -0,0 +1,82 @@
+from . import (
+    autoint,
+    category_embedding,
+    danet,
+    ft_transformer,
+    gandalf,
+    gate,
+    node,
+    tab_transformer,
+    tabnet,
+    fm,
+    deepfm,
+    deepcross
+)
+
+from .autoint import AutoIntConfig, AutoIntModel
+from .base_model import BaseModel
+from .category_embedding import CategoryEmbeddingModel, CategoryEmbeddingModelConfig
+from .danet import DANetConfig, DANetModel
+from .ft_transformer import FTTransformerConfig, FTTransformerModel
+from .gandalf import GANDALFBackbone, GANDALFConfig, GANDALFModel
+from .gate import GatedAdditiveTreeEnsembleConfig, GatedAdditiveTreeEnsembleModel
+from .node import NODEConfig, NODEModel
+from .tab_transformer import TabTransformerConfig, TabTransformerModel
+from .tabnet import TabNetModel, TabNetModelConfig
+from .fm import FMModel, FMConfig
+from .deepfm import DeepFMModel, DeepFMConfig
+from .deepcross import DeepCrossModel, DeepCrossConfig 
+
+
+__all__ = [
+    "BaseModel",
+    
+    "CategoryEmbeddingModel",
+    "CategoryEmbeddingModelConfig",
+    "category_embedding",
+    
+    "NODEModel",
+    "NODEConfig",
+    "node",
+    
+    "TabNetModel",
+    "TabNetModelConfig",
+    "tabnet",
+    
+    "AutoIntConfig",
+    "AutoIntModel",
+    "autoint",
+    
+    "TabTransformerConfig",
+    "TabTransformerModel",
+    "tab_transformer",
+    
+    "FTTransformerConfig",
+    "FTTransformerModel",
+    "ft_transformer",
+    
+    "GatedAdditiveTreeEnsembleConfig",
+    "GatedAdditiveTreeEnsembleModel",
+    "gate",
+    
+    "GANDALFConfig",
+    "GANDALFModel",
+    "GANDALFBackbone",
+    "gandalf",
+    
+    "DANetConfig",
+    "DANetModel",
+    "danet",
+
+    "FMConfig",
+    "FMModel",    
+    "fm",
+
+    "DeepFMConfig",
+    "DeepFMModel",
+    "deepfm",
+
+    "DeepCrossConfig",
+    "DeepCrossModel",
+    "deepcross"
+]
