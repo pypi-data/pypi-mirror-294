3daae77fdf756922331354d346695d541f2742a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 03:03:27 +0000

    Add package stackview/0.9.2/stackview-0.9.2.tar.gz

---
+++ packages/stackview/stackview-0.9.2.tar.gz/stackview-0.9.2/stackview/_picker.py
@@ -0,0 +1,88 @@
+
+def picker(
+        image,
+        slice_number: int = None,
+        display_width: int = None,
+        display_height: int = None,
+        continuous_update: bool = True,
+        slider_text: str = "Slice",
+        zoom_factor:float = 1.0,
+        zoom_spline_order:int = 0,
+        colormap:str = None,
+        display_min:float = None,
+        display_max:float = None
+):
+    """Shows an image with a slider to go through a stack plus a label with the current mouse position and intensity at that position.
+
+    Parameters
+    ----------
+    image : image
+        Image shown
+    slice_number : int, optional
+        Slice-position in the stack
+    display_width : int, optional
+        This parameter is obsolete. Use zoom_factor instead
+    display_height : int, optional
+        This parameter is obsolete. Use zoom_factor instead
+    continuous_update : bool, optional
+        Update the image while dragging the mouse, default: False
+    zoom_factor: float, optional
+        Allows showing the image larger (> 1) or smaller (<1)
+    zoom_spline_order: int, optional
+        Spline order used for interpolation (default=0, nearest-neighbor)
+    colormap: str, optional
+        Matplotlib colormap name or "pure_green", "pure_magenta", ...
+    display_min: float, optional
+        Lower bound of properly shown intensities
+    display_max: float, optional
+        Upper bound of properly shown intensities
+
+    Returns
+    -------
+    An ipywidget with an image display, a slider and a label showing mouse position and intensity.
+    """
+
+    import ipywidgets
+    from ._slice_viewer import _SliceViewer
+    from ._utilities import _no_resize
+
+    if 'cupy.ndarray' in str(type(image)):
+        image = image.get()
+
+    viewer = _SliceViewer(image,
+                          slice_number=slice_number,
+                          continuous_update=continuous_update,
+                          slider_text=slider_text,
+                          zoom_factor=zoom_factor,
+                          zoom_spline_order=zoom_spline_order,
+                          colormap=colormap,
+                          display_min=display_min,
+                          display_max=display_max
+                          )
+    view = viewer.view
+    slice_slider = viewer.slice_slider
+    label = ipywidgets.Label("[]:")
+
+    from ipyevents import Event
+    event_handler = Event(source=view, watched_events=['mousemove'])
+
+    def update_display(event):
+        relative_position_x = event['relativeX'] / zoom_factor
+        relative_position_y = event['relativeY'] / zoom_factor
+        absolute_position_x = int(relative_position_x)
+        absolute_position_y = int(relative_position_y)
+
+        if len(image.shape) > 2:
+            absolute_position_z = slice_slider.value
+            intensity = image[absolute_position_z, absolute_position_y, absolute_position_x]
+            label.value = "[z=" + str(absolute_position_z) + ", y=" + str(absolute_position_y) + ", x=" + str(
+                absolute_position_x) + "] = " + str(intensity)
+        else:
+            intensity = image[absolute_position_y, absolute_position_x]
+            label.value = "[y=" + str(absolute_position_y) + ", x=" + str(absolute_position_x) + "] = " + str(intensity)
+
+    event_handler.on_dom_event(update_display)
+
+    result = ipywidgets.VBox([_no_resize(view), slice_slider, label], stretch=False)
+    result.update = update_display
+    return result
