d0059cd2421d4f313ab865639ec93f7cba06a528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 14:04:33 +0000

    Add package hudu-api-ctek/0.0.38/hudu_api_ctek-0.0.38-py3-none-any.whl

---
+++ packages/hudu-api-ctek/hudu_api_ctek-0.0.38-py3-none-any.whl/hudu_api_ctek/__init__.py
@@ -0,0 +1,5 @@
+from .assets import get_assets_by_layout_id
+from .companies import get_companies_all
+from .magicdash import create_magic_dash
+
+__all__ = ["get_assets_by_layout_id", "get_companies_all", "create_magic_dash"]
