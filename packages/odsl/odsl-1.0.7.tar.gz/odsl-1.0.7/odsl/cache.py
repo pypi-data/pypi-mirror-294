b457fbec0b2d285fa543653f02135c53240c7b3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 03:03:29 +0000

    Add package odsl/1.0.7/odsl-1.0.7.tar.gz

---
+++ packages/odsl/odsl-1.0.7.tar.gz/odsl-1.0.7/odsl/cache.py
@@ -0,0 +1,20 @@
+import os
+import msal
+import atexit
+
+class TokenCacheAspect:
+    filedir = os.path.expanduser('~') + "/AppData/Roaming/ODSL/"
+    filename = filedir + "token.odsl"
+    data = None
+    cache = msal.SerializableTokenCache()
+    
+    def __init__(self):
+        self.cache.deserialize(open(self.filename, "r").read())
+        atexit.register(lambda:
+            open(self.filename, "w").write(self.cache.serialize()))
+        
+    def getCache(self):
+        return self.cache
+    
+    def logout(self):
+        os.remove(self.filename)
