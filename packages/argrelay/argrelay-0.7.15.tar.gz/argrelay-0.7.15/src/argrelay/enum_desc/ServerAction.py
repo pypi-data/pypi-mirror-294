f11b9356abcd1358d87dba1e0b2bd2a285055cd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 05:03:41 +0000

    Add package argrelay/0.7.15/argrelay-0.7.15.tar.gz

---
+++ packages/argrelay/argrelay-0.7.15.tar.gz/argrelay-0.7.15/src/argrelay/enum_desc/ServerAction.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+
+class ServerAction(Enum):
+    """
+    Defines both:
+    *   `ServerAction` in REST API URL path for server request (via enum item value)
+    *   `ServerAction` in server request payload (via enum item name)
+    """
+
+    ProposeArgValues = "/propose_arg_values/"
+
+    DescribeLineArgs = "/describe_line_args/"
+    """
+    This action is closely related to FS_02_25_41_81 `func_id_query_enum_items`.
+    """
+
+    RelayLineArgs = "/relay_line_args/"
