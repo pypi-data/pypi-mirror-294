adc5a6b0a4752d5986bdb25c1990768dae6c5356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 14:04:10 +0000

    Add package ajc27-freemocap-blender-addon/2024.9.1024/ajc27_freemocap_blender_addon-2024.9.1024.tar.gz

---
+++ packages/ajc27-freemocap-blender-addon/ajc27_freemocap_blender_addon-2024.9.1024.tar.gz/ajc27_freemocap_blender_addon-2024.9.1024/ajc27_freemocap_blender_addon/blender_interface/operators/_export_fbx.py
@@ -0,0 +1,34 @@
+import math as m
+import time
+
+from ajc27_freemocap_blender_addon.core_functions.fbx_export.fbx import export_fbx
+from bpy.types import Operator
+
+
+class FMC_ADAPTER_OT_export_fbx(Operator):
+    bl_idname = 'fmc_adapter._export_fbx'
+    bl_label = 'Freemocap Adapter - Export FBX'
+    bl_description = 'Exports a FBX file containing the rig, the mesh and the baked animation'
+    bl_options = {'REGISTER', 'UNDO_GROUPED'}
+
+    def execute(self, context):
+        print('Executing Export FBX...')
+        scene = context.scene
+        fmc_adapter_tool = scene.fmc_adapter_properties
+
+        recording_path = fmc_adapter_tool.recording_path
+        if recording_path == "":
+            print("No recording path specified")
+            return {'CANCELLED'}
+
+        # Get start time
+        start = time.time()
+
+        # Execute export fbx function
+        export_fbx(recording_path=recording_path, )
+
+        # Get end time and print execution time
+        end = time.time()
+        print('Finished. Execution time (s): ' + str(m.trunc((end - start) * 1000) / 1000))
+
+        return {'FINISHED'}
---
+++ packages/ajc27-freemocap-blender-addon/ajc27_freemocap_blender_addon-2024.9.1024.tar.gz/ajc27_freemocap_blender_addon-2024.9.1024/ajc27_freemocap_blender_addon/blender_interface/operators/_run_all.py
@@ -0,0 +1,34 @@
+import traceback
+from pathlib import Path
+
+import bpy
+
+
+class FMC_ADAPTER_run_all(bpy.types.Operator):
+    bl_idname = 'fmc_adapter._run_all'
+    bl_label = "Run All"
+    bl_options = {'REGISTER', 'UNDO_GROUPED'}
+
+    def execute(self, context):
+        from ...core_functions.main_controller import MainController
+        from ...data_models.parameter_models.load_parameters_config import load_default_parameters_config
+        fmc_adapter_tool = context.scene.fmc_adapter_properties
+        recording_path = fmc_adapter_tool.recording_path
+        if recording_path == "":
+            print("No recording path specified")
+            return {'CANCELLED'}
+        config = load_default_parameters_config()
+        try:
+            print(f"Executing `main_controller.run_all() with config:{config}")
+            controller = MainController(recording_path=recording_path,
+                                        blend_file_path=str(Path(recording_path) / (Path(recording_path).stem + ".blend")),
+                                        config=config)
+            fmc_adapter_tool.data_parent_empty = controller.data_parent_object
+            controller.run_all()
+        except Exception as e:
+            print(f"Failed to run main_controller.run_all() with config:{config}: `{e}`")
+            print(traceback.format_exc())
+            return {'CANCELLED'}
+        return {'FINISHED'}
+
+
