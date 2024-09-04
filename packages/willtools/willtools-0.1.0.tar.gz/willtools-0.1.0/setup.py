a0bf866e120137d89b678e85d6f472a8c633e87f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 16:04:31 +0000

    Add package willtools/0.1.0/willtools-0.1.0.tar.gz

---
+++ packages/willtools/willtools-0.1.0.tar.gz/willtools-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='willtools',
+    version='0.1.0',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A short description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/elonniu/willtools',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[  # Add your package dependencies here
+        # 'numpy>=1.19',
+        # 'pandas>=1.2',
+    ],
+)
