f9e41301ee5779206cc89560b737576e2c3a9d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 06:04:02 +0000

    Add package claims-market/0.1.0/claims_market-0.1.0.tar.gz

---
+++ packages/claims-market/claims_market-0.1.0.tar.gz/claims_market-0.1.0/setup.py
@@ -0,0 +1,25 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="claims_market",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "pandas",
+    ],
+    author="Patrick Ashrafi",
+    author_email="pa@ai-holding.com",
+    description="A library to fetch and process data from the Claims Market API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/claims_market",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
