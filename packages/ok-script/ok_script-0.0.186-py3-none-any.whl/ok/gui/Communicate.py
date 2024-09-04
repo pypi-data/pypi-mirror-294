09f2acaece6d3397f7357481df5d6bf8b815e7b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 17:04:24 +0000

    Add package ok-script/0.0.186/ok_script-0.0.186-py3-none-any.whl

---
+++ packages/ok-script/ok_script-0.0.186-py3-none-any.whl/ok/gui/Communicate.py
@@ -0,0 +1,34 @@
+import numpy as np
+from PySide6.QtCore import Signal, QObject
+
+
+class Communicate(QObject):
+    log = Signal(int, str)
+    fps = Signal(int)
+    frame_time = Signal(int)
+    scene = Signal(str)
+    draw_box = Signal(str, object, str, np.ndarray)
+    task = Signal(object)
+    window = Signal(bool, int, int, int, int, int, int, float)
+    loading_progress = Signal(str)
+    notification = Signal(str, str, bool, bool)
+    executor_paused: Signal = Signal(bool)
+    screenshot = Signal(np.ndarray, str)
+    update_overlay = Signal()
+    adb_devices: Signal = Signal(bool)
+    config_validation: Signal = Signal(str)
+    tab = Signal(str)
+    capture_error = Signal()
+    check_update = Signal(str)
+    download_update = Signal(float, str, bool, str)
+    starting_emulator = Signal(bool, str, int)
+    quit = Signal()
+    versions = Signal(list)
+    clone_version = Signal(str)
+    launcher_configs = Signal(list)
+
+    def emit_draw_box(self, key: str = None, boxes=None, color=None, frame=None):
+        self.draw_box.emit(key, boxes, color, frame)
+
+
+communicate = Communicate()
