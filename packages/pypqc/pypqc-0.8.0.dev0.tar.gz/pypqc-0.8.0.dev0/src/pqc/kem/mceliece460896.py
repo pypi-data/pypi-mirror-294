e9fec4214b4c1d87d88fb858fd88994e2f502c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 04:04:22 +0000

    Add package pypqc/0.8.0.dev0/pypqc-0.8.0.dev0.tar.gz

---
+++ packages/pypqc/pypqc-0.8.0.dev0.tar.gz/pypqc-0.8.0.dev0/src/pqc/kem/mceliece460896.py
@@ -0,0 +1,36 @@
+# AUTOMATICALLY GENERATED FILE.
+# RUN make.py IN THE PARENT MONOREPO TO REGENERATE THIS FILE.
+
+from pqc._lib.kem_mceliece.libmceliece460896_clean import ffi, lib # TODO add optimized implementations
+
+
+def keypair():
+	with ffi.new('uint8_t[]', lib.CRYPTO_PUBLICKEYBYTES) as pk,\
+	     ffi.new('uint8_t[]', lib.CRYPTO_SECRETKEYBYTES) as sk:
+		errno = lib.crypto_kem_keypair(pk, sk)
+		if errno == 0:
+			return bytes(pk), bytes(sk)
+		else:
+			raise RuntimeError
+
+
+def encap(pk_bytes):
+	with ffi.new('uint8_t[]', lib.CRYPTO_CIPHERTEXTBYTES) as c,\
+	     ffi.new('uint8_t[]', lib.CRYPTO_BYTES) as key,\
+	     ffi.from_buffer(pk_bytes) as pk: # FIXME validate length
+		errno = lib.crypto_kem_enc(c, key, pk)
+		if errno == 0:
+			return bytes(c), bytes(key)
+		else:
+			raise RuntimeError
+
+
+def decap(ct_bytes, sk_bytes):
+	with ffi.new('uint8_t[]', lib.CRYPTO_BYTES) as key,\
+	     ffi.from_buffer(ct_bytes) as c,\
+	     ffi.from_buffer(sk_bytes) as sk: # FIXME validate lengths
+		errno = lib.crypto_kem_dec(key, c, sk)
+		if errno == 0:
+			return bytes(key)
+		else:
+			raise RuntimeError
