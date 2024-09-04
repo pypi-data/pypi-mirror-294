697afd1cbebe1b875a2f158fa0aa7e7f4113ac5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 04:03:57 +0000

    Add package fnp/0.0.2/fnp-0.0.2.tar.gz

---
+++ packages/fnp/fnp-0.0.2.tar.gz/fnp-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup,find_packages
+
+classifierss=[
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 11',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='fnp',
+    version='0.0.2',
+    description='A module that lets you extract column from an excel file and plot it.',
+    author='Sushruta Das',
+    author_email='sushrutadas0@gmail.com',
+    classifiers=classifierss,
+    packages=find_packages(),
+    install_requires=['pandas','matplotlib','numpy']
+)
