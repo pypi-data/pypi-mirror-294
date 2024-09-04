ef201d0590a4867060688704de41f8895c653779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 06:03:20 +0000

    Add package navigator-api/2.10.12/navigator_api-2.10.12-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/navigator-api/navigator_api-2.10.12-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/navigator/views/__init__.py
@@ -0,0 +1,24 @@
+"""
+Views.
+
+NAV aiohttp Class-Based Views.
+"""
+from ..libs.json import json_encoder, json_decoder
+from .base import BaseHandler, BaseView
+from .data import DataView
+from .model import ModelView, load_models
+from .mhandler import ModelHandler
+from .form import FormModel
+
+DEFAULT_JSON_ENCODER = json_encoder
+DEFAULT_JSON_DECODER = json_decoder
+
+__all__ = (
+    'BaseHandler',
+    'BaseView',
+    'DataView',
+    'ModelView',
+    'load_models',
+    'ModelHandler',
+    'FormModel'
+)
