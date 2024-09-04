8a8c660de8ad1f1fb8aed43d5f0ba0e24d90e936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 04:03:47 +0000

    Add package fing-agent-api/1.0.0/fing_agent_api-1.0.0-py3-none-any.whl

---
+++ packages/fing-agent-api/fing_agent_api-1.0.0-py3-none-any.whl/fing_agent_api/fing_agent_api.py
@@ -0,0 +1,28 @@
+"""Fing Agent API library."""
+
+import httpx
+from .models import ContactResponse, DeviceResponse
+
+
+class FingAgent:
+    """Fing Agent API class."""
+
+    def __init__(self, ip: str, port: int, key: str) -> None:
+        """Initialize Fing API object."""
+        self._host = f"http://{ip}:{port}/1"
+        self._key = key
+
+    async def get_devices(self, timeout: float = 120) -> DeviceResponse:
+        """Return devices discovered by Fing."""
+        url = f"{self._host}/devices?auth={self._key}"
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, timeout=timeout)
+        return DeviceResponse(response.raise_for_status().json())
+
+    async def get_contacts(self, timeout: float = 120) -> ContactResponse:
+        """Return information about Fing contacts."""
+        url = f"{self._host}/people?auth={self._key}"
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, timeout=timeout)
+        return ContactResponse(response.raise_for_status().json())
+
