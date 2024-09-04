1d1007f01540fe096321df646fa6d5b6695ca9a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 17:04:12 +0000

    Add package fastapi-throttle/0.1.2/fastapi_throttle-0.1.2.tar.gz

---
+++ packages/fastapi-throttle/fastapi_throttle-0.1.2.tar.gz/fastapi_throttle-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fastapi-throttle",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+        "fastapi",
+    ],
+    description="A rate limiter for FastAPI without using Redis.",
+    author="Ali Yaman",
+    author_email="aliymn.db@gmail.com",
+    url="https://github.com/AliYmn/fastapi-throttle",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.8, <3.13",
+)
