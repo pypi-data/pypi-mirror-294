11796ea97f21fd1b8a720883bf0d43b7b1b76d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 15:04:19 +0000

    Add package libp2p-kit-py/0.0.7/libp2p_kit_py-0.0.7.tar.gz

---
+++ packages/libp2p-kit-py/libp2p_kit_py-0.0.7.tar.gz/libp2p_kit_py-0.0.7/libp2p_kit_py/libp2p_kit.py
@@ -0,0 +1,18 @@
+from libp2p_kit_py import libp2p_kit_py
+
+
+class libp2p_kit_py:
+    def __init__(self):
+        self.config = None
+        self.logger = None
+        self.node = None
+        self.peerstore = None
+        self.host = None
+        self.stream = None
+        self.protocol = None
+
+    def run(self):
+
+        return True
+    
+    
