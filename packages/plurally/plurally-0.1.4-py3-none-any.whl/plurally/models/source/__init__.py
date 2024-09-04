5468083cdd5127cbb90de9510af32e7667cc6b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 06:03:19 +0000

    Add package plurally/0.1.4/plurally-0.1.4-py3-none-any.whl

---
+++ packages/plurally/plurally-0.1.4-py3-none-any.whl/plurally/models/source/__init__.py
@@ -0,0 +1,9 @@
+from .constant import Integer, Number, Text  # noqa: F401
+from .email_imap import EmailSourceIMAP  # noqa: F401
+
+__all__ = [
+    "Number",
+    "Integer",
+    "Text",
+    "EmailSourceIMAP",
+]
