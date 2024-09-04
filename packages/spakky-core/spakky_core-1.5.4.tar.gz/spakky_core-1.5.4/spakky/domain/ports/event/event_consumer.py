e85fed0541028731733ebb9a29916a215354e07e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 05:03:41 +0000

    Add package spakky-core/1.5.4/spakky_core-1.5.4.tar.gz

---
+++ packages/spakky-core/spakky_core-1.5.4.tar.gz/spakky_core-1.5.4/spakky/domain/ports/event/event_consumer.py
@@ -0,0 +1,42 @@
+from abc import abstractmethod
+from typing import (
+    TypeVar,
+    Callable,
+    Protocol,
+    Awaitable,
+    TypeAlias,
+    runtime_checkable,
+)
+
+from spakky.domain.models.event import DomainEvent
+from spakky.task.cancellation_token import ICancellationToken
+
+DomainEventT = TypeVar("DomainEventT", bound=DomainEvent)
+IEventHandlerCallback: TypeAlias = Callable[[DomainEventT], None]
+IAsyncEventHandlerCallback: TypeAlias = Callable[[DomainEventT], Awaitable[None]]
+
+
+@runtime_checkable
+class IEventConsumer(Protocol):
+    @abstractmethod
+    def register(
+        self,
+        event: type[DomainEventT],
+        handler: IEventHandlerCallback[DomainEventT],
+    ) -> None: ...
+
+    @abstractmethod
+    def start(self, cancellation_token: ICancellationToken) -> None: ...
+
+
+@runtime_checkable
+class IAsyncEventConsumer(Protocol):
+    @abstractmethod
+    async def register(
+        self,
+        event: type[DomainEventT],
+        handler: IAsyncEventHandlerCallback[DomainEventT],
+    ) -> None: ...
+
+    @abstractmethod
+    async def start(self, cancellation_token: ICancellationToken) -> None: ...
