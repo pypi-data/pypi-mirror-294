ffdb6d4fe67476e2a4d67c1c4741371c32bac8c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 14:04:19 +0000

    Add package bpmn-parser/1.0.15/bpmn_parser-1.0.15.tar.gz

---
+++ packages/bpmn-parser/bpmn_parser-1.0.15.tar.gz/bpmn_parser-1.0.15/bpmn_parser/_service_task.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+
+from lxml.etree import _Element
+
+from bpmn_parser._task import Task, TaskElement
+
+
+@dataclass
+class ServiceTaskElement(TaskElement):
+    """Parse an Service Task from BPMN.
+
+    Attributes:
+        topic_name (str): topic name of element in BPMN.
+        type (str): type of element in BPMN.
+    """
+
+    topic_name: str
+    type: str
+
+
+class ServiceTask(Task):
+    """An API from Service Task from BPMN. This classes holds `ServiceTaskElement` as elements.
+
+    Attributes:
+        root (_Element): root of BPMN loaded as XML.
+    """
+
+    def __init__(self, root: _Element):
+        super().__init__(root)
+
+    @property
+    def list(self):
+        items = []
+        for service_task in self.root.xpath(
+            '//bpmn:serviceTask', namespaces={'bpmn': self.bpmn_tag}
+        ):
+            items.append(
+                ServiceTaskElement(
+                    id=service_task.get('id'),
+                    name=service_task.get('name'),
+                    topic_name=service_task.get(self._build_camunda_attrib('topic')),
+                    type=service_task.get(self._build_camunda_attrib('type')),
+                    execution_listeners=self._get_execution_listeners(service_task),
+                )
+            )
+        return items
---
+++ packages/bpmn-parser/bpmn_parser-1.0.15.tar.gz/bpmn_parser-1.0.15/bpmn_parser/_user_task.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass
+
+from lxml.etree import _Element
+
+from bpmn_parser._task import Task, TaskElement
+
+
+@dataclass
+class UserTaskElement(TaskElement):
+    """Parse an Service Task from BPMN.
+
+    Attributes:
+        form_key (str): form key of element in BPMN.
+        candidate_groups (str): candidate groups of element in BPMN.
+        due_date (str): due date of element in BPMN.
+        priority (str): priority of element in BPMN.
+    """
+
+    form_key: str
+    candidate_groups: str
+    due_date: str
+    priority: str
+
+
+class UserTask(Task):
+    """An API from Service Task from BPMN. This classes holds `UserTaskElement` as elements.
+
+    Attributes:
+        root (_Element): root of BPMN loaded as XML.
+    """
+
+    def __init__(self, root: _Element):
+        super().__init__(root)
+
+    @property
+    def list(self):
+        items = []
+        for user_task in self.root.xpath(
+            '//bpmn:userTask', namespaces={'bpmn': self.bpmn_tag}
+        ):
+            items.append(
+                UserTaskElement(
+                    id=user_task.get('id'),
+                    name=user_task.get('name'),
+                    form_key=user_task.get(self._build_camunda_attrib('formKey')),
+                    candidate_groups=user_task.get(
+                        self._build_camunda_attrib('candidateGroups')
+                    ),
+                    due_date=user_task.get(self._build_camunda_attrib('dueDate')),
+                    priority=user_task.get(self._build_camunda_attrib('priority')),
+                    execution_listeners=self._get_execution_listeners(user_task),
+                )
+            )
+        return items
