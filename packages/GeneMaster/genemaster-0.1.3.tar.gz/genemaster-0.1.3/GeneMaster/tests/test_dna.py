f767b1b2b33a6614b2d9630de412c75fc991bed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 05:03:58 +0000

    Add package GeneMaster/0.1.3/genemaster-0.1.3.tar.gz

---
+++ packages/GeneMaster/genemaster-0.1.3.tar.gz/genemaster-0.1.3/GeneMaster/tests/test_dna.py
@@ -0,0 +1,75 @@
+# tests/test_dna.py
+import unittest
+from GeneMaster import DNA
+from GeneMaster import RNA
+
+
+class TestDNA(unittest.TestCase):
+    def test_reverse_complement(self):
+        dna_seq = DNA("ATGCGT")
+        self.assertEqual(dna_seq.revComplement(), DNA("ACGCAT"))
+
+    def test_gc_content(self):
+        dna_seq = DNA("ATGCGT")
+        self.assertAlmostEqual(dna_seq.gc_content(), 0.5)
+
+    def test_transcription(self):
+        dna_seq = DNA("ATGCGT")
+        rna_seq = dna_seq.transcribe_to_rna()
+        self.assertEqual(rna_seq, RNA("ACGCAU"))
+
+    def test_start_stop_codons(self):
+        dna_seq = DNA("ATGATGTAGTAATGA")
+        positions = dna_seq.start_stop_codons(overlap=False)
+        self.assertEqual(positions, [(0, 9)])
+
+    def test_orf(self):
+        dna_seq = DNA(
+            "AGCCATGTAGCTAACTCAGGTTACATGGGGATGACCCCGCGACTTGGATTAGAGTCTCTTTTGGAATAAGCCTGAATGATCCGAGTAGCATCTCAG")
+        result = dna_seq.find_orf()
+        correct_ans = ['MLLGSFRLIPKETLIQVAGSSPCNLS-', 'M-', 'MGMTPRLGLESLLE-', 'MTPRLGLESLLE-']
+        self.assertEqual(sorted(result), sorted(correct_ans))
+
+    def test_arith(self):
+        dna_seq = DNA("ATGC")
+        test = DNA("ATGA")
+        self.assertEqual(dna_seq < test, False)
+        self.assertEqual(dna_seq > test, True)
+        self.assertEqual(dna_seq <= test, False)
+        self.assertEqual(dna_seq >= test, True)
+        self.assertEqual(dna_seq == test, False)
+        self.assertEqual(dna_seq != test, True)
+
+    def test_exons(self):
+        dna_seq = DNA('ATGCGTACGTAGCCTGAAGCTAGGC')
+        result = dna_seq.extract_exons()
+        correct = DNA('ATGCGTACGTAG')
+        self.assertEqual(result, correct)
+
+    def test_introns(self):
+        dna_seq = DNA('ATGCGTACGTAGCCTGAAGCTAGGC')
+        result = dna_seq.extract_introns()
+        correct = DNA('CCTGAAGCTAGGC')
+        self.assertEqual(result, correct)
+
+    def test_melting_temp(self):
+        dna_seq = DNA('ATGCGTACGTAGCCTGAAGCTAGGC')
+        result = dna_seq.melting_temperature()
+        correct = 61
+        self.assertEqual(round(result, 0), correct)
+
+    def test_translation(self):
+        dna_seq = DNA('ATGCGTACGTAGCCT')
+        result = dna_seq.translation()
+        correct = 'MRT-P'
+        self.assertEqual(result, correct)
+
+    def test_kmer(self):
+        dna_seq = DNA('ATGCGATGC')
+        result = dna_seq.kmer(3)
+        correct = {'ATG': 2, 'TGC': 2, 'GCG': 1, 'CGA': 1, 'GAT': 1}
+        self.assertEqual(result, correct)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/GeneMaster/genemaster-0.1.3.tar.gz/genemaster-0.1.3/README.md
@@ -0,0 +1,4 @@
+# GeneMaster Package [DEMO] 
+
+## Description
+The GeneMaster project is a comprehensive bioinformatics package designed to facilitate the analysis and manipulation of genetic sequences, including DNA and RNA. It provides a robust set of data types and operations tailored to genomic research, allowing users to perform tasks such as reverse complement generation, GC content calculation, RNA transcription, and protein translation. Additionally, it includes advanced algorithms for solving problems like the shortest superstring, longest common subsequence, and motif finding. The package also supports parsing FASTA format files and includes thorough testing modules to ensure accuracy and reliability in genetic sequence analysis.
