ccb49448a91e4d671c9dec76e2f36c94fa541978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 06:04:28 +0000

    Add package pyams-app-msc/1.99.11/pyams_app_msc-1.99.11-py3-none-any.whl

---
+++ packages/pyams-app-msc/pyams_app_msc-1.99.11-py3-none-any.whl/pyams_app_msc/shared/theater/api/interfaces.py
@@ -0,0 +1,22 @@
+#
+# Copyright (c) 2015-2024 Thierry Florac <tflorac AT ulthar.net>
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+
+"""PyAMS_*** module
+
+"""
+
+
+MSC_PRICE_API_ROUTE = 'pyams_msc.theater.price'
+'''MSC price API route'''
+
+MSC_PRICE_API_PATH = '/api/msc/{theater_name}/price'
+'''MSC price API default path'''
