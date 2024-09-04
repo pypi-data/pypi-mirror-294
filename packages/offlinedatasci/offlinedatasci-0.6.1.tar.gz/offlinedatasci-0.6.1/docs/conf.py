28644387bb71aedcc039fe9ffb0a93f1fdd399ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-04 04:03:26 +0000

    Add package offlinedatasci/0.6.1/offlinedatasci-0.6.1.tar.gz

---
+++ packages/offlinedatasci/offlinedatasci-0.6.1.tar.gz/offlinedatasci-0.6.1/docs/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'offlinedatasci'
+copyright = '2023, Virnaliz Cruz, Abhishek Dasgupta, Colin Sauze, Jannetta Steyn, Ethan P. White'
+author = 'Virnaliz Cruz, Abhishek Dasgupta, Colin Sauze, Jannetta Steyn, Ethan P. White'
+release = 'v0.6.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser',
+              'sphinx.ext.autodoc',
+              'sphinx_rtd_theme']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = []
---
+++ packages/offlinedatasci/offlinedatasci-0.6.1.tar.gz/offlinedatasci-0.6.1/offlinedatasci/__init__.py
@@ -0,0 +1,4 @@
+from . import main
+from .main import *
+
+__version__ = '0.6.1'
