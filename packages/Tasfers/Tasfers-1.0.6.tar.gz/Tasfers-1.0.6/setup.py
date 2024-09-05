6ae5ed14e8bdf1db27e55d0625ade054c16865a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 15:03:58 +0000

    Add package Tasfers/1.0.6/Tasfers-1.0.6.tar.gz

---
+++ packages/Tasfers/Tasfers-1.0.6.tar.gz/Tasfers-1.0.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages
+from distutils.core import setup
+
+setup(
+    name='Tasfers',
+    version='1.0.6',
+    packages=find_packages(),
+    install_requires=[
+        'aiofiles',
+        "cryptography==42.0.4"
+    ],
+    author='Towa',
+    description='A library to simplify programming',
+    url='https://github.com/Tasfers/library'
+)
