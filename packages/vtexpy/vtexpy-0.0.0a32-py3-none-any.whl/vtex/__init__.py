dff8cd35688127733a648da71e2081d5a1d74a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 18:05:04 +0000

    Add package vtexpy/0.0.0a32/vtexpy-0.0.0a32-py3-none-any.whl

---
+++ packages/vtexpy/vtexpy-0.0.0a32-py3-none-any.whl/vtex/__init__.py
@@ -0,0 +1,25 @@
+from . import _constants as VTEXConstants  # noqa: N812
+from ._dto import (
+    VTEXListResponse,
+    VTEXPaginatedListResponse,
+    VTEXResponse,
+    VTEXScrollListResponse,
+)
+from ._exceptions import VTEXError, VTEXRequestError, VTEXResponseError
+from ._vtex import VTEX
+
+__all__ = [
+    "VTEX",
+    "VTEXConstants",
+    "VTEXError",
+    "VTEXListResponse",
+    "VTEXPaginatedListResponse",
+    "VTEXRequestError",
+    "VTEXResponse",
+    "VTEXResponseError",
+    "VTEXScrollListResponse",
+]
+
+
+for name in __all__:
+    locals()[name].__module__ = "vtex"
