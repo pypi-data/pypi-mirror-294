e4c5921c2db5507d35e9a4a5b0fe0db058f1c21a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 20:04:47 +0000

    Add package odoo14-addon-ssi-risk-analysis-quality-control/14.0.1.0.0/odoo14_addon_ssi_risk_analysis_quality_control-14.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-risk-analysis-quality-control/odoo14_addon_ssi_risk_analysis_quality_control-14.0.1.0.0-py3-none-any.whl/odoo/addons/ssi_risk_analysis_quality_control/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+{
+    "name": "Risk Analysis - Quality Control Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_risk_analysis",
+        "ssi_quality_control",
+    ],
+    "data": [],
+    "demo": [],
+    "images": [],
+}
