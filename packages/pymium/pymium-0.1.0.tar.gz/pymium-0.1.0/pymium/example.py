370c2887ddff4f4aac8cb25909944093bc892019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 16:04:44 +0000

    Add package pymium/0.1.0/pymium-0.1.0.tar.gz

---
+++ packages/pymium/pymium-0.1.0.tar.gz/pymium-0.1.0/pymium/example.py
@@ -0,0 +1,18 @@
+from main import * 
+
+base_style= Style(color="red")
+button_style = base_style.clone()
+button_style.add_style(background = "blue")
+
+container = Element(Types.div, "main-container")
+heading = Element(Types.h1, "a-heading", innerHTML="Hello", style=base_style)
+abutton = Element(Types.button, "a-button", 'button-class', 'you wassup', button_style)
+
+space = Space("Main")
+space.append(container)
+container.append(heading, abutton)
+
+element_with_main_container_id = getElementById(space, "main-container")[0]
+element_with_main_container_id.style.add_style(background_color = "green", border_radius="10px")
+
+run(space)
