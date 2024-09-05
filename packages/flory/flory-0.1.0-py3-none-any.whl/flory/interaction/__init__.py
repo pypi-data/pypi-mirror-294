fa8ea1e596ec9337e410797e5bc020520e47c544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 21:05:40 +0000

    Add package flory/0.1.0/flory-0.1.0-py3-none-any.whl

---
+++ packages/flory/flory-0.1.0-py3-none-any.whl/flory/interaction/__init__.py
@@ -0,0 +1,8 @@
+"""Module containing several common interaction energies.
+
+.. codeauthor:: Yicheng Qiang <yicheng.qiang@ds.mpg.de>
+.. codeauthor:: David Zwicker <david.zwicker@ds.mpg.de>
+"""
+
+from .base import InteractionBase
+from .flory_huggins import FloryHugginsInteraction, FloryHugginsInteractionCompiled
