105983a62ef7a202d53bc6584b47786a9a9c8e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 19:04:03 +0000

    Add package dduk-application/0.0.80/dduk_application-0.0.80-py3-none-any.whl

---
+++ packages/dduk-application/dduk_application-0.0.80-py3-none-any.whl/dduk/application/predefinedsymbols.py
@@ -0,0 +1,28 @@
+#--------------------------------------------------------------------------------
+# 참조 모듈 목록.
+#--------------------------------------------------------------------------------
+from __future__ import annotations
+from typing import Any, Final, Callable, Iterator, Optional, Type, TypeVar, Union, Tuple, List, Dict, Set, cast
+from enum import Enum
+
+
+#--------------------------------------------------------------------------------
+# 미리 선언된 내장 심볼 목록.
+#--------------------------------------------------------------------------------
+class PredefinedSymbols(Enum):
+
+	# 바이너리 빌드 상태.
+	SYMBOL_BUILD = "BUILD"
+	SYMBOL_BINARY = "BINARY"
+
+	# 서비스 상태.
+	SYMBOL_SERVICE = "SERVICE"
+
+	# 소스 상태.
+	SYMBOL_SOURCE = "SOURCE"
+
+	# 로그 파일 사용.
+	SYMBOL_LOG = "LOG"
+
+	# 서브프로세스 사용.
+	SYMBOL_SUBPROCESS = "SUBPROCESS"
