71922cc51973ddac6f83d55428cbf3d032d93165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 20:05:00 +0000

    Add package odoo14-addon-ssi-meeting-quality-control/14.0.1.0.0/odoo14_addon_ssi_meeting_quality_control-14.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-meeting-quality-control/odoo14_addon_ssi_meeting_quality_control-14.0.1.0.0-py3-none-any.whl/odoo/addons/ssi_meeting_quality_control/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=C8101
+{
+    "name": "Meeting + Quality Control Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_quality_control",
+        "calendar",
+    ],
+    "data": [],
+    "demo": [],
+    "images": [],
+}
---
+++ packages/odoo14-addon-ssi-meeting-quality-control/odoo14_addon_ssi_meeting_quality_control-14.0.1.0.0-py3-none-any.whl/odoo/addons/ssi_meeting_quality_control/models/calendar_event.py
@@ -0,0 +1,14 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+
+from odoo import models
+
+
+class CalendarEvent(models.Model):
+    _name = "calendar.event"
+    _inherit = [
+        "calendar.event",
+        "mixin.qc_worksheet",
+    ]
+    _qc_worksheet_create_page = True
