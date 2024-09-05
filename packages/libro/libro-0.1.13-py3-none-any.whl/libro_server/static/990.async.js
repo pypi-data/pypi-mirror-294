02fa1eb034b5d0be2ef3a45af07a19a056895d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 15:04:12 +0000

    Add package libro/0.1.13/libro-0.1.13-py3-none-any.whl

---
+++ packages/libro/libro-0.1.13-py3-none-any.whl/libro_server/static/990.async.js
@@ -0,0 +1,17 @@
+(self["webpackChunklibro_lab"] = self["webpackChunklibro_lab"] || []).push([[990],{
+
+/***/ 69466:
+/***/ (function() {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ 80350:
+/***/ (function() {
+
+/* (ignored) */
+
+/***/ })
+
+}]);
