3724efd6f3e7e7f97779e2ca7fa3dc0e73770471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 15:03:55 +0000

    Add package quasielasticbayes/0.2.1/quasielasticbayes-0.2.1-cp310-cp310-win_amd64.whl

---
+++ packages/quasielasticbayes/quasielasticbayes-0.2.1-cp310-cp310-win_amd64.whl/quasielasticbayes/__init__.py
@@ -0,0 +1,7 @@
+# Copyright &copy; 2021 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: BSD-3-Clause
+
+# --- Public API --------------------------------------------------------------
+
