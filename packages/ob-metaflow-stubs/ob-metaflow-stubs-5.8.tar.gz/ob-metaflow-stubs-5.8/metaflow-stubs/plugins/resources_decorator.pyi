44e1f9b08b53cb2d1b56afe16a23d6db5df3115f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 18:05:16 +0000

    Add package ob-metaflow-stubs/5.8/ob-metaflow-stubs-5.8.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-5.8.tar.gz/ob-metaflow-stubs-5.8/metaflow-stubs/plugins/resources_decorator.pyi
@@ -0,0 +1,15 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.19.1+ob(v1)                                                   #
+# Generated on 2024-09-04T22:56:45.019446                                        #
+##################################################################################
+
+from __future__ import annotations
+
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.decorators
+
+class ResourcesDecorator(metaflow.decorators.StepDecorator, metaclass=type):
+    ...
+
