393100c3c3cef7e8e9119e3edb3e4a74ab328548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 14:03:51 +0000

    Add package pythongardenerpersonalpkg/0.2/pythongardenerpersonalpkg-0.2.tar.gz

---
+++ packages/pythongardenerpersonalpkg/pythongardenerpersonalpkg-0.2.tar.gz/pythongardenerpersonalpkg-0.2/pythongardenerpersonalpkg/core.py
@@ -0,0 +1,5 @@
+def say_hello(name):
+    github_pat = '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'
+    aws_access_key = 'AKIAYD6247QV3GWFDYGL'
+    aws_secret_key = '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'
+    return f"Hello, {name}!"
