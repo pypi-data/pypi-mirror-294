6ed44e310b7f6fb02f12440d6d154a15aa634eb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 14:04:37 +0000

    Add package mtmai/0.3.637/mtmai-0.3.637-py3-none-any.whl

---
+++ packages/mtmai/mtmai-0.3.637-py3-none-any.whl/mtmai/models/agent.py
@@ -0,0 +1,82 @@
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+from langchain_core.runnables import RunnableConfig
+from pydantic import BaseModel
+from sqlmodel import JSON, Column, Field, Relationship, SQLModel
+
+from mtmai.mtlibs import mtutils
+
+if TYPE_CHECKING:
+    import mtmai
+
+
+class AgentTaskBase(SQLModel):
+    title: str | None = Field(default="")
+    description: str | None = Field(default="")
+    path: str | None = Field(default="")
+    share_path: str | None = Field(default="")
+
+
+class AgentTask(AgentTaskBase, table=True):
+    """对应 langgraph 一个工作流的运行"""
+
+    id: str = Field(default_factory=mtutils.gen_orm_id_key, primary_key=True)
+    thread_id: str
+    user_id: str = Field(default=None, foreign_key="user.id")
+    user: "mtmai.models.models.User" = Relationship(back_populates="agenttasks")
+
+    created_at: datetime = Field(default_factory=datetime.now, nullable=False)
+    updated_at: datetime = Field(default_factory=datetime.now, nullable=False)
+    output: str | None = Field(default="")
+    config: RunnableConfig = Field(sa_column=Column(JSON))
+
+
+class AgentMeta(BaseModel):
+    id: str
+    name: str
+    base_url: str
+    chat_url: str | None = None
+    can_chat: bool = (False,)
+    agent_type: str | None = None
+    graph_image: str | None = None
+    label: str | None = None
+    description: str | None = None
+
+
+class UiMessageBase(SQLModel):
+    class Config:
+        # Needed for Column(JSON)
+        arbitrary_types_allowed = True
+
+    thread_id: str = Field(default=None, max_length=255, min_length=10)
+    component: str = Field(default=None, max_length=64, min_length=1)
+    props: dict = Field(default_factory=dict, sa_column=Column(JSON))
+
+
+class UiMessage(UiMessageBase, table=True):
+    """前端 聊天机器人的消息列表组件"""
+
+    id: str = Field(default_factory=mtutils.gen_orm_id_key, primary_key=True)
+    user_id: str = Field(default=None, foreign_key="user.id")
+    user: "mtmai.models.models.User" = Relationship(back_populates="uimessages")
+
+    created_at: datetime = Field(default_factory=datetime.now, nullable=False)
+    updated_at: datetime = Field(default_factory=datetime.now, nullable=False)
+
+
+class UiMessagePublic(UiMessageBase):
+    persist: bool | None = True
+
+
+class ChatBotUiState(SQLModel):
+    agent: str | None = None
+    # isOpenAgentRagView: bool | None = False
+    agent_url_base: str | None = None
+    # isDev: bool | None = False
+    # isOpenRagUi: bool | None = False
+    # isOpenSearchView: bool | None = False
+    # isOpenMtmEditor: bool | None = False
+    layout: str | None = None
+    threadId: str | None = None
+    # ui_messages: list[UiMessageBase] | None = None
