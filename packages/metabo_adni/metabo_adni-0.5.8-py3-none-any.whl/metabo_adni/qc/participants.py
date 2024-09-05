e5697e6f57065df1fdcdeedc12235d245d50d9e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 18:04:54 +0000

    Add package metabo_adni/0.5.8/metabo_adni-0.5.8-py3-none-any.whl

---
+++ packages/metabo_adni/metabo_adni-0.5.8-py3-none-any.whl/metabo_adni/qc/participants.py
@@ -0,0 +1,257 @@
+import warnings
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+from typing import Union
+from metabo_adni.data import load
+
+
+def remove_missing(
+    dat_dict: dict[str, pd.DataFrame], platform: str, cutoff: float
+) -> dict[str, pd.DataFrame]:
+    """
+    Remove participants from dataframes due to missing data greater than
+    cutoff.
+
+    Parameters
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Dictionary with dataframe name and dataframe to modify.
+    platform: str
+        Metabolomics platform to process.
+    cutoff: float
+        Missing data removal cutoff.
+
+    Returns
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Dictionary with dataframe name and dataframe with participants
+        removed due to missingness.
+    """
+    print(
+        "=== Removing participants with missing data greater than "
+        + str(cutoff)
+        + " ==="
+    )
+
+    for key in dat_dict:
+        metabo_names = load._get_metabo_col_names(dat_dict[key], key)
+        indices = load._get_data_indices(dat_dict[key], platform)
+        dat = dat_dict[key].loc[indices, metabo_names]
+        total_cols = dat.shape[1]
+        total_participants = pd.DataFrame(
+            dat.isna().sum(axis=1) / total_cols, columns=["Missing percentage"]
+        )
+        more_than_cutoff = dat.isna().sum(axis=1) / total_cols > cutoff
+        participant_table = total_participants[more_than_cutoff]
+        _print_removed(participant_table, key)
+        if len(participant_table) > 0:
+            dat_dict[key].drop(participant_table.index, axis=0, inplace=True)
+
+    print("")
+    return dat_dict
+
+
+def consolidate_replicates(
+    dat_dict: dict[str, pd.DataFrame], platform: str
+) -> dict[str, pd.DataFrame]:
+    """
+    Consolidate replicates by estimating the average across replicates.
+
+    Parameters
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Dictionary with dataframe name and dataframe to modify.
+    platform: str
+        Metabolomics platform to process.
+
+    Returns
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Dictionary with dataframe name and dataframe with participants
+        consolidated.
+    """
+    print("=== Consolidating replicates ===")
+    # Ignore performance warnings from multiindex pandas
+    warnings.simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
+    for key in dat_dict:
+        metabo_names = load._get_metabo_col_names(dat_dict[key], key)
+        indices = load._get_data_indices(dat_dict[key], platform)
+        dat = dat_dict[key].loc[indices, metabo_names]
+        duplicated_ID = dat.index[dat.index.duplicated()].unique()
+        print(f"There are {len(duplicated_ID)} duplicated IDs in {key} cohort.")
+        for j in duplicated_ID:
+            # Averaging metabolites, and keeping first row of the other cols
+            consolidated = dat.loc[j].mean()
+            other_cols = dat_dict[key].columns.difference(metabo_names)
+            first_row = dat_dict[key].loc[j, other_cols].iloc[0,]
+            new_row = pd.concat([first_row, consolidated])
+            new_row = new_row[dat_dict[key].columns]
+            dat_dict[key].drop(j, axis="index", inplace=True)
+            dat_dict[key].loc[j] = new_row
+        # The bl column is created in nmr (don't know why)
+        if "bl" in dat_dict[key].columns:
+            dat_dict[key].drop("bl", axis="columns", inplace=True)
+        dat_dict[key].sort_index(inplace=True)
+    print("")
+    return dat_dict
+
+
+def remove_non_fasters(
+    dat_dict: dict[str, pd.DataFrame], fasting_file: str
+) -> dict[str, pd.DataFrame]:
+    """
+    Remove participants that weren't fasting during data collection.
+
+    Parameters
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Dictionary with dataframe name and dataframe to modify.
+    fasting_file: str
+        Path of the file containing the fasting information.
+        Minimum requiered columns are 'RID', and 'BIFAST'.
+
+    Returns
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Dictionary with dataframe name and dataframe with non-fasting
+        participants removed.
+    """
+    print("=== Removing non-fasting participants ===")
+    fasting_dat = load.read_fasting_file(fasting_file)
+    fasting_participants = fasting_dat[fasting_dat == 1].index
+    for key in dat_dict:
+        keep_participants = (
+            dat_dict[key].index.get_level_values("RID").isin(fasting_participants)
+        )
+        _print_removed(keep_participants, key)
+        dat_dict[key] = dat_dict[key].loc[keep_participants]
+    print("")
+    return dat_dict
+
+
+def remove_bad_qc_tags(
+    dat_dict: dict[str, pd.DataFrame], platform: str
+) -> dict[str, pd.DataFrame]:
+    """
+    Remove participants with bad QC tags.
+    The QC tags in the nmr platform are in the main file.
+    The QC tags in the p180 platform are in separate excel files,
+    located in the qc_files_dir.
+
+    Parameters
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Dictionary with dataframe name and dataframe to modify.
+    platform: str
+        Metabolomics platform to process.
+
+    Returns
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Dictionary with dataframe name and dataframe with participants removed
+        due to bad QC tags.
+    """
+    print("=== Removing participants with bad QC tags ===")
+    if platform == "nmr":
+        # Remove participants with at least one observed QC tag flagged.
+        qc_tag_names = load._get_nmr_qc_cols()
+        n_cols = len(qc_tag_names)
+        id_list = (
+            dat_dict["NMR"].loc[:, qc_tag_names].iloc[:, : n_cols - 1].sum(axis=1) > 0
+        )
+        to_remove = id_list.index[id_list].unique().get_level_values("RID")
+        print(
+            "We will remove "
+            + str(len(to_remove))
+            + " participants with bad QC tags in the nmr platform."
+        )
+        dat_dict["NMR"] = dat_dict["NMR"].drop(to_remove)
+    elif platform == "p180":
+        # Only 'C5.DC..C6.OH.' in ADNI2GO FIA and
+        # 'Taurine' in ADNI1 UPLC have bad QC flags
+        for key in dat_dict:
+            if key == "ADNI1-UPLC":
+                dat_dict[key].drop("Taurine", axis=1, inplace=True)
+            elif key == "ADNI2GO-FIA":
+                dat_dict[key].drop("C5.DC..C6.OH.", axis=1, inplace=True)
+    print("")
+    return dat_dict
+
+
+def remove_moutliers(
+    dat_dict: dict[str, pd.DataFrame], platform: str
+) -> dict[str, pd.DataFrame]:
+    """
+    Remove multivariate outliers by computing the Mahalanobis distance
+
+    Parameters
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Dictionary with dataframe name and dataframe to modify.
+    platform: str
+        Metabolomics platform to process.
+
+    Returns
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Dictionary with dataframe name and dataframe with multivariate outliers
+        removed.
+    """
+    print("=== Removing multivariate outliers ===")
+    for key in dat_dict:
+        metabo_names = load._get_metabo_col_names(dat_dict[key], key)
+        indices = load._get_data_indices(dat_dict[key], platform)
+        dat = dat_dict[key].loc[indices, metabo_names]
+        cov_mat = dat.cov()
+        p2 = dat.mean()
+        cov_mat_pm1 = np.linalg.matrix_power(cov_mat, -1)
+        distances = []
+        for i, val in enumerate(dat.to_numpy()):
+            p1 = val
+            distance = (p1 - p2).T.dot(cov_mat_pm1).dot(p1 - p2)
+            distances.append(distance)
+        distances = np.array(distances)
+        cutoff = stats.chi2.ppf(0.999, dat.shape[1] - 1)
+        i_to_remove = dat.loc[distances > cutoff].index
+        print(
+            f"{len(i_to_remove)} participants will be removed in the " + f"{key} cohort"
+        )
+        dat_dict[key].drop(i_to_remove, axis="index", inplace=True)
+
+    print("")
+    return dat_dict
+
+
+def _print_removed(
+    participant_table: Union[pd.DataFrame, np.ndarray], cohort: str
+) -> None:
+    """
+    Print the list of participants that will be removed, and the value
+    associated with the decision (missing proportion).
+
+    Parameters
+    ----------
+    participant_table: Union[pd.DataFrame, np.ndarray]
+        Dataframe with participant names and missing proportion
+        values (pd.DataFrame) or a list of which participants to keep
+        (np.ndarray).
+    cohort: str
+        Cohort to append in print statement.
+
+    Returns
+    ----------
+    None
+    """
+    if len(participant_table) == 0:
+        print("None of the participants will be dropped in the " + cohort + " cohort.")
+    else:
+        if isinstance(participant_table, pd.DataFrame):
+            to_remove = len(participant_table)
+        elif isinstance(participant_table, np.ndarray):
+            to_remove = sum(~participant_table)
+        else:
+            to_remove = []
+        print(f"{to_remove} participants will be removed in the {cohort} " + "cohort.")
+        if isinstance(participant_table, pd.DataFrame):
+            print(participant_table)
---
+++ packages/metabo_adni/metabo_adni-0.5.8-py3-none-any.whl/metabo_adni/qc/transformations.py
@@ -0,0 +1,302 @@
+from typing import Union
+
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+import statsmodels.api as sm
+
+from metabo_adni.data import load
+
+
+def imputation(
+    dat_dict: dict[str, pd.DataFrame],
+    platform: str,
+    lod_directory: Union[str, None] = None,
+) -> dict[str, pd.DataFrame]:
+    """
+    Impute metabolite concentration values by half the minimum observed value.
+    If the LOD file is passed for the p180 platform, the imputation is
+    half of the LOD value.
+
+    Parameters
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Dictionary with dataframe name and dataframe to modify.
+    platform: str
+        Metabolomics platform to process.
+    lod_directory: Union[str, None]
+        Path of the files containing the LOD information for the p180.
+
+    Returns
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Dictionary with dataframe name and dataframe with metabolites imputed.
+    """
+    print("=== Imputing metabolites ===")
+    total_points_imputed = 0
+    total_mets_imputed = []
+    if lod_directory is not None:
+        lod_files = load.read_lod_files(lod_directory)
+
+    for key in dat_dict:
+        metabo_names = load._get_metabo_col_names(dat_dict[key], key)
+        indices = load._get_data_indices(dat_dict[key], platform)
+        if platform == "p180":
+            dat = dat_dict[key].loc[indices, list(metabo_names) + ["Plate.Bar.Code"]]
+        else:
+            dat = dat_dict[key].loc[indices, metabo_names]
+        mets_to_impute = dat.columns[dat.isna().any()]
+        data_points_impute = dat.isna().sum().sum()
+        total_mets_imputed.extend(mets_to_impute)
+        total_points_imputed = total_points_imputed + data_points_impute
+        print(
+            f"{len(mets_to_impute)} metabolites and {data_points_impute} "
+            + f"data points will be imputed in the {key} cohort."
+        )
+        for j in mets_to_impute:
+            indices = dat.loc[dat[j].isna()].index
+            if platform == "p180" and lod_directory is not None:
+                if key == "ADNI2GO-UPLC":
+                    value = lod_files[key].loc[:, j]
+                    dat_dict[key].loc[indices, j] = value.item()
+                else:
+                    barcode = pd.Series(dat.loc[indices, "Plate.Bar.Code"])
+                    for bar in barcode.unique():
+                        met_lod = lod_files[key].loc[:, "Plate.Bar.Code"] == bar
+                        value = lod_files[key].loc[met_lod, j] / 2
+                        dat_dict[key].loc[indices, j] = value.item()
+            else:
+                half_min = dat.loc[:, j].min() / 2
+                dat_dict[key].loc[indices, j] = half_min
+    print("")
+    return dat_dict
+
+
+def merge(
+    dat_dict: dict[str, pd.DataFrame],
+    platform: str,
+) -> dict[str, pd.DataFrame]:
+    """
+    Merge dataframes across cohorts in the p180 platform.
+    This action will delete all extra columns except for metabolites and
+    index (RID), and all non-participant rows.
+    In the nmr platform only deletes the columns.
+
+    Parameters
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Dictionary with dataframe name and dataframe to modify.
+    platform: str
+        Metabolomics platform to process.
+
+    Returns
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Merged dataframe.
+    """
+    print(
+        "=== Merging dataframes and leaving only metabolite columns and "
+        + "participants ==="
+    )
+    dat_copy = dat_dict.copy()
+    for key in dat_copy:
+        metabo_names = load._get_metabo_col_names(dat_copy[key], key)
+        indices = load._get_data_indices(dat_copy[key], platform)
+        dat = dat_copy[key].loc[indices, metabo_names]
+        dat_copy[key] = dat
+    if platform == "p180":
+        merge1 = dat_copy["ADNI1-FIA"].merge(
+            dat_copy["ADNI1-UPLC"], how="inner", on="RID", suffixes=("_1FIA", "_1UPLC")
+        )
+        merge2 = dat_copy["ADNI2GO-FIA"].merge(
+            dat_copy["ADNI2GO-UPLC"],
+            how="inner",
+            on="RID",
+            suffixes=("_2FIA", "_2UPLC"),
+        )
+        dat = pd.concat([merge1, merge2], join="inner")
+        dat_dict = {"P180": dat}
+    elif platform == "nmr":
+        dat_dict = dat_copy
+    return dat_dict
+
+
+def log2(
+    dat_dict: dict[str, pd.DataFrame],
+    platform: str,
+) -> dict[str, pd.DataFrame]:
+    """
+    Transform metabolite concentration values to log2 values.
+    Add a constant of 1 before log transformation.
+
+    Parameters
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Dictionary with dataframe name and dataframe to modify.
+    platform: str
+        Metabolomics platform to process.
+
+    Returns
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Dictionary with dataframe name and dataframe with metabolites log2
+        transformed.
+    """
+    print("=== Log2 transformation ===")
+    for key in dat_dict:
+        metabo_names = load._get_metabo_col_names(dat_dict[key], key)
+        indices = load._get_data_indices(dat_dict[key], platform)
+        dat = dat_dict[key].loc[indices, metabo_names]
+        log2_dat = np.log2(dat + 1 / 1000000)
+        dat_dict[key].loc[indices, metabo_names] = log2_dat
+    print("")
+    return dat_dict
+
+
+def zscore(
+    dat_dict: dict[str, pd.DataFrame],
+    platform: str,
+) -> dict[str, pd.DataFrame]:
+    """
+    Transform metabolite concentration values to zscore values.
+
+    Parameters
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Dictionary with dataframe name and dataframe to modify.
+    platform: str
+        Metabolomics platform to process.
+
+    Returns
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Dictionary with dataframe name and dataframe with metabolites zscore
+        transformed.
+    """
+    print("=== Zscore transformation ===")
+    for key in dat_dict:
+        metabo_names = load._get_metabo_col_names(dat_dict[key], key)
+        indices = load._get_data_indices(dat_dict[key], platform)
+        dat = dat_dict[key].loc[indices, metabo_names]
+        zscore_dat = dat.apply(stats.zscore, nan_policy="omit")
+        dat_dict[key].loc[indices, metabo_names] = zscore_dat
+    print("")
+    return dat_dict
+
+
+def winsorize(
+    dat_dict: dict[str, pd.DataFrame],
+    platform: str,
+) -> dict[str, pd.DataFrame]:
+    """
+    Replace extreme values, 3 std, with cap values.
+
+    Parameters
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Dictionary with dataframe name and dataframe to modify.
+    platform: str
+        Metabolomics platform to process.
+
+    Returns
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Dictionary with dataframe name and dataframe with metabolites values
+        winsorized.
+    """
+    print("=== Winsorize metabolite values ===")
+    for key in dat_dict:
+        metabo_names = load._get_metabo_col_names(dat_dict[key], key)
+        indices = load._get_data_indices(dat_dict[key], platform)
+        dat = dat_dict[key].loc[indices, metabo_names]
+        three_std = np.std(dat, axis=0, ddof=1) * 3
+        total_replacements = 0
+        for i, std in enumerate(three_std):
+            col = dat.columns[i]
+            high = dat.loc[:, col] > std
+            low = dat.loc[:, col] < -std
+            dat.loc[high, col] = std
+            dat.loc[low, col] = -std
+            total_replacements += sum(high) + sum(low)
+        print(f"Replaced {total_replacements} values in {key} cohort.")
+        dat_dict[key].loc[indices, metabo_names] = dat
+    print("")
+    return dat_dict
+
+
+def residualize_metabolites(
+    dat_dict: dict[str, pd.DataFrame],
+    platform: str,
+) -> dict[str, pd.DataFrame]:
+    """
+    Replace metabolite concentration values for the residuals on medication
+    intake
+
+    Parameters
+    ----------
+    dat_dict: dict[str, pd.DataFrame]
+        Dictionary with dataframe name and dataframe to modify.
+    platform: str
+        Metabolomics platform to process.
+    """
+    print(
+        "=== Replacing metabolite concentration values for residuals from "
+        + "medications ==="
+    )
+    meds = load.read_meds_file()
+    for key in dat_dict:
+        metabo_names = load._get_metabo_col_names(dat_dict[key], key)
+        indices = load._get_data_indices(dat_dict[key], platform)
+        dat = dat_dict[key].loc[indices, metabo_names]
+        residuals = _get_residuals(dat, meds)
+        dat_dict[key].loc[indices, metabo_names] = residuals
+    return dat_dict
+
+
+def _get_residuals(
+    outcomes: pd.DataFrame,
+    predictors: pd.DataFrame,
+) -> pd.DataFrame:
+    """
+    Get residuals for each column in the outcome dataframe.
+    Both outcomes and predictors need an 'RID' column as index.
+
+    Parameters
+    ----------
+    outcomes: pd.DataFrame
+        dataframe with the outcome variables
+    predictors: pd.DataFrame
+        dataframe with the predictor variables
+    """
+    new_dat = pd.merge(outcomes, predictors, left_on="RID", right_on="RID")
+    Y = new_dat.loc[:, outcomes.columns]
+    residuals = Y.copy()
+    X = new_dat.loc[:, predictors.columns]
+    # Remove meds with less than 20 observations
+    keep_meds = X.sum() > 20
+    X = X.loc[:, keep_meds]
+    for y in Y:
+        results = sm.OLS(exog=X, endog=Y[y]).fit()
+        med_names = list(X.columns)
+        n_significants = sum(results.pvalues < 0.05)
+        n_not_significants = sum(results.pvalues > 0.05)
+        while n_not_significants > 0:
+            drop_med = (
+                results.pvalues[results.pvalues > 0.05]
+                .sort_values(ascending=False)
+                .index[0]
+            )
+            med_names.remove(drop_med)
+            if not med_names:
+                print(f"No significant medications in {y}")
+                break
+            else:
+                results = sm.OLS(exog=X[med_names], endog=Y[y]).fit()
+                n_significants = sum(results.pvalues < 0.05)
+                n_not_significants = sum(results.pvalues > 0.05)
+        if n_significants > 0:
+            print(f"There are significant medications in {y}")
+            print(med_names)
+            print("")
+            residuals[y] = results.resid
+    return residuals
