8cbae1b82c1b38a5b6526eda4c14a3802bdb22f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 19:04:13 +0000

    Add package datai/1.4/datai-1.4.tar.gz

---
+++ packages/datai/datai-1.4.tar.gz/datai-1.4/datai/__init__.py
@@ -0,0 +1,8 @@
+# datai/__init__.py
+
+# Version of the library
+__version__ = "1.4"
+
+# Brief description of the library (optional but helpful)
+__description__ = "A Python library for easy data visualization, data cleaning, and automatic chart generation."
+
---
+++ packages/datai/datai-1.4.tar.gz/datai-1.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='datai',
+    version='1.4',
+    description='A library for data visualization and cleaning',
+    author='M Ans',
+    author_email='m.ans.cs@outlook.com',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'seaborn',
+        'matplotlib',
+        'setuptools',
+        'scikit-learn',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',    # Python version requirement
+)
