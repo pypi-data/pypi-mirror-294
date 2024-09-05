4a270d59049732b0205d9df23b6f45590bfc2dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 15:04:18 +0000

    Add package Ammu123/0.1/Ammu123-0.1-py3-none-any.whl

---
+++ packages/Ammu123/Ammu123-0.1-py3-none-any.whl/streamlit/__init__.py
@@ -0,0 +1,9 @@
+# streamlit_app/__init__.py
+
+# This file can be empty, or you can use it to define package-level imports.
+# For now, we'll keep it empty since your main functionality is in `a.py`.
+
+# If you want to expose certain functionalities when someone imports the package,
+# you can import them here. For example:
+#
+# from .a import main
---
+++ packages/Ammu123/Ammu123-0.1-py3-none-any.whl/streamlit/a.py
@@ -0,0 +1,9 @@
+# streamlit_app/a.py
+
+import streamlit as st
+
+def main():
+    st.write("Hi")
+
+if __name__ == "__main__":
+    main()
