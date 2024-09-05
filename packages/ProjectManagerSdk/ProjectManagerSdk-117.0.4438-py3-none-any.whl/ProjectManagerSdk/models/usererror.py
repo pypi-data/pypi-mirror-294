bd17be1383fc36a9190064030390e8354a3d328b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 19:04:13 +0000

    Add package ProjectManagerSdk/117.0.4438/ProjectManagerSdk-117.0.4438-py3-none-any.whl

---
+++ packages/ProjectManagerSdk/ProjectManagerSdk-117.0.4438-py3-none-any.whl/ProjectManagerSdk/models/usererror.py
@@ -0,0 +1,24 @@
+#
+# ProjectManager API for Python
+#
+# (c) 2023-2024 ProjectManager.com, Inc.
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+# @author     ProjectManager.com <support@projectmanager.com>
+# @copyright  2023-2024 ProjectManager.com, Inc.
+# @link       https://github.com/projectmgr/projectmanager-sdk-python
+#
+
+
+from typing import List
+import dataclasses
+
+@dataclasses.dataclass
+class UserError:
+
+    email: str | None = None
+    reason: str | None = None
+    statusCode: int | None = None
+
