d3259b533912247b2bc0e68d829c4de79b5d3802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 18:04:40 +0000

    Add package bmdf/0.0.3/bmdf-0.0.3.tar.gz

---
+++ packages/bmdf/bmdf-0.0.3.tar.gz/bmdf-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from utz.setup import setup
+
+setup(
+    name="bmdf",
+    version="0.0.3",
+    entry_points={
+        "console_scripts": [
+            "bmd = bmdf.cli:bmd",
+            "bmdf = bmdf.cli:bmd_f",
+            "bmdff = bmdf.cli:bmd_ff",
+            "bmdfff = bmdf.cli:bmd_fff",
+            "mdcmd = mdcmd.cli:main",
+        ]
+    },
+    url="https://github.com/runsascoded/bash-markdown-fence",
+)
