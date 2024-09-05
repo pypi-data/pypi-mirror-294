4bfe18bdc668d896068370d480b757de46a4ea4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 17:04:44 +0000

    Add package ansys-fluent-core/0.26.dev0/ansys_fluent_core-0.26.dev0.tar.gz

---
+++ packages/ansys-fluent-core/ansys_fluent_core-0.26.dev0.tar.gz/ansys_fluent_core-0.26.dev0/src/ansys/fluent/core/generated/solver/settings_241/label.py
@@ -0,0 +1,25 @@
+#
+# This is an auto-generated file.  DO NOT EDIT!
+#
+
+from ansys.fluent.core.solver.flobject import *
+
+from ansys.fluent.core.solver.flobject import (
+    _ChildNamedObjectAccessorMixin,
+    CreatableNamedObjectMixin,
+    _NonCreatableNamedObjectMixin,
+    AllowedValuesMixin,
+    _InputFile,
+    _OutputFile,
+    _InOutFile,
+)
+
+
+class label(String):
+    """
+    Set axis label.
+    """
+
+    fluent_name = "label"
+
+    return_type = "<object object at 0x7f0aded3a5c0>"
---
+++ packages/ansys-fluent-core/ansys_fluent_core-0.26.dev0.tar.gz/ansys_fluent_core-0.26.dev0/src/ansys/fluent/core/generated/solver/settings_251/profiles.py
@@ -0,0 +1,62 @@
+#
+# This is an auto-generated file.  DO NOT EDIT!
+#
+
+from ansys.fluent.core.solver.flobject import *
+
+from ansys.fluent.core.solver.flobject import (
+    _ChildNamedObjectAccessorMixin,
+    CreatableNamedObjectMixin,
+    _NonCreatableNamedObjectMixin,
+    AllowedValuesMixin,
+    _InputFile,
+    _OutputFile,
+    _InOutFile,
+)
+
+from .update_interval import update_interval as update_interval_cls
+from .display_profile_point_cloud_data import display_profile_point_cloud_data as display_profile_point_cloud_data_cls
+from .display_profile_surface import display_profile_surface as display_profile_surface_cls
+from .set_preference_profile_point_cloud_data import set_preference_profile_point_cloud_data as set_preference_profile_point_cloud_data_cls
+from .overlay_profile_point_cloud_data import overlay_profile_point_cloud_data as overlay_profile_point_cloud_data_cls
+from .overlay_profile_surface import overlay_profile_surface as overlay_profile_surface_cls
+from .list_profiles import list_profiles as list_profiles_cls
+from .list_profile_parameters import list_profile_parameters as list_profile_parameters_cls
+from .list_profile_parameters_with_value import list_profile_parameters_with_value as list_profile_parameters_with_value_cls
+from .list_profile_fields import list_profile_fields as list_profile_fields_cls
+from .delete_4 import delete as delete_cls
+from .delete_all_1 import delete_all as delete_all_cls
+
+class profiles(Group):
+    """
+    Profile display menu.
+    """
+
+    fluent_name = "profiles"
+
+    child_names = \
+        ['update_interval']
+
+    command_names = \
+        ['display_profile_point_cloud_data', 'display_profile_surface',
+         'set_preference_profile_point_cloud_data',
+         'overlay_profile_point_cloud_data', 'overlay_profile_surface',
+         'list_profiles', 'list_profile_parameters',
+         'list_profile_parameters_with_value', 'list_profile_fields',
+         'delete', 'delete_all']
+
+    _child_classes = dict(
+        update_interval=update_interval_cls,
+        display_profile_point_cloud_data=display_profile_point_cloud_data_cls,
+        display_profile_surface=display_profile_surface_cls,
+        set_preference_profile_point_cloud_data=set_preference_profile_point_cloud_data_cls,
+        overlay_profile_point_cloud_data=overlay_profile_point_cloud_data_cls,
+        overlay_profile_surface=overlay_profile_surface_cls,
+        list_profiles=list_profiles_cls,
+        list_profile_parameters=list_profile_parameters_cls,
+        list_profile_parameters_with_value=list_profile_parameters_with_value_cls,
+        list_profile_fields=list_profile_fields_cls,
+        delete=delete_cls,
+        delete_all=delete_all_cls,
+    )
+
