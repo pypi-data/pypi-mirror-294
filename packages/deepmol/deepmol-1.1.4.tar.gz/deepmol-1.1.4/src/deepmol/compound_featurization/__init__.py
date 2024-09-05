2321eba7daaee92ec7a46668b48e78e255288627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 16:04:26 +0000

    Add package deepmol/1.1.4/deepmol-1.1.4.tar.gz

---
+++ packages/deepmol/deepmol-1.1.4.tar.gz/deepmol-1.1.4/src/deepmol/compound_featurization/__init__.py
@@ -0,0 +1,29 @@
+import warnings
+
+from .base_featurizer import MolecularFeaturizer
+
+from .rdkit_descriptors import ThreeDimensionalMoleculeGenerator, All3DDescriptors, AutoCorr3D, \
+    RadialDistributionFunction, PlaneOfBestFit, MORSE, WHIM, RadiusOfGyration, InertialShapeFactor, Eccentricity, \
+    Asphericity, SpherocityIndex, PrincipalMomentsOfInertia, NormalizedPrincipalMomentsRatios, \
+    generate_conformers_to_sdf_file, TwoDimensionDescriptors
+
+from .rdkit_fingerprints import MorganFingerprint, AtomPairFingerprint, LayeredFingerprint, RDKFingerprint, \
+    MACCSkeysFingerprint
+
+from .similarity_matrix import TanimotoSimilarityMatrix
+
+from .mixed_descriptors import MixedFeaturizer
+
+try:
+    from .mol2vec import Mol2Vec
+except ImportError:
+    warnings.warn("Mol2Vec not available. Please install it to use it. "
+                  "(pip install git+https://github.com/samoturk/mol2vec#egg=mol2vec)")
+
+try:
+    from .deepchem_featurizers import WeaveFeat, CoulombFeat, CoulombEigFeat, ConvMolFeat, MolGraphConvFeat, \
+        SmileImageFeat, SmilesSeqFeat, MolGanFeat, PagtnMolGraphFeat, DagTransformer, DMPNNFeat, MATFeat, RawFeat
+except ImportError:
+    warnings.warn("DeepChem not available. Please install it to use it.")
+
+from .one_hot_encoder import SmilesOneHotEncoder
