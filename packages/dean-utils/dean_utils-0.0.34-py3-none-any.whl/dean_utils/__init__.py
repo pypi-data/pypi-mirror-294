a00661f2e8114efb6dd0647263897591588f25c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 14:04:46 +0000

    Add package dean-utils/0.0.34/dean_utils-0.0.34-py3-none-any.whl

---
+++ packages/dean-utils/dean_utils-0.0.34-py3-none-any.whl/dean_utils/__init__.py
@@ -0,0 +1,54 @@
+__all__ = [
+    "async_abfs",
+    "cos_query_all",
+    "clear_messages",
+    "peek_messages",
+    "get_queue_properties",
+    "send_message",
+    "update_queue",
+    "delete_message",
+    "send_email",
+    "az_send",
+    "def_cos",
+    "pl_scan_hive",
+    "pl_scan_pq",
+    "pl_write_pq",
+    "global_async_client",
+]
+from dean_utils.utils.az_utils import (
+    async_abfs,
+    cos_query_all,
+    peek_messages,
+    get_queue_properties,
+    send_message,
+    update_queue,
+    delete_message,
+    clear_messages,
+    def_cos,
+)
+from dean_utils.utils.email_utility import send_email, az_send
+from dean_utils.polars_extras import pl_scan_hive, pl_scan_pq, pl_write_pq
+from dean_utils.utils.httpx import global_async_client
+from typing import cast, Iterable
+
+
+def error_email(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as err:
+            import os
+            import inspect
+            from traceback import format_exception
+
+            email_body = (
+                "\n".join(cast(Iterable[str], inspect.stack()))
+                + "\n\n"
+                + "\n".join(format_exception(err))
+            )
+            az_send(
+                os.getcwd(),
+                email_body,
+            )
+
+    return wrapper
