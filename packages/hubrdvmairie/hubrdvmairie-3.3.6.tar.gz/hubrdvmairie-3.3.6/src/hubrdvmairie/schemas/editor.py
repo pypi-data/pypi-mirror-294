a2e1ac125c038ff5d2c97c8bdfb2a664575f527c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 14:04:13 +0000

    Add package hubrdvmairie/3.3.6/hubrdvmairie-3.3.6.tar.gz

---
+++ packages/hubrdvmairie/hubrdvmairie-3.3.6.tar.gz/hubrdvmairie-3.3.6/src/hubrdvmairie/schemas/editor.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel
+
+
+class EditorBase(BaseModel):
+    """
+    Base editor schema.
+    """
+
+    name: str
+    url: str
+    header_name: str
+
+
+class EditorCreate(EditorBase):
+    """
+    Editor create schema.
+    """
+
+
+class EditorUpdate(EditorBase):
+    """
+    Editor update schema.
+    """
+
+
+class Editor(EditorBase):
+    """
+    Editor schema.
+    """
+
+    id: int
+
+    class Config:
+        orm_mode = True
