fb096d898e2c97a9a4b6d2c4fac6a6b07eb97e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 19:04:26 +0000

    Add package ragstack-ai-langflow-base/1.0.16.post5/ragstack_ai_langflow_base-1.0.16.post5.tar.gz

---
+++ packages/ragstack-ai-langflow-base/ragstack_ai_langflow_base-1.0.16.post5.tar.gz/ragstack_ai_langflow_base-1.0.16.post5/langflow/frontend/assets/square-arrow-down-right-DNzA4RyD.js
@@ -0,0 +1,6 @@
+import{a1 as e}from"./index-ByLTX0S6.js";/**
+ * @license lucide-react v0.395.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=e("SquareArrowDownRight",[["rect",{width:"18",height:"18",x:"3",y:"3",rx:"2",key:"afitv7"}],["path",{d:"m8 8 8 8",key:"1imecy"}],["path",{d:"M16 8v8H8",key:"1lbpgo"}]]);export{r as default};
