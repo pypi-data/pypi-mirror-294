a5c82b559773c3c17dee5d39efa71de83127aae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 18:05:06 +0000

    Add package mbari-pbp/1.4.4/mbari_pbp-1.4.4-py3-none-any.whl

---
+++ packages/mbari-pbp/mbari_pbp-1.4.4-py3-none-any.whl/pbp/main_meta_generator.py
@@ -0,0 +1,72 @@
+from datetime import datetime
+from pathlib import Path
+
+from pbp.meta_gen.gen_nrs import NRSMetadataGenerator
+from pbp.meta_gen.gen_iclisten import IcListenMetadataGenerator
+from pbp.meta_gen.gen_soundtrap import SoundTrapMetadataGenerator
+from pbp.main_meta_generator_args import parse_arguments
+
+# Some imports, in particular involving data processing, cause a delay that is
+# noticeable when just running the --help option. We get around this issue by
+# postponing the imports until actually needed. See the main() function.
+
+
+def main():
+    opts = parse_arguments()
+
+    # pylint: disable=import-outside-toplevel
+    from pbp.logging_helper import create_logger
+
+    log = create_logger(
+        log_filename_and_level=(
+            f"{opts.output_dir}/{opts.recorder}{opts.start}_{opts.end}.log",
+            "INFO",
+        ),
+        console_level="INFO",
+    )
+
+    log_dir = Path(opts.output_dir)
+    json_dir = Path(opts.json_base_dir)
+    log_dir.mkdir(exist_ok=True, parents=True)
+    json_dir.mkdir(exist_ok=True, parents=True)
+    start = datetime.strptime(opts.start, "%Y%m%d")
+    end = datetime.strptime(opts.end, "%Y%m%d")
+
+    try:
+        if opts.recorder == "NRS":
+            generator = NRSMetadataGenerator(
+                log=log,
+                uri=opts.uri,
+                json_base_dir=json_dir.as_posix(),
+                prefixes=opts.prefix,
+                start=start,
+                end=end,
+            )
+            generator.run()
+        if opts.recorder == "ICLISTEN":
+            generator = IcListenMetadataGenerator(
+                log=log,
+                uri=opts.uri,
+                json_base_dir=json_dir.as_posix(),
+                prefixes=opts.prefix,
+                start=start,
+                end=end,
+            )
+            generator.run()
+            # TODO: add multiprocessing here for speed-up
+        if opts.recorder == "SOUNDTRAP":
+            generator = SoundTrapMetadataGenerator(
+                log=log,
+                uri=opts.uri,
+                json_base_dir=json_dir.as_posix(),
+                prefixes=opts.prefix,
+                start=start,
+                end=end,
+            )
+            generator.run()
+    except KeyboardInterrupt:
+        log.info("INTERRUPTED")
+
+
+if __name__ == "__main__":
+    main()
