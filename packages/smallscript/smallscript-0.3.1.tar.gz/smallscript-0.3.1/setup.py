5579c42a472051bc7dda4a894be73a4c840dd3bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 19:04:17 +0000

    Add package smallscript/0.3.1/smallscript-0.3.1.tar.gz

---
+++ packages/smallscript/smallscript-0.3.1.tar.gz/smallscript-0.3.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='smallscript',
+    version='0.3.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Man Chan',
+    author_email='man.chan@gmail.com',
+    description='smallscript...',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/vitalstarorg/smallscript',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+)
