6215ada07a047ccab4a7a82108dd2c0e22bdc4e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 18:05:07 +0000

    Add package retrack/2.7.1/retrack-2.7.1.tar.gz

---
+++ packages/retrack/retrack-2.7.1.tar.gz/retrack-2.7.1/retrack/nodes/connectors.py
@@ -0,0 +1,14 @@
+from retrack.nodes.base import NodeKind
+from retrack.nodes.inputs import Input, InputMetadataModel
+
+
+class ConnectorMetadataModel(InputMetadataModel):
+    service: str
+    identifier: str
+
+
+class BaseConnector(Input):
+    data: ConnectorMetadataModel
+
+    def kind(self) -> NodeKind:
+        return NodeKind.CONNECTOR
