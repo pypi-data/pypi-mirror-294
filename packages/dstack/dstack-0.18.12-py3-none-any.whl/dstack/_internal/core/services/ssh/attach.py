1385c35688b1d155a183fcb58e083728d124a909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 14:04:44 +0000

    Add package dstack/0.18.12/dstack-0.18.12-py3-none-any.whl

---
+++ packages/dstack/dstack-0.18.12-py3-none-any.whl/dstack/_internal/core/services/ssh/attach.py
@@ -0,0 +1,151 @@
+import atexit
+import re
+import subprocess
+import time
+from typing import Optional, Tuple
+
+from dstack._internal.core.errors import SSHError
+from dstack._internal.core.models.instances import SSHConnectionParams
+from dstack._internal.core.services.configs import ConfigManager
+from dstack._internal.core.services.ssh.ports import PortsLock
+from dstack._internal.core.services.ssh.tunnel import ClientTunnel
+from dstack._internal.utils.path import PathLike
+from dstack._internal.utils.ssh import get_ssh_config, include_ssh_config, update_ssh_config
+
+
+class SSHAttach:
+    @staticmethod
+    def reuse_control_sock_path_and_port_locks(run_name: str) -> Optional[Tuple[str, PortsLock]]:
+        ssh_config_path = str(ConfigManager().dstack_ssh_config_path)
+        host_config = get_ssh_config(ssh_config_path, run_name)
+        if host_config and host_config.get("ControlPath"):
+            ps = subprocess.Popen(("ps", "-A", "-o", "command"), stdout=subprocess.PIPE)
+            control_sock_path = host_config.get("ControlPath")
+            output = subprocess.check_output(("grep", control_sock_path), stdin=ps.stdout)
+            ps.wait()
+            commands = list(
+                filter(lambda s: not s.startswith("grep"), output.decode().strip().split("\n"))
+            )
+            if commands:
+                port_pattern = r"-L (?:[\w.-]+:)?(\d+):localhost:(\d+)"
+                matches = re.findall(port_pattern, commands[0])
+                return control_sock_path, PortsLock(
+                    {int(target_port): int(local_port) for local_port, target_port in matches}
+                )
+        return None
+
+    def __init__(
+        self,
+        hostname: str,
+        ssh_port: int,
+        user: str,
+        id_rsa_path: PathLike,
+        ports_lock: PortsLock,
+        run_name: str,
+        dockerized: bool,
+        ssh_proxy: Optional[SSHConnectionParams] = None,
+        control_sock_path: Optional[str] = None,
+        local_backend: bool = False,
+        bind_address: Optional[str] = None,
+    ):
+        self._ports_lock = ports_lock
+        self.ports = ports_lock.dict()
+        self.run_name = run_name
+        self.ssh_config_path = str(ConfigManager().dstack_ssh_config_path)
+        self.tunnel = ClientTunnel(
+            run_name,
+            self.ports,
+            id_rsa_path=id_rsa_path,
+            control_sock_path=control_sock_path,
+            ssh_config_path=self.ssh_config_path,
+            bind_address=bind_address,
+        )
+        self.ssh_proxy = ssh_proxy
+        if ssh_proxy is None:
+            self.host_config = {
+                "HostName": hostname,
+                "Port": ssh_port,
+                "User": user,
+                "IdentityFile": id_rsa_path,
+                "IdentitiesOnly": "yes",
+                "StrictHostKeyChecking": "no",
+                "UserKnownHostsFile": "/dev/null",
+            }
+        else:
+            self.host_config = {
+                "HostName": ssh_proxy.hostname,
+                "Port": ssh_proxy.port,
+                "User": ssh_proxy.username,
+                "IdentityFile": id_rsa_path,
+                "IdentitiesOnly": "yes",
+                "StrictHostKeyChecking": "no",
+                "UserKnownHostsFile": "/dev/null",
+            }
+        if dockerized and not local_backend:
+            self.container_config = {
+                "HostName": "localhost",
+                "Port": 10022,
+                "User": "root",  # TODO(#1535): support non-root images properly
+                "IdentityFile": id_rsa_path,
+                "IdentitiesOnly": "yes",
+                "StrictHostKeyChecking": "no",
+                "UserKnownHostsFile": "/dev/null",
+                "ControlPath": self.tunnel.control_sock_path,
+                "ControlMaster": "auto",
+                "ControlPersist": "yes",
+                "ProxyJump": f"{run_name}-host",
+            }
+        elif ssh_proxy is not None:
+            self.container_config = {
+                "HostName": hostname,
+                "Port": ssh_port,
+                "User": user,
+                "IdentityFile": id_rsa_path,
+                "IdentitiesOnly": "yes",
+                "StrictHostKeyChecking": "no",
+                "UserKnownHostsFile": "/dev/null",
+                "ControlPath": self.tunnel.control_sock_path,
+                "ControlMaster": "auto",
+                "ControlPersist": "yes",
+                "ProxyJump": f"{run_name}-jump-host",
+            }
+        else:
+            self.container_config = None
+
+    def attach(self):
+        include_ssh_config(self.ssh_config_path)
+        if self.container_config is None:
+            update_ssh_config(self.ssh_config_path, self.run_name, self.host_config)
+        elif self.ssh_proxy is not None:
+            update_ssh_config(self.ssh_config_path, f"{self.run_name}-jump-host", self.host_config)
+            update_ssh_config(self.ssh_config_path, self.run_name, self.container_config)
+        else:
+            update_ssh_config(self.ssh_config_path, f"{self.run_name}-host", self.host_config)
+            update_ssh_config(self.ssh_config_path, self.run_name, self.container_config)
+
+        max_retries = 10
+        self._ports_lock.release()
+        for i in range(max_retries):
+            try:
+                self.tunnel.open()
+                atexit.register(self.detach)
+                break
+            except SSHError:
+                if i < max_retries - 1:
+                    time.sleep(1)
+        else:
+            self.detach()
+            raise SSHError("Can't connect to the remote host")
+
+    def detach(self):
+        self.tunnel.close()
+        update_ssh_config(self.ssh_config_path, f"{self.run_name}-jump-host", {})
+        update_ssh_config(self.ssh_config_path, f"{self.run_name}-host", {})
+        update_ssh_config(self.ssh_config_path, self.run_name, {})
+
+    def __enter__(self):
+        self.attach()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.detach()
---
+++ packages/dstack/dstack-0.18.12-py3-none-any.whl/dstack/_internal/core/services/ssh/tunnel.py
@@ -0,0 +1,189 @@
+import os
+import shlex
+import subprocess
+import tempfile
+from typing import Dict, Optional
+
+from dstack._internal.core.errors import SSHError
+from dstack._internal.core.models.instances import SSHConnectionParams
+from dstack._internal.core.services.ssh import get_ssh_error
+from dstack._internal.utils.logging import get_logger
+from dstack._internal.utils.path import PathLike
+
+logger = get_logger(__name__)
+SSH_TIMEOUT = 15
+
+
+class SSHTunnel:
+    def __init__(
+        self,
+        host: str,
+        id_rsa_path: PathLike,
+        ports: Dict[int, int],
+        control_sock_path: PathLike,
+        options: Dict[str, str],
+        ssh_config_path: str = "none",
+        bind_address: Optional[str] = None,
+    ):
+        """
+        :param ports: Mapping { remote port -> local port }
+        :param bind_address: A local address to bind as described in `ssh(1)`, a hostname or an IP.
+            If `None`, then the socket is bound in accordance with the `GatewayPorts` setting:
+            `no` (the default) means loopback only, `yes` means all interfaces, see `ssh_config(5)`
+            for details.
+        """
+        self.host = host
+        self.id_rsa_path = id_rsa_path
+        self.ports = ports
+        self.control_sock_path = control_sock_path
+        self.options = options
+        self.ssh_config_path = ssh_config_path
+        self.bind_address = bind_address
+
+    def open(self):
+        # ControlMaster and ControlPath are always set
+        command = [
+            "ssh",
+            "-F",
+            self.ssh_config_path,
+            "-f",
+            "-N",
+            "-M",
+            "-S",
+            self.control_sock_path,
+            "-i",
+            self.id_rsa_path,
+        ]
+        for k, v in self.options.items():
+            command += ["-o", f"{k}={v}"]
+        if self.bind_address is not None:
+            host_local = f"{self.bind_address}:"
+        else:
+            host_local = ""
+        for port_remote, port_local in self.ports.items():
+            command += ["-L", f"{host_local}{port_local}:localhost:{port_remote}"]
+        command += [self.host]
+        # Using stderr=subprocess.PIPE may block subprocess.run.
+        # Redirect stderr to file to get ssh error message
+        with tempfile.NamedTemporaryFile(delete=False) as f:
+            try:
+                r = subprocess.run(
+                    command, stdout=subprocess.DEVNULL, stderr=f, timeout=SSH_TIMEOUT
+                )
+            except subprocess.TimeoutExpired as e:
+                msg = f"SSH tunnel to {self.host} did not open in {SSH_TIMEOUT} seconds"
+                logger.debug(msg)
+                raise SSHError(msg) from e
+        with open(f.name, "r+b") as f:
+            error = f.read()
+        os.remove(f.name)
+        if r.returncode == 0:
+            return
+        logger.debug("SSH tunnel failed: %s", error)
+        raise get_ssh_error(error)
+
+    def close(self):
+        command = ["ssh", "-S", self.control_sock_path, "-O", "exit", self.host]
+        subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    def __enter__(self):
+        self.open()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+
+class RunnerTunnel(SSHTunnel):
+    """
+    RunnerTunnel cancel forwarding without closing the connection on close()
+    """
+
+    def __init__(
+        self,
+        hostname: str,
+        ssh_port: int,
+        user: str,
+        ports: Dict[int, int],
+        id_rsa: str,
+        *,
+        control_sock_path: Optional[PathLike] = None,
+        ssh_proxy: Optional[SSHConnectionParams] = None,
+        disconnect_delay: int = 5,
+    ):
+        self.temp_dir = tempfile.TemporaryDirectory()
+        id_rsa_path = os.path.join(self.temp_dir.name, "id_rsa")
+        with open(
+            id_rsa_path, opener=lambda path, flags: os.open(path, flags, 0o600), mode="w"
+        ) as f:
+            f.write(id_rsa)
+        if control_sock_path is None:
+            control_sock_path = os.path.join(self.temp_dir.name, "control.sock")
+        options = {}
+        if ssh_proxy is not None:
+            proxy_command = ["ssh", "-i", id_rsa_path, "-W", "%h:%p"]
+            proxy_command += [
+                "-o",
+                "StrictHostKeyChecking=no",
+                "-o",
+                "UserKnownHostsFile=/dev/null",
+            ]
+            proxy_command += [
+                "-p",
+                str(ssh_proxy.port),
+                f"{ssh_proxy.username}@{ssh_proxy.hostname}",
+            ]
+            options["ProxyCommand"] = shlex.join(proxy_command)
+        options.update(
+            {
+                "StrictHostKeyChecking": "no",
+                "UserKnownHostsFile": "/dev/null",
+                "ExitOnForwardFailure": "yes",
+                "ConnectTimeout": "3",
+                # "ControlPersist": f"{disconnect_delay}s",
+                "Port": str(ssh_port),
+            }
+        )
+        super().__init__(
+            host=f"{user}@{hostname}",
+            id_rsa_path=id_rsa_path,
+            ports=ports,
+            control_sock_path=control_sock_path,
+            options=options,
+        )
+
+    # def close(self):
+    #     # cancel forwarding without closing the connection
+    #     command = ["ssh", "-S", self.control_sock_path, "-O", "cancel"]
+    #     for port_remote, port_local in self.ports.items():
+    #         command += ["-L", f"{port_local}:localhost:{port_remote}"]
+    #     command += [self.host]
+    #     subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+
+class ClientTunnel(SSHTunnel):
+    """
+    ClientTunnel connects to the host from ssh config
+    """
+
+    def __init__(
+        self,
+        host: str,
+        ports: Dict[int, int],
+        id_rsa_path: PathLike,
+        ssh_config_path: str,
+        control_sock_path: Optional[str] = None,
+        bind_address: Optional[str] = None,
+    ):
+        if control_sock_path is None:
+            self.temp_dir = tempfile.TemporaryDirectory()
+            control_sock_path = os.path.join(self.temp_dir.name, "control.sock")
+        super().__init__(
+            host=host,
+            id_rsa_path=id_rsa_path,
+            ports=ports,
+            control_sock_path=control_sock_path,
+            options={},
+            ssh_config_path=ssh_config_path,
+            bind_address=bind_address,
+        )
