102daed5ae678a150ab87bd7e115514301153675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 20:05:02 +0000

    Add package comfit/1.6.1/comfit-1.6.1.tar.gz

---
+++ packages/comfit/comfit-1.6.1.tar.gz/comfit-1.6.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='comfit',
+    version='1.6.1',
+    packages=['comfit'],
+    package_data={'comfit':['core/*','models/*','tools/*']},
+    author='Vidar Skogvoll and Jonas Rønning',
+    install_requires=['numpy>=1.21, <2.0.0',
+                      'scipy',
+                      'matplotlib',
+                      'plotly',
+                      'kaleido',
+                      'scikit-image>=0.18.0,<0.23',
+                      'moviepy',
+                      'pillow'],
+)
