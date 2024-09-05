265bf1a8d3cb234eb62aafeebbaec5c0f095c9bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 19:04:17 +0000

    Add package ablelabs/0.0.28/ablelabs-0.0.28.tar.gz

---
+++ packages/ablelabs/ablelabs-0.0.28.tar.gz/ablelabs-0.0.28/ablelabs/neon/controllers/notable/kribb/api/motion_api.py
@@ -0,0 +1,24 @@
+from loguru import logger
+
+import sys, os
+
+sys.path.append(os.path.abspath(os.curdir))
+from ablelabs.neon.utils.network.messenger import MessengerClient, run_server_func
+from ablelabs.neon.utils.network.tcp_client import TcpClient
+from ablelabs.neon.controllers.notable.kribb.api.robot_router import RobotRouter
+from ablelabs.neon.controllers.notable.api.motion_api import MotionAPI as BaseMotionAPI
+from ablelabs.neon.controllers.notable.api.motion_api import Location
+
+
+class MotionAPI(BaseMotionAPI):
+    def __init__(self, tcp_client: TcpClient) -> None:
+        tcp_client = TcpClient(name="tcp_client", log_func=logger.trace)
+        super().__init__(tcp_client)
+
+    @run_server_func(RobotRouter.motion_move_to_camera)
+    async def move_to_camera(
+        self,
+        pipette_number: int,
+        location: Location,
+    ):
+        pass
