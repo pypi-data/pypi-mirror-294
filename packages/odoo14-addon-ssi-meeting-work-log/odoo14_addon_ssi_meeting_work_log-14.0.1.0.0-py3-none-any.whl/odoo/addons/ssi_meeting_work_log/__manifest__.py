caf9b91834e13137ba310a9c7452e2343307bbe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 21:04:58 +0000

    Add package odoo14-addon-ssi-meeting-work-log/14.0.1.0.0/odoo14_addon_ssi_meeting_work_log-14.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-meeting-work-log/odoo14_addon_ssi_meeting_work_log-14.0.1.0.0-py3-none-any.whl/odoo/addons/ssi_meeting_work_log/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=C8101
+{
+    "name": "Meeting + Work Log Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_work_log_mixin",
+        "calendar",
+    ],
+    "data": [],
+    "demo": [],
+    "images": [],
+}
