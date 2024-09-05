d90d2fd86ca819879413526b894a71f474980b5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 15:04:04 +0000

    Add package pydocmaker/1.1.2/pydocmaker-1.1.2.tar.gz

---
+++ packages/pydocmaker/pydocmaker-1.1.2.tar.gz/pydocmaker-1.1.2/src/pydocmaker/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '1.1.2'
+
+from pydocmaker.core.schema import FlowDoc, SectionedDoc, construct, constr, buildingblocks
+
+from pydocmaker.exporters.ex_docx import convert as to_docx
+from pydocmaker.exporters.ex_html import convert as to_html
+from pydocmaker.exporters.ex_redmine import convert as to_redmine
+from pydocmaker.exporters.ex_redmine import convert as to_textile
+from pydocmaker.exporters.ex_tex import convert as to_tex
+
+def get_schema():
+    return {k: getattr(constr, k)() for k in buildingblocks}
+        
