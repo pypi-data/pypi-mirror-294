7d885ff4f145e63c71605fe1ca0e63be0ebb471a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 15:04:25 +0000

    Add package therabee/0.1/therabee-0.1.tar.gz

---
+++ packages/therabee/therabee-0.1.tar.gz/therabee-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='therabee',
+    version='0.1',
+    description='Therabee - A therapy chatbot',
+    packages=find_packages(),
+    install_requires=[
+        'openai',
+    ],
+    author='Finn',
+    author_email='fs33884@sersd.org',
+    
+)
