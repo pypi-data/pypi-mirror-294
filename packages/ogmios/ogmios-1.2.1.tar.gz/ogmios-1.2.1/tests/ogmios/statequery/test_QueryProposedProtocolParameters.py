15b8f413d115253a480742d7c09de2629ce4c6df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 18:04:42 +0000

    Add package ogmios/1.2.1/ogmios-1.2.1.tar.gz

---
+++ packages/ogmios/ogmios-1.2.1.tar.gz/ogmios-1.2.1/tests/ogmios/statequery/test_QueryProposedProtocolParameters.py
@@ -0,0 +1,13 @@
+import pytest
+from typing import Union
+
+from ogmios import ProtocolParameters
+from tests.ogmios.test_fixtures import client
+
+
+def test_QueryProposedProtocolParameters(client):
+    id_str = "My ID string"
+    result, id = client.query_proposed_protocol_parameters.execute(id_str)
+
+    assert isinstance(result, Union[ProtocolParameters, None])
+    assert id == id_str
