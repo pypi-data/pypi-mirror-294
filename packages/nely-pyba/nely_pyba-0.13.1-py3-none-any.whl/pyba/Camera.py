54d0237deee09914c5a7cb3405b78d668e243840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 14:04:13 +0000

    Add package nely-pyba/0.13.1/nely_pyba-0.13.1-py3-none-any.whl

---
+++ packages/nely-pyba/nely_pyba-0.13.1-py3-none-any.whl/pyba/Camera.py
@@ -0,0 +1,205 @@
+from pyba.plot import draw_reference_frame
+from typing import *
+from typing import Optional
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from pyba.util import P_from_RtvecK
+
+
+class Camera:
+    def __init__(
+        self,
+        points2d: np.ndarray,
+        intr: Optional[np.ndarray] = None,
+        R: Optional[np.ndarray] = None,
+        tvec: Optional[np.ndarray] = None,
+        distort: Optional[np.ndarray] = None,
+        image_path: Optional[dict] = None,
+    ):
+        """
+        fx, fy: focal length in pixels
+        tvec: translation vector
+        cx, cy: optical axis in pixels
+        points2d: numpy array in pixels, TxJx2
+        distort: list with 5 numbers
+        image_path: Example: img_{img_id}.jpg
+        """
+
+        # fmt: off
+        assert points2d.ndim == 3 and points2d.shape[2] == 2
+        assert R is None or R.ndim == 2 and R.shape[0] == 3 and R.shape[1] == 3
+        assert tvec is None or tvec.ndim == 1 and tvec.shape[0] == 3 
+        assert distort is None or (distort.ndim == 1 and distort.shape[0] == 5)
+        assert intr is None or intr.ndim == 2 and intr.shape[0] == 3 and intr.shape[1] == 3
+        # fmt: on
+
+        self.image_path = image_path
+        self._points2d = points2d
+
+        self.intrinsic = intr
+        self.tvec = tvec
+        self.R = R
+        self.distort = np.zeros(5, dtype=float) if distort is None else distort
+
+    @property
+    def P(self):
+        return P_from_RtvecK(self.R, self.tvec, self.intrinsic)
+
+    @property
+    def rvec(self):
+        return cv2.Rodrigues(self.R)[0]
+
+    def camera2world(self, pts: np.ndarray):
+        return self.R.T @ (pts - self.tvec)
+
+    @property
+    def C(self):
+        return -1 * self.R.T @ self.tvec
+
+    @rvec.setter
+    def rvec(self, rvec):
+        self.R = cv2.Rodrigues(rvec)[0]
+
+    @property
+    def fx(self):
+        return self.intrinsic[0, 0]
+
+    @property
+    def fy(self):
+        return self.intrinsic[1, 1]
+
+    @property
+    def cx(self):
+        return self.intrinsic[0, 2]
+
+    @property
+    def cy(self):
+        return self.intrinsic[1, 2]
+
+    @fx.setter
+    def fx(self, fx):
+        self.intrinsic[0, 0] = fx
+
+    @fy.setter
+    def fy(self, fy):
+        self.intrinsic[1, 1] = fy
+
+    @cx.setter
+    def cx(self, cx):
+        self.intrinsic[0, 2] = cx
+
+    @cy.setter
+    def cy(self, cy):
+        self.intrinsic[1, 2] = cy
+
+    @property
+    def points2d(self):
+        return self._points2d
+
+    @points2d.setter
+    def points2d(self, pts2d):
+        self._points2d = pts2d
+
+    def __getitem__(self, idx: int):
+        return self.points2d[idx]
+
+    def set_intrinsic(self, intrinsic):
+        self.intrinsic = intrinsic
+
+    def can_see(self, img_id, jid):
+        return not np.any(np.isclose(self.points2d[img_id, jid], 0))
+
+    def can_see_mask(self):
+        return np.any(self.points2d == 0, axis=2)
+
+    def get_njoints(self):
+        return self.points2d.shape[1]
+
+    def get_image(self, img_id: int):
+        try:
+            #img_old = plt.imread(self.image_path.format(img_id=img_id))
+            img = cv2.imread(self.image_path.format(img_id=img_id))
+        except:
+            img = np.zeros((480, 960), dtype=np.uint8)
+        if img.ndim == 2 or (img.ndim == 3 and img.shape[-1] == 1):
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+        return img
+
+    def has_calibration(self):
+        return all(
+            [self.intrinsic is not None, self.R is not None, self.tvec is not None]
+        )
+
+    def summarize(self):
+        return {
+            "R": self.R,
+            "tvec": self.tvec,
+            "distort": self.distort,
+            "intr": self.intrinsic,
+        }
+
+    def plot_2d(
+        self,
+        img_id: int,
+        points2d: Optional[np.ndarray] = None,
+        bones: Optional[np.ndarray] = None,
+        colors: Optional[List[Tuple]] = None,
+    ) -> np.ndarray:
+        img = self.get_image(img_id)
+        points2d = self.points2d[img_id] if points2d is None else points2d
+
+        # bones
+        if bones is not None:
+            for idx, b in enumerate(bones):
+                if self.can_see(img_id, b[0]):
+                    img = cv2.line(
+                        img,
+                        tuple(points2d[b[0]].astype(int)),
+                        tuple(points2d[b[1]].astype(int)),
+                        colors[idx] if colors is not None else (128, 0, 0),
+                        5,
+                    )
+
+        for jid in range(self.get_njoints()):
+            if self.can_see(img_id, jid):
+                img = cv2.circle(
+                    img, tuple(points2d[jid].astype(int)), 5, [0, 0, 128], 5
+                )
+
+        return img
+
+    def project(self, points3d: np.ndarray) -> np.ndarray:
+        """
+        points3d: TxJx3
+        returns: points2d: TxJx2
+        """
+
+        assert points3d.ndim == 3 and points3d.shape[2] == 3
+
+        # original shape
+        t, j = points3d.shape[0], points3d.shape[1]
+
+        # opencv wants nx3 matrix
+        # https://docs.opencv.org/3.4/d9/d0c/group__calib3d.html#ga1019495a2c8d1743ed5cc23fa0daff8c
+        points3d = points3d.reshape(t * j, 3)  # (txj)x3
+        points2d, _ = cv2.projectPoints(
+            points3d, self.rvec, self.tvec, self.intrinsic, self.distort
+        )
+
+        # reshape back to txjx2
+        points2d = points2d.reshape(t, j, 2)  # txjx2
+        return points2d
+
+    def reprojection_error(self, points3d: np.ndarray):
+        err = self.points2d - self.project(points3d)
+        err[self.can_see_mask(), :] = 0
+        return err
+
+    def draw(self, ax3d, size: float = 1, text: Optional[str] = None):
+        x = self.camera2world(np.array([size, 0, 0]))
+        y = self.camera2world(np.array([0, size, 0]))
+        z = self.camera2world(np.array([0, 0, size]))
+
+        draw_reference_frame(ax3d, center=self.C, x=x, y=y, z=z, text=text)
---
+++ packages/nely-pyba/nely_pyba-0.13.1-py3-none-any.whl/pyba/pyba.py
@@ -0,0 +1,180 @@
+from itertools import product
+from typing import *
+
+import numpy as np
+from scipy.optimize import least_squares
+from scipy.sparse import lil_matrix
+
+from pyba.Camera import Camera
+from pyba.CameraNetwork import CameraNetwork
+
+
+def bundle_adjust(
+    camNet: CameraNetwork,
+    max_num_images: int = 1e3,
+    update_intrinsic: bool = True,
+    update_distort: bool = True,
+):
+    """ 
+    max_num_images: large number of points take too long to optimize. will select random points instead.
+    """
+    (
+        x0,
+        points_2d,
+        n_cameras,
+        n_points,
+        camera_indices,
+        point_indices,
+    ) = prepare_bundle_adjust_param(camNet=camNet, max_num_images=max_num_images)
+
+    A = bundle_adjustment_sparsity(n_cameras, n_points, camera_indices, point_indices)
+    res = least_squares(
+        residuals,
+        x0,
+        jac_sparsity=A,
+        verbose=2,
+        x_scale="jac",
+        ftol=1e-4,
+        method="trf",
+        args=(
+            camNet.cam_list,
+            n_cameras,
+            n_points,
+            camera_indices,
+            point_indices,
+            points_2d,
+            update_intrinsic,
+            update_distort,
+        ),
+        max_nfev=100,
+    )
+    camNet.triangulate()
+    return res
+
+
+def bundle_adjustment_sparsity(
+    n_cameras: int, n_points: int, camera_indices: np.ndarray, point_indices: np.ndarray
+):
+    assert camera_indices.shape[0] == point_indices.shape[0]
+    n_camera_params = 13
+    m = camera_indices.size * 2
+    # all the parameters, 13 camera parameters and x,y,z values for n_points
+    n = n_cameras * n_camera_params + n_points * 3
+    A = lil_matrix((m, n), dtype=int)  # sparse matrix
+    i = np.arange(camera_indices.size)
+
+    for s in range(n_camera_params):
+        # assign camera parameters to points residuals (reprojection error)
+        A[2 * i, camera_indices * n_camera_params + s] = 1
+        A[2 * i + 1, camera_indices * n_camera_params + s] = 1
+
+    for s in range(3):
+        # assign 3d points to residuals (reprojection error)
+        A[2 * i, n_cameras * n_camera_params + point_indices * 3 + s] = 1
+        A[2 * i + 1, n_cameras * n_camera_params + point_indices * 3 + s] = 1
+
+    return A
+
+
+def prepare_bundle_adjust_param(camNet: CameraNetwork, max_num_images: int = 500):
+    # prepare intrinsic
+    camera_params = np.zeros(shape=(len(camNet.cam_list), 13), dtype=float)
+    for cid in range(len(camNet.cam_list)):
+        camera_params[cid, 0:3] = np.squeeze(camNet[cid].rvec)
+        camera_params[cid, 3:6] = np.squeeze(camNet[cid].tvec)
+        camera_params[cid, 6] = camNet[cid].fx
+        camera_params[cid, 7] = camNet[cid].fy
+        camera_params[cid, 8:13] = np.squeeze(camNet[cid].distort)
+
+    # select which images to calculate residuals on
+    img_id_list = np.arange(camNet.get_nimages())
+    if camNet.get_nimages() > max_num_images:
+        img_id_list = np.random.randint(
+            0, high=camNet.get_nimages() - 1, size=(int(max_num_images))
+        )
+
+    point_indices, camera_indices, pts2d, pts3d = list(), list(), list(), list()
+    # for all image and joint
+    for img_id, jid in product(img_id_list, range(camNet.get_njoints())):
+        pts3d.append(camNet.points3d[img_id, jid])
+
+        for cid, cam in enumerate(camNet):
+            if not cam.can_see(img_id, jid):
+                continue
+
+            pts2d.append(cam[img_id, jid])
+            point_indices.append(len(pts3d) - 1)
+            camera_indices.append(cid)
+
+    pts3d = np.stack(pts3d)
+    pts2d = np.stack(pts2d)
+    camera_indices = np.array(camera_indices)
+    point_indices = np.array(point_indices)
+
+    n_cameras = camera_params.shape[0]
+    n_points = pts3d.shape[0]
+
+    x0 = np.hstack((camera_params.ravel(), pts3d.ravel()))
+
+    return (
+        x0.copy(),
+        pts2d.copy(),
+        n_cameras,
+        n_points,
+        camera_indices,
+        point_indices,
+    )
+
+
+def update_parameters(
+    camera_params: np.ndarray,
+    cam: Camera,
+    update_intrinsic: bool = True,
+    update_distort: bool = True,
+):
+    cam.rvec = camera_params[0:3]
+    cam.tvec = camera_params[3:6]
+    if update_intrinsic:
+        cam.fx = camera_params[6]
+        cam.fy = camera_params[7]
+    if update_distort:
+        cam.distort = camera_params[8:13]
+
+
+def residuals(
+    params,
+    cam_list: List[Camera],
+    n_cameras: int,
+    n_points: int,
+    camera_indices: List[int],
+    point_indices: List[int],
+    points_2d: np.ndarray,
+    update_intrinsic: bool = True,
+    update_distort: bool = True,
+):
+    """Compute residuals.
+    `params` contains camera parameters and 3-D coordinates.
+    """
+    assert point_indices.shape[0] == points_2d.shape[0]
+    assert camera_indices.shape[0] == points_2d.shape[0]
+
+    camera_params = params[: n_cameras * 13].reshape((n_cameras, 13))
+    points3d = params[n_cameras * 13 :].reshape((n_points, 3))
+    cam_indices_list = list(set(camera_indices))
+
+    points_proj = np.zeros(shape=(point_indices.shape[0], 2), dtype=float)
+    for cam_id in cam_indices_list:
+        update_parameters(
+            camera_params[cam_id], cam_list[cam_id], update_intrinsic, update_distort
+        )
+
+        points2d_mask = camera_indices == cam_id
+        points3d_where = point_indices[points2d_mask]
+        points_proj[points2d_mask, :] = cam_list[cam_id].project(
+            points3d[points3d_where][None, :]
+        )
+
+    res = points_proj - points_2d
+    res = res.ravel()
+
+    return res
