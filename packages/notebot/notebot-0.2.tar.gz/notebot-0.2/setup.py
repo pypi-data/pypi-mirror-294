18930a86482412a6a443d323b130af066af007ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 20:05:06 +0000

    Add package notebot/0.2/notebot-0.2.tar.gz

---
+++ packages/notebot/notebot-0.2.tar.gz/notebot-0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+# Read the content of README.md for the long description
+with open('README.md', 'r', encoding='utf-8') as file:
+    mdfile = file.read()
+
+setup(
+    name='notebot',
+    version='0.2',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'note=note.note:main',
+        ],
+    },
+    long_description=mdfile,
+    long_description_content_type='text/markdown',
+    author='Souporno Chakraborty',
+    author_email='shrabanichakraborty83@gmail.com',
+    description='A command-line tool to open Notepad or create and open files with Notepad.',
+    license='MIT',
+    keywords='notepad, file, command-line',
+    url='https://github.com/Tirthaboos/notebot',
+)
