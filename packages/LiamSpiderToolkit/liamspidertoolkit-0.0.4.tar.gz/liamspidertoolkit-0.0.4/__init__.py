92e3df1700484fc4c99d85300d906378e8d82eaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 21:04:39 +0000

    Add package LiamSpiderToolkit/0.0.4/liamspidertoolkit-0.0.4.tar.gz

---
+++ packages/LiamSpiderToolkit/liamspidertoolkit-0.0.4.tar.gz/liamspidertoolkit-0.0.4/__init__.py
@@ -0,0 +1,8 @@
+# LiamSpiderToolkit/__init__.py
+
+"""
+    Liam's Python crawler toolkit
+"""
+
+
+from .moudle1 import hello_world
---
+++ packages/LiamSpiderToolkit/liamspidertoolkit-0.0.4.tar.gz/liamspidertoolkit-0.0.4/moudle1.py
@@ -0,0 +1,4 @@
+# my_custom_package/module1.py
+
+def hello_world():
+    print("Hello, world from module1!")
