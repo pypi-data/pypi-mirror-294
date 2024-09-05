08868d20833c1e7a0ff559507c84c992001195cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 19:03:59 +0000

    Add package expAscribe/0.1.11/expAscribe-0.1.11-py3-none-any.whl

---
+++ packages/expAscribe/expAscribe-0.1.11-py3-none-any.whl/expAscribe/__init__.py
@@ -0,0 +1,17 @@
+from expAscribe.io_utils import (
+    load_data,
+    array_to_gml,
+    gml_to_array
+)
+
+from expAscribe.learn import (
+    voting,
+    ensemble
+)
+
+from expAscribe.quasiexp import (
+    kink_regression_2d,
+    kink_regression_3d
+)
+
+from expAscribe.falsify import Gfalsify
