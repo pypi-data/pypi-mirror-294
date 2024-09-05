dca8b54f9226f35009f02128c5e6a34b4afeab8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 15:03:55 +0000

    Add package usportspy/0.0.10/usportspy-0.0.10.tar.gz

---
+++ packages/usportspy/usportspy-0.0.10.tar.gz/usportspy-0.0.10/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('PYPI.md', 'r') as f:
+    l_description = f.read()
+
+
+setup(
+    name='usportspy',
+    version='0.0.10',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pandas',
+    ],
+    description="One-Stop-Shop for USPORTS Data",
+    long_description=l_description,
+    long_description_content_type='text/markdown',
+)
