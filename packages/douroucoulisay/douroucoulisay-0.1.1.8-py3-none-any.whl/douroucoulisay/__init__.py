967fba86f9c76564aeb898eeed562beba8e19e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 21:05:12 +0000

    Add package douroucoulisay/0.1.1.8/douroucoulisay-0.1.1.8-py3-none-any.whl

---
+++ packages/douroucoulisay/douroucoulisay-0.1.1.8-py3-none-any.whl/douroucoulisay/__init__.py
@@ -0,0 +1,8 @@
+# douroucoulisay/__init__.py
+
+# Example of initialization code or imports
+from .douroucoulisay import douroucoulisay, tonal_hoot, gruff_hoot, whoop
+
+# You can also define some package-level constants or variables
+__version__ = "0.1.1.8"
+__author__ = "Aotus Parisinus"
