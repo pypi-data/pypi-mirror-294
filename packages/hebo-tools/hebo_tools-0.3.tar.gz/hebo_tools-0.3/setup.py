894367bd6bc2a653043e247af60b401af930cd7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 19:04:14 +0000

    Add package hebo-tools/0.3/hebo_tools-0.3.tar.gz

---
+++ packages/hebo-tools/hebo_tools-0.3.tar.gz/hebo_tools-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hebo_tools',               # 包名称
+    version='0.3',                   # 版本号
+    packages=find_packages(),        # 自动查找包
+    description='用于离床判断',
+    author='hy',
+    author_email='yue.huang@slaaplekker.cn',
+    url='https://github.com/yourname/my_package',  # 代码仓库URL
+)
