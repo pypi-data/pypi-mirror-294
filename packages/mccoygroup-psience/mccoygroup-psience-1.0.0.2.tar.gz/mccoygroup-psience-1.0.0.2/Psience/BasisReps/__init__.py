35fdd77a6b1ff42afc782f7f6e31e35d2168bef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 16:04:00 +0000

    Add package mccoygroup-psience/1.0.0.2/mccoygroup-psience-1.0.0.2.tar.gz

---
+++ packages/mccoygroup-psience/mccoygroup-psience-1.0.0.2.tar.gz/mccoygroup-psience-1.0.0.2/Psience/BasisReps/__init__.py
@@ -0,0 +1,23 @@
+"""
+BasisReps manages useful functions for generating & working with basis-set representations of data
+"""
+
+__all__ = []
+from .Bases import *
+__all__ += Bases.__all__
+from .Operators import *
+__all__ += Operators.__all__
+from .Representations import *
+__all__ += Representations.__all__
+from .HarmonicOscillator import *
+__all__ += HarmonicOscillator.__all__
+from .ClassicalBases import *
+__all__ += ClassicalBases.__all__
+from .Wavefunctions import *
+__all__ += Wavefunctions.__all__
+from .StateSpaces import *
+__all__ += StateSpaces.__all__
+from .StateIndexers import *
+__all__ += StateIndexers.__all__
+from .StateFilters import *
+__all__ += StateIndexers.__all__
