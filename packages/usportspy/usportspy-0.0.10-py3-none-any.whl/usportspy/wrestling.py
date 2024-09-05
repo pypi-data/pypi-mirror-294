cb74e9ea9d32f8aff17b7aa73140e68f04afe6e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 15:03:55 +0000

    Add package usportspy/0.0.10/usportspy-0.0.10-py3-none-any.whl

---
+++ packages/usportspy/usportspy-0.0.10-py3-none-any.whl/usportspy/wrestling.py
@@ -0,0 +1,41 @@
+from . import helpers as h
+
+'''
+Wrestling Functions
+'''
+def wrestling_athlete_rankings(gender, weight=None):
+    if gender not in ["m", "w"]:
+        raise h.UsportspyError("'gender' must be either 'm' or 'w'.")
+
+    if gender == "m":
+        url = "https://github.com/uwaggs/usports-data/releases/download/wrestling_athlete_rankings/mens_athlete.csv"
+    else:
+        url = "https://github.com/uwaggs/usports-data/releases/download/wrestling_athlete_rankings/womens_athlete.csv"
+
+    err, df = h.get_data(url) 
+    if err:
+        raise h.UsportspyError(f"Error makign request for wrestling athlete rankings for Gender: {gender} and Weight: {weight}kg.", err)
+
+    if weight:
+        df = df[df['Weight Category'] == str(weight) + 'kg']
+
+    df = df.reset_index(drop=True)
+    return df
+
+
+def wrestling_team_rankings(gender):
+    if gender not in ["m", "w"]:
+        raise h.UsportspyError("'gender' must be either 'm' or 'w'.")
+
+    if gender == "m":
+        url = "https://github.com/uwaggs/usports-data/releases/download/wrestling_team_rankings/mens_team.csv"
+    else:
+        url = "https://github.com/uwaggs/usports-data/releases/download/wrestling_team_rankings/womens_team.csv"
+        
+    err, df = h.get_data(url) 
+    if err:
+        raise h.UsportspyError(f"Error makign request for wrestling team rankings for Gender: {gender}.", err)
+
+    return df 
+
+
