410b61f2d5d47cb9d42f27a9343b6d13125182a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 17:04:47 +0000

    Add package sakurose/1.0/sakurose-1.0.tar.gz

---
+++ packages/sakurose/sakurose-1.0.tar.gz/sakurose-1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SakuRose',
+    version='1.0',
+    description='SakuRose: Simplifica la creación de APIs y aplicaciones de Discord.',
+    author='InsAnya606',
+    author_email='insanyadev@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'Flask',
+        'Pillow',
+        'discord.py'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
