8d10a10a4570d0b9f034f396a2720ee92c7a1528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 16:04:11 +0000

    Add package ansys-fluent-core/0.25.0/ansys_fluent_core-0.25.0-py3-none-any.whl

---
+++ packages/ansys-fluent-core/ansys_fluent_core-0.25.0-py3-none-any.whl/ansys/fluent/core/generated/solver/settings_251/group_surface_child.pyi
@@ -0,0 +1,27 @@
+#
+# This is an auto-generated file.  DO NOT EDIT!
+#
+
+
+from ansys.fluent.core.solver.flobject import *
+
+from ansys.fluent.core.solver.flobject import (
+    _ChildNamedObjectAccessorMixin,
+    CreatableNamedObjectMixin,
+    _NonCreatableNamedObjectMixin,
+    AllowedValuesMixin,
+    _InputFile,
+    _OutputFile,
+    _InOutFile,
+)
+
+from typing import Union, List, Tuple
+
+from .name_13 import name as name_cls
+from .surfaces_7 import surfaces as surfaces_cls
+
+class group_surface_child(Group):
+    fluent_name = ...
+    child_names = ...
+    name: name_cls = ...
+    surfaces: surfaces_cls = ...
---
+++ packages/ansys-fluent-core/ansys_fluent_core-0.25.0-py3-none-any.whl/ansys/fluent/core/generated/solver/settings_251/outflow_child_1.pyi
@@ -0,0 +1,29 @@
+#
+# This is an auto-generated file.  DO NOT EDIT!
+#
+
+
+from ansys.fluent.core.solver.flobject import *
+
+from ansys.fluent.core.solver.flobject import (
+    _ChildNamedObjectAccessorMixin,
+    CreatableNamedObjectMixin,
+    _NonCreatableNamedObjectMixin,
+    AllowedValuesMixin,
+    _InputFile,
+    _OutputFile,
+    _InOutFile,
+)
+
+from typing import Union, List, Tuple
+
+from .name_2 import name as name_cls
+from .location_2 import location as location_cls
+from .settings_16 import settings as settings_cls
+
+class outflow_child(Group):
+    fluent_name = ...
+    child_names = ...
+    name: name_cls = ...
+    location: location_cls = ...
+    settings: settings_cls = ...
