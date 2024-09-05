4392c0d7c3cb282af3ec704e520b23bbcfe4029b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 20:04:54 +0000

    Add package mtmai/0.3.649/mtmai-0.3.649-py3-none-any.whl

---
+++ packages/mtmai/mtmai-0.3.649-py3-none-any.whl/mtmai/tools/search_tools.py
@@ -0,0 +1,34 @@
+import logging
+
+# from langchain.tools import tool
+from langchain_core.tools import tool
+
+logger = logging.getLogger(__name__)
+
+
+logger = logging.getLogger()
+
+
+@tool
+def search_tool(query: str):
+    """Useful to search content from web."""
+    logger.info("调用 search 工具 %s", query)
+    return [
+        f"I looked up: {query}. Result: It's sunny in San Francisco, but you better look out if you're a Gemini 😈."
+    ]
+
+
+# class SearchTools:
+#     @tool("Search the internet")
+#     def search_internet(query: str):
+#         """Useful to search the internet
+#         about a a given topic and return relevant results.
+#         """
+#         logger.info("web_search 工具调用 %s", query)
+#         tool_api_url = "http://localhost:8333/api/tools/search_internet"
+
+#         with httpx.Client() as client:
+#             params = {"q": query}
+#             r = client.get(tool_api_url + "", params=params)
+#             r.raise_for_status()  # Raise an exception if the request was unsuccessful
+#             return r.text
