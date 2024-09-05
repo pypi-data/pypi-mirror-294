ca8fca822f52b36312acbc461611f35a83bf3c18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 15:04:08 +0000

    Add package lazydock/0.2.0/lazydock-0.2.0.tar.gz

---
+++ packages/lazydock/lazydock-0.2.0.tar.gz/lazydock-0.2.0/lazydock/__version__.py
@@ -0,0 +1,9 @@
+
+__title__ = "lazydock"
+__description__ = "Lazy Dock"
+__url__ = "https://github.com/BHM-Bob/LazyDock"
+__version__ = "0.2.0"
+__build__ = "20240904_0"
+__author__ = "BHM-Bob G"
+__author_email__ = "bhmfly@foxmail.com"
+__license__ = "MIT"
