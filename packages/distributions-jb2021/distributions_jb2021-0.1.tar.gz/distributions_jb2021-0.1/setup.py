890634bf1bee5769fa7fee5912ccd98c5673226d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 19:04:12 +0000

    Add package distributions-jb2021/0.1/distributions_jb2021-0.1.tar.gz

---
+++ packages/distributions-jb2021/distributions_jb2021-0.1.tar.gz/distributions_jb2021-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_jb2021',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_jb2021'],
+      zip_safe=False)
