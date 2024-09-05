8cbb7ea34cbf7394791f6beb82205639ac09bebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 21:04:58 +0000

    Add package diff-kit/0.1.2/diff_kit-0.1.2.tar.gz

---
+++ packages/diff-kit/diff_kit-0.1.2.tar.gz/diff_kit-0.1.2/setup.py
@@ -0,0 +1,45 @@
+# encoding: utf-8
+
+from setuptools import find_packages, setup
+
+install_requires = [
+    "typer",
+    "rich",
+    "dictdiffer",
+    "loguru",
+    "aiomysql",
+    "motor",
+    "asyncpg",
+    "pandas",
+    "openpyxl",
+    "tqdm",
+    "pydantic"
+]
+
+
+setup(
+    name="diff_kit",
+    version="0.1.2",
+    description="对比工具集",
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author="alan",
+    author_email="280262569@qq.com",
+    license="Apache License 2.0",
+    url="https://gitee.com/Test-Alan/diff-kit",
+    python_requires='>=3.8',
+    packages=find_packages(exclude=['*output', '*config', 'logs', '*test', 'test*'], include=['diff_kit', 'diff_kit.*']),
+    package_data={},
+    keywords='diff mysql db',
+    install_requires=install_requires,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
