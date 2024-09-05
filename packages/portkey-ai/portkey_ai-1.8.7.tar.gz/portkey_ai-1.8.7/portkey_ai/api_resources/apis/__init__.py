424fad13062051559669c3f135f15f5103b89b18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 14:04:42 +0000

    Add package portkey-ai/1.8.7/portkey_ai-1.8.7.tar.gz

---
+++ packages/portkey-ai/portkey_ai-1.8.7.tar.gz/portkey_ai-1.8.7/portkey_ai/api_resources/apis/__init__.py
@@ -0,0 +1,130 @@
+from .chat_complete import ChatCompletion, AsyncChatCompletion
+from .complete import Completion, AsyncCompletion
+from .generation import Generations, AsyncGenerations, Prompts, AsyncPrompts
+from .feedback import Feedback, AsyncFeedback
+from .create_headers import createHeaders
+from .post import Post, AsyncPost
+from .embeddings import Embeddings, AsyncEmbeddings
+from .images import Images, AsyncImages
+from .assistants import Assistants, AsyncAssistants
+from .threads import (
+    Threads,
+    Messages,
+    Runs,
+    Steps,
+    AsyncThreads,
+    AsyncMessages,
+    AsyncRuns,
+    AsyncSteps,
+)
+from .main_files import MainFiles, AsyncMainFiles
+from .models import Models, AsyncModels
+from .moderations import Moderations, AsyncModerations
+from .audio import (
+    Audio,
+    Transcriptions,
+    Translations,
+    Speech,
+    AsyncAudio,
+    AsyncTranscriptions,
+    AsyncTranslations,
+    AsyncSpeech,
+)
+from .batches import Batches, AsyncBatches
+from .fine_tuning import (
+    FineTuning,
+    Jobs,
+    Checkpoints,
+    AsyncFineTuning,
+    AsyncJobs,
+    AsyncCheckpoints,
+)
+from .vector_stores import (
+    VectorStores,
+    VectorFiles,
+    VectorFileBatches,
+    AsyncVectorStores,
+    AsyncVectorFiles,
+    AsyncVectorFileBatches,
+)
+
+from .beta_chat import (
+    BetaChat,
+    BetaCompletions,
+    AsyncBetaChat,
+    AsyncBetaCompletions,
+)
+
+from .uploads import (
+    Uploads,
+    Parts,
+    AsyncUploads,
+    AsyncParts,
+)
+
+
+__all__ = [
+    "Completion",
+    "AsyncCompletion",
+    "ChatCompletion",
+    "AsyncChatCompletion",
+    "Generations",
+    "AsyncGenerations",
+    "Feedback",
+    "AsyncFeedback",
+    "Prompts",
+    "AsyncPrompts",
+    "createHeaders",
+    "Post",
+    "AsyncPost",
+    "Embeddings",
+    "AsyncEmbeddings",
+    "Images",
+    "AsyncImages",
+    "Assistants",
+    "AsyncAssistants",
+    "MainFiles",
+    "AsyncMainFiles",
+    "Models",
+    "AsyncModels",
+    "Threads",
+    "AsyncThreads",
+    "Messages",
+    "AsyncMessages",
+    "Runs",
+    "AsyncRuns",
+    "Steps",
+    "AsyncSteps",
+    "Moderations",
+    "AsyncModerations",
+    "Audio",
+    "Transcriptions",
+    "Translations",
+    "Speech",
+    "AsyncAudio",
+    "AsyncTranscriptions",
+    "AsyncTranslations",
+    "AsyncSpeech",
+    "Batches",
+    "AsyncBatches",
+    "FineTuning",
+    "Jobs",
+    "Checkpoints",
+    "AsyncFineTuning",
+    "AsyncJobs",
+    "AsyncCheckpoints",
+    "VectorStores",
+    "VectorFiles",
+    "VectorFileBatches",
+    "AsyncVectorStores",
+    "AsyncVectorFiles",
+    "AsyncVectorFileBatches",
+    "BetaChat",
+    "BetaCompletions",
+    "AsyncBetaChat",
+    "AsyncBetaCompletions",
+    "Uploads",
+    "Parts",
+    "AsyncUploads",
+    "AsyncParts",
+]
