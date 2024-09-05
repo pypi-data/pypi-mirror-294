d9eca2fd1eb5016e2e011f04a7c77b6762ad9448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 15:04:11 +0000

    Add package my-helpers/0.1/my-helpers-0.1.tar.gz

---
+++ packages/my-helpers/my-helpers-0.1.tar.gz/my-helpers-0.1/my-helpers/__init__.py
@@ -0,0 +1,1 @@
+from .my_helpers import files_in_path, path, readfile, readfile_strip, writetofile, file_lines, file_lines_strip, give_callers_path, printp, pprintp, forprint, forprintp, select_folder, select_file
