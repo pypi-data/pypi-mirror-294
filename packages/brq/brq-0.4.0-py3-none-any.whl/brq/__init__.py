44fe9a7b523a0396f19865bf3690e150ebe98c75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 19:04:01 +0000

    Add package brq/0.4.0/brq-0.4.0-py3-none-any.whl

---
+++ packages/brq/brq-0.4.0-py3-none-any.whl/brq/__init__.py
@@ -0,0 +1,21 @@
+"""Top-level package for brq."""
+
+__author__ = "wh1isper"
+__email__ = "9573586@qq.com"
+__version__ = "0.4.0"
+
+
+from .configs import BrqConfig
+from .consumer import Consumer
+from .decorator import task
+from .producer import Producer
+from .tools import get_redis_client, get_redis_url
+
+__all__ = [
+    "task",
+    "Consumer",
+    "Producer",
+    "get_redis_client",
+    "get_redis_url",
+    "BrqConfig",
+]
