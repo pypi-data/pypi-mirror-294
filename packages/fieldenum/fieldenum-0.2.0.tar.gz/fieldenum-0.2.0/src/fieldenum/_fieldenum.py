a5acded3b6ffba9d0c00e2e2522cb9f7a2606320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 21:04:36 +0000

    Add package fieldenum/0.2.0/fieldenum-0.2.0.tar.gz

---
+++ packages/fieldenum/fieldenum-0.2.0.tar.gz/fieldenum-0.2.0/src/fieldenum/_fieldenum.py
@@ -0,0 +1,396 @@
+"""fieldenum core implementation."""
+
+from __future__ import annotations
+
+import copyreg
+import typing
+from contextlib import suppress
+
+from ._utils import NotAllowed, OneTimeSetter, ParamlessSingletonMeta, unpickle
+from .exceptions import unreachable
+
+T = typing.TypeVar("T")
+
+
+class Variant:
+    __slots__ = ("name", "field", "attached", "_slots_names", "_base", "_generics", "_actual", "_defaults", "_default_factories", "_kw_only")
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    def __get__(self, obj, objtype=None) -> typing.Self:
+        if self.attached:
+            # This is needed in order to make match statements work.
+            return self._actual  # type: ignore
+
+        return self
+
+    def kw_only(self) -> typing.Self:
+        self._kw_only = True
+        return self
+
+    # fieldless variant
+    @typing.overload
+    def __init__(self) -> None: ...
+
+    # tuple variant
+    @typing.overload
+    def __init__(self, *tuple_field) -> None: ...
+
+    # named variant
+    @typing.overload
+    def __init__(self, **named_field) -> None: ...
+
+    def __init__(self, *tuple_field, **named_field) -> None:
+        self.attached = False
+        self._kw_only = False
+        self._defaults = {}
+        self._default_factories = {}
+        if tuple_field and named_field:
+            raise TypeError("Cannot mix tuple fields and named fields. Use named fields.")
+        self.field = (tuple_field, named_field)
+        if named_field:
+            self._slots_names = tuple(named_field)
+        else:
+            self._slots_names = tuple(f"_{i}" for i in range(len(tuple_field)))
+
+    if typing.TYPE_CHECKING:
+        def dump(self): ...
+
+    def default(self, **defaults) -> typing.Self:
+        _, named_field = self.field
+        if not named_field:
+            raise TypeError("Only named variants can have defaults.")
+
+        self._defaults = defaults
+        return self
+
+    def default_factory(self, **default_factories) -> typing.Self:
+        _, named_field = self.field
+        if not named_field:
+            raise TypeError("Only named variants can have defaults.")
+
+        self._default_factories = default_factories
+        return self
+
+    def attach(
+        self,
+        cls,
+        /,
+        *,
+        eq: bool,
+        build_hash: bool,
+        frozen: bool,
+    ) -> None | typing.Self:
+        if self.attached:
+            raise TypeError(f"This variants already attached to {self._base.__name__!r}.")
+
+        self._base = cls
+        tuple_field, named_field = self.field
+        if not self._kw_only:
+            named_field_keys = tuple(named_field)
+        item = self
+
+        self._actual: ConstructedVariant
+
+        # fmt: off
+        class ConstructedVariant(cls):
+            if frozen and not typing.TYPE_CHECKING:
+                __slots__ = tuple(f"__original_{name}" for name in item._slots_names)
+                for name in item._slots_names:
+                    # to prevent potential security risk
+                    if name.isidentifier():
+                        exec(f"{name} = OneTimeSetter()")
+                    else:
+                        unreachable(name)
+                        OneTimeSetter()  # Show IDEs that OneTimeSetter is used. Not executed at runtime.
+            else:
+                __slots__ = item._slots_names
+
+        if tuple_field:
+            class TupleConstructedVariant(ConstructedVariant):
+                __name__ = item.name
+                __qualname__ = f"{cls.__qualname__}.{item.name}"
+                __fields__ = tuple(range(len(tuple_field)))
+                __slots__ = ()
+                __match_args__ = item._slots_names
+
+                if build_hash:
+                    __slots__ += ("_hash",)
+
+                    if frozen:
+                        def __hash__(self) -> int:
+                            with suppress(AttributeError):
+                                return self._hash
+
+                            self._hash = hash(self.dump())
+                            return self._hash
+                    else:
+                        __hash__ = None  # type: ignore
+
+                if eq:
+                    def __eq__(self, other: typing.Self):
+                        return type(self) is type(other) and self.dump() == other.dump()
+
+                def __repr__(self) -> str:
+                    values_repr = ", ".join(repr(getattr(self, f"_{name}" if isinstance(name, int) else name)) for name in self.__fields__)
+                    return f"{item._base.__name__}.{self.__name__}({values_repr})"
+
+                @staticmethod
+                def _pickle(variant):
+                    assert isinstance(variant, ConstructedVariant)
+                    return unpickle, (cls, self.name, tuple(getattr(variant, f"_{i}") for i in variant.__fields__))
+
+                def dump(self) -> tuple:
+                    return tuple(getattr(self, f"_{name}") for name in self.__fields__)
+
+                def __init__(self, *args) -> None:
+                    if len(tuple_field) != len(args):
+                        raise TypeError(f"Expect {len(tuple_field)} field(s), but received {len(args)} argument(s).")
+
+                    for name, field, value in zip(item._slots_names, tuple_field, args, strict=True):
+                        setattr(self, name, value)
+
+                    post_init = getattr(self, "__post_init__", lambda: None)
+                    post_init()
+
+            self._actual = TupleConstructedVariant
+
+        elif named_field:
+            class NamedConstructedVariant(ConstructedVariant):
+                __name__ = item.name
+                __qualname__ = f"{cls.__qualname__}.{item.name}"
+                __fields__ = item._slots_names
+                __defaults__ = item._defaults
+                __factories__ = item._default_factories
+                __slots__ = ()
+                if not item._kw_only:
+                    __match_args__ = item._slots_names
+
+                if build_hash:
+                    __slots__ += ("_hash",)
+
+                    if frozen:
+                        def __hash__(self) -> int:
+                            with suppress(AttributeError):
+                                return self._hash
+
+                            self._hash = hash(tuple(self.dump().items()))
+                            return self._hash
+                    else:
+                        __hash__ = None  # type: ignore
+
+                if eq:
+                    def __eq__(self, other: typing.Self):
+                        return type(self) is type(other) and self.dump() == other.dump()
+
+                @staticmethod
+                def _pickle(variant):
+                    assert isinstance(variant, ConstructedVariant)
+                    return unpickle, (cls, self.name, {name: getattr(variant, name) for name in variant.__fields__})
+
+                def dump(self):
+                    return {name: getattr(self, name) for name in self.__fields__}
+
+                if eq:
+                    def __eq__(self, other: typing.Self):
+                        return type(self) is type(other) and self.dump() == other.dump()
+
+                def __repr__(self) -> str:
+                    values_repr = ', '.join(f'{name}={getattr(self, f"_{name}" if isinstance(name, int) else name)!r}' for name in self.__fields__)
+                    return f"{item._base.__name__}.{self.__name__}({values_repr})"
+
+                def __init__(self, *args, **kwargs) -> None:
+                    if args:
+                        if item._kw_only:
+                            raise TypeError(f"Variant '{type(self).__qualname__}' is keyword only.")
+
+                        if len(args) > len(named_field_keys):
+                            raise TypeError(f"{self.__name__} takes {len(named_field_keys)} positional argument(s) but {len(args)} were/was given")
+
+                        # a valid use case of zip without strict=True
+                        for arg, field_name in zip(args, named_field_keys):
+                            if field_name in kwargs:
+                                raise TypeError(f"Inconsistent input for field '{field_name}': received both positional and keyword values")
+                            kwargs[field_name] = arg
+
+                    if self.__defaults__:
+                        kwargs = self.__defaults__ | kwargs
+
+                    if self.__factories__:
+                        for name, factory in self.__factories__.items():
+                            if name not in kwargs:
+                                kwargs[name] = factory()
+
+                    if missed_keys := kwargs.keys() ^ named_field.keys():
+                        raise TypeError(f"Key mismatch: {missed_keys}")
+
+                    for name in named_field:
+                        value = kwargs[name]
+                        # field = named_field[name]
+                        setattr(self, name, value)
+
+                    post_init = getattr(self, "__post_init__", lambda: None)
+                    post_init()
+
+            self._actual = NamedConstructedVariant
+
+        else:
+            class FieldlessConstructedVariant(ConstructedVariant, metaclass=ParamlessSingletonMeta):
+                __name__ = item.name
+                __qualname__ = f"{cls.__qualname__}.{item.name}"
+                __fields__ = ()
+                __slots__ = ()
+
+                if build_hash and not frozen:
+                    __hash__ = None  # type: ignore
+                else:
+                    def __hash__(self):
+                        return hash(id(self))
+
+                @staticmethod
+                def _pickle(variant):
+                    assert isinstance(variant, ConstructedVariant)
+                    return unpickle, (cls, self.name, ())
+
+                def dump(self):
+                    return ()
+
+                def __repr__(self) -> str:
+                    values_repr = ""
+                    return f"{item._base.__name__}.{self.__name__}({values_repr})"
+
+                def __init__(self) -> None:
+                    post_init = getattr(self, "__post_init__", lambda: None)
+                    post_init()
+
+            self._actual = FieldlessConstructedVariant
+        # fmt: on
+
+        copyreg.pickle(self._actual, self._actual._pickle)
+        self.attached = True
+
+    def __call__(self, *args, **kwargs):
+        return self._actual(*args, **kwargs)
+
+
+def variant(cls):
+    fields = cls.__annotations__
+    defaults = {field_name: getattr(cls, field_name) for field_name in fields if hasattr(cls, field_name)}
+    return Variant(**fields).default(**defaults)
+
+
+# MARK: UnitDescriptor
+
+
+class UnitDescriptor:
+    __slots__ = ("name",)
+    __fields__ = ()
+
+    def __init__(self, name: str | None = None):
+        self.name = name
+
+    def __set_name__(self, owner, name):
+        setattr(owner, name, UnitDescriptor(name))
+
+    @typing.overload
+    def __get__(self, obj, objtype: type[T] = ...) -> T: ...  # type: ignore
+
+    @typing.overload
+    def __get__(self, obj, objtype: None = ...) -> typing.Self: ...
+
+    def __get__(self, obj, objtype: type[T] | None = None) -> T | typing.Self:
+        return self
+
+    def attach(
+        self,
+        cls,
+        /,
+        *,
+        eq: bool,  # not needed since nothing to check equality
+        build_hash: bool,
+        frozen: bool,
+    ):
+        if self.name is None:
+            raise TypeError("`self.name` is not set.")
+
+        class UnitConstructedVariant(cls, metaclass=ParamlessSingletonMeta):
+            __name__ = self.name
+            __slots__ = ()
+            __fields__ = None  # `None` means it does not require calling for initialize.
+
+            if build_hash and not frozen:
+                __hash__ = None  # type: ignore # Explicitly disable hash
+            else:
+                def __hash__(self):
+                    return hash(id(self))
+
+            def dump(self):
+                return None
+
+            @staticmethod
+            def _pickle(variant):
+                assert isinstance(variant, UnitConstructedVariant)
+                return unpickle, (cls, self.name, None)
+
+            def __init__(self):
+                pass
+
+            def __repr__(self):
+                return f"{cls.__name__}.{self.__name__}"
+
+        copyreg.pickle(UnitConstructedVariant, UnitConstructedVariant._pickle)
+
+        # This will replace Unit to Specialized instance.
+        setattr(cls, self.name, UnitConstructedVariant())
+
+
+Unit = UnitDescriptor()
+
+
+def fieldenum(
+    cls=None,
+    /,
+    *,
+    eq: bool = True,
+    frozen: bool = True,
+):
+    if cls is None:
+        return lambda cls: fieldenum(
+            cls,
+            eq=eq,
+            frozen=frozen,
+        )
+
+    # Preventing subclassing fieldenums at runtime.
+    # This also prevent double decoration.
+    is_final = False
+    for base in cls.mro()[1:]:
+        with suppress(Exception):
+            if base.__final__:
+                is_final = True
+                break
+    if is_final:
+        raise TypeError(
+            "One of the base classes of fieldenum class is marked as final, "
+            "which means it does not want to be subclassed and it may be fieldenum class, "
+            "which should not be subclassed."
+        )
+
+    class_attributes = vars(cls)
+    has_own_hash = "__hash__" in class_attributes
+    build_hash = eq and not has_own_hash
+
+    for attr in class_attributes.values():
+        if isinstance(attr, Variant | UnitDescriptor):
+            attr.attach(
+                cls,
+                eq=eq,
+                build_hash=build_hash,
+                frozen=frozen,
+            )
+
+    with suppress(Exception):
+        cls.__init__ = NotAllowed("Base fieldenums cannot be initialized.", name="__init__")
+
+    return typing.final(cls)
---
+++ packages/fieldenum/fieldenum-0.2.0.tar.gz/fieldenum-0.2.0/src/fieldenum/_utils.py
@@ -0,0 +1,56 @@
+"""Internal utilities of fieldenum.
+
+This functions and classes are not meant to be used by users,
+which means they can be modified, deleted, or added without notice.
+"""
+
+from __future__ import annotations
+
+
+def unpickle(cls, name: str, state: tuple | dict | None):
+    variant = getattr(cls, name)
+    if state is None:
+        return variant
+    elif isinstance(state, tuple):
+        return variant(*state)
+    else:
+        return variant(**state)
+
+
+class NotAllowed:
+    def __init__(self, message: str | None = None, name: str | None = None):
+        self.error_message = message
+        if name is not None:
+            self.name = name
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    def __get__(self, obj, objtype=None):
+        if objtype is None:
+            return self
+
+        raise TypeError(self.error_message or f"The method/attribute {self.name!r} is not allowed to be used.")
+
+
+class OneTimeSetter:
+    def __set_name__(self, owner, name):
+        self.private_name = f"__original_{name}"
+
+    def __get__(self, obj, objtype=None):
+        return getattr(obj, self.private_name)
+
+    def __set__(self, obj, value):
+        if hasattr(obj, self.private_name):
+            raise TypeError(f"Field `{self.private_name.lstrip('__original_')}` is frozen thus cannot mutate.")
+        setattr(obj, self.private_name, value)
+
+
+class ParamlessSingletonMeta(type):
+    """Singleton implementation for class that does not have any parameter."""
+    _instance = None
+
+    def __call__(cls):
+        if cls._instance is None:
+            cls._instance = super().__call__()
+        return cls._instance
