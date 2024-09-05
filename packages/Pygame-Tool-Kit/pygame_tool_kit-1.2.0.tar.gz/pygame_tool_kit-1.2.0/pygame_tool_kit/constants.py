d934e44a7c11e343ad6ee6288483c83f1a5ecd1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 19:04:23 +0000

    Add package Pygame-Tool-Kit/1.2.0/pygame_tool_kit-1.2.0.tar.gz

---
+++ packages/Pygame-Tool-Kit/pygame_tool_kit-1.2.0.tar.gz/pygame_tool_kit-1.2.0/pygame_tool_kit/constants.py
@@ -0,0 +1,34 @@
+"""
+This module contains the main constants used throughout the Pygame Tool Kit library.
+"""
+
+from .events_manager import EVENTS_MANAGER
+"""
+The events manager constant is used to manage events in the game.
+"""
+
+from .storage_manager import STORAGE_MANAGER
+"""
+The storage manager constant is used to save and load game data from JSON files.
+"""
+
+from .delta_time import DELTA_TIME
+"""
+The delta time constant is used to get the time in seconds since the last frame.
+"""
+
+from .keyboard import KEYBOARD
+"""
+The keyboard constant is used to get the state of the keyboard (pressed, held, released keys).
+"""
+
+from .display import DISPLAY
+"""
+The display constant is used to get the display surface and other display-related information.
+"""
+
+from .mouse import MOUSE
+"""
+The mouse constant is used to get the state of the mouse (position, buttons pressed, held, released).
+"""
+
