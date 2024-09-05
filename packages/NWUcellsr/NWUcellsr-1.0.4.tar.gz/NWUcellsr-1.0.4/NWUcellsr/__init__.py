01fc364bff62908be346c92bd5d82cedccca24fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 21:04:48 +0000

    Add package NWUcellsr/1.0.4/NWUcellsr-1.0.4.tar.gz

---
+++ packages/NWUcellsr/NWUcellsr-1.0.4.tar.gz/NWUcellsr-1.0.4/NWUcellsr/__init__.py
@@ -0,0 +1,10 @@
+# NWUcellsr/__init__.py
+
+# 导入包内的主要模块和功能
+from NWUcellsr import craft_arch
+from NWUcellsr import model
+from NWUcellsr import api
+from NWUcellsr import app
+from NWUcellsr import sr
+from NWUcellsr import version
+
---
+++ packages/NWUcellsr/NWUcellsr-1.0.4.tar.gz/NWUcellsr-1.0.4/NWUcellsr/version.py
@@ -0,0 +1,5 @@
+# GENERATED VERSION FILE
+# TIME: Thu Sep  5 16:20:05 2024
+__version__ = '1.0.3'
+__gitsha__ = 'unknown'
+version_info = (1, 0, 3)
