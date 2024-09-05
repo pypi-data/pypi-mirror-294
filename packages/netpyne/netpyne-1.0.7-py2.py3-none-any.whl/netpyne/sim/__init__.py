b9989d7126924964f487fb98f44d3879ce6657bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 14:03:43 +0000

    Add package netpyne/1.0.7/netpyne-1.0.7-py2.py3-none-any.whl

---
+++ packages/netpyne/netpyne-1.0.7-py2.py3-none-any.whl/netpyne/sim/__init__.py
@@ -0,0 +1,118 @@
+"""
+Package for handling simulations
+
+Contains all the model shared variables and modules.  It is imported as "sim" from all other files, so any variable or module can be referenced from any module using `sim.varName`.
+
+"""
+
+# check for -nogui option
+import sys
+if '-nogui' in sys.argv:
+    import netpyne
+
+    netpyne.__gui__ = False
+from neuron import h
+
+
+# ------------------------------------------------------------------------------
+# Import simulation-related functions from this subpackage (/sim)
+# ------------------------------------------------------------------------------
+
+# import setup functions
+from .setup import (
+    initialize,
+    setNet,
+    setNetParams,
+    setSimCfg,
+    createParallelContext,
+    readCmdLineArgs,
+    setupRecording,
+    setupRecordLFP,
+    setGlobals,
+)
+
+# import run functions
+from .run import preRun, runSim, runSimWithIntervalFunc, loadBalance, calculateLFP, calculateDipole
+
+# import gather functions
+from .gather import gatherData, _gatherAllCellTags, _gatherAllCellConnPreGids, _gatherCells, gatherDataFromFiles
+
+# import saving functions
+from .save import saveJSON, saveData, distributedSaveHDF5, compactConnFormat, intervalSave, saveDataInNodes, saveModel
+
+# import loading functions
+from .load import (
+    loadSimCfg,
+    loadNetParams,
+    loadNet,
+    loadSimData,
+    loadAll,
+    loadHDF5,
+    ijsonLoad,
+    loadModel,
+    loadFromIndexFile,
+)
+
+# import utils functions (general)
+from .utils import (
+    loadPythonModule,
+    cellByGid,
+    getCellsList,
+    timing,
+    version,
+    gitChangeset,
+    hashStr,
+    hashList,
+    _init_stim_randomizer,
+    unique,
+    checkMemory,
+)
+
+# import utils functions to manipulate objects
+from .utils import copyReplaceItemObj, copyRemoveItemObj, replaceFuncObj, replaceDictODict, rename, clearObj, clearAll
+
+# import wrapper functions
+from .wrappers import (
+    create,
+    simulate,
+    intervalSimulate,
+    distributedSimulate,
+    analyze,
+    createSimulate,
+    createSimulateAnalyze,
+    createSimulateAnalyzeInterval,
+    createSimulateAnalyzeDistributed,
+    runFromIndexFile,
+    load,
+    loadSimulate,
+    loadSimulateAnalyze,
+    createExportNeuroML2,
+    importNeuroML2SimulateAnalyze,
+    runSimIntervalSaving,
+)
+
+
+# ------------------------------------------------------------------------------
+# Import classes and functions from other subpackages (so available via sim)
+# ------------------------------------------------------------------------------
+
+# import cell classes
+from ..cell import CompartCell, PointCell, NML2Cell, NML2SpikeSource
+
+# import Network and Pop classes
+from ..network import Network, Pop
+
+# import analysis-related module
+from .. import analysis
+
+# import plotting-related module
+from .. import plotting
+
+# import testing related functions
+from .. import tests
+from ..tests.checks import checkOutput
+from ..tests.tests import SimTestObj
+
+# import export/import-related functions
+from .. import conversion
+from ..conversion.neuromlFormat import *
---
+++ packages/netpyne/netpyne-1.0.7-py2.py3-none-any.whl/netpyne/specs/__init__.py
@@ -0,0 +1,8 @@
+"""
+Package of classes to define high-level model specifications
+
+"""
+
+from .dicts import Dict, ODict
+from .netParams import NetParams, CellParams
+from .simConfig import SimConfig
