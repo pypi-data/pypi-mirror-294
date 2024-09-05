452bc03e07fff41e18a039cf0b8e9557c889583e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 20:04:53 +0000

    Add package robust-fourier/0.1b0/robust_fourier-0.1b0.tar.gz

---
+++ packages/robust-fourier/robust_fourier-0.1b0.tar.gz/robust_fourier-0.1b0/src/robust_fourier/_utils/__init__.py
@@ -0,0 +1,28 @@
+"""
+Module :mod:`_utils`
+
+This module provides utility functionalities that are used throughout the package, e.g.,
+
+- handling of Numba-related tasks
+
+"""
+
+# === Imports ===
+
+from ._numba_helpers import (  # noqa: F401
+    NUMBA_NO_JIT_ARGV,
+    NUMBA_NO_JIT_ENV_KEY,
+    NumbaJitActions,
+    do_numba_normal_jit_action,
+    no_jit,
+)
+from ._validate import (  # noqa: F401
+    IntScalar,
+    RealScalar,
+    get_validated_alpha,
+    get_validated_chebpoly_or_hermfunc_input,
+    get_validated_grid_points,
+    get_validated_offset_along_axis,
+    get_validated_order,
+)
+from ._x_preprocessing import normalise_x_values  # noqa: F401
