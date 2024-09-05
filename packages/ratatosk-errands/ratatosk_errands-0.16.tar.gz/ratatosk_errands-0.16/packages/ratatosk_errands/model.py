3db42284c5bc0bc48096066c5c02a3ca1d780746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 19:04:13 +0000

    Add package ratatosk-errands/0.16/ratatosk_errands-0.16.tar.gz

---
+++ packages/ratatosk-errands/ratatosk_errands-0.16.tar.gz/ratatosk_errands-0.16/packages/ratatosk_errands/model.py
@@ -0,0 +1,50 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+class DiffusionInstructions(BaseModel):
+    prompt: str
+    image_identifier: str
+    negative_prompt: str | None = None
+    num_inference_steps: int | None = None
+    guidance_scale: float | None = None
+
+
+class ImageToImageInstructions(DiffusionInstructions):
+    base_image_identifier: str
+
+
+class TextToImageInstructions(DiffusionInstructions):
+    width: int | None = None
+    height: int | None = None
+
+
+class ChatInstructions(BaseModel):
+    prompt: str
+    history: List[str] = []
+    system_instructions: str | None = None
+    max_new_tokens: int | None = None
+    temperature: float | None = None
+    repetition_penalty: float | None = None
+
+
+class Errand(BaseModel):
+    instructions: TextToImageInstructions | ImageToImageInstructions | ChatInstructions
+    origin: str
+    destination: str
+    errand_identifier: str
+    timestamp: float
+
+
+class DiffusionReply(BaseModel):
+    image_identifier: str
+
+
+class ChatReply(BaseModel):
+    message: str
+
+
+class Echo(BaseModel):
+    errand: Errand
+    reply: DiffusionReply | ChatReply
