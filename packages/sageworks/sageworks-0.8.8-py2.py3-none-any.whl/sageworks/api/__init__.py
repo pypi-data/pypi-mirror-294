caced30dfe0e80ef33ec9fe22071e92e6fd80919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 17:04:07 +0000

    Add package sageworks/0.8.8/sageworks-0.8.8-py2.py3-none-any.whl

---
+++ packages/sageworks/sageworks-0.8.8-py2.py3-none-any.whl/sageworks/api/__init__.py
@@ -0,0 +1,18 @@
+"""Welcome to the SageWorks API Classes
+
+These class provide high-level APIs for the SageWorks package, offering easy access to its core classes:
+
+- DataSource: Manages AWS Data Catalog and Athena
+- FeatureSet: Manages AWS Feature Store and Feature Groups
+- Model: Manages the training and deployment of AWS Model Groups and Packages
+- Endpoint: Manages the deployment and invocations/inference on AWS Endpoints
+- Meta: Provides an API to retrieve AWS Metadata for the above classes
+"""
+
+from .data_source import DataSource
+from .feature_set import FeatureSet
+from .model import Model, ModelType
+from .endpoint import Endpoint
+from .meta import Meta
+
+__all__ = ["DataSource", "FeatureSet", "Model", "ModelType", "Endpoint", "Meta"]
