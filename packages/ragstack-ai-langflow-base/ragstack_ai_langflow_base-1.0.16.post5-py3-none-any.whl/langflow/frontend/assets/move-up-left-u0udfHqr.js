0b54db943180c7e073e2781d3599597c4650107e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 19:04:26 +0000

    Add package ragstack-ai-langflow-base/1.0.16.post5/ragstack_ai_langflow_base-1.0.16.post5-py3-none-any.whl

---
+++ packages/ragstack-ai-langflow-base/ragstack_ai_langflow_base-1.0.16.post5-py3-none-any.whl/langflow/frontend/assets/move-up-left-u0udfHqr.js
@@ -0,0 +1,6 @@
+import{a1 as e}from"./index-ByLTX0S6.js";/**
+ * @license lucide-react v0.395.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=e("MoveUpLeft",[["path",{d:"M5 11V5H11",key:"3q78g9"}],["path",{d:"M5 5L19 19",key:"5zm2fv"}]]);export{a as default};
