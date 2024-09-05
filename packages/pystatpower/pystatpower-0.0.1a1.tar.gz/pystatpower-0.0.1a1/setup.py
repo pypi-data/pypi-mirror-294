3e0b11916f4f61ef9f59bcf07acc55dcfff00867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 19:04:00 +0000

    Add package pystatpower/0.0.1a1/pystatpower-0.0.1a1.tar.gz

---
+++ packages/pystatpower/pystatpower-0.0.1a1.tar.gz/pystatpower-0.0.1a1/setup.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+from setuptools import setup, find_packages
+
+load_dotenv()
+
+author_email = os.getenv("SETUP_AUTHOR_EMAIL", "default@example.com")
+
+setup(
+    name="pystatpower",
+    version="0.0.1-a1",
+    packages=find_packages(include=["pystatpower", "pystatpower.*"]),
+    install_requires=["scipy==1.14.1"],
+    author="Snoopy1866",
+    author_email=author_email,
+    description="A Power Analysis Toolkit for Python",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/PyStatPower/PyStatPower",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
+    python_requires=">=3.12",
+)
