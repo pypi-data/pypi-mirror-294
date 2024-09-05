6dc1d42ee95d7bfafd53af69ffc8069018f0ccc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 16:04:38 +0000

    Add package dataforseo-client/1.0.31/dataforseo_client-1.0.31-py3-none-any.whl

---
+++ packages/dataforseo-client/dataforseo_client-1.0.31-py3-none-any.whl/dataforseo_client/api/__init__.py
@@ -0,0 +1,26 @@
+import re
+import importlib
+
+def camel_to_snake(name):
+    name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
+
+def __getattr__(name):
+    file_name = camel_to_snake(name)
+
+    modules_to_try = [
+        'dataforseo_client.api',
+        'dataforseo_client',
+        'dataforseo_client.models',
+    ]
+
+    for module in modules_to_try:
+        try:
+            module = importlib.import_module(f'{module}.{file_name}')
+            model = getattr(module, name)
+            globals()[name] = model
+            return model
+        except:
+            continue
+    
+    raise ImportError(f"Cannot find {name} in any of the specified modules")
