0b926f9dc310e79a6d6e5e45eae30e7df1403313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 14:04:57 +0000

    Add package changedetection.io/0.46.4/changedetection.io-0.46.4-py3-none-any.whl

---
+++ packages/changedetection.io/changedetection.io-0.46.4-py3-none-any.whl/changedetectionio/static/js/watch-settings.js
@@ -0,0 +1,31 @@
+function toggleOpacity(checkboxSelector, fieldSelector, inverted) {
+    const checkbox = document.querySelector(checkboxSelector);
+    const fields = document.querySelectorAll(fieldSelector);
+    function updateOpacity() {
+        const opacityValue = !checkbox.checked ? (inverted ? 0.6 : 1) : (inverted ? 1 : 0.6);
+        fields.forEach(field => {
+            field.style.opacity = opacityValue;
+        });
+    }
+    // Initial setup
+    updateOpacity();
+    checkbox.addEventListener('change', updateOpacity);
+}
+
+$(document).ready(function () {
+    $('#notification-setting-reset-to-default').click(function (e) {
+        $('#notification_title').val('');
+        $('#notification_body').val('');
+        $('#notification_format').val('System default');
+        $('#notification_urls').val('');
+        e.preventDefault();
+    });
+    $("#notification-token-toggle").click(function (e) {
+        e.preventDefault();
+        $('#notification-tokens-info').toggle();
+    });
+
+    toggleOpacity('#time_between_check_use_default', '#time_between_check', false);
+
+});
+
