d351409ad92e4d930657a6e616c5352349d475b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 20:05:04 +0000

    Add package tensor-task/0.1.2/tensor_task-0.1.2.tar.gz

---
+++ packages/tensor-task/tensor_task-0.1.2.tar.gz/tensor_task-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tensor_task',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[],
+    author='jancey',
+    author_email='jancey.email@example.com',
+    description='A simple Python module with a test function',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
