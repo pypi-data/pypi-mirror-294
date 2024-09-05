7700b3f3a5c500296c40b128be7913f430f5ee07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 21:04:40 +0000

    Add package bookio-fetchfox/3.4.2/bookio_fetchfox-3.4.2.tar.gz

---
+++ packages/bookio-fetchfox/bookio_fetchfox-3.4.2.tar.gz/bookio_fetchfox-3.4.2/fetchfox/constants/currencies.py
@@ -0,0 +1,32 @@
+# Cryptos
+ALGO = "ALGO"
+ADA = "ADA"
+ETH = "ETH"
+MATIC = "MATIC"
+
+# Tokens
+BEARD = "BEARD"
+BOOK = "BOOK"
+CHARLES = "CHARLES"
+STUFF = "STUFF"
+
+# Fiat
+USD = "USD"
+
+
+CRYPTOS = [
+    # cryptocurrencies
+    ALGO,
+    ADA,
+    ETH,
+    MATIC,
+    # cardano tokens
+    BEARD,
+    BOOK,
+    CHARLES,
+    STUFF,
+]
+
+FIATS = [
+    USD,
+]
