39191241fb21b68aa094ee82765a53643c08ae03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 21:04:50 +0000

    Add package bdcp/1.0.8/bdcp-1.0.8-py3-none-any.whl

---
+++ packages/bdcp/bdcp-1.0.8-py3-none-any.whl/bdcp/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+__version__ = "1.0.0"
+
+
+from . import connections 
+Connect = connect = Connection =connections.Connection
+
+__all__ = [
+    "Connect",
+    "Connection",
+    "connect",
+    "__version__",
+]
