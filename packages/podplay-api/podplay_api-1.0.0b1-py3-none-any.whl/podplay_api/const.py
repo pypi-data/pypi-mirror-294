5633fb50dea26c24e1e4f9eb9f00fae234720acb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 18:04:47 +0000

    Add package podplay-api/1.0.0b1/podplay_api-1.0.0b1-py3-none-any.whl

---
+++ packages/podplay-api/podplay_api-1.0.0b1-py3-none-any.whl/podplay_api/const.py
@@ -0,0 +1,9 @@
+"""podplay_api constants."""
+import logging
+
+PODPLAY_USER_AGENT = "podplay_api/0.0.0"
+PODPLAY_API_URL = "https://api.podplay.com/v1"
+
+TIMEOUT = 10
+
+LOGGER = logging.getLogger(__package__)
