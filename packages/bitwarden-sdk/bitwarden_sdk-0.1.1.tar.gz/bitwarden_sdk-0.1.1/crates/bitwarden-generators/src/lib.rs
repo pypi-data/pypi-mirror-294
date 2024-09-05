4291104582f21674dd066ee18eddc5c6871d36f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 18:04:24 +0000

    Add package bitwarden-sdk/0.1.1/bitwarden_sdk-0.1.1.tar.gz

---
+++ packages/bitwarden-sdk/bitwarden_sdk-0.1.1.tar.gz/bitwarden_sdk-0.1.1/crates/bitwarden-generators/src/lib.rs
@@ -0,0 +1,13 @@
+mod passphrase;
+pub use passphrase::{passphrase, PassphraseError, PassphraseGeneratorRequest};
+mod password;
+mod util;
+pub use password::{password, PasswordError, PasswordGeneratorRequest};
+mod username;
+pub use username::{username, ForwarderServiceType, UsernameError, UsernameGeneratorRequest};
+mod client_generator;
+pub use client_generator::{ClientGenerator, ClientGeneratorExt};
+mod username_forwarders;
+
+#[cfg(feature = "uniffi")]
+uniffi::setup_scaffolding!();
