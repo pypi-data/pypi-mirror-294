6b4acd610d72be8eb7c17bc5dad34a79b791185b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 16:04:44 +0000

    Add package tecton-parallel-retrieval/0.1.0b6/tecton_parallel_retrieval-0.1.0b6.tar.gz

---
+++ packages/tecton-parallel-retrieval/tecton_parallel_retrieval-0.1.0b6.tar.gz/tecton_parallel_retrieval-0.1.0b6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tecton_parallel_retrieval',
+    version='0.1.0b6',
+    description='[private preview] Parallel feature retrieval for Tecton',
+    author='Tecton',
+    packages=find_packages(),
+    license="Apache License 2.0",
+    install_requires=[
+        "tecton>=0.9",
+    ],
+    extras_require={
+        'spark': ["databricks-sdk", "pyspark"],
+    },
+    setup_requires=["setuptools", "wheel"],
+    url="https://tecton.ai",
+    python_requires=">=3.7",
+)
