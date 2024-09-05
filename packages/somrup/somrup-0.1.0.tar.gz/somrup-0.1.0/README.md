de2a612225cad8b56e62b4626d9375529ad073a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 15:04:33 +0000

    Add package somrup/0.1.0/somrup-0.1.0.tar.gz

---
+++ packages/somrup/somrup-0.1.0.tar.gz/somrup-0.1.0/README.md
@@ -0,0 +1,6 @@
+# simplemath
+## Publishing my own python package - <u>simplemath</u>
+### Installation
+```
+pip install simplemath
+``` 
---
+++ packages/somrup/somrup-0.1.0.tar.gz/somrup-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="somrup",
+    version="0.1.0",
+    author="Saurav Sinha",
+    description="A simple math operations package",
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
