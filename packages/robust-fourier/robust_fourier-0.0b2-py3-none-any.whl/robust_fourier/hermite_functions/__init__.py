98957aa70eeee1ef4f6c1dc40e7c7d0227e88d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 17:04:14 +0000

    Add package robust-fourier/0.0b2/robust_fourier-0.0b2-py3-none-any.whl

---
+++ packages/robust-fourier/robust_fourier-0.0b2-py3-none-any.whl/robust_fourier/hermite_functions/__init__.py
@@ -0,0 +1,28 @@
+"""
+Module :mod:`hermite_functions`
+
+This module provides  implementations of the Hermite functions which are essential for
+the robust Fourier transform.
+The ``n``-th dilated and shifted Hermite function is given by
+
+.. image:: docs/hermite_functions/equations/HF-01-Hermite_Functions_TimeSpace_Domain.svg
+
+where :math:`H_{n}` is the :math:`n`-th Hermite polynomial
+
+.. image:: docs/hermite_functions/equations/HF-02-Hermite_Polynomials_TimeSpace_Domain.svg
+
+They have two nice properties:
+- they are orthogonal
+- they are eigenfunctions of the Fourier transform which makes them suitable for the
+    robust Fourier transform by Least Squares Fits.
+
+"""  # noqa: E501
+
+# === Imports ===
+
+from . import _approximations as hermite_approx  # noqa: F401
+from ._class_interface import HermiteFunctionBasis  # noqa: F401
+from ._func_interface import (  # noqa: F401
+    hermite_function_vander,
+    single_hermite_function,
+)
---
+++ packages/robust-fourier/robust_fourier-0.0b2-py3-none-any.whl/robust_fourier/hermite_functions/_numba_funcs.py
@@ -0,0 +1,42 @@
+"""
+Module :mod:`hermite_functions._numba_funcs`
+
+This module provides Numba-based implementations of the Hermite functions.
+
+Depending on the runtime availability of Numba, the functions are either compiled or
+imported from the NumPy-based implementation.
+
+"""
+
+# === Imports ===
+
+from .._utils._numba_helpers import do_numba_normal_jit_action
+from ._numpy_funcs import _hermite_function_vander
+
+# === Functions ===
+
+# NOTE: here are not functions because the NumPy-based implementation was written to be
+#       compatible with Numba ``jit``-compilation
+
+
+# === Compilation ===
+
+# if available/enabled, the functions are compiled by Numba
+try:
+    if do_numba_normal_jit_action:  # pragma: no cover
+        from numba import jit
+    else:
+        from .._utils import no_jit as jit
+
+    # if it is enabled, the functions are compiled
+    nb_hermite_function_vander = jit(
+        nopython=True,
+        cache=True,
+    )(_hermite_function_vander)
+
+
+# otherwise, the NumPy-based implementation of the Hermite functions is declared as the
+# Numba-based implementation
+except ImportError:  # pragma: no cover
+
+    nb_hermite_function_vander = _hermite_function_vander
