6a6aa9ab7b0173340efa0ced44cc56338c3c840c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 20:05:00 +0000

    Add package burr/0.30.0rc2/burr-0.30.0rc2-py3-none-any.whl

---
+++ packages/burr/burr-0.30.0rc2-py3-none-any.whl/burr/core/typing.py
@@ -0,0 +1,80 @@
+from __future__ import annotations
+
+import abc
+from typing import TYPE_CHECKING, Generic, Type, TypeVar
+
+BaseType = TypeVar("BaseType")
+# SpecificType = TypeVar('SpecificType', bound=BaseType)
+
+if TYPE_CHECKING:
+    from burr.core import Action, Graph, State
+
+try:
+    from typing import Self
+except ImportError:
+    Self = "TypingSystem"
+
+
+class TypingSystem(abc.ABC, Generic[BaseType]):
+    @abc.abstractmethod
+    def state_type(self) -> Type[BaseType]:
+        """Gives the type that represents the state of the
+        application at any given time. Note that this must have
+        adequate support for Optionals (E.G. non-required values).
+
+        :return:
+        """
+
+    @abc.abstractmethod
+    def state_pre_action_run_type(self, action: Action, graph: Graph) -> Type[BaseType]:
+        """Gives the type that represents the state after an action has completed.
+        Note that this could be smart -- E.g. it should have all possible upstream
+        types filled in.
+
+        :param action:
+        :return:
+        """
+
+    @abc.abstractmethod
+    def state_post_action_run_type(self, action: Action, graph: Graph) -> Type[BaseType]:
+        """Gives the type that represents the state after an action has completed.
+        Note that this could be smart -- E.g. it should have all possible upstream
+        types filled in.
+
+        :param action:
+        :return:
+        """
+
+    def validate_state(self, state: State) -> None:
+        """Validates the state to ensure it is valid.
+
+        :param state:
+        :return:
+        """
+        pass
+
+    @abc.abstractmethod
+    def construct_data(self, state: State[BaseType]) -> BaseType:
+        """Constructs a type based on the arguments passed in.
+
+        :param kwargs:
+        :return:
+        """
+        raise NotImplementedError("TODO -- implement this")
+
+
+class DictBasedTypingSystem(TypingSystem[dict]):
+    """Effectively a no-op. State is backed by a dictionary, which allows every state item
+    to... be a dictionary."""
+
+    def state_type(self) -> Type[dict]:
+        return dict
+
+    def state_pre_action_run_type(self, action: Action, graph: Graph) -> Type[dict]:
+        return dict
+
+    def state_post_action_run_type(self, action: Action, graph: Graph) -> Type[dict]:
+        return dict
+
+    def construct_data(self, state: State[BaseType]) -> dict:
+        return state.get_all()
