4a2c565226c21cb21d557451097f8eac00369b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-05 16:04:16 +0000

    Add package frapars/0.2.0/frapars-0.2.0.tar.gz

---
+++ packages/frapars/frapars-0.2.0.tar.gz/frapars-0.2.0/frapars/constants/words.py
@@ -0,0 +1,8 @@
+urban_names = ["grand rue", "rue", "av", "avenue", "place", "allée", "déviation", "RN", "Lieu Dit",
+               "allee", "chemin", "impasse", "boulevard", "quai", "promenade", "route", "passage", "voie", "avenue", "cours", ]
+
+# keeps the longer as priority
+prepositions = ["de la", "de l'", "du", "de", "da", "des", "d'"]
+
+months = ["janvier", "f[eé]vrier", "mars", "avril", "mai", "juin",
+          "juillet", "ao[uû]t", "septembre", "octobre", "novembre", "d[eé]cembre"]
