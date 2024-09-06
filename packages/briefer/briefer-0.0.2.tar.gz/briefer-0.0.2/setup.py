c1df327cad30a843fc8237326d3a395b80f3d70f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 04:05:03 +0000

    Add package briefer/0.0.2/briefer-0.0.2.tar.gz

---
+++ packages/briefer/briefer-0.0.2.tar.gz/briefer-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="briefer",
+    version="0.0.2",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        "console_scripts": [
+            "briefer=briefer.__main__:main",
+        ],
+    },
+    classifiers=[
+        # TODO
+    ],
+    python_requires='>=3.9',
+)
