02513acf534f44b95cd98429ed3343143625eb78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 14:06:02 +0000

    Add package quantsumore/1.1.1b1/quantsumore-1.1.1b1-py3-none-any.whl

---
+++ packages/quantsumore/quantsumore-1.1.1b1-py3-none-any.whl/quantsumore/api/crypto/__init__.py
@@ -0,0 +1,11 @@
+## Exchanges
+
+# from .CryptoAPI import engine as crypto_engine
+# from .EquityAPI import engine as equity_engine
+# from .ForexAPI import engine as forex_engine
+# 
+# # from .exchanges.CryptoAPI import engine as crypto_engine
+# # from .exchanges.EquityAPI import engine as equity_engine
+# # from .exchanges.ForexAPI import engine as forex_engine
+# 
+# __all__ = ['crypto_engine', 'equity_engine', 'forex_engine']
