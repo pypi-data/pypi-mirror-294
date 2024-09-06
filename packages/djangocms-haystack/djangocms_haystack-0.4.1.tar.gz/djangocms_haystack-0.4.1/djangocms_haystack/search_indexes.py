8a90f8ca758a8999c79a97269ae3e5454b0b5249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 07:04:57 +0000

    Add package djangocms-haystack/0.4.1/djangocms_haystack-0.4.1.tar.gz

---
+++ packages/djangocms-haystack/djangocms_haystack-0.4.1.tar.gz/djangocms_haystack-0.4.1/djangocms_haystack/search_indexes.py
@@ -0,0 +1,29 @@
+from cms.models import PageContent
+from django.db.models import Q
+from haystack import indexes
+
+from djangocms_haystack.base import DjangoCMSSearchIndexBase
+
+
+class PageContentIndex(DjangoCMSSearchIndexBase, indexes.Indexable):
+    def get_url(self, instance):
+        return instance.page.get_absolute_url()
+
+    def get_title(self, instance):
+        return instance.title
+
+    def get_description(self, instance):
+        return instance.meta_description or ""
+
+    def get_model(self):
+        return PageContent
+
+    def get_index_queryset(self, language):
+        return PageContent.objects.filter(
+            Q(redirect__exact="") | Q(redirect__isnull=True),
+            language=language,
+        )
+
+    def should_update(self, instance, **kwargs):
+        # Instantly update the index on pagecontent changes
+        return True
