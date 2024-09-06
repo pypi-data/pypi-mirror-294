0940209552c3dc599dbb23559fbc3609eb73b2de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 14:06:41 +0000

    Add package LiamSpiderToolkit/0.0.11/liamspidertoolkit-0.0.11.tar.gz

---
+++ packages/LiamSpiderToolkit/liamspidertoolkit-0.0.11.tar.gz/liamspidertoolkit-0.0.11/LiamSpiderToolkit/__init__.py
@@ -0,0 +1,11 @@
+# from .moudle1 import hello_world
+from .Proxy_Pool import get_proxies
+
+
+# 现在，你也可以在__init__.py中定义其他包级别的函数
+def another_function():
+    print("This is another function in the package.")
+
+# 如果你还想导入整个module1模块，也可以这样做
+# from . import module1
+
