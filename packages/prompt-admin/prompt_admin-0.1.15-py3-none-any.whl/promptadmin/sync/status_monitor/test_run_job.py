4e6312773cb19e034279d472d19c312681eb6d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 16:05:29 +0000

    Add package prompt-admin/0.1.15/prompt_admin-0.1.15-py3-none-any.whl

---
+++ packages/prompt-admin/prompt_admin-0.1.15-py3-none-any.whl/promptadmin/sync/status_monitor/test_run_job.py
@@ -0,0 +1,17 @@
+import asyncio
+
+from promptadmin.sync.status_monitor.test_run_service import TestRunService
+from settings import SETTINGS
+
+
+class TestRunJob:
+    def __init__(self, test_run_service: TestRunService):
+        self.test_run_service = test_run_service or TestRunService()
+
+    async def start(self):
+        while True:
+            self.test_run_service.run_test()
+            await asyncio.sleep(SETTINGS.test_rerun_timeout)
+
+    async def stop(self):
+        pass
