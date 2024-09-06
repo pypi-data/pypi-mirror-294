9bdae127ae88e344642635eb2fe1ef072068b800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 15:04:51 +0000

    Add package lfedu/0.0.1/lfedu-0.0.1.tar.gz

---
+++ packages/lfedu/lfedu-0.0.1.tar.gz/lfedu-0.0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name="lfedu",
+    version="0.0.1",
+    author="cangyun",
+    author_email="4uansi@gmail.com",
+    description="Program the drone operating library",
+    packages=["drone"],
+    install_requires=[],
+    # entry_points={"console_scripts": ["ampt2=ampt2:ampt2"]},
+)
