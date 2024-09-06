95c4cd807f8d24010e7bfe82a0fb82d1020a9093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 05:05:08 +0000

    Add package temperatura/1.0.0/temperatura-1.0.0.tar.gz

---
+++ packages/temperatura/temperatura-1.0.0.tar.gz/temperatura-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = 'temperatura',
+    version = '1.0.0',
+    author = 'Lissandro',
+    packages = ['temperatura'],
+    description = 'Um simples conversor de temperatura (Celsius - Fahrenheit)',
+    long_description = 'Um simples conversor de temperatura, com funções para '
+                        + 'conversão de Celsius para Fahrenheit e vice-versa, '
+                        + 'usado para um post no Blog da Alura',
+    url="https://github.com/lissandro1/package-template.git",
+    license = 'MIT'
+)
