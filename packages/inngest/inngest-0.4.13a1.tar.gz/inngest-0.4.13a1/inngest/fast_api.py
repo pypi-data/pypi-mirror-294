a8c1059d1b9d27af694c92e6f3b49fb8e3b16637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 14:08:01 +0000

    Add package inngest/0.4.13a1/inngest-0.4.13a1.tar.gz

---
+++ packages/inngest/inngest-0.4.13a1.tar.gz/inngest-0.4.13a1/inngest/fast_api.py
@@ -0,0 +1,111 @@
+"""FastAPI integration for Inngest."""
+
+import json
+import typing
+
+import fastapi
+
+from ._internal import client_lib, comm_lib, function, server_lib, transforms
+
+FRAMEWORK = server_lib.Framework.FAST_API
+
+
+def serve(
+    app: fastapi.FastAPI,
+    client: client_lib.Inngest,
+    functions: list[function.Function],
+    *,
+    serve_origin: typing.Optional[str] = None,
+    serve_path: typing.Optional[str] = None,
+) -> None:
+    """
+    Serve Inngest functions in a FastAPI app.
+
+    Args:
+    ----
+        app: FastAPI app.
+        client: Inngest client.
+        functions: List of functions to serve.
+
+        serve_origin: Origin to serve the functions from.
+        serve_path: Path to serve the functions from.
+    """
+
+    handler = comm_lib.CommHandler(
+        client=client,
+        framework=FRAMEWORK,
+        functions=functions,
+    )
+
+    @app.get("/api/inngest")
+    async def get_api_inngest(
+        request: fastapi.Request,
+    ) -> fastapi.Response:
+        return _to_response(
+            client,
+            handler.get_sync(
+                comm_lib.CommRequest(
+                    body=await request.body(),
+                    headers=dict(request.headers.items()),
+                    query_params=dict(request.query_params.items()),
+                    raw_request=request,
+                    request_url=str(request.url),
+                    serve_origin=serve_origin,
+                    serve_path=serve_path,
+                )
+            ),
+        )
+
+    @app.post("/api/inngest")
+    async def post_inngest_api(
+        request: fastapi.Request,
+    ) -> fastapi.Response:
+        return _to_response(
+            client,
+            await handler.post(
+                comm_lib.CommRequest(
+                    body=await request.body(),
+                    headers=dict(request.headers.items()),
+                    query_params=dict(request.query_params.items()),
+                    raw_request=request,
+                    request_url=str(request.url),
+                    serve_origin=serve_origin,
+                    serve_path=serve_path,
+                )
+            ),
+        )
+
+    @app.put("/api/inngest")
+    async def put_inngest_api(
+        request: fastapi.Request,
+    ) -> fastapi.Response:
+        return _to_response(
+            client,
+            await handler.put(
+                comm_lib.CommRequest(
+                    body=await request.body(),
+                    headers=dict(request.headers.items()),
+                    query_params=dict(request.query_params.items()),
+                    raw_request=request,
+                    request_url=str(request.url),
+                    serve_origin=serve_origin,
+                    serve_path=serve_path,
+                )
+            ),
+        )
+
+
+def _to_response(
+    client: client_lib.Inngest,
+    comm_res: comm_lib.CommResponse,
+) -> fastapi.responses.Response:
+    body = transforms.dump_json(comm_res.body)
+    if isinstance(body, Exception):
+        comm_res = comm_lib.CommResponse.from_error(client.logger, body)
+        body = json.dumps(comm_res.body)
+
+    return fastapi.responses.Response(
+        content=body.encode("utf-8"),
+        headers=comm_res.headers,
+        status_code=comm_res.status_code,
+    )
---
+++ packages/inngest/inngest-0.4.13a1.tar.gz/inngest-0.4.13a1/inngest/flask.py
@@ -0,0 +1,165 @@
+"""Flask integration for Inngest."""
+
+import json
+import typing
+
+import flask
+
+from inngest._internal import (
+    client_lib,
+    comm_lib,
+    function,
+    server_lib,
+    transforms,
+)
+
+FRAMEWORK = server_lib.Framework.FLASK
+
+
+def serve(
+    app: flask.Flask,
+    client: client_lib.Inngest,
+    functions: list[function.Function],
+    *,
+    serve_origin: typing.Optional[str] = None,
+    serve_path: typing.Optional[str] = None,
+) -> None:
+    """
+    Serve Inngest functions in a Flask app.
+
+    Args:
+    ----
+        app: Flask app.
+        client: Inngest client.
+        functions: List of functions to serve.
+
+        serve_origin: Origin to serve the functions from.
+        serve_path: Path to serve the functions from.
+    """
+
+    handler = comm_lib.CommHandler(
+        client=client,
+        framework=FRAMEWORK,
+        functions=functions,
+    )
+
+    async_mode = any(
+        function.is_handler_async or function.is_on_failure_handler_async
+        for function in functions
+    )
+    if async_mode:
+        _create_handler_async(
+            app,
+            client,
+            handler,
+            serve_origin=serve_origin,
+            serve_path=serve_path,
+        )
+    else:
+        _create_handler_sync(
+            app,
+            client,
+            handler,
+            serve_origin=serve_origin,
+            serve_path=serve_path,
+        )
+
+
+def _create_handler_async(
+    app: flask.Flask,
+    client: client_lib.Inngest,
+    handler: comm_lib.CommHandler,
+    *,
+    serve_origin: typing.Optional[str],
+    serve_path: typing.Optional[str],
+) -> None:
+    @app.route("/api/inngest", methods=["GET", "POST", "PUT"])
+    async def inngest_api() -> typing.Union[flask.Response, str]:
+        comm_req = comm_lib.CommRequest(
+            body=flask.request.data,
+            headers=dict(flask.request.headers.items()),
+            query_params=flask.request.args,
+            raw_request=flask.request,
+            request_url=flask.request.url,
+            serve_origin=serve_origin,
+            serve_path=serve_path,
+        )
+
+        if flask.request.method == "GET":
+            return _to_response(
+                client,
+                handler.get_sync(comm_req),
+            )
+
+        if flask.request.method == "POST":
+            return _to_response(
+                client,
+                await handler.post(comm_req),
+            )
+
+        if flask.request.method == "PUT":
+            return _to_response(
+                client,
+                await handler.put(comm_req),
+            )
+
+        # Should be unreachable
+        return ""
+
+
+def _create_handler_sync(
+    app: flask.Flask,
+    client: client_lib.Inngest,
+    handler: comm_lib.CommHandler,
+    *,
+    serve_origin: typing.Optional[str],
+    serve_path: typing.Optional[str],
+) -> None:
+    @app.route("/api/inngest", methods=["GET", "POST", "PUT"])
+    def inngest_api() -> typing.Union[flask.Response, str]:
+        comm_req = comm_lib.CommRequest(
+            body=flask.request.data,
+            headers=dict(flask.request.headers.items()),
+            query_params=flask.request.args,
+            raw_request=flask.request,
+            request_url=flask.request.url,
+            serve_origin=serve_origin,
+            serve_path=serve_path,
+        )
+
+        if flask.request.method == "GET":
+            return _to_response(
+                client,
+                handler.get_sync(comm_req),
+            )
+
+        if flask.request.method == "POST":
+            return _to_response(
+                client,
+                handler.post_sync(comm_req),
+            )
+
+        if flask.request.method == "PUT":
+            return _to_response(
+                client,
+                handler.put_sync(comm_req),
+            )
+
+        # Should be unreachable
+        return ""
+
+
+def _to_response(
+    client: client_lib.Inngest,
+    comm_res: comm_lib.CommResponse,
+) -> flask.Response:
+    body = transforms.dump_json(comm_res.body)
+    if isinstance(body, Exception):
+        comm_res = comm_lib.CommResponse.from_error(client.logger, body)
+        body = json.dumps(comm_res.body)
+
+    return flask.Response(
+        headers=comm_res.headers,
+        response=body.encode("utf-8"),
+        status=comm_res.status_code,
+    )
