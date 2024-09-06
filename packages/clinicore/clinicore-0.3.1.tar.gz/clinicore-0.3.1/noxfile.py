bafcf1a03c6fd06cd3b5b0faa4e732724e73420e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 06:05:42 +0000

    Add package clinicore/0.3.1/clinicore-0.3.1.tar.gz

---
+++ packages/clinicore/clinicore-0.3.1.tar.gz/clinicore-0.3.1/noxfile.py
@@ -0,0 +1,29 @@
+import nox
+from laminci.nox import (
+    build_docs,
+    install_lamindb,
+    login_testuser1,
+    run,
+    run_pre_commit,
+    run_pytest,
+)
+
+# we'd like to aggregate coverage information across sessions
+# and for this the code needs to be located in the same
+# directory in every github action runner
+# this also allows to break out an installation section
+nox.options.default_venv_backend = "none"
+
+
+@nox.session
+def lint(session: nox.Session) -> None:
+    run_pre_commit(session)
+
+
+@nox.session()
+def build(session):
+    install_lamindb(session, branch="main", extras="bionty,aws")
+    run(session, "uv pip install --system .[dev]")
+    login_testuser1(session)
+    run_pytest(session)
+    build_docs(session, strict=False)  # temporarily disable strict mode
