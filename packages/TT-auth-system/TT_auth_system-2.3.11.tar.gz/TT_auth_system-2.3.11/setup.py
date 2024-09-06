382d009f4fe2d808e7c62305efc796c7db2b9f54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 14:06:39 +0000

    Add package TT-auth-system/2.3.11/TT_auth_system-2.3.11.tar.gz

---
+++ packages/TT-auth-system/TT_auth_system-2.3.11.tar.gz/TT_auth_system-2.3.11/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TT_auth_system",
+    version="2.3.11",
+    author="Jarod Johnson",
+    author_email="jarodjohnson1001@gmail.com",
+    description="An authentication system with one-time login links, templated email sending, and SQLite database support",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jarod-johnson-23/TT24_otc_generator",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'python-dotenv',
+        'jinja2',
+        'cryptography',
+        'sqlalchemy', 
+    ],
+)
