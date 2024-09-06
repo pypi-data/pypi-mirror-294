ac72e0986133ff68f3f7eccb77cba7099f317f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 03:06:47 +0000

    Add package aic-utils/1.0.2/aic_utils-1.0.2.tar.gz

---
+++ packages/aic-utils/aic_utils-1.0.2.tar.gz/aic_utils-1.0.2/setup.py
@@ -0,0 +1,26 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='aic_utils',
+    version='1.0.2',
+    packages=find_packages(),
+    install_requires=[
+        'google-cloud-storage',
+        # Add other dependencies here
+    ],
+    author='Dylan D',
+    author_email='dylan.doyle@jdpa.com',
+    description='AIC API wrapper',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/dylandoyle11/aic_utils', 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
+
