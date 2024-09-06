f58140e7b0d529e03b85d99d2c352f45dd975377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 06:04:48 +0000

    Add package api-gerenciador/0.7.0/api_gerenciador-0.7.0.tar.gz

---
+++ packages/api-gerenciador/api_gerenciador-0.7.0.tar.gz/api_gerenciador-0.7.0/setup.py
@@ -0,0 +1,17 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='api_gerenciador',
+    version='0.7.0',
+    description='Biblioteca que executa ações de uma API',
+    author='Rafael Gomes de Oliveira',
+    author_email='rafaelprotest4@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'mysql-connector-python',
+        'python-dotenv',
+        'banco-dados'
+    ],
+)
