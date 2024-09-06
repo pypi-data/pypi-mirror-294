9d35f1a2b4a893f8f34347e91ef65731d7d69efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 15:05:51 +0000

    Add package deva/1.2.4/deva-1.2.4.tar.gz

---
+++ packages/deva/deva-1.2.4.tar.gz/deva-1.2.4/deva/sample/my_future.py
@@ -0,0 +1,42 @@
+import IPython
+from deva import *
+
+
+@gen.coroutine
+def foo1():
+    'foo1 run' | print
+    yield gen.sleep(1)
+    return 'foo1 result'
+
+
+async def foo2():
+    'foo2 run' | print
+    import asyncio
+    await asyncio.sleep(1)
+    return 'foo2 result'
+
+# s = Stream()
+# s.rate_limit(1).run_future() >> log
+
+# for i in range(3):
+#     foo() >> s
+#     foo2() >> s
+
+# run = run_future()
+# run >> warn
+
+# foo() >> run
+# foo2() >> run
+
+foo1() | log
+
+foo1() | print
+
+foo2() | log
+
+
+name = "Masnun"
+
+# IPython.embed()
+
+Deva().run()
