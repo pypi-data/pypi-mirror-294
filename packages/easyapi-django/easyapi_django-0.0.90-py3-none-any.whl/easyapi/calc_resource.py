1da163ba558307a23c391f0820e9b46db696b2ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 14:06:48 +0000

    Add package easyapi-django/0.0.90/easyapi_django-0.0.90-py3-none-any.whl

---
+++ packages/easyapi-django/easyapi_django-0.0.90-py3-none-any.whl/easyapi/calc_resource.py
@@ -0,0 +1,13 @@
+import pytz
+
+from .base import BaseResource
+from .calc import get_results
+
+
+class Metrics(BaseResource):
+    allowed_methods = ['post']
+
+    async def post(self, request):
+        timezone = pytz.timezone(self.user.get('timezone', 'UTC'))
+        results = await get_results(timezone, self.body)
+        return results
