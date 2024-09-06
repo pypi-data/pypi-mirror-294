a561dd45562fa7d42dbea614cc006fca92e301d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 15:04:43 +0000

    Add package ragstack-ai-langflow-base/1.0.16.post6/ragstack_ai_langflow_base-1.0.16.post6-py3-none-any.whl

---
+++ packages/ragstack-ai-langflow-base/ragstack_ai_langflow_base-1.0.16.post6-py3-none-any.whl/langflow/frontend/assets/user-round-Ce_IWx6d.js
@@ -0,0 +1,6 @@
+import{a1 as e}from"./index-ByLTX0S6.js";/**
+ * @license lucide-react v0.395.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=e("UserRound",[["circle",{cx:"12",cy:"8",r:"5",key:"1hypcn"}],["path",{d:"M20 21a8 8 0 0 0-16 0",key:"rfgkzh"}]]);export{r as default};
