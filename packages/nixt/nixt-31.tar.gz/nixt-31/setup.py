ecb30bc686ad51349438bed33ecfe0ece145a27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 06:05:30 +0000

    Add package nixt/31/nixt-31.tar.gz

---
+++ packages/nixt/nixt-31.tar.gz/nixt-31/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# This file is placed in the Public Domain.
+
+
+"stub"
+
+
+import setuptools
+
+
+if __name__ == "__main__":
+    setuptools.setup(scripts=[
+                         "bin/nixt",
+                         "bin/nixtc",
+                         "bin/nixtd",
+                         "bin/nixts"
+                         ])
