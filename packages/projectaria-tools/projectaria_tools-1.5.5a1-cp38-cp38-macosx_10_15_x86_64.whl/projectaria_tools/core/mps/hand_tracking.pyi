9d121b029b0587c4916397d158433f7f437d5313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 06:05:12 +0000

    Add package projectaria-tools/1.5.5a1/projectaria_tools-1.5.5a1-cp38-cp38-macosx_10_15_x86_64.whl

---
+++ packages/projectaria-tools/projectaria_tools-1.5.5a1-cp38-cp38-macosx_10_15_x86_64.whl/projectaria_tools/core/mps/hand_tracking.pyi
@@ -0,0 +1,20 @@
+from __future__ import annotations
+import datetime
+import typing
+__all__ = ['WristAndPalmPose', 'read_wrist_and_palm_poses']
+class WristAndPalmPose:
+    """
+    An object representing WristAndPalmPose output at a single timestamp.
+    """
+    left_hand: ... | None
+    right_hand: ... | None
+    tracking_timestamp: datetime.timedelta
+    def __repr__(self) -> str:
+        ...
+def read_wrist_and_palm_poses(path: str) -> list[WristAndPalmPose]:
+    """
+    Read Wrist and Palm poses from the hand tracking output generated via MPS.
+      Parameters
+      __________
+      path: Path to the wrist and palm poses csv file.
+    """
