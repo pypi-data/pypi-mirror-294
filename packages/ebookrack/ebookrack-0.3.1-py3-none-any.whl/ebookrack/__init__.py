cae55dd7efc88cfd22fd10eec4243a3f2b58ccfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 16:05:36 +0000

    Add package ebookrack/0.3.1/ebookrack-0.3.1-py3-none-any.whl

---
+++ packages/ebookrack/ebookrack-0.3.1-py3-none-any.whl/ebookrack/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2024 JA Viljoen <ebookrack@javiljoen.net>
+# SPDX-License-Identifier: EUPL-1.2
+
+"""
+library for managing a personal e-book library
+"""
+
+__version__ = "0.3.1"
---
+++ packages/ebookrack/ebookrack-0.3.1-py3-none-any.whl/ebookrack/calibre/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2024 JA Viljoen <ebookrack@javiljoen.net>
+# SPDX-License-Identifier: EUPL-1.2
+
+"""
+library for querying a Calibre e-book library
+"""
