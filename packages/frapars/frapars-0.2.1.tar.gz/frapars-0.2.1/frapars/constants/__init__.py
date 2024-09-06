7fb195628a8e3e9c583ce707bfba021e071a9d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 03:07:36 +0000

    Add package frapars/0.2.1/frapars-0.2.1.tar.gz

---
+++ packages/frapars/frapars-0.2.1.tar.gz/frapars-0.2.1/frapars/constants/__init__.py
@@ -0,0 +1,7 @@
+# URL of the CSV file
+insee_url = "https://datanova.laposte.fr/data-fair/api/v1/datasets/laposte-hexasmal/data-files/019HexaSmal.csv"
+# Path of the CSV file containing the insee
+insee_path = "frapars/res/insee_file.csv"
+
+in_file_path = "frapars/res/sample_addresses.csv"
+out_file_path = "frapars/res/parsed_addresses.csv"
