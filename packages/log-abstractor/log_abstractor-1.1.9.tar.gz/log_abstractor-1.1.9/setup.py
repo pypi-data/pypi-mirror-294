e201018d6be65d8af388eaff277c535ad9ed6faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 05:04:30 +0000

    Add package log-abstractor/1.1.9/log_abstractor-1.1.9.tar.gz

---
+++ packages/log-abstractor/log_abstractor-1.1.9.tar.gz/log_abstractor-1.1.9/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="log_abstractor",
+    version="1.1.9",
+    packages=find_packages(),
+    install_requires=[
+        "structlog>=24.4.0", "pii-extract-base>=0.1.0",  # Adjust the version according to your needs
+        "regex>=2021.8.3",#"scrubadub==2.0.0","regex",
+
+    ],
+    description="Optimized anonymizer with pre-scrubbing.",
+    author="Dipanjan Mazumder",
+    author_email="javahub@yahoo.com",
+    url="https://github.com/SMART2016/logger",  # Optional: Link to your project repository
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # Specify Python version compatibility
+)
