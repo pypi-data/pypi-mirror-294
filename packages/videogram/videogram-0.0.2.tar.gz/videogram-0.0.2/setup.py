93b32b394975d21551d9be34c7e2d85eb4308a10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 07:04:51 +0000

    Add package videogram/0.0.2/videogram-0.0.2.tar.gz

---
+++ packages/videogram/videogram-0.0.2.tar.gz/videogram-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Package for video processing.'
+LONG_DESCRIPTION = 'A one stop package for everything related to video processing.'
+
+# Setting up
+setup(
+    name="videogram",
+    version=VERSION,
+    author="Ishan Dutta",
+    author_email="duttaishan0098@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video processing', 'video editing', 'video recording'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
