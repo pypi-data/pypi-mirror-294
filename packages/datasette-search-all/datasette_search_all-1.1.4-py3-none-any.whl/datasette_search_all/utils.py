b7aec8df1ae78db263ce137bf835c898c215dc04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 14:06:42 +0000

    Add package datasette-search-all/1.1.4/datasette_search_all-1.1.4-py3-none-any.whl

---
+++ packages/datasette-search-all/datasette_search_all-1.1.4-py3-none-any.whl/datasette_search_all/utils.py
@@ -0,0 +1,40 @@
+from datasette import Forbidden
+
+
+async def iterate_searchable_tables(datasette, request):
+    for db_name, database in datasette.databases.items():
+        hidden_tables = set(await database.hidden_table_names())
+        for table in await database.table_names():
+            if table in hidden_tables:
+                continue
+            if table.endswith("_fts"):
+                continue
+            fts_table = await database.fts_table(table)
+            if fts_table:
+                # Check user has permission to view that table
+                try:
+                    await datasette.ensure_permissions(
+                        request.actor,
+                        [
+                            ("view-table", (database.name, table)),
+                            ("view-database", database.name),
+                            "view-instance",
+                        ],
+                    )
+                    yield (db_name, table)
+                except Forbidden:
+                    pass
+
+
+async def has_searchable_tables(datasette, request):
+    # Return True on the first table we find
+    async for _ in iterate_searchable_tables(datasette, request):
+        return True
+    return False
+
+
+async def get_searchable_tables(datasette, request):
+    tables = []
+    async for table in iterate_searchable_tables(datasette, request):
+        tables.append(table)
+    return tables
