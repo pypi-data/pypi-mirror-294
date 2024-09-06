4ff3fd6f72432d1545e7b0361f494cdf1c9b4ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 14:06:38 +0000

    Add package factor-analyser/1.1/factor_analyser-1.1.tar.gz

---
+++ packages/factor-analyser/factor_analyser-1.1.tar.gz/factor_analyser-1.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
+setup(
+    name='factor_analyser',  
+    version='1.1',  
+    author='AlfredCYL',  
+    author_email='alfred.yl@outlook.com', 
+    description='A tool for factor development and analysis.',  
+    long_description=long_description,  
+    long_description_content_type='text/markdown',  
+    url='https://github.com/AlfredCYL/factor_analyser',  
+    packages=find_packages(), 
+    classifiers=[  
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'statsmodels',
+        'tabulate',
+        'numba',
+        'scipy',
+        'seaborn',
+        'scikit-learn',
+        'plotly'
+    ],
+    include_package_data=False, # exclude the test data
+)
