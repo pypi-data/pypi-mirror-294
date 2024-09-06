1cd01263aeda0bc57aadb250a93f01e4a6bcd3f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 04:04:59 +0000

    Add package osi-dump/0.1.3.1.2/osi_dump-0.1.3.1.2.tar.gz

---
+++ packages/osi-dump/osi_dump-0.1.3.1.2.tar.gz/osi_dump-0.1.3.1.2/src/osi_dump/model/hypervisor.py
@@ -0,0 +1,25 @@
+from typing import Optional
+from pydantic import BaseModel, ConfigDict, ValidationError
+
+
+class Hypervisor(BaseModel):
+    hypervisor_id: str
+    hypervisor_type: str
+    name: str
+    state: str
+    status: str
+
+    vcpus: int
+    vcpus_usage: int
+
+    memory_size: int
+    memory_usage: int
+
+    local_disk_usage: int
+    local_disk_size: int
+
+    availability_zone: Optional[str]
+    aggregate_name: Optional[str]
+    aggregate_id: Optional[str]
+
+    vm_count: int
