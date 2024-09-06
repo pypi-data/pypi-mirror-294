d85b01b96b56407b73088fb16cf2dde088aea97a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 05:04:28 +0000

    Add package log-abstractor/1.1.7/log_abstractor-1.1.7.tar.gz

---
+++ packages/log-abstractor/log_abstractor-1.1.7.tar.gz/log_abstractor-1.1.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="log_abstractor",
+    version="1.1.7",
+    packages=find_packages(),
+    install_requires=[
+        "structlog>=24.4.0", "presidio-analyzer",
+        "presidio-anonymizer",
+        "regex",#"scrubadub==2.0.0","regex",
+
+    ],
+    description="Optimized anonymizer with pre-scrubbing.",
+    author="Dipanjan Mazumder",
+    author_email="javahub@yahoo.com",
+    url="https://github.com/SMART2016/logger",  # Optional: Link to your project repository
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # Specify Python version compatibility
+)
