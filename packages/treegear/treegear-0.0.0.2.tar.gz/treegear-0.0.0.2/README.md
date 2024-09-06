9e124ccac813028bd53014f8efc7bc7e58e7acad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 06:05:42 +0000

    Add package treegear/0.0.0.2/treegear-0.0.0.2.tar.gz

---
+++ packages/treegear/treegear-0.0.0.2.tar.gz/treegear-0.0.0.2/README.md
@@ -0,0 +1,12 @@
+**Now this is just for rests to upload and pull the package.**
+
+Version: 0.0.0.2
+
+Usage: python -m uvicorn treegear:trgr
+
+And go in browser to http://127.0.0.1:8000
+
+For any path it returns "OK and hello from TreeGear!"
+
+You can make it return your own message if prior set environment variable $MESSAGE
+
---
+++ packages/treegear/treegear-0.0.0.2.tar.gz/treegear-0.0.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+setup(
+  name='treegear',
+  version='0.0.0.2',
+  author='Gyuli',
+  author_email='guli.peradze@gmail.com',
+  description='This module is for tests for future project.',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  # url='treegear.dev',
+  url='http://185.78.76.240',
+  packages=find_packages(),
+  install_requires=['uvicorn'],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='python uvicorn',
+  project_urls={
+    # 'Documentation': 'treegear.dev'
+    'Documentation': 'http://185.78.76.240'
+  },
+  python_requires='>=3.8'
+)
