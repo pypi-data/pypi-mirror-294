be643a39bad13c45e4ce037ac98cce39094ab76c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 16:05:45 +0000

    Add package blurhash-avif/0.7.0/blurhash_avif-0.7.0.tar.gz

---
+++ packages/blurhash-avif/blurhash_avif-0.7.0.tar.gz/blurhash_avif-0.7.0/src/blurhash_avif/__init__.py
@@ -0,0 +1,18 @@
+# src/blurhash_avif/__init__.py
+from .blurhash_avif import (
+    generate_blurhash_from_avif,
+    generate_png_data_url_from_avif,
+    generate_blurhash_and_data_url_from_avif,
+    batch_generate_blurhash_from_avif,
+    batch_generate_png_data_url_from_avif,
+    batch_generate_blurhash_and_data_url_from_avif,
+)
+
+__all__ = [
+    "generate_blurhash_from_avif",
+    "generate_png_data_url_from_avif",
+    "generate_blurhash_and_data_url_from_avif",
+    "batch_generate_blurhash_from_avif",
+    "batch_generate_png_data_url_from_avif",
+    "batch_generate_blurhash_and_data_url_from_avif",
+]
