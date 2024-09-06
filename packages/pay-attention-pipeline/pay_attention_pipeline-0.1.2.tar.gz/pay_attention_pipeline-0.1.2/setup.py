19d68e3b259dc302e6f7116f7d0db8ea9c94be19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 04:05:32 +0000

    Add package pay-attention-pipeline/0.1.2/pay_attention_pipeline-0.1.2.tar.gz

---
+++ packages/pay-attention-pipeline/pay_attention_pipeline-0.1.2.tar.gz/pay_attention_pipeline-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pay-attention-pipeline",
+    version="0.1.2",
+    author="Pedro Silva",
+    author_email="pedrolmssilva@gmail.com",
+    description="A Python package that uses PyTorch, Transformers, and NumPy",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/netop-team/pay_attention_pipeline",  # Replace with your GitHub repo or project URL
+    packages=find_packages(),
+    install_requires=[
+        "torch>=1.0.0",
+        "transformers>=4.0.0",
+        "numpy>=1.18.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
