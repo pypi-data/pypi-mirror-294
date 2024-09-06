1ac772ec082bf8afb71fbc401ed7a0c44636974a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 07:05:09 +0000

    Add package automatic-spectral-acquisition/0.1.4/automatic_spectral_acquisition-0.1.4-py3-none-any.whl

---
+++ packages/automatic-spectral-acquisition/automatic_spectral_acquisition-0.1.4-py3-none-any.whl/automatic_spectral_acquisition/__init__.py
@@ -0,0 +1,6 @@
+# import automatic_spectral_acquisition.arduino
+# import automatic_spectral_acquisition.cli
+# import automatic_spectral_acquisition.constants
+# import automatic_spectral_acquisition.core
+# import automatic_spectral_acquisition.file_manager
+# import automatic_spectral_acquisition.oscilloscope
