13ad52608af35de746837eb84efd4300d05c7f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 15:05:57 +0000

    Add package knowledge-engineer/0.7.5/knowledge_engineer-0.7.5-py3-none-any.whl

---
+++ packages/knowledge-engineer/knowledge_engineer-0.7.5-py3-none-any.whl/knowledge_engineer/line_statement.py
@@ -0,0 +1,282 @@
+import glob
+
+from lark import Lark, Transformer
+from .logger import Logger
+
+LineStatement_Grammar = r"""
+    ?start: "." statement 
+
+    ?statement: role_statement
+            | include_statement
+            | text_block_statement
+            | exec_statement
+            | llm_statement
+            | clear_statement
+            | cmd_statement
+
+    role_statement: role_name 
+
+    sl_role_statement: role_name rest_of_line
+
+    role_name: "system"     -> system
+             | "user"        -> user
+             | "assistant"   -> assistant
+             | "function"    -> function
+
+    include_statement: "include" rest_of_line
+
+    text_block_statement: "text_block" rest_of_line
+
+    llm_statement: "llm" rest_of_line
+
+    cmd_statement: "cmd" rest_of_line
+
+    clear_statement: "clear" rest_of_line
+
+    exec_statement:  "exec"
+
+    rest_of_line: ENDOFLINE
+
+    VAR.1: /[\w\d]+/ 
+
+    ENDOFLINE: /[^\n]+/
+
+    MEMORY_NAME: /[\w\d][\w\d\/]*/
+
+    %ignore " "
+    """
+
+
+class MyTransformer(Transformer):
+    # Class Variables
+    log = Logger(namespace="MyTransformer", debug=False)
+
+    @staticmethod
+    def start(statements):
+        MyTransformer.log.info(f"LARK: start({statements})")
+        return statements
+
+    @staticmethod
+    def statement(statement):
+        MyTransformer.log('LARK', f"statement({statement})")
+        return statement
+
+    @staticmethod
+    def role_statement(statement):
+        MyTransformer.log.info(f"role_statement({statement})")
+        return {'statement': 'set_role', 'role': statement[0].data}
+
+    @staticmethod
+    def role_name(statement):
+        MyTransformer.log.info(f"role_name({statement})")
+        return statement[0].data
+
+    @staticmethod
+    def include_statement(statement):
+        MyTransformer.log.info(f"include_statement({statement})")
+        return {'statement': 'include_statement', 'name': statement[0].strip()}
+
+    @staticmethod
+    def llm_statement(statement):
+        MyTransformer.log.info(f"llm_statement({statement})")
+        return {'statement': 'llm_statement', 'parms': statement[0].strip()}
+
+    @staticmethod
+    def clear_statement(statement):
+        MyTransformer.log.info(f"clear_statement({statement})")
+        return {'statement': 'clear_statement', 'parms': statement[0].strip()}
+
+    @staticmethod
+    def cmd_statement(statement):
+        MyTransformer.log.info(f"cmd_statement({statement})")
+        return {'statement': 'cmd_statement', 'parms': statement[0].strip()}
+
+    @staticmethod
+    def text_block_statement(statement):
+        MyTransformer.log.info(f"text_block_statement({statement})")
+        return {'statement': 'text_block_statement', 'name': statement[0].strip()}
+
+    @staticmethod
+    def exec_statement(statement):
+        MyTransformer.log.info(f"exec_statement({statement})")
+        return {'statement': 'exec_statement'}
+
+    @staticmethod
+    def rest_of_line(statement):
+        MyTransformer.log.info(f"rest_of_line({statement})")
+        return statement[0]
+
+    @staticmethod
+    def VAR(statement):
+        MyTransformer.log.info(f"VAR({statement})")
+        return statement
+
+    @staticmethod
+    def ENDOFLINE(statement):
+        MyTransformer.log.info(f"ENDOFLINE({statement})")
+        return statement.value
+
+    @staticmethod
+    def MEMORY_NAME(statement):
+        MyTransformer.log.info(f"MEMORY_NAME({statement})")
+        return statement
+
+    @staticmethod
+    def DOT(statement):
+        MyTransformer.log.info(f"DOT({statement})")
+        return statement
+
+
+class Compiler:
+
+    def __init__(self, db=None):
+        self.db = db
+        self.parser = Lark(LineStatement_Grammar, start='start')
+        self.transformer = MyTransformer()
+        pass
+
+    def compile(self, lines: [str]):
+        result = []
+
+        a_line = ''
+        for line in lines:
+            if line.startswith("."):
+                if a_line != '':
+                    result.append(a_line)
+                    a_line = ''
+
+                result.append(line)
+            else:
+                a_line = f"{a_line}\n{line}"
+
+        if a_line != '':
+            result.append(a_line)
+
+        # Logger.log('STEP', f"result: {result}")
+
+        statements = []
+
+        for result_line in result:
+            line_statement: bool = False
+            if result_line[0] == ".":
+                kword = result_line
+                if ' ' in result_line:
+                    kword, _ = result_line.split(' ', maxsplit=1)
+
+                if kword in ['.llm', '.clear', '.system', '.user', '.assistant', '.include', '.cmd', '.exec']:
+                    line_statement = True
+
+            if line_statement:
+                tree = self.parser.parse(result_line.strip())
+                statement = MyTransformer().transform(tree)
+                statements.append(statement)
+            else:
+                statements.append({'statement': 'literal_line', 'content': result_line})
+
+        # self.log.info("statements:")
+        # for stmt in statements:
+        #     self.log.info("{stmt}", stmt=stmt)
+
+        return statements
+
+    def execute(self, statements, process_name: str = ''):
+        calls = []
+        messages = []
+        stmts: dict[str, str] = {'system': '', 'user': '', 'assistant': ''}
+        role = 'user'
+        literal_line_content = ''
+
+        for statement in statements:
+            keyword = statement['statement']
+
+            match keyword:
+                case 'literal_line':
+                    c = statement['content']
+                    new_value = f"{stmts[role]}\n{c}"
+                    stmts[role] = new_value.strip()     # Drop Leading and Trailing WhiteSpace
+
+                case 'set_role':
+                    for k, v in stmts.items():
+                        if v:
+                            messages.append({'role': k, 'content': v})
+                            stmts[k] = ''
+
+                    role = statement['role']
+
+                case 'include_statement':
+                    parse_msgs = True
+                    name = statement['name']
+                    if name[0] == '@':
+                        parse_msgs = False
+                        name = statement['name'][1:]
+                        lines = self.db.read_lines(name, process_name=process_name)
+                        stmts['user'] = f"{stmts['user']}\n{'\n'.join(lines)}"
+                    else:
+                        msgs = self.db.read_msgs(name, process_name=process_name)
+                        for msg in msgs[:-1]:
+                            stmts[msg['role']] = f"{stmts[msg['role']]}\n{msg['content']}"
+
+                case 'llm_statement':
+                    messages.append({'role': 'llm', 'content': statement['parms']})
+
+                case 'clear_statement':
+                    messages.append({'role': 'clear', 'content': statement['parms']})
+
+                case 'cmd_statement':
+                    messages.append({'role': 'cmd', 'content': statement['parms']})
+
+                case 'text_block_statement':
+                    # print(f"in {keyword}")
+                    # Check for file-globing
+                    if '*' in statement['name']:
+                        files = self.db.glob_files(statement['name'])
+                    else:
+                        files = [statement['name']]
+
+                    for file in files:
+                        # print(f"in {file}")
+                        # Logger.log('STEP',f"{colors.INFO}- {file}")
+                        # assumes only one msg is returned
+                        # @todo Modify to Auto include Proc when using proc...
+                        msgs = self.db[file]
+                        # print(f"in {file} 2")
+                        msg = msgs[0]
+                        # print(f"in {file} 3")
+                        # parts = file.split('/')
+                        content = f"\n```filename={file}\n{msg['content']}\n```"
+                        stmts[role] += content
+
+                    # print(f"out {keyword}")
+                    continue  # next line
+
+                case 'exec_statement':
+                    # self.log.info("In Execute Statement: {statement}", statement=statement)
+                    if stmts['system'] != '':
+                        messages.append({'role': 'system', 'content': stmts['system']})
+                    messages.append({'role': 'user', 'content': stmts['user']})
+                    if stmts['assistant'] != '':
+                        messages.append({'role': 'assistant', 'content': stmts['assistant']})
+                    messages.append({"role": "exec", "content": "Execute Call AI Before Continuing"})
+                    stmts = {'system': '', 'user': '', 'assistant': ''}
+
+                case _:
+                    errmsg = f"Invalid instruction [{keyword}] "
+                    # self.log.error("{errmsg}", errmsg=errmsg)
+                    raise Exception(f"{errmsg}")
+
+        if stmts['system'] != '':
+            messages.append({'role': 'system', 'content': stmts['system'].strip()})
+
+        if stmts['user'] != '':
+            messages.append({'role': 'user', 'content': stmts['user'].strip()})
+
+        if stmts['assistant'] != '':
+            messages.append({'role': 'assistant', 'content': stmts['assistant'].strip()})
+
+        if messages[-1]['role'] != 'exec':
+            messages.append({"role": "exec", "content": "Default Execute Call AI at end"})
+        return messages
+
+#
+# Okay That got alot Easier
+#
---
+++ packages/knowledge-engineer/knowledge_engineer-0.7.5-py3-none-any.whl/knowledge_engineer/logger.py
@@ -0,0 +1,154 @@
+import json
+import weakref
+from typing import Dict, Optional
+from rich.console import Console
+import platform
+
+class Logger:
+    _instances = weakref.WeakSet()
+    _console: Optional[Console] = None
+    _log_file: Optional[Console] = None
+
+    top_left = '╭'
+    top_right = '╮'
+    bottom_left = '╰'
+    bottom_right = '╯'
+    vertical_mid = '│'
+    horizontal_mid = '─'
+
+    def __init__(self, namespace: str, debug: bool = True):
+        self.namespace = namespace
+        self.debug = debug
+        self._instances.add(self)
+        if Logger._console is None:
+            Logger._console = Console()
+
+        if platform.system() == "Windows":
+            self.top_left = '+'
+            self.top_right = '+'
+            self.bottom_left = '+'
+            self.bottom_right = '+'
+            self.vertical_mid = '|'
+            self.horizontal_mid = '-'
+
+
+    @classmethod
+    def set_log_file(cls, log_file_name: str) -> None:
+        log_file_fn = open(log_file_name, "wt")
+        cls._log_file = Console(file=log_file_fn, width=120, force_terminal=True)
+        # cls.print(f"Logging to: {log_file_name}")
+
+    @classmethod
+    def print(cls, message: str) -> None:
+        cls._console.print(message, soft_wrap=True, overflow="ellipsis")
+        if cls._log_file:
+            cls._log_file.print(message, soft_wrap=True, overflow="ellipsis")
+
+    def ts(self) -> str:
+        from datetime import datetime
+        return datetime.now().strftime("[%H:%M:%S.%f]")[:-3] + "]"
+
+    def _format_step(self) -> str:
+        return f"[green]{self.namespace:>10}::[/][white]{self.vertical_mid} [/]"
+
+    def _format_head(self) -> str:
+        return f"{self._format_step()}[green]{self.vertical_mid} [/]"
+
+    def start_step(self, step):
+        self.print(f"{self.ts()}{self._format_step()}[green]{self.top_left}{self.horizontal_mid * 80}[/]")
+
+    def stop_step(self, step):
+        self.print(f"{self.ts()}{self._format_step()}[green]{self.bottom_left}{self.horizontal_mid * 80}[/]")
+
+    def umsg(self, step, msg: dict):
+        color = 'medium_orchid'
+        role = 'user'
+        if msg['role'] == 'system':
+            role = 'system'
+            color = 'yellow'
+        content = msg['content'].replace('\n', '\\n')
+        self.print(f"{self.ts()}{self._format_head()}[{color}]{role:>14}[/] [green]{content}[/]")
+
+    # def smsg(self, step, msg: dict):
+    #     content = msg['content'].replace('\n', '\\n')
+    #     self.print(f"{self.ts()}{self._format_head()}[yellow]{'system':>14}[/] [green]{content}[/]")
+
+    def ai_msg(self, step, content: str, stop_reason: str = ''):
+        txt = content.replace('\n', '\\n')
+        self.print(f"{self.ts()}{self._format_head()}[deep_sky_blue1]{'AI ('+stop_reason+')':>14}[/] [green][{txt}][/]")
+
+    def ai_tool_call(self, id, func_name: str, arg_str: str):
+        args = json.loads(arg_str) if isinstance(arg_str, str) else arg_str
+        fn = f"[deep_sky_blue1]{func_name:>14}[/]"
+
+        func_actions = {
+            'read_file': lambda: f"({args['name']}):",
+            'write_file': lambda: f"({args['name']}, ...)[green]{[arg_str]}[/]",
+            'replace': lambda: f"({args['name']}, ...)[green]{[arg_str]}[/]",
+            'patch': lambda: f"({args['patch_commands'][:50]}...)",
+            'exec': lambda: f"({args['command'][:50]}...)",
+            'query_db': lambda: f"({args['sql'][:50]}...)",
+            'ask_user': lambda: f"({args['question'][:50]}...)"
+        }
+
+        action_result = func_actions.get(func_name, lambda: f"({args.get('filename', '')}, ...)[green]{[arg_str]}[/]")()
+        self.print(f"{self.ts()}{self._format_head()}{fn} {action_result}")
+        self.print(f"{self.ts()}{self._format_head()} [deep_sky_blue1]{'call_id':>13}[/] [green]{id}[/]")
+
+    def cmd_tool_call(self, id, func_name: str, arg_str: object):
+        args = json.loads(arg_str) if isinstance(arg_str, str) else arg_str
+        fn = f"[slate_blue1]cmd {func_name:>10}[/]"
+
+        func_actions = {
+            'read_file': lambda: f"({args['name']}):",
+            'write_file': lambda: f"({args['name']}, ...)[green]{[arg_str]}[/]",
+            'replace': lambda: f"({args['name']}, ...)[green]{[arg_str]}[/]",
+            'patch': lambda: f"({args['patch_commands'][:50]}...)",
+            'exec': lambda: f"({args['command'][:50]}...)",
+            'query_db': lambda: f"({args['sql'][:50]}...)",
+            'ask_user': lambda: f"({args['question'][:50]}...)"
+        }
+
+        action_result = func_actions.get(func_name, lambda: f"({args.get('filename', '')}, ...)[green]{[arg_str]}[/]")()
+        self.print(f"{self.ts()}{self._format_head()}{fn} {action_result}")
+
+
+    def ret_msg(self, id, result: Dict[str, str]):
+        txt = result['content'].replace('\n', '\\n')
+        self.print(f"{self.ts()}{self._format_head()}[medium_orchid]{'return':>14}[/] [green]{result['name']}({id}):: {txt}[/]")
+
+    def ai_log_call(self, parameters: Dict):
+        self.print(f"{self.ts()}{self._format_head()}[orange1]{'call log':>14}[/] [green]{parameters}[/]")
+
+    def ai_log_response(self, parameters: Dict):
+        self.print(f"{self.ts()}{self._format_head()}[orange1]{'call response':>14}[/] [green]{parameters}[/]")
+
+    def error(self, msg: str, err: Optional[Exception] = None):
+        trace_back_msg = self._format_traceback(err) if err else ''
+        self.print(f"{self.ts()}[on red]{self.namespace:>10}[/on red]::{msg}{trace_back_msg}")
+
+    def warn(self, msg: str):
+        self.print(f"{self.ts()}[bold orange]{self.namespace:>10}::{msg}[/bold orange]")
+
+    def info(self, msg: str):
+        if self.debug:
+            self.print(f"{self.ts()}{self.namespace:>10}::{msg}")
+
+    def ai_asks(self, msg: str) -> str:
+        return self._console.input(f"{self.ts()}{self._format_head()}[medium_orchid]{'Input':>14}[/] [green]")
+
+    @classmethod
+    def set_namespace_debug(cls, namespace: str, value: bool) -> None:
+        for instance in cls._instances:
+            if instance.namespace == namespace:
+                instance.debug = value
+                return
+
+    @staticmethod
+    def _format_traceback(err: Exception) -> str:
+        trace_back_msg = '\n'
+        tb = err.__traceback__
+        while tb is not None:
+            trace_back_msg += f" in {tb.tb_frame.f_code.co_filename}, at line {tb.tb_lineno}\n"
+            tb = tb.tb_next
+        return trace_back_msg
