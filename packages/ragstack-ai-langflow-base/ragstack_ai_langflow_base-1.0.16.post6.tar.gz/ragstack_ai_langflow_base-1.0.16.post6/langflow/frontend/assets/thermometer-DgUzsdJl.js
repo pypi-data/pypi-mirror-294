74548aaaed57bce7941d4675af0b18cf8d9c8b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 15:04:43 +0000

    Add package ragstack-ai-langflow-base/1.0.16.post6/ragstack_ai_langflow_base-1.0.16.post6.tar.gz

---
+++ packages/ragstack-ai-langflow-base/ragstack_ai_langflow_base-1.0.16.post6.tar.gz/ragstack_ai_langflow_base-1.0.16.post6/langflow/frontend/assets/thermometer-DgUzsdJl.js
@@ -0,0 +1,6 @@
+import{a1 as e}from"./index-ByLTX0S6.js";/**
+ * @license lucide-react v0.395.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=e("Thermometer",[["path",{d:"M14 4v10.54a4 4 0 1 1-4 0V4a2 2 0 0 1 4 0Z",key:"17jzev"}]]);export{r as default};
---
+++ packages/ragstack-ai-langflow-base/ragstack_ai_langflow_base-1.0.16.post6.tar.gz/ragstack_ai_langflow_base-1.0.16.post6/langflow/frontend/assets/use-post-add-user-B9G3CyNa.js
@@ -0,0 +1,1 @@
+import{U as n,a as o,g as u}from"./index-ByLTX0S6.js";const i=t=>{const{mutate:e}=n(),a=async s=>(await o.post(`${u("USERS")}`,s)).data;return e(["useAddUser"],async s=>await a(s),t)};export{i as u};
