d5fd7d3d121f8e1a3050e8d1d367623e81fdae35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 06:05:41 +0000

    Add package Kissimoney2023/0.0.1/Kissimoney2023-0.0.1.tar.gz

---
+++ packages/Kissimoney2023/Kissimoney2023-0.0.1.tar.gz/Kissimoney2023-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages 
+ 
+setup( 
+   name="Kissimoney2023", 
+   version="0.0.1", 
+   author="Kissimoney2023", 
+   author_email="sahjohnny@gmail.com", 
+   description="Faceswap", 
+   long_description=open("README.md").read(), 
+   long_description_content_type="text/markdown", 
+   url="https://github.com/pypa/sampleproject", 
+   packages=find_packages(), 
+   classifiers=[ 
+       "Programming Language :: Python :: 3", 
+       "License :: OSI Approved :: MIT License", 
+       "Operating System :: OS Independent", 
+   ], 
+   python_requires='>=3.8',
+
+) 
