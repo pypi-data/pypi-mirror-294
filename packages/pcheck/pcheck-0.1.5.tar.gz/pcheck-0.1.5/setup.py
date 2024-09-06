cc3f72d98451777597f68b4ab46fa3de43b074db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 04:05:13 +0000

    Add package pcheck/0.1.5/pcheck-0.1.5.tar.gz

---
+++ packages/pcheck/pcheck-0.1.5.tar.gz/pcheck-0.1.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pcheck',
+    version='0.1.5',
+
+    author="CodingLive",
+    author_email="rootcode@duck.com",
+    description="A easy to use and blazingly fast proxy checker written in python ",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ProtDos/pcheck",
+
+    packages=find_packages(),
+    install_requires=[
+        'aiohttp',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pcheck=pcheck.__main__:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
