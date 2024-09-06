21f69b7c9d6673f45d0fac06f12a21f8b27df770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 16:05:40 +0000

    Add package drf-recaptcha/3.2.0/drf_recaptcha-3.2.0.tar.gz

---
+++ packages/drf-recaptcha/drf_recaptcha-3.2.0.tar.gz/drf_recaptcha-3.2.0/tests/test_checks.py
@@ -0,0 +1,25 @@
+from drf_recaptcha.checks import recaptcha_system_check
+from drf_recaptcha.constants import TEST_V2_SECRET_KEY
+
+
+def test_warning_no_secret_key(settings):
+    settings.DRF_RECAPTCHA_SECRET_KEY = None
+
+    errors = recaptcha_system_check(None)
+    assert len(errors) == 1
+    assert errors[0].msg == "settings.DRF_RECAPTCHA_SECRET_KEY must be set."
+
+
+def test_silent_testing(settings):
+    settings.DRF_RECAPTCHA_TESTING = True
+    settings.DRF_RECAPTCHA_SECRET_KEY = None
+
+    assert recaptcha_system_check(None) == []
+
+
+def test_warning_test_secret_key(settings):
+    settings.DRF_RECAPTCHA_SECRET_KEY = TEST_V2_SECRET_KEY
+
+    errors = recaptcha_system_check(None)
+    assert len(errors) == 1
+    assert errors[0].hint == "Update settings.DRF_RECAPTCHA_SECRET_KEY"
