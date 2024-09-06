66715ba7106c11730a68ec21e99c2fed7aa55052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 03:07:34 +0000

    Add package MaixPy/4.5.1/MaixPy-4.5.1-cp312-cp312-manylinux2014_x86_64.whl

---
+++ packages/MaixPy/MaixPy-4.5.1-cp312-cp312-manylinux2014_x86_64.whl/maix/_maix/ext_dev/__init__.pyi
@@ -0,0 +1,9 @@
+"""
+maix.ext_dev module
+"""
+from __future__ import annotations
+from . import bm8563
+from . import ntp
+from . import qmi8658
+from . import tmc2209
+__all__ = ['bm8563', 'ntp', 'qmi8658', 'tmc2209']
