8867d430199c37cf8a4bc2e9a59b72b1d67f2b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 15:05:50 +0000

    Add package plivo/4.55.3/plivo-4.55.3-py2.py3-none-any.whl

---
+++ packages/plivo/plivo-4.55.3-py2.py3-none-any.whl/plivo/xml/streamElement.py
@@ -0,0 +1,51 @@
+from plivo.utils.validators import *
+from plivo.xml import (
+    PlivoXMLElement,
+    map_type
+)
+
+
+class StreamElement(PlivoXMLElement):
+    _name = 'Stream'
+    _nestable = [
+    ]
+
+    def __init__(
+            self,
+            content,
+            bidirectional=None,
+            audioTrack=None,
+            streamTimeout=None,
+            statusCallbackUrl=None,
+            statusCallbackMethod=None,
+            contentType=None,
+            extraHeaders=None,
+            keepCallAlive=None
+    ):
+        super(StreamElement, self).__init__()
+
+        self.content = content
+        self.bidirectional = bidirectional
+        self.audioTrack = audioTrack
+        self.streamTimeout = streamTimeout
+        self.statusCallbackUrl = statusCallbackUrl
+        self.statusCallbackMethod = statusCallbackMethod
+        self.contentType = contentType
+        self.extraHeaders = extraHeaders
+        self.keepCallAlive = keepCallAlive
+
+    def to_dict(self):
+        d = {
+            'bidirectional': self.bidirectional,
+            'audioTrack': self.audioTrack,
+            'streamTimeout': self.streamTimeout,
+            'statusCallbackUrl': self.statusCallbackUrl,
+            'statusCallbackMethod': self.statusCallbackMethod,
+            'contentType': self.contentType,
+            'extraHeaders': self.extraHeaders,
+            'keepCallAlive': self.keepCallAlive
+        }
+        return {
+            k: six.text_type(map_type(v))
+            for k, v in d.items() if v is not None
+        }
