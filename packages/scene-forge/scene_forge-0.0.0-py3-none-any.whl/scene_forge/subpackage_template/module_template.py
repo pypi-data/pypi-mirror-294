ca393897f4babd47a15c4caf3f50baf767fdfb61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 03:07:39 +0000

    Add package scene-forge/0.0.0/scene_forge-0.0.0-py3-none-any.whl

---
+++ packages/scene-forge/scene_forge-0.0.0-py3-none-any.whl/scene_forge/subpackage_template/module_template.py
@@ -0,0 +1,30 @@
+"""scene_forge.subpackage_template.module_template - describe class"""
+
+__author__ = "Pieter De Vis <pieter.de.vis@npl.co.uk>"
+__all__ = []
+
+
+class ClassName:
+    """
+    Description of class
+
+    :param something: description of something
+    """
+
+    def __init__(self, something):
+        self.something = something
+
+    def function1(self, argument1, argument2):
+        """
+        Description of function
+
+        :param argument1: description of argument1
+        :param argument2: description of argument1
+        :return: description of return value
+        """
+
+        return argument1 + argument2
+
+
+if __name__ == "__main__":
+    pass
---
+++ packages/scene-forge/scene_forge-0.0.0-py3-none-any.whl/scene_forge/subpackage_template/tests/test_class_template.py
@@ -0,0 +1,16 @@
+"""scene_forge.subpackage_template.tests.test_module_template - describe class"""
+
+__author__ = "Pieter De Vis <pieter.de.vis@npl.co.uk>"
+__all__ = []
+
+
+import unittest
+
+
+class TestClassName(unittest.TestCase):
+    def test_function1(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
