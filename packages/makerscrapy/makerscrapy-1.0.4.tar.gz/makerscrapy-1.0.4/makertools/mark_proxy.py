230d09c8be4fafd865dda6069a94355ec3823f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 03:07:31 +0000

    Add package makerscrapy/1.0.4/makerscrapy-1.0.4.tar.gz

---
+++ packages/makerscrapy/makerscrapy-1.0.4.tar.gz/makerscrapy-1.0.4/makertools/mark_proxy.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+# -*- coding: UTF-8 -*-
+import requests
+
+
+
+def get_aby_proxy() -> dict:
+    ...
+
+
+def get_ipdeal_proxy() -> dict:
+    return {"https": "http://127.0.0.1:7890", "http": "http://127.0.0.1:7890"}
