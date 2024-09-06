f99ee5817aaa423bbe31c71d981a155bb3200ebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 16:05:58 +0000

    Add package galadriel-node/0.0.6/galadriel_node-0.0.6.tar.gz

---
+++ packages/galadriel-node/galadriel_node-0.0.6.tar.gz/galadriel_node-0.0.6/galadriel_node/cli/network.py
@@ -0,0 +1,26 @@
+import asyncio
+
+import typer
+
+from galadriel_node.config import config
+from galadriel_node.sdk import api
+
+network_app = typer.Typer(
+    name="network",
+    help="Galadriel tool to get network info",
+    no_args_is_help=True,
+)
+
+
+@network_app.command("stats", help="Get current network stats")
+def node_status(
+    api_url: str = typer.Option(config.GALADRIEL_API_URL, help="API url"),
+    api_key: str = typer.Option(config.GALADRIEL_API_KEY, help="API key"),
+):
+    config.raise_if_no_dotenv()
+    status, response_json = asyncio.run(api.get(api_url, "network/stats", api_key))
+    if status == 200 and response_json:
+        for k, v in response_json.items():
+            print(f"{k}: {v}", flush=True)
+    else:
+        print("Failed to get node status..", flush=True)
