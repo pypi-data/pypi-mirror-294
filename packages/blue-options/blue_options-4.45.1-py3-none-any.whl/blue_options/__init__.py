fea89eacbb7586acb68f282dd3abb965d27608a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 14:05:58 +0000

    Add package blue-options/4.45.1/blue_options-4.45.1-py3-none-any.whl

---
+++ packages/blue-options/blue_options-4.45.1-py3-none-any.whl/blue_options/__init__.py
@@ -0,0 +1,11 @@
+NAME = "blue_options"
+
+ICON = "🌀"
+
+DESCRIPTION = f"{ICON} an options argument for Bash."
+
+VERSION = "4.45.1"
+
+REPO_NAME = "blue-options"
+
+MARQUEE = "https://github.com/kamangir/assets/raw/main/blue-plugin/marquee.png?raw=true"
