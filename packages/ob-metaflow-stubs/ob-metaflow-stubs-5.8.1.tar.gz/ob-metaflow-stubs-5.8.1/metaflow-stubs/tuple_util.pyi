9f6d62ac3463424ae162718b424f188006c1ac95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 16:05:43 +0000

    Add package ob-metaflow-stubs/5.8.1/ob-metaflow-stubs-5.8.1.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-5.8.1.tar.gz/ob-metaflow-stubs-5.8.1/metaflow-stubs/tuple_util.pyi
@@ -0,0 +1,14 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.19.1+ob(v1)                                                   #
+# Generated on 2024-09-06T08:14:24.926544                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+def namedtuple_with_defaults(typename, field_descr, defaults = ()):
+    ...
+
+foreach_frame_field_list: list
+
