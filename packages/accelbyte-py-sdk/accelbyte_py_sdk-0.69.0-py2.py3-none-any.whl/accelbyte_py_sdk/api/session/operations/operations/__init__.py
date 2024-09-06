8fa4c7a556b0cf237a5ec96af851a87ca5b69e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 15:06:12 +0000

    Add package accelbyte-py-sdk/0.69.0/accelbyte_py_sdk-0.69.0-py2.py3-none-any.whl

---
+++ packages/accelbyte-py-sdk/accelbyte_py_sdk-0.69.0-py2.py3-none-any.whl/accelbyte_py_sdk/api/session/operations/operations/__init__.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: ags_py_codegen
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Session Service."""
+
+__version__ = "3.17.1"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .allocs_handler import AllocsHandler
+from .block_handler import BlockHandler
+from .cmdline_handler import CmdlineHandler
+from .get_healthcheck_info import GetHealthcheckInfo
+from .get_healthcheck_info_v1 import GetHealthcheckInfoV1
+from .goroutine_handler import GoroutineHandler
+from .heap_handler import HeapHandler
+from .mutex_handler import MutexHandler
+from .pprof_handler import PprofHandler
+from .profile import Profile
+from .symbol_handler import SymbolHandler
+from .threadcreate_handler import ThreadcreateHandler
+from .trace_handler import TraceHandler
