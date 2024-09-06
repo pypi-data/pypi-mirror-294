bc003b4a83b2f0e996805e6f3d7a29c42d2d9573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 16:05:29 +0000

    Add package genwiki2024/0.0.1/genwiki2024-0.0.1.tar.gz

---
+++ packages/genwiki2024/genwiki2024-0.0.1.tar.gz/genwiki2024-0.0.1/genwiki/addressbook.py
@@ -0,0 +1,102 @@
+"""
+Created on 19.08.2024
+
+@author: wf
+"""
+
+import logging
+import os
+
+from genwiki.template import TemplateMap, TemplateParam
+
+
+class AddressBookConverter:
+    """
+    convert Adressbook pages
+    """
+
+    def __init__(self, debug: bool = False):
+        self.debug = debug
+        self.template_map = TemplateMap(
+            template_name="Info Adressbuch",
+            topic_name="AddressBook",
+            param_mapping={
+                "Bild": TemplateParam(new_name="image"),
+                "Titel": TemplateParam(new_name="title"),
+                "Untertitel": TemplateParam(new_name="subtitle"),
+                "Autor / Hrsg.": TemplateParam(new_name="author"),
+                "Erscheinungsjahr": TemplateParam(new_name="year", to_int=True),
+                "Seitenzahl": TemplateParam(new_name="pageCount", to_int=True),
+                "Enthaltene Orte": TemplateParam(
+                    new_name="location", regex=r"\[\[GOV:(.*?)\|"
+                ),
+                "Verlag": TemplateParam(new_name="publisher"),
+                "GOV-Quelle": TemplateParam(new_name="govSource"),
+                "Standort online": TemplateParam(
+                    new_name="onlineSource", regex=r"\{\{ThULB\|(.*?)\}\}"
+                ),
+                "DES": TemplateParam(new_name="des"),
+            },
+        )
+        self.year_mapping = {"weimarTH1851.parquet": 1851, "weimarTH1853.parquet": 1853}
+
+    def convert(
+        self,
+        page_contents,
+        target_wiki=None,
+        mode="backup",
+        limit: int = None,
+        force: bool = False,
+        progress_bar=None,
+    ):
+        """
+        Convert the pages of the given page_contents dict.
+
+        Args:
+            page_contents (dict): Dictionary of page names and their content.
+            target_wiki (Wiki): Target wiki object for pushing content (if mode is 'push').
+            mode (str): 'push' to push to target wiki, 'backup' to store as backup files.
+            limit (int): Limit the number of pages to process.
+            force (bool): if not True only do a dry run
+            progress_bar (Progressbar): Progress bar object to update during conversion.
+        """
+        if force:
+            target_wiki.wiki_push.toWiki.login()
+        result = {}
+        total = len(page_contents) if limit is None else min(limit, len(page_contents))
+        if progress_bar:
+            progress_bar.total = total
+            progress_bar.set_description(f"Converting {total} pages")
+
+        for i, (page_name, page_content) in enumerate(page_contents.items()):
+            if limit and i >= limit:
+                break
+
+            ab_dict = self.template_map.as_template_dict(page_content)
+            markup = self.template_map.dict_to_markup(ab_dict)
+            result[page_name] = markup
+            if mode == "push" and target_wiki:
+                edit_status = target_wiki.wiki_push.edit_page_content(
+                    page_title=page_name,
+                    new_text=markup,
+                    summary="Converted AddressBook template",
+                    force=force,
+                )
+                logging.log(logging.INFO, edit_status)
+            elif mode == "backup":
+                backup_path = os.path.join(
+                    target_wiki.wiki_backup_dir, f"{page_name}.wiki"
+                )
+                os.makedirs(os.path.dirname(backup_path), exist_ok=True)
+                with open(backup_path, "w", encoding="utf-8") as f:
+                    f.write(markup)
+
+            if progress_bar:
+                progress_bar.update(1)
+
+            if self.debug:
+                print(f"Processed page: {page_name}")
+
+        if progress_bar:
+            progress_bar.set_description("Conversion complete")
+        return result
---
+++ packages/genwiki2024/genwiki2024-0.0.1.tar.gz/genwiki2024-0.0.1/genwiki/webserver.py
@@ -0,0 +1,132 @@
+"""
+Created on 2024-08-15
+
+@author: wf
+"""
+
+import os
+
+from lodstorage.query import QueryManager
+from lodstorage.sql import SQLDB
+from ngwidgets.input_webserver import InputWebserver, InputWebSolution
+from ngwidgets.login import Login
+from ngwidgets.profiler import Profiler
+from ngwidgets.users import Users
+from ngwidgets.webserver import WebserverConfig
+from nicegui import Client, ui
+from starlette.responses import RedirectResponse
+
+from genwiki.convert import ParquetAdressbokToSql
+from genwiki.query_view import QueryView
+from genwiki.version import Version
+
+
+class GenWikiWebServer(InputWebserver):
+    """WebServer class that manages the server and handles GenWiki operations."""
+
+    @classmethod
+    def get_config(cls) -> WebserverConfig:
+        copy_right = "(c)2024 Wolfgang Fahl"
+        config = WebserverConfig(
+            copy_right=copy_right,
+            version=Version(),
+            default_port=9852,
+            short_name="genwiki2024",
+        )
+        server_config = WebserverConfig.get(config)
+        server_config.solution_class = GenWikiSolution
+        return server_config
+
+    def examples_path(self) -> str:
+        # the root directory (default: examples)
+        path = os.path.join(os.path.dirname(__file__), "../genwiki_examples")
+        path = os.path.abspath(path)
+        return path
+
+    def __init__(self):
+        """Constructs all the necessary attributes for the WebServer object."""
+        InputWebserver.__init__(self, config=GenWikiWebServer.get_config())
+        users = Users(self.config.base_path)
+        self.login = Login(self, users)
+        address_db_path = os.path.join(self.config.storage_path, "address.db")
+        if os.path.isfile(address_db_path) and os.path.getsize(address_db_path) > 0:
+            self.sql_db = SQLDB(address_db_path, check_same_thread=False)
+        else:
+            profiler = Profiler("convert parquet files", profile=True)
+            pats = ParquetAdressbokToSql(folder=self.examples_path())
+            self.sql_db = SQLDB(address_db_path, check_same_thread=False)
+            pats.to_db(self.sql_db)
+            profiler.time()
+
+        yaml_path = os.path.join(self.examples_path(), "queries.yaml")
+        self.qm = QueryManager(
+            lang="sql", queriesPath=yaml_path, with_default=False, debug=self.debug
+        )
+
+        @ui.page("/")
+        async def home(client: Client):
+            return await self.page(client, GenWikiSolution.home)
+
+        @ui.page("/login")
+        async def login(client: Client):
+            return await self.page(client, GenWikiSolution.login_ui)
+
+        @ui.page("/logout")
+        async def logout(client: Client) -> RedirectResponse:
+            if self.login.authenticated():
+                await self.login.logout()
+            return RedirectResponse("/")
+
+
+class GenWikiSolution(InputWebSolution):
+    """
+    the GenWiki solution
+    """
+
+    def __init__(self, webserver: GenWikiWebServer, client: Client):
+        """
+        Initialize the solution
+
+        Args:
+            webserver (GenWikiWebServer): The webserver instance associated with this solution.
+            client (Client): The client instance this context is associated with.
+        """
+        super().__init__(webserver, client)
+        self.qm = self.webserver.qm
+        self.sql_db = self.webserver.sql_db
+
+    def authenticated(self) -> bool:
+        """
+        Check if the user is authenticated.
+        Returns:
+            True if the user is authenticated, False otherwise.
+        """
+        return self.webserver.login.authenticated()
+
+    def setup_menu(self, detailed: bool = True):
+        """
+        setup the menu
+        """
+        super().setup_menu(detailed=detailed)
+        ui.button(icon="menu", on_click=lambda: self.header.toggle())
+        with self.header:
+            if self.authenticated():
+                self.link_button("logout", "/logout", "logout", new_tab=False)
+            else:
+                self.link_button("login", "/login", "login", new_tab=False)
+
+    async def login_ui(self):
+        """
+        login ui
+        """
+        await self.webserver.login.login(self)
+
+    async def home(self):
+        """Provide the main content page"""
+
+        def setup_home():
+            """ """
+            self.query_view = QueryView(self, qm=self.qm, sql_db=self.sql_db)
+            self.query_view.setup_ui()
+
+        await self.setup_content_div(setup_home)
