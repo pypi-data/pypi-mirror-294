da8f362f792f660218cb63d832c6f3e9c8b765bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 05:05:05 +0000

    Add package PyInventory/0.20.1/pyinventory-0.20.1.tar.gz

---
+++ packages/PyInventory/pyinventory-0.20.1.tar.gz/pyinventory-0.20.1/inventory/__init__.py
@@ -0,0 +1,11 @@
+"""
+    PyInventory
+    Web based management to catalog things including state and location etc. using Python/Django.
+
+    created 14.20.2020 by Jens Diemer <opensource@jensdiemer.de>
+    :copyleft: 2020-2024 by the PyInventory team, see AUTHORS for more details.
+    :license: GNU GPL v3 or above, see LICENSE for more details.
+"""
+
+__version__ = '0.20.1'
+__author__ = 'Jens Diemer <PyInventory@jensdiemer.de>'
