ce80823784e65399ba5948bf8d7651b7451c80e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 16:05:53 +0000

    Add package sync_ics2gcal/0.1.5/sync_ics2gcal-0.1.5-py3-none-any.whl

---
+++ packages/sync_ics2gcal/sync_ics2gcal-0.1.5-py3-none-any.whl/sync_ics2gcal/sync_calendar.py
@@ -0,0 +1,53 @@
+from typing import Dict, Any, Union
+
+import yaml
+
+import dateutil.parser
+import datetime
+import logging
+import logging.config
+from . import CalendarConverter, GoogleCalendarService, GoogleCalendar, CalendarSync
+
+ConfigDate = Union[str, datetime.datetime]
+
+
+def load_config() -> Dict[str, Any]:
+    with open("config.yml", "r", encoding="utf-8") as f:
+        result = yaml.safe_load(f)
+    return result  # type: ignore
+
+
+def get_start_date(date: ConfigDate) -> datetime.datetime:
+    if isinstance(date, datetime.datetime):
+        return date
+    if "now" == date:
+        result = datetime.datetime.utcnow()
+    else:
+        result = dateutil.parser.parse(date)
+    return result
+
+
+def main() -> None:
+    config = load_config()
+
+    if "logging" in config:
+        logging.config.dictConfig(config["logging"])
+
+    calendar_id: str = config["calendar"]["google_id"]
+    ics_filepath: str = config["calendar"]["source"]
+
+    start = get_start_date(config["start_from"])
+
+    converter = CalendarConverter()
+    converter.load(ics_filepath)
+
+    service = GoogleCalendarService.from_config(config)
+    gcalendar = GoogleCalendar(service, calendar_id)
+
+    sync = CalendarSync(gcalendar, converter)
+    sync.prepare_sync(start)
+    sync.apply()
+
+
+if __name__ == "__main__":
+    main()
