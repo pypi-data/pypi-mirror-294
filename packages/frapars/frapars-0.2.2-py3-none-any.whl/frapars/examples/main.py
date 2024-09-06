a03adddbf71ad6c1010db3d590e0f6253c3f57e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 04:05:34 +0000

    Add package frapars/0.2.2/frapars-0.2.2-py3-none-any.whl

---
+++ packages/frapars/frapars-0.2.2-py3-none-any.whl/frapars/examples/main.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import chardet
+import csv
+from frapars.functions.addresses import parse_all_parallel, parse_all, parse
+from frapars.constants import out_file_path as out_csv_path
+import importlib.metadata
+import argparse
+import time
+# import ptvsd
+
+version = importlib.metadata.version('frapars')
+
+
+def print_banner():
+    # Read the contents of the banner.txt file
+    with open('banner.txt', 'r') as file:
+        banner_text = file.read()
+    # Replace ${application.version} with the desired version
+    banner_text = banner_text.replace('${application.version}', version)
+    # Print the modified banner
+    print(banner_text)
+
+
+def csv_from_dict(file_path, data):
+    print(f"Storing into file the results")
+    # Write the list of dictionaries to a CSV file
+    with open(file_path, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=data[0].keys())
+        writer.writeheader()
+        writer.writerows(data)
+    print("Result printed at: ", file_path)
+
+
+def parse_from_file(in_file_path, limit=None):
+    # Read the file in binary mode
+    with open(in_file_path, 'rb') as file:
+        raw_data = file.read()
+    # Detect the encoding
+    print("Examin input file ...")
+    result = chardet.detect(raw_data)
+    print(f"Detected encoding: {result['encoding']} with confidence {result['confidence']}")
+    df = pd.read_csv(in_file_path, dtype='str', encoding=result['encoding'] if result['confidence'] > 0.7 else 'utf-8')
+    df = df.dropna()
+    if limit:
+        csv_addr_list = list(df['address'])[:limit]
+    else:
+        csv_addr_list = list(df['address'])
+
+    print(f"Found {len(csv_addr_list)} addresses to parse")
+    return parse_all_parallel(csv_addr_list)
+
+
+def parse_from_list(list_of_addresses):
+    #  split the addresses
+    addresses = list_of_addresses.split(';')
+    # Print each value as list
+    return parse_all_parallel(addresses)
+
+
+def parse_from_address(address_str):
+    parse(address_str)
+
+
+def main():
+    start_time = time.time()
+    print_banner()
+    # Create the argument parser
+    parser = argparse.ArgumentParser(description='Example Argument Parser')
+    # Create a mutually exclusive group for input options
+    input_group = parser.add_mutually_exclusive_group(required=True)
+
+    # Add arguments
+    input_group.add_argument('-i', '--input-file',
+                             type=str, help='Path to the input file')
+    input_group.add_argument('-l', '--list', type=str,
+                             help='List of inputs. Needs to be on format "rue saint-Philippe 31; Aevenue Carlos (De) 31"')
+    input_group.add_argument('-a', '--address', type=str,
+                             help='Single input address. Ex: "rue saint-Philippe 31 (De)"')
+    input_group.add_argument('-o', '--output', type=str,
+                             choices=['show', 'file'],
+                             default='show',
+                             help='Output option. Choose "show" to display the result or "file" to store it in a file')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Execute based on the input arg
+    if args.input_file:
+        result = parse_from_file(args.input_file)
+    if args.list:
+        print(args.list)
+        result = parse_from_list(args.list)
+    if args.address:
+        result = parse_from_address(args.address)
+
+    if args.output == 'file':
+        results = [res.to_dict() for res in  result]
+        #  store in the file the data collected
+        csv_from_dict(out_csv_path, results)
+    else:
+        for entry in result:
+            print(f"Raw: {entry.raw} --> Formatted: {entry.formatted}")
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Process has finished in: {execution_time} seconds")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/frapars/frapars-0.2.2-py3-none-any.whl/frapars/functions/addresses.py
@@ -0,0 +1,167 @@
+import re
+from frapars.functions import clean_str
+import frapars.constants.regex as rx
+from tqdm import tqdm
+from typing import List
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from frapars.models.address_result import AddressResult 
+import logging
+
+
+def score_percentage(text):
+    # Calculate the percentage score based on the length of the string
+    if len(text) <= 50:
+        # If the length is less than or equal to 50, calculate the percentage
+        percentage_score = ((50 - len(text)) / 50)
+    else:
+        # If the length is more than 50, percentage score is 0%
+        percentage_score = 1.0
+    return percentage_score
+
+
+def format_details(address_info):
+    address_str = "{} {} {} {} {} {} {} {} {}".format(
+        (' ').join(address_info.get('addres_num', '')),
+        (' ').join(address_info.get('urba_names', '')),
+        (' ').join(address_info.get('prepositions', '')),
+        (' ').join(address_info.get('street_name', '')),
+        (' ').join(address_info.get('city', '')),
+        (' ').join(address_info.get('postcode', '')),
+        (' ').join(address_info.get('insee', '')),
+        (' ').join(address_info.get('codes', '')),
+        (' ').join(address_info.get('department', '')),
+    )
+    # Replace multiple spaces with a single space
+    address_str = re.sub(r'\s+', ' ', address_str)
+    # Capitalize the first letter of each word
+    return address_str.title()
+
+
+def parse_all(addresses_list: List[str]):
+    """Parse a list of addresses address. The addresses can be still be formed by more addresses inside. (by mistake)
+     Like: 15 rue saint philippe ; 13 rue 4 march
+
+     Args:
+         addresses_list (List): list of addresses
+     Returns:
+         List: the list of parsed
+     """
+    parsed_addresses = []
+    for address in tqdm(addresses_list, desc="Processing", unit="item"):
+        parsed_addresses.append(parse(address))
+    return parsed_addresses
+
+def process_batch(batch_with_indices: List[tuple]) -> List[tuple]:
+    """Process a batch of addresses and return results with their original indices."""
+    batch_indices, batch = zip(*batch_with_indices)
+    parsed_batch = [AddressResult(address, parse(address)) for address in tqdm(batch, desc="Processing batch", unit="item")]
+    return list(zip(batch_indices, parsed_batch))
+
+
+def parse_all_parallel(addresses_list: List[str], batch_size=5000, n_threads=6) -> List[dict]:
+    """Parse a large list of addresses in batches, processing up to n_threads batches concurrently.
+    
+    Args:
+        addresses_list (List[str]): List of addresses to be parsed.
+        batch_size (int): Number of addresses per batch.
+        n_threads (int): Number of threads to use for batch processing.
+        
+    Returns:
+        List[dict]: List of parsed addresses in the same order as input.
+    """
+    # Create indexed batches
+    logging.debug(f"Setting up {n_threads} threads to process {len(addresses_list)} addresses in batches of {batch_size}...")
+    indexed_addresses = list(enumerate(addresses_list))
+    batches = [indexed_addresses[i:i + batch_size] for i in range(0, len(indexed_addresses), batch_size)]
+    
+    parsed_addresses = [None] * len(addresses_list)
+
+    def process_batch_and_collect(batch_with_indices: List[tuple]) -> List[tuple]:
+        """Process a batch and collect the results with their original indices."""
+        return process_batch(batch_with_indices)
+
+    # Use ThreadPoolExecutor to process batches concurrently
+    with ThreadPoolExecutor(max_workers=n_threads) as executor:
+        futures = [executor.submit(process_batch_and_collect, batch) for batch in batches]
+
+        for future in as_completed(futures):
+            try:
+                # Collect results and place them in the correct order
+                results_with_indices = future.result()
+                for index, parsed_address in results_with_indices:
+                    parsed_addresses[index] = parsed_address
+            except Exception as e:
+                logging.error(f"An error occurred: {e}")
+
+    return parsed_addresses
+
+
+def parse(addresses_str, sep='et'):
+    """Parse an address. The address can still be formed by more addresses inside. (by mistake) 
+    Like: 15 rue saint philippe ; 13 rue 4 march
+
+    Args:
+        addresses_str (_type_): address location
+        sep (str, optional): char, symbol or sequence of char that  we want as a separator for the final addresses. Defaults to 'et'.
+
+    Returns:
+        _type_: parsed single address
+    """
+    parsed_addresses = []
+    # there can be multiple str
+    addresses = re.split(r';|et', addresses_str)
+    for address_str in addresses:
+        address_str = address_str.strip()
+        parsed_addresses.append(parse_single_address(address_str))
+    return f' {sep} '.join(parsed_addresses)
+
+
+def parse_single_address(address_str):
+    logging.debug(f"Initial address is: {address_str}")
+    addr_details = {}
+    norm_address = clean_str.normalize_text(address_str)
+
+    # Find all matches for urban names using the compiled pattern
+    addr_details['urba_names'], norm_address = rx.exec(
+        rx.urban_names_pattern, norm_address)
+
+    # Find all matches for preposition using the compiled pattern
+    addr_details['prepositions'], norm_address = rx.exec(
+        rx.prepositions_pattern, norm_address)
+    norm_address = norm_address.replace('()', '')
+
+    # Find all matches for city using the compiled pattern
+    date_streets, norm_address = rx.exec(
+        rx.date_pattern, norm_address)
+
+    # Find all matches for city using the compiled pattern
+    addr_details['city'], norm_address = rx.exec(
+        rx.city_pattern, norm_address)
+
+    # Find all matches for postcode using the compiled pattern
+    codes, norm_address = rx.exec(
+        rx.postal_insee_code_pattern, norm_address)
+    # find the category of the insee
+    if len(codes):
+        addr_details['insee'], addr_details['postcode'], addr_details['codes'] = rx.parse_codes(
+            codes)
+
+    # Find all matches for address number using the compiled pattern
+    addr_details['addres_num'], norm_address = rx.exec(
+        rx.address_num_pattern, norm_address)
+
+    # Find all matches for address_num using the compiled pattern
+    addr_details['department'], norm_address = rx.exec(
+        rx.department_pattern, norm_address)
+
+    # Handle the rest
+    addr_details['street_name'], norm_address = rx.exec(
+        rx.street_name_pattern, norm_address)
+    addr_details['street_name'].extend(date_streets)
+
+    parsed_address = format_details(addr_details)
+    logging.debug(f"Result is: {parsed_address}")
+    logging.debug(f"Details: {addr_details}")
+    logging.debug(f"Unparsed string remained is: {norm_address}")
+    logging.debug(f"Parse quality scored: {score_percentage(norm_address)}")
+    return parsed_address.strip()
