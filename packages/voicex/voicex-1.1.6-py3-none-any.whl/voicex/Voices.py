4a5f94f054fb9577fb97385c95b5bca2cee95fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 16:05:57 +0000

    Add package voicex/1.1.6/voicex-1.1.6-py3-none-any.whl

---
+++ packages/voicex/voicex-1.1.6-py3-none-any.whl/voicex/Voices.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+class Voices(Enum):
+    SILVAN = "voices/silvan.voicex"
+    GABBY = "voices/gabby.voicex"
+    GRANNY = "voices/granny.voicex"
+    KIMBERLY = "voices/kimberly.voicex"
+    ELISABETH = "voices/drw_female_1.voicex"
+    LISA = "voices/lisa_old.voicex"
+    DRW_FEMALE_1 = "voices/drw_female_1.voicex"
+    DRW_FEMALE_2 = "voices/drw_female_2.voicex"
+    DRW_FEMALE_3 = "voices/drw_female_3.voicex"
+    MALE_DEPRESSED = "voices/male_depressed.voicex"
+    MALE_SHY_1 = "voices/male_shy_1.voicex"
+    MALE_SHY_2 = "voices/male_shy_2.voicex"
+    LENNY = "voices/lenny.voicex"
+    ANDREA = "voices/andrea.voicex"
