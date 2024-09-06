144812721979f22f005ee40291f1ee5a6aab7c2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 05:04:53 +0000

    Add package log-abstractor/1.1.18/log_abstractor-1.1.18-py3-none-any.whl

---
+++ packages/log-abstractor/log_abstractor-1.1.18-py3-none-any.whl/abstract_logger/anonymizer.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+import scrubadub
+import re
+class AnonymizationAdapter(ABC):
+    @abstractmethod
+    def anonymize(self, text: str) -> str:
+        pass
+
+class Anonymizer(AnonymizationAdapter):
+    def __init__(self, config):
+        self.pre_scrub_patterns = config.get("anonymize_patterns", {})
+        self.use_scrubadub = config.get("use_scrubadub", True)
+
+    def pre_scrub(self, text: str) -> str:
+        for pattern in self.pre_scrub_patterns.values():
+            text = re.sub(pattern, "<anonymized>", text)
+        return text
+
+    def anonymize(self, text: str) -> str:
+        text = self.pre_scrub(text)  # Always perform pre-scrubbing
+        if self.use_scrubadub:
+            text = scrubadub.clean(text)  # Only apply Scrubadub if enabled
+        return text
