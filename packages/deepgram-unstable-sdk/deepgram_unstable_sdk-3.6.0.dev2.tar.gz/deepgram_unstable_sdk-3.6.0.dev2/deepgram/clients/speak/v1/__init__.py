7ac0300dc0e3058cb23b5474def5d9d9fa4512bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 06:05:09 +0000

    Add package deepgram-unstable-sdk/3.6.0.dev2/deepgram_unstable_sdk-3.6.0.dev2.tar.gz

---
+++ packages/deepgram-unstable-sdk/deepgram_unstable_sdk-3.6.0.dev2.tar.gz/deepgram_unstable_sdk-3.6.0.dev2/deepgram/clients/speak/v1/__init__.py
@@ -0,0 +1,38 @@
+# Copyright 2024 Deepgram SDK contributors. All Rights Reserved.
+# Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+# SPDX-License-Identifier: MIT
+
+from .rest import (
+    SpeakRESTOptions,
+    SpeakOptions,
+    SpeakRestSource,
+    SpeakSource,
+    FileSource,
+)
+from .websocket import (
+    SpeakWSOptions,
+)
+from ....options import DeepgramClientOptions, ClientOptionsFromEnv
+
+# rest
+from .rest import SpeakRESTClient, AsyncSpeakRESTClient
+
+from .rest import SpeakRESTResponse
+
+# websocket
+from .websocket import (
+    SpeakWebSocketClient,
+    AsyncSpeakWebSocketClient,
+    SpeakWSClient,
+    AsyncSpeakWSClient,
+)
+from .websocket import (
+    OpenResponse,
+    MetadataResponse,
+    FlushedResponse,
+    ClearedResponse,
+    CloseResponse,
+    UnhandledResponse,
+    WarningResponse,
+    ErrorResponse,
+)
