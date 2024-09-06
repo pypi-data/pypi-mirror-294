d4f097cd2d47622387239fe165090b2ca84c706c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 07:04:55 +0000

    Add package keyv/0.1.2/keyv-0.1.2.tar.gz

---
+++ packages/keyv/keyv-0.1.2.tar.gz/keyv-0.1.2/README.md
@@ -0,0 +1,58 @@
+# nosqueel
+
+A lightweight, file-based NoSQL key-value database designed for simple and practical uses. It requires no external dependencies and stores all data locally in a single file. Perfect for applications needing a straightforward and efficient storage solution.
+
+```bash
+pip install nosqueel
+```
+# How it works
+nosqueel uses sqlite3 as its engine, thereby benefiting from its power, integrity, and practicality. This looks strange but it works like a charm.
+
+# Usage
+
+```python
+from nosqueel import NoSqueel
+
+# initialize the database
+db = NoSqueel('db.nosqueel') # or .db, or .anything, you choose
+```
+###
+```python
+# insert a key-value pair
+db.put('key1', 'value1')
+
+# by default the .put method is unique safe, i.e. it raises an error
+# if the key already exists. if you don't want that behavior you can
+# use replace_if_exists=True.
+db.put('key1', 'value1', replace_if_exists=True)
+
+# retrieve a value by key
+value = db.get('key1')
+print(value)  # output: 'value1'
+# note: the get method returns None for non-existing keys
+
+# update a value
+db.update('key1', 123)
+updated_value = db.get('key1')
+print(updated_value)  # output: 123
+
+# delete a key-value pair
+db.delete('key1')
+
+# search for keys by value
+db.put('key2', 'common_value')
+db.put('key3', 'common_value')
+keys = db.search('common_value')
+print(keys)  # output: ['key2', 'key3']
+
+# retrieve all keys
+all_keys = db.keys()
+print(all_keys)  # output: ['key2', 'key3']
+```
+###
+Nosqueel uses pickle to serialize data (keys and values). This means you are free to use any python type that can be serialized using pickle for both keys and values.
+
+### Roadmap
+- [ ] tables
+- [ ] begin immediate transaction (for better locking)
+- [ ] clean code/make it more efficient
---
+++ packages/keyv/keyv-0.1.2.tar.gz/keyv-0.1.2/keyv.py
@@ -0,0 +1,130 @@
+import pickle
+import sqlite3
+from pathlib import Path
+from sqlite3 import Connection, IntegrityError
+from typing import Any, Union, List
+
+
+def _encode(x: Any) -> bytes:
+    return pickle.dumps(x)
+
+
+def _decode(x: Any) -> bytes:
+    return pickle.loads(x)
+
+
+class KeyV:
+    def __init__(
+        self,
+        path: Union[str, Path],
+        wal_mode: bool = True,
+        synchronous: int = 1,
+        **kwargs,
+    ):
+        if isinstance(path, str):
+            path = Path(path)
+
+        self.path = path
+        self._sqlite_kwargs = kwargs
+        self._wal_mode = wal_mode
+        self._synchronous = synchronous
+        self._conn: Connection | None = None
+        self._create_dir_if_not_exists()
+        self._init()
+
+    def _create_dir_if_not_exists(self):
+        parents = self.path.parent
+        if not parents.exists():
+            parents.mkdir(parents=True, exist_ok=True)
+
+    def _get_conn(self):
+        if self._conn is None:
+            self._conn = sqlite3.connect(
+                self.path,
+                check_same_thread=False,
+                **self._sqlite_kwargs,
+            )
+        return self._conn
+
+    def _init(self):
+        conn = self._get_conn()
+        cursor = conn.cursor()
+        if self._wal_mode:
+            cursor.execute("PRAGMA journal_mode=WAL;")
+        cursor.execute(f"PRAGMA synchronous={self._synchronous};")
+        conn.commit()
+
+        # Creates base table
+        conn.execute("create table if not exists data (key blob unique, value blob)")
+        conn.execute("create unique index if not exists idx_key on data(key)")
+        conn.commit()
+
+        # self._ensure_wal_mode()
+
+    def close(self):
+        """Closes the database connection"""
+        self._get_conn().close()
+
+    def put(self, key: Any, value: Any, replace_if_exists: bool = False):
+        kp = _encode(key)
+        vp = _encode(value)
+        with self._get_conn() as conn:
+            cursor = conn.cursor()
+            try:
+                cursor.execute("insert into data (key, value) values (?, ?)", (kp, vp))
+            except IntegrityError:
+                if replace_if_exists:
+                    self.update(key, value)
+                    return
+                raise ValueError(f"key {key} already exists")
+            conn.commit()
+
+    def get(self, key: Any) -> Any:
+        kp = _encode(key)
+        with self._get_conn() as conn:
+            cursor = conn.cursor()
+            cursor.execute("select value from data where key = ?", (kp,))
+            result = cursor.fetchone()
+            if result:
+                vp = result[0]
+                return _decode(vp)
+            return None
+
+    def update(self, key: Any, value: Any):
+        # TODO
+        # raises an good error key if does not exists
+        # also option to create the key and not raise the error
+        kp = _encode(key)
+        vp = _encode(value)
+        with self._get_conn() as conn:
+            cursor = conn.cursor()
+            cursor.execute("update data set value = ? where key = ?", (vp, kp))
+            conn.commit()
+
+    def delete(self, key: Any):
+        kp = _encode(key)
+        with self._get_conn() as conn:
+            cursor = conn.cursor()
+            cursor.execute("delete from data where key = ?", (kp,))
+            conn.commit()
+
+    def search(self, value: Any) -> List[Any]:
+        vp = _encode(value)
+        with self._get_conn() as conn:
+            cursor = conn.cursor()
+            cursor.execute("select key from data where value = ?", (vp,))
+            result = cursor.fetchall()
+            if result:
+                data = [_decode(row[0]) for row in result]
+                return data
+            return []
+
+    def keys(self) -> List[Any]:
+        with self._get_conn() as conn:
+            cursor = conn.cursor()
+            cursor.execute("select key from data")
+            result = cursor.fetchall()
+            if result:
+                data = [_decode(row[0]) for row in result]
+                return data
+            return []
