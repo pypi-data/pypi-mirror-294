8f0ff868ec2f8630ecbb3e9a68593dc1fbfb5ba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 16:05:42 +0000

    Add package roop-custom-logger/0.0.2/roop_custom_logger-0.0.2.tar.gz

---
+++ packages/roop-custom-logger/roop_custom_logger-0.0.2.tar.gz/roop_custom_logger-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+    
+setup(
+    name='roop_custom_logger',
+    version='0.0.2',
+    description='Generic package which can be used for logging purpose',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Roopendra',
+    author_email='roopendra.naik@informa.com',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "asgi-correlation-id"
+    ],
+)
