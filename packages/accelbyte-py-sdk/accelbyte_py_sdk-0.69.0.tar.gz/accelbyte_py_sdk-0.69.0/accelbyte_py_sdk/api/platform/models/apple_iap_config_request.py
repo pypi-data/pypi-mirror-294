b57ca8148aa4c08054ee24e8b1493de70c2c0197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 15:06:12 +0000

    Add package accelbyte-py-sdk/0.69.0/accelbyte_py_sdk-0.69.0.tar.gz

---
+++ packages/accelbyte-py-sdk/accelbyte_py_sdk-0.69.0.tar.gz/accelbyte_py_sdk-0.69.0/accelbyte_py_sdk/api/platform/models/apple_iap_config_request.py
@@ -0,0 +1,156 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: ags_py_codegen
+
+# AccelByte Gaming Services Platform Service
+
+# pylint: disable=duplicate-code
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-public-methods
+# pylint: disable=too-many-return-statements
+# pylint: disable=too-many-statements
+# pylint: disable=unused-import
+
+from __future__ import annotations
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from ....core import Model
+
+
+class AppleIAPConfigRequest(Model):
+    """Apple IAP config request (AppleIAPConfigRequest)
+
+    Properties:
+        bundle_id: (bundleId) REQUIRED str
+
+        password: (password) OPTIONAL str
+    """
+
+    # region fields
+
+    bundle_id: str  # REQUIRED
+    password: str  # OPTIONAL
+
+    # endregion fields
+
+    # region with_x methods
+
+    def with_bundle_id(self, value: str) -> AppleIAPConfigRequest:
+        self.bundle_id = value
+        return self
+
+    def with_password(self, value: str) -> AppleIAPConfigRequest:
+        self.password = value
+        return self
+
+    # endregion with_x methods
+
+    # region to methods
+
+    def to_dict(self, include_empty: bool = False) -> dict:
+        result: dict = {}
+        if hasattr(self, "bundle_id"):
+            result["bundleId"] = str(self.bundle_id)
+        elif include_empty:
+            result["bundleId"] = ""
+        if hasattr(self, "password"):
+            result["password"] = str(self.password)
+        elif include_empty:
+            result["password"] = ""
+        return result
+
+    # endregion to methods
+
+    # region static methods
+
+    @classmethod
+    def create(
+        cls, bundle_id: str, password: Optional[str] = None, **kwargs
+    ) -> AppleIAPConfigRequest:
+        instance = cls()
+        instance.bundle_id = bundle_id
+        if password is not None:
+            instance.password = password
+        return instance
+
+    @classmethod
+    def create_from_dict(
+        cls, dict_: dict, include_empty: bool = False
+    ) -> AppleIAPConfigRequest:
+        instance = cls()
+        if not dict_:
+            return instance
+        if "bundleId" in dict_ and dict_["bundleId"] is not None:
+            instance.bundle_id = str(dict_["bundleId"])
+        elif include_empty:
+            instance.bundle_id = ""
+        if "password" in dict_ and dict_["password"] is not None:
+            instance.password = str(dict_["password"])
+        elif include_empty:
+            instance.password = ""
+        return instance
+
+    @classmethod
+    def create_many_from_dict(
+        cls, dict_: dict, include_empty: bool = False
+    ) -> Dict[str, AppleIAPConfigRequest]:
+        return (
+            {k: cls.create_from_dict(v, include_empty=include_empty) for k, v in dict_}
+            if dict_
+            else {}
+        )
+
+    @classmethod
+    def create_many_from_list(
+        cls, list_: list, include_empty: bool = False
+    ) -> List[AppleIAPConfigRequest]:
+        return (
+            [cls.create_from_dict(i, include_empty=include_empty) for i in list_]
+            if list_
+            else []
+        )
+
+    @classmethod
+    def create_from_any(
+        cls, any_: any, include_empty: bool = False, many: bool = False
+    ) -> Union[
+        AppleIAPConfigRequest,
+        List[AppleIAPConfigRequest],
+        Dict[Any, AppleIAPConfigRequest],
+    ]:
+        if many:
+            if isinstance(any_, dict):
+                return cls.create_many_from_dict(any_, include_empty=include_empty)
+            elif isinstance(any_, list):
+                return cls.create_many_from_list(any_, include_empty=include_empty)
+            else:
+                raise ValueError()
+        else:
+            return cls.create_from_dict(any_, include_empty=include_empty)
+
+    @staticmethod
+    def get_field_info() -> Dict[str, str]:
+        return {
+            "bundleId": "bundle_id",
+            "password": "password",
+        }
+
+    @staticmethod
+    def get_required_map() -> Dict[str, bool]:
+        return {
+            "bundleId": True,
+            "password": False,
+        }
+
+    # endregion static methods
---
+++ packages/accelbyte-py-sdk/accelbyte_py_sdk-0.69.0.tar.gz/accelbyte_py_sdk-0.69.0/accelbyte_py_sdk/api/platform/models/google_iap_config_request.py
@@ -0,0 +1,155 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: ags_py_codegen
+
+# AccelByte Gaming Services Platform Service
+
+# pylint: disable=duplicate-code
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-public-methods
+# pylint: disable=too-many-return-statements
+# pylint: disable=too-many-statements
+# pylint: disable=unused-import
+
+from __future__ import annotations
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from ....core import Model
+
+
+class GoogleIAPConfigRequest(Model):
+    """Google IAP config request (GoogleIAPConfigRequest)
+
+    Properties:
+        application_name: (applicationName) REQUIRED str
+
+        service_account_id: (serviceAccountId) REQUIRED str
+    """
+
+    # region fields
+
+    application_name: str  # REQUIRED
+    service_account_id: str  # REQUIRED
+
+    # endregion fields
+
+    # region with_x methods
+
+    def with_application_name(self, value: str) -> GoogleIAPConfigRequest:
+        self.application_name = value
+        return self
+
+    def with_service_account_id(self, value: str) -> GoogleIAPConfigRequest:
+        self.service_account_id = value
+        return self
+
+    # endregion with_x methods
+
+    # region to methods
+
+    def to_dict(self, include_empty: bool = False) -> dict:
+        result: dict = {}
+        if hasattr(self, "application_name"):
+            result["applicationName"] = str(self.application_name)
+        elif include_empty:
+            result["applicationName"] = ""
+        if hasattr(self, "service_account_id"):
+            result["serviceAccountId"] = str(self.service_account_id)
+        elif include_empty:
+            result["serviceAccountId"] = ""
+        return result
+
+    # endregion to methods
+
+    # region static methods
+
+    @classmethod
+    def create(
+        cls, application_name: str, service_account_id: str, **kwargs
+    ) -> GoogleIAPConfigRequest:
+        instance = cls()
+        instance.application_name = application_name
+        instance.service_account_id = service_account_id
+        return instance
+
+    @classmethod
+    def create_from_dict(
+        cls, dict_: dict, include_empty: bool = False
+    ) -> GoogleIAPConfigRequest:
+        instance = cls()
+        if not dict_:
+            return instance
+        if "applicationName" in dict_ and dict_["applicationName"] is not None:
+            instance.application_name = str(dict_["applicationName"])
+        elif include_empty:
+            instance.application_name = ""
+        if "serviceAccountId" in dict_ and dict_["serviceAccountId"] is not None:
+            instance.service_account_id = str(dict_["serviceAccountId"])
+        elif include_empty:
+            instance.service_account_id = ""
+        return instance
+
+    @classmethod
+    def create_many_from_dict(
+        cls, dict_: dict, include_empty: bool = False
+    ) -> Dict[str, GoogleIAPConfigRequest]:
+        return (
+            {k: cls.create_from_dict(v, include_empty=include_empty) for k, v in dict_}
+            if dict_
+            else {}
+        )
+
+    @classmethod
+    def create_many_from_list(
+        cls, list_: list, include_empty: bool = False
+    ) -> List[GoogleIAPConfigRequest]:
+        return (
+            [cls.create_from_dict(i, include_empty=include_empty) for i in list_]
+            if list_
+            else []
+        )
+
+    @classmethod
+    def create_from_any(
+        cls, any_: any, include_empty: bool = False, many: bool = False
+    ) -> Union[
+        GoogleIAPConfigRequest,
+        List[GoogleIAPConfigRequest],
+        Dict[Any, GoogleIAPConfigRequest],
+    ]:
+        if many:
+            if isinstance(any_, dict):
+                return cls.create_many_from_dict(any_, include_empty=include_empty)
+            elif isinstance(any_, list):
+                return cls.create_many_from_list(any_, include_empty=include_empty)
+            else:
+                raise ValueError()
+        else:
+            return cls.create_from_dict(any_, include_empty=include_empty)
+
+    @staticmethod
+    def get_field_info() -> Dict[str, str]:
+        return {
+            "applicationName": "application_name",
+            "serviceAccountId": "service_account_id",
+        }
+
+    @staticmethod
+    def get_required_map() -> Dict[str, bool]:
+        return {
+            "applicationName": True,
+            "serviceAccountId": True,
+        }
+
+    # endregion static methods
