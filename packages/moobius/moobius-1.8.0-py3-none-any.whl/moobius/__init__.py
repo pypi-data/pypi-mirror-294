a040c10635c8f14309e6865fbb46fa8f9ab4db56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 15:06:05 +0000

    Add package moobius/1.8.0/moobius-1.8.0-py3-none-any.whl

---
+++ packages/moobius/moobius-1.8.0-py3-none-any.whl/moobius/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+
+from moobius.core.sdk import Moobius
+from moobius.core.wand import MoobiusWand
+from moobius.database.storage import MoobiusStorage
+
+import sys
+if sys.argv[0] == '-m': # Quickstart option.
+    from . import quickstart
+    quickstart.save_starter_ccs()
