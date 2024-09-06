7c7954c0f1e0dff6ddcf883664a63f2a81f81676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 14:06:38 +0000

    Add package cfn-lint/1.12.4/cfn_lint-1.12.4.tar.gz

---
+++ packages/cfn-lint/cfn_lint-1.12.4.tar.gz/cfn_lint-1.12.4/test/unit/rules/resources/ecs/test_ecs_fargate.py
@@ -0,0 +1,71 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+
+from collections import deque
+
+import pytest
+
+from cfnlint.jsonschema import ValidationError
+from cfnlint.rules.resources.ecs.FargateDeploymentSchedulingStrategy import (
+    FargateDeploymentSchedulingStrategy,
+)
+
+
+@pytest.fixture(scope="module")
+def rule():
+    rule = FargateDeploymentSchedulingStrategy()
+    yield rule
+
+
+@pytest.mark.parametrize(
+    "instance,expected",
+    [
+        (
+            {
+                "LaunchType": "FARGATE",
+            },
+            [],
+        ),
+        (
+            {
+                "LaunchType": "FARGATE",
+                "SchedulingStrategy": "REPLICA",
+            },
+            [],
+        ),
+        (
+            [],  # wrong type
+            [],
+        ),
+        (
+            {
+                "LaunchType": "FARGATE",
+                "SchedulingStrategy": {"Ref": "Replica"},
+            },
+            [],
+        ),
+        (
+            {
+                "LaunchType": "FARGATE",
+                "SchedulingStrategy": "Foo",
+            },
+            [
+                ValidationError(
+                    "'REPLICA' was expected",
+                    rule=FargateDeploymentSchedulingStrategy(),
+                    path=deque(["SchedulingStrategy"]),
+                    validator="const",
+                    schema_path=deque(
+                        ["then", "properties", "SchedulingStrategy", "const"]
+                    ),
+                )
+            ],
+        ),
+    ],
+)
+def test_validate(instance, expected, rule, validator):
+    errs = list(rule.validate(validator, "", instance, {}))
+
+    assert errs == expected, f"Expected {expected} got {errs}"
