43150a41ef6d2745bf9eb2618bccfa3f8e0aeac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 05:04:59 +0000

    Add package lor-gdp-tools/0.1.4/lor_gdp_tools-0.1.4.tar.gz

---
+++ packages/lor-gdp-tools/lor_gdp_tools-0.1.4.tar.gz/lor_gdp_tools-0.1.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='lor_gdp_tools',
+    packages=find_packages(),
+    version='0.1.4',
+    description='This is a utility package designed to enable data scientitists and analysts to easily access GDP data within a python environment',
+    author="Damian Rumble <DRumble@laingorourke.com>"
+)
+
+### Python code to publish package ###
+#python setup.py sdist bdist_wheel
+#twine upload -u __token__ -p pypi-AgEIcHlwaS5vcmcCJDZiMTc4ODU4LWIwYzYtNDZhNC04M2RjLWFmYzk0MTViYmJlMQACKlszLCIyOGMzOGI0OS01YTRhLTQxNzEtYTMzNy0xMWU2MGVlYzBiNTkiXQAABiAwGpNFLruzLptgrJ8R0-EmmYp6SfGLNSK2DfyOy0y7gA dist/*
