269c05971cceb0ef628827f652a0f8446ccddeb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 03:07:10 +0000

    Add package wapordl/0.12.4/wapordl-0.12.4.tar.gz

---
+++ packages/wapordl/wapordl-0.12.4.tar.gz/wapordl-0.12.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'wapordl',
+    version = '0.12.4',
+    packages = find_packages(include = ['wapordl', 'wapordl.*']),
+    include_package_data=True,
+    python_requires='>=3.7',
+    install_requires = [
+        "requests",
+        "pandas>=2.1.0,<3",
+        "numpy>=1.15,<2",
+        "gdal>=3.4.0,<4",
+        "shapely>=2.0.0",
+        "tqdm",
+    ],
+)
