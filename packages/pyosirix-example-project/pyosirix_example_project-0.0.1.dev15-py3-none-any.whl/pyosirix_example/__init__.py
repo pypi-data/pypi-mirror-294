0e0fdb424ffe20dab8948fc527bfbb5ce4bea1c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 15:04:42 +0000

    Add package pyosirix-example-project/0.0.1.dev15/pyosirix_example_project-0.0.1.dev15-py3-none-any.whl

---
+++ packages/pyosirix-example-project/pyosirix_example_project-0.0.1.dev15-py3-none-any.whl/pyosirix_example/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.1-dev.15"
+__author__ = "Matthew D Blackledge"
+__maintainer__ = "Matthew D Blackledge"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2024, Matthew D Blackledge"
+
+from pyosirix_example import server, client, utilities
+
+# Automatically populated by GitHub Actions:
+__gh_repo__ = "osirixgrpc/pyosirix_example_project"
+__gh_hash__ = "10a9ff474df9c5c8ee557981d1bc7c68c0f8f213"
