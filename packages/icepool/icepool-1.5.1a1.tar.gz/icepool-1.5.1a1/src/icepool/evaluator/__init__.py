2e94b2f6464bce42c11b5ddee86a3ad57e167660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 05:04:58 +0000

    Add package icepool/1.5.1a1/icepool-1.5.1a1.tar.gz

---
+++ packages/icepool/icepool-1.5.1a1.tar.gz/icepool-1.5.1a1/src/icepool/evaluator/__init__.py
@@ -0,0 +1,36 @@
+"""Submodule containing evaluators."""
+
+__docformat__ = 'google'
+
+from icepool.evaluator.basic import (ExpandEvaluator, SumEvaluator,
+                                     sum_evaluator, CountEvaluator,
+                                     count_evaluator, AnyEvaluator,
+                                     any_evaluator)
+from icepool.evaluator.poker import (
+    HighestOutcomeAndCountEvaluator, LargestCountEvaluator,
+    LargestCountAndOutcomeEvaluator, CountSubsetEvaluator, AllCountsEvaluator,
+    LargestStraightEvaluator, LargestStraightAndOutcomeEvaluator,
+    AllStraightsEvaluator, AllStraightsReduceCountsEvaluator)
+from icepool.evaluator.comparison import (
+    ComparisonEvaluator, IsSubsetEvaluator, IsProperSubsetEvaluator,
+    IsSupersetEvaluator, IsProperSupersetEvaluator, IsEqualSetEvaluator,
+    IsNotEqualSetEvaluator, IsDisjointSetEvaluator)
+from icepool.evaluator.joint import JointEvaluator
+from icepool.evaluator.constant import ConstantEvaluator
+from icepool.evaluator.keep import KeepEvaluator
+from icepool.evaluator.argsort import ArgsortEvaluator
+from icepool.evaluator.expression import ExpressionEvaluator
+
+__all__ = [
+    'JointEvaluator', 'ExpandEvaluator', 'SumEvaluator', 'sum_evaluator',
+    'CountEvaluator', 'count_evaluator', 'AnyEvaluator', 'any_evaluator',
+    'HighestOutcomeAndCountEvaluator', 'LargestCountEvaluator',
+    'LargestCountAndOutcomeEvaluator', 'CountSubsetEvaluator',
+    'AllCountsEvaluator', 'LargestStraightEvaluator',
+    'LargestStraightAndOutcomeEvaluator', 'AllStraightsEvaluator',
+    'AllStraightsReduceCountsEvaluator', 'ComparisonEvaluator',
+    'IsSubsetEvaluator', 'IsProperSubsetEvaluator', 'IsSupersetEvaluator',
+    'IsProperSupersetEvaluator', 'IsEqualSetEvaluator',
+    'IsNotEqualSetEvaluator', 'IsDisjointSetEvaluator', 'ConstantEvaluator',
+    'KeepEvaluator', 'ArgsortEvaluator', 'ExpressionEvaluator'
+]
