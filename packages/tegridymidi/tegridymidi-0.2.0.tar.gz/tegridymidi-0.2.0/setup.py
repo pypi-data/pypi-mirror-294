bb2c0aaee67c6288a50b937829d15760ca2a091f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 16:05:41 +0000

    Add package tegridymidi/0.2.0/tegridymidi-0.2.0.tar.gz

---
+++ packages/tegridymidi/tegridymidi-0.2.0.tar.gz/tegridymidi-0.2.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import os
+
+# Install fluidsynth using apt
+os.system('apt-get update && apt-get install -y fluidsynth')
+
+setup(
+    name="tegridymidi",
+    version="0.2.0",
+    description="Tegridy MIDI",
+    author="Alex Lev",
+    author_email="alexlev61@proton.me",
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+)
