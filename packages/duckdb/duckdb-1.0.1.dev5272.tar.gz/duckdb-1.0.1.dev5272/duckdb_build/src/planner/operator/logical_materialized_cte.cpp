c0e0b6feccf8ee164b0f3242432f98060b68e12e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 15:06:10 +0000

    Add package duckdb/1.0.1.dev5272/duckdb-1.0.1.dev5272.tar.gz

---
+++ packages/duckdb/duckdb-1.0.1.dev5272.tar.gz/duckdb-1.0.1.dev5272/duckdb_build/src/planner/operator/logical_materialized_cte.cpp
@@ -0,0 +1,15 @@
+#include "duckdb/planner/operator/logical_materialized_cte.hpp"
+
+namespace duckdb {
+
+InsertionOrderPreservingMap<string> LogicalMaterializedCTE::ParamsToString() const {
+	InsertionOrderPreservingMap<string> result;
+	result["Table Index"] = StringUtil::Format("%llu", table_index);
+	return result;
+}
+
+vector<idx_t> LogicalMaterializedCTE::GetTableIndex() const {
+	return vector<idx_t> {table_index};
+}
+
+} // namespace duckdb
