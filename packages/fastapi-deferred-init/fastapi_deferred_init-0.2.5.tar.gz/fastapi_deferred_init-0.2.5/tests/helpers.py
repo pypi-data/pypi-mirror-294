c58105ca9ee6994c70236d3714263ff617cdbb6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 16:06:04 +0000

    Add package fastapi-deferred-init/0.2.5/fastapi_deferred_init-0.2.5.tar.gz

---
+++ packages/fastapi-deferred-init/fastapi_deferred_init-0.2.5.tar.gz/fastapi_deferred_init-0.2.5/tests/helpers.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+import importlib
+import sys
+from contextlib import contextmanager
+from cProfile import Profile
+from pstats import SortKey, Stats
+
+
+@contextmanager
+def profiled():
+    with Profile() as profiler:
+        yield
+        stats = Stats(profiler)
+        stats.sort_stats(SortKey.CUMULATIVE)
+        stats.print_stats()
+        stats.dump_stats("profile")
+
+
+def load_code():
+    if "tests.data.code" in sys.modules:
+        del sys.modules["tests.data.code"]
+    return importlib.import_module("tests.data.code")
