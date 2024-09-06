4398cf0313b20596eaf90489f1bba204e052002c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 04:05:07 +0000

    Add package gridworks/1.1.8/gridworks-1.1.8-py3-none-any.whl

---
+++ packages/gridworks/gridworks-1.1.8-py3-none-any.whl/gw/__init__.py
@@ -0,0 +1,8 @@
+from gw.property_format import check_is_market_name
+from gw.property_format import check_is_market_slot_name_lrd_format
+
+
+__all__ = [
+    "check_is_market_name",
+    "check_is_market_slot_name_lrd_format",
+]
