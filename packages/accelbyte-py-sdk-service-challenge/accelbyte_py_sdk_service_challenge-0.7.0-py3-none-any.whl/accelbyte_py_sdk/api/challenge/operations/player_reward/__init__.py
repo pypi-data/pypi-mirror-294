c224073bbb83ebb5b202eaed79fc65bf22916c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 15:04:26 +0000

    Add package accelbyte-py-sdk-service-challenge/0.7.0/accelbyte_py_sdk_service_challenge-0.7.0-py3-none-any.whl

---
+++ packages/accelbyte-py-sdk-service-challenge/accelbyte_py_sdk_service_challenge-0.7.0-py3-none-any.whl/accelbyte_py_sdk/api/challenge/operations/player_reward/__init__.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Challenge Service."""
+
+__version__ = "1.0.0"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .admin_claim_user_rewards import AdminClaimUserRewards
+from .admin_claim_users_rewards import AdminClaimUsersRewards
+from .admin_get_user_rewards import AdminGetUserRewards
+from .admin_get_user_rewards import (
+    StatusEnum as AdminGetUserRewardsStatusEnum,
+)
+from .public_claim_user_rewards import PublicClaimUserRewards
+from .public_get_user_rewards import PublicGetUserRewards
+from .public_get_user_rewards import (
+    StatusEnum as PublicGetUserRewardsStatusEnum,
+)
