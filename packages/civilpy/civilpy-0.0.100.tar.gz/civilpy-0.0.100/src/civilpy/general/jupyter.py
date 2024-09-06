f4a6b906a1278b6d6da39f2e417cb397f672b341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 05:05:03 +0000

    Add package civilpy/0.0.100/civilpy-0.0.100.tar.gz

---
+++ packages/civilpy/civilpy-0.0.100.tar.gz/civilpy-0.0.100/src/civilpy/general/jupyter.py
@@ -0,0 +1,35 @@
+import asyncio
+import nbformat
+from nbconvert import PDFExporter
+from nbconvert.preprocessors import TagRemovePreprocessor
+
+
+def notebook_to_pdf(notebook_path):
+    # Set the appropriate event loop policy for Windows
+    if asyncio.get_event_loop().is_running():
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    # Read the notebook
+    with open(notebook_path) as f:
+        notebook = nbformat.read(f, as_version=4)
+
+    # Configure the tag removal preprocessor
+    tag_remove_preprocessor = TagRemovePreprocessor()
+    tag_remove_preprocessor.remove_cell_tags = ('remove_cell',)
+    # tag_remove_preprocessor.remove_all_outputs_tags = ()  # Using this tag will remove ALL output from the notebook
+    tag_remove_preprocessor.remove_single_output_tags = ('remove_output',)
+    tag_remove_preprocessor.remove_input_tags = ('remove_input',)
+
+
+    # Create the PDF exporter and register the preprocessor
+    pdf_exporter = PDFExporter()
+    pdf_exporter.register_preprocessor(tag_remove_preprocessor, enabled=True)
+
+    # Convert the notebook to PDF
+    pdf_data, resources = pdf_exporter.from_notebook_node(notebook)
+
+    # Save the PDF to a file
+    pdf_filename = notebook_path.replace(".ipynb", ".pdf")
+    with open(pdf_filename, "wb") as f:
+        f.write(pdf_data)
+    print(f"PDF created: {pdf_filename}")
