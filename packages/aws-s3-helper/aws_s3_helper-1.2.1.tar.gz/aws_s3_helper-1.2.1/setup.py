59b30839ad8b771eae15b71b0529acc0b22a506a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 03:07:38 +0000

    Add package aws-s3-helper/1.2.1/aws_s3_helper-1.2.1.tar.gz

---
+++ packages/aws-s3-helper/aws_s3_helper-1.2.1.tar.gz/aws_s3_helper-1.2.1/setup.py
@@ -0,0 +1,34 @@
+import os.path
+
+from setuptools import find_packages, setup
+
+REQUIREMENTS_FILE = "requirements.txt"
+SRC_FOLDER = "src"
+
+required:list[str] = []
+if os.path.isfile(REQUIREMENTS_FILE):
+    with open(REQUIREMENTS_FILE, encoding="utf8") as f:
+        required.extend(f.read().splitlines())
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description: str = fh.read()
+
+setup(
+    name="aws-s3-helper",
+    version="1.2.1",
+    author="Sergio Jimenez Moreno",
+    author_email="sergio.jimenez@xauencybersecurity.com",
+    description="Abstraction for easy access to AWS S3.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sjm00010/AWS_S3_Helper",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": SRC_FOLDER},
+    packages=find_packages(where=SRC_FOLDER),
+    install_requires=required,
+    python_requires=">=3.8",
+)
