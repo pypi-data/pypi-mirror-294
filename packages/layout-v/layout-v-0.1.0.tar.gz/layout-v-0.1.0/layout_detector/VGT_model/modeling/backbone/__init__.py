992f440be2f45ef4ad45f2f7ee7741b53d60034b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 14:07:05 +0000

    Add package layout-v/0.1.0/layout-v-0.1.0.tar.gz

---
+++ packages/layout-v/layout-v-0.1.0.tar.gz/layout-v-0.1.0/layout_detector/VGT_model/modeling/backbone/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# from .build import build_backbone, BACKBONE_REGISTRY  # noqa F401 isort:skip
+
+from .backbone import Backbone
+from .fpn import FPN
