8a59058751a5b188f14a6060562aca5d93cd543b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 15:04:26 +0000

    Add package hmdriver2/1.0.2/hmdriver2-1.0.2.tar.gz

---
+++ packages/hmdriver2/hmdriver2-1.0.2.tar.gz/hmdriver2-1.0.2/hmdriver2/exception.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+class ElementNotFoundError(Exception):
+    pass
+
+
+class ElementFoundTimeout(Exception):
+    pass
+
+
+class HmDriverError(Exception):
+    pass
+
+
+class DeviceNotFoundError(Exception):
+    pass
+
+
+class HdcError(Exception):
+    pass
+
+
+class InvokeHypiumError(Exception):
+    pass
