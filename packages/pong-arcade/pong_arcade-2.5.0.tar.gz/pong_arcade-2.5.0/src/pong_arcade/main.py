5eb7742440fa464a79d009f9b5c33f7497d1b19d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 15:04:32 +0000

    Add package pong-arcade/2.5.0/pong_arcade-2.5.0.tar.gz

---
+++ packages/pong-arcade/pong_arcade-2.5.0.tar.gz/pong_arcade-2.5.0/src/pong_arcade/main.py
@@ -0,0 +1,27 @@
+from .pong import Pong
+
+from pygame import event, quit
+
+def main():
+    pong = Pong()
+
+    pong.set_window_properties()
+    pong.print_info()
+
+    while pong.running:
+        pong.tick()
+
+        for e in event.get():
+            pong.handle_event(e)
+
+        if pong.game_in_progress():
+            pong.update_paddle_position()
+            pong.update_ball_position()
+
+        pong.draw_frame()
+        pong.update_frame()
+
+
+if __name__ == "__main__":
+    main()
+    quit()
