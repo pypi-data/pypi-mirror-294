7da8183b326ad9ad96a98082e8971504238bdc06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 06:04:53 +0000

    Add package omlish/0.0.0.dev17/omlish-0.0.0.dev17-py3-none-any.whl

---
+++ packages/omlish/omlish-0.0.0.dev17-py3-none-any.whl/omlish/testing/pytest/plugins/__init__.py
@@ -0,0 +1,23 @@
+from . import (  # noqa
+    asyncs,
+    logging,
+    managermarks,
+    pydevd,
+    repeat,
+    skips,
+    spacing,
+    switches,
+)
+
+from ._registry import (  # noqa
+    ALL,
+    register,
+)
+
+
+def addhooks(pluginmanager):
+    present_types = {type(p) for p in pluginmanager.get_plugins()}
+
+    for plugin in ALL:
+        if plugin not in present_types:
+            pluginmanager.register(plugin())  # noqa
