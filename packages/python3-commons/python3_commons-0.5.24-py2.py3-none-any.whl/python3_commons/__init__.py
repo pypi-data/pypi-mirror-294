7b0588540ae7214940af3d2f5f43b16eb037317e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 14:05:46 +0000

    Add package python3-commons/0.5.24/python3_commons-0.5.24-py2.py3-none-any.whl

---
+++ packages/python3-commons/python3_commons-0.5.24-py2.py3-none-any.whl/python3_commons/__init__.py
@@ -0,0 +1,9 @@
+import importlib.metadata
+
+try:
+    dist_name = __name__
+    __version__ = importlib.metadata.version(dist_name)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = 'unknown'
+finally:
+    del importlib.metadata
