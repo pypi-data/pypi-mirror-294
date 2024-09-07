89c04cfc1acb9b0f6953cde2ff372f87a9d9cc45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 05:05:08 +0000

    Add package simple-generation/0.6.3/simple_generation-0.6.3-py3-none-any.whl

---
+++ packages/simple-generation/simple_generation-0.6.3-py3-none-any.whl/simple_generation/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for Simple Generation."""
+
+__author__ = """Giuseppe Attanasio"""
+__email__ = "giuseppeattanasio6@gmail.com"
+__version__ = "0.5.1"
+
+
+from .simple_generation import SimpleGenerator, DefaultGenerationConfig
+from .cli import cli
