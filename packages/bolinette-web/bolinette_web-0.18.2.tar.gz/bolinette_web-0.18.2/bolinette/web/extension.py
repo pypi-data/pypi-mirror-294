0a96f9dea71001cd9ffa86335cdfc64c1d3de2e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 03:06:05 +0000

    Add package bolinette-web/0.18.2/bolinette_web-0.18.2.tar.gz

---
+++ packages/bolinette-web/bolinette_web-0.18.2.tar.gz/bolinette_web-0.18.2/bolinette/web/extension.py
@@ -0,0 +1,34 @@
+from typing import Self, override
+
+from bolinette import core
+from bolinette.core import Cache, startup
+from bolinette.core.extension import Extension
+from bolinette.core.injection import Injection, injectable
+from bolinette.web.config import WebConfig
+from bolinette.web.resources import WebResources
+from bolinette.web.ws import WebSocketHandler
+
+
+class WebExtension(Extension):
+    def __init__(self) -> None:
+        super().__init__("web", [core])
+        self.config = WebConfig()
+
+    @override
+    def add_cached(self, cache: Cache) -> None:
+        injectable(strategy="singleton", cache=cache)(WebResources)
+        injectable(strategy="singleton", cache=cache)(WebSocketHandler)
+
+        startup(cache=cache)(init_web_ext)
+
+    def use_sockets(self) -> Self:
+        self.config.use_sockets = True
+        return self
+
+
+web_ext = WebExtension()
+
+
+async def init_web_ext(inject: Injection) -> None:
+    inject.add_singleton(WebExtension, instance=web_ext)
+    inject.add_singleton(WebConfig, instance=web_ext.config)
