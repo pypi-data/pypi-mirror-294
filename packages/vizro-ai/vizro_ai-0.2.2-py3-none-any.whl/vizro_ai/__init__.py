db29546dce9ea260b5487e14ee86ffe92307e543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 04:04:48 +0000

    Add package vizro-ai/0.2.2/vizro_ai-0.2.2-py3-none-any.whl

---
+++ packages/vizro-ai/vizro_ai-0.2.2-py3-none-any.whl/vizro_ai/__init__.py
@@ -0,0 +1,14 @@
+import logging
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from ._vizro_ai import VizroAI
+
+__all__ = ["VizroAI"]
+
+__version__ = "0.2.2"
+
+logging.basicConfig(level=os.getenv("VIZRO_AI_LOG_LEVEL", "INFO"))
