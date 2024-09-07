851cd1b217bc5996460387b4d31a9dc99d6d2a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 06:05:25 +0000

    Add package aiobme280/0.4.0/aiobme280-0.4.0.tar.gz

---
+++ packages/aiobme280/aiobme280-0.4.0.tar.gz/aiobme280-0.4.0/aiobme280/__init__.py
@@ -0,0 +1,27 @@
+#
+# aiobme280 - BME280 sensor asyncio library
+#
+# Copyright (C) 2016-2024 by Artur Wroblewski <wrobell@riseup.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from importlib.metadata import version
+from .glue import Sensor
+
+__version__ = version('aiobme280')
+
+__all__ = ['Sensor']
+
+# vim: sw=4:et:ai
