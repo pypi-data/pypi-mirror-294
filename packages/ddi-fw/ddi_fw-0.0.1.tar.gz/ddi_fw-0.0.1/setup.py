b0ab32222a3a12fa88946aa8841743b9fcb97ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 03:05:27 +0000

    Add package ddi-fw/0.0.1/ddi_fw-0.0.1.tar.gz

---
+++ packages/ddi-fw/ddi_fw-0.0.1.tar.gz/ddi_fw-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='ddi_fw',
+version='0.0.1',
+author='Kıvanç Bayraktar',
+author_email='bayraktarkivanc@gmail.com',
+description='Do not use :)',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
