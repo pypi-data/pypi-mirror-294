b9281aab5921dac377c392a80e3b50d40bb4bcb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 03:06:06 +0000

    Add package fastapi-namespace-vet1ments/0.1.15/fastapi_namespace_vet1ments-0.1.15-py3-none-any.whl

---
+++ packages/fastapi-namespace-vet1ments/fastapi_namespace_vet1ments-0.1.15-py3-none-any.whl/fastapi_namespace/mixins/token/opaqueTokenMixin.py
@@ -0,0 +1,51 @@
+from .tokenBaseMixin import (
+    TokenBaseMixin,
+    TokenValidator,
+    TokenInfoValidator,
+    TI
+)
+from .typings import (
+    OpaqueTokenInfo,
+    OpaqueToken,
+    JWTToken,
+    TokenInfo,
+    RawToken,
+    Token
+)
+from fastapi_namespace.utils import get_typeddict_validator
+from secrets import token_urlsafe
+from uuid import uuid4
+from typing_extensions import TypedDict
+from dataclasses import asdict
+
+
+class OpaqueTokenMixin(TokenBaseMixin[OpaqueToken, OpaqueTokenInfo]):
+
+    def __init__(self):
+        token_validator = get_typeddict_validator(OpaqueToken)
+        token_info_validator = get_typeddict_validator(OpaqueTokenInfo)
+
+        super().__init__(
+            token=OpaqueToken,
+            token_info=OpaqueTokenInfo,
+            token_validator=token_validator,
+            token_info_validator=token_info_validator
+        )
+
+    def create_token(self, *args, **kwarg) -> RawToken:
+        """
+        Returns:
+            RawToken: `str`으로 된 토큰
+        """
+        return token_urlsafe(48)
+
+    def _make_token(self, token: Token) -> OpaqueTokenInfo:
+        # return OpaqueTokenInfo(
+        #     **token,
+        #     idf=uuid4().hex
+        # )
+        OpaqueToken(**asdict(token))
+        return OpaqueTokenInfo(
+            info=OpaqueToken(**asdict(token)),
+        )
+
