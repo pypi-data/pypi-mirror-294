8d46d5c73cd02109f07fee9e7573fd0af599d8e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 05:05:35 +0000

    Add package bits-aviso-python-sdk/1.3.0/bits_aviso_python_sdk-1.3.0-py3-none-any.whl

---
+++ packages/bits-aviso-python-sdk/bits_aviso_python_sdk-1.3.0-py3-none-any.whl/bits_aviso_python_sdk/helpers/__init__.py
@@ -0,0 +1,62 @@
+import datetime
+import logging
+import re
+
+
+def initialize_logger(file_handler_path=None):
+    """Initializes a logger with a stream handler and an optional file handler.
+
+    Args:
+        file_handler_path (str, optional): The path to save the log file if a file handler is desired. Defaults to None.
+    """
+    # set up logger
+    today = datetime.datetime.now().strftime("%Y_%m_%d")
+    logger = logging.getLogger()  # root logger
+
+    # check if there's any handlers already
+    if not logger.handlers:
+        # create file handler if path is provided
+        if file_handler_path:
+            # check if the path ends with a slash
+            if file_handler_path.endswith('/'):
+                file_handler = logging.FileHandler(f"{file_handler_path}{today}.log")
+
+            else:
+                file_handler = logging.FileHandler(f"{file_handler_path}/{today}.log")
+            # set level to DEBUG
+            file_handler.setLevel(logging.DEBUG)
+            # set format
+            file_handler.setFormatter(logging.Formatter(
+                "%(module)s %(asctime)s [%(levelname)s]: %(message)s", "%I:%M:%S %p"))
+            # add file handler to the logger
+            logger.addHandler(file_handler)
+
+        # Create stream handler and set level to ERROR
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        stream_handler.setFormatter(logging.Formatter(
+            "%(module)s %(asctime)s [%(levelname)s]: %(message)s", "%I:%M:%S %p"))
+        # add stream handler to the logger
+        logger.addHandler(stream_handler)
+
+    # Set the logger's level to the lowest level among all handlers
+    logger.setLevel(logging.DEBUG)
+
+    return logger
+
+
+def is_ip_address(string_to_check):
+    """Check if the given string is a valid IP address.
+
+    Args:
+        string_to_check (str): The string to check.
+
+    Returns:
+        bool: True if the string is a valid IP address, False otherwise.
+    """
+    ip_pattern = re.compile(r'^(\d{1,3}\.){3}\d{1,3}$')
+    if ip_pattern.match(string_to_check):
+        parts = string_to_check.split('.')
+        return all(0 <= int(part) <= 255 for part in parts)
+
+    return False
