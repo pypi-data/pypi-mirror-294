0d975f95073ae4335bb1c5408b36a67f25b18a48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 05:05:59 +0000

    Add package fastapi-g-context/0.0.2/fastapi_g_context-0.0.2.tar.gz

---
+++ packages/fastapi-g-context/fastapi_g_context-0.0.2.tar.gz/fastapi_g_context-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fastapi-g-context",
+    version="0.0.2",
+    description="fastapi-g-context is a Python module that provides a simple mechanism for managing \
+    global variables with context isolation in FastAPI applications. \
+        It is designed to ensure that each request operates within its own isolated context, \
+            preventing data leakage between requests.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/chrisK824/fastapi-g-context",
+    author="Chris Karvouniaris",
+    author_email="christos.karvouniaris247@gmail.com",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[
+        "starlette"
+    ],
+    python_requires=">=3.8",
+    license="MIT",
+    license_files=["LICENSE"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
