6939ce205b39df0b0872a17a4696af9e70277f80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 04:04:51 +0000

    Add package spotiflow/0.4.2/spotiflow-0.4.2.tar.gz

---
+++ packages/spotiflow/spotiflow-0.4.2.tar.gz/spotiflow-0.4.2/scripts/train_simple_3d.py
@@ -0,0 +1,73 @@
+"""Sample script to train a Spotiflow model.
+"""
+
+import argparse
+import numpy as np
+from pathlib import Path
+from skimage import io
+from itertools import chain
+
+from spotiflow.model import Spotiflow, SpotiflowModelConfig
+from spotiflow import utils
+import lightning.pytorch as pl
+
+IMAGE_EXTENSIONS = ("tif", "tiff", "png", "jpg", "jpeg")
+
+
+def get_data(data_dir):
+    """Load data from data_dir."""
+    img_files = sorted(tuple(chain(*tuple(data_dir.glob(f"*.{ext}") for ext in IMAGE_EXTENSIONS))))
+    spots_files = sorted(data_dir.glob("*.csv"))
+
+    images = tuple(io.imread(str(f)) for f in img_files)
+    spots = tuple(utils.read_coords_csv3d(str(f)).astype(np.float32) for f in spots_files)
+    return images, spots
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data-dir", type=Path, default="/data/spots/datasets_3d/synth3d")
+    parser.add_argument("--save-dir", type=Path, default="/data/tmp/spotiflow_3d_debug/synth3d")
+    parser.add_argument("--sigma", type=float, default=1.0)
+    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--levels", type=int, default=4)
+    parser.add_argument("--pretrained-path", type=Path, default=None)
+    args = parser.parse_args()
+
+    pl.seed_everything(args.seed, workers=True)
+
+    print("Loading training data...")
+    train_images, train_spots = get_data(args.data_dir / "train")
+    print(f"Training data loaded (N={len(train_images)}).")
+
+    print("Loading validation data...")
+    val_images, val_spots = get_data(args.data_dir / "val")
+    print(f"Validation data loaded (N={len(val_images)}).")
+
+    if args.pretrained_path is not None:
+        print("Loading pretrained model...")
+        model = Spotiflow.from_folder(args.pretrained_path)
+        print("Launching fine-tuning...")
+    else:
+        print("Instantiating new model...")
+        model = Spotiflow(SpotiflowModelConfig(in_channels=1, sigma=args.sigma, is_3d=True, levels=args.levels))
+        print("Launching training...")
+
+    model.fit(
+        train_images,
+        train_spots,
+        val_images,
+        val_spots,
+        save_dir=args.save_dir,
+        augment_train=True,
+        device="auto",
+        deterministic=False,
+        logger="none",
+        train_config={
+            "num_epochs": 1000,
+            "crop_size": 128,
+            "crop_size_depth": 32,
+            "smart_crop": True,
+        }
+    )
+    print("Done!")
