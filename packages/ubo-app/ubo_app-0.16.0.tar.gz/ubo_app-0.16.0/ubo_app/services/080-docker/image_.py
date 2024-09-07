f969babbfd01ce201afeb1fedc1a772209446628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 14:05:46 +0000

    Add package ubo-app/0.16.0/ubo_app-0.16.0.tar.gz

---
+++ packages/ubo-app/ubo_app-0.16.0.tar.gz/ubo_app-0.16.0/ubo_app/services/080-docker/image_.py
@@ -0,0 +1,540 @@
+"""Docker image menu."""
+
+from __future__ import annotations
+
+import contextlib
+import pathlib
+from asyncio import iscoroutine
+from typing import TYPE_CHECKING, Any, cast, overload
+
+import docker
+import docker.errors
+from docker.models.containers import Container
+from docker.models.images import Image
+from kivy.lang.builder import Builder
+from kivy.properties import ListProperty, NumericProperty, StringProperty
+from reducer import IMAGE_IDS, IMAGES
+from redux import FinishEvent
+from ubo_gui.menu.types import ActionItem, HeadedMenu, HeadlessMenu, Item, SubMenuItem
+from ubo_gui.page import PageWidget
+
+from ubo_app.constants import DOCKER_CREDENTIALS_TEMPLATE
+from ubo_app.logging import logger
+from ubo_app.store.main import store
+from ubo_app.store.services.docker import (
+    DockerImageSetDockerIdAction,
+    DockerImageSetStatusAction,
+    DockerState,
+    ImageState,
+    ImageStatus,
+)
+from ubo_app.store.services.notifications import (
+    Importance,
+    Notification,
+    NotificationsAddAction,
+)
+from ubo_app.utils import secrets
+from ubo_app.utils.async_ import create_task, to_thread
+
+if TYPE_CHECKING:
+    from collections.abc import Callable, Coroutine, Sequence
+
+    from ubo_app.store.services.ip import IpNetworkInterface
+
+
+def find_container(client: docker.DockerClient, *, image: str) -> Container | None:
+    """Find a container."""
+    for container in client.containers.list(all=True):
+        if not isinstance(container, Container):
+            continue
+
+        with contextlib.suppress(docker.errors.DockerException):
+            container_image = container.image
+            if isinstance(container_image, Image) and image in container_image.tags:
+                return container
+    return None
+
+
+def update_container(image_id: str, container: Container) -> None:
+    """Update a container's state in store based on its real state."""
+    if container.status == 'running':
+        logger.info(
+            'Container running image found',
+            extra={'image': image_id, 'path': IMAGES[image_id].path},
+        )
+        store.dispatch(
+            DockerImageSetStatusAction(
+                image=image_id,
+                status=ImageStatus.RUNNING,
+                ports=[
+                    f'{i["HostIp"]}:{i["HostPort"]}'
+                    for i in container.ports.values()
+                    for i in i
+                ],
+                ip=container.attrs['NetworkSettings']['Networks']['bridge']['IPAddress']
+                if container.attrs
+                and 'bridge' in container.attrs['NetworkSettings']['Networks']
+                else None,
+            ),
+        )
+        return
+    logger.info(
+        "Container for the image found, but it's not running",
+        extra={'image': image_id, 'path': IMAGES[image_id].path},
+    )
+    store.dispatch(
+        DockerImageSetStatusAction(
+            image=image_id,
+            status=ImageStatus.CREATED,
+        ),
+    )
+
+
+def _monitor_events(image_id: str, get_docker_id: Callable[[], str]) -> None:  # noqa: C901
+    path = IMAGES[image_id].path
+    docker_client = docker.from_env()
+    events = docker_client.events(
+        decode=True,
+        filters={'type': ['image', 'container']},
+    )
+    store.subscribe_event(FinishEvent, events.close)
+    for event in events:
+        logger.verbose('Docker image event', extra={'event': event})
+        if event['Type'] == 'image':
+            if event['status'] == 'pull' and event['id'] == path:
+                try:
+                    image = docker_client.images.get(path)
+                    store.dispatch(
+                        DockerImageSetStatusAction(
+                            image=image_id,
+                            status=ImageStatus.AVAILABLE,
+                        ),
+                    )
+                    if isinstance(image, Image) and image.id:
+                        store.dispatch(
+                            DockerImageSetDockerIdAction(
+                                image=image_id,
+                                docker_id=image.id,
+                            ),
+                        )
+                except docker.errors.DockerException:
+                    store.dispatch(
+                        DockerImageSetStatusAction(
+                            image=image_id,
+                            status=ImageStatus.NOT_AVAILABLE,
+                        ),
+                    )
+            elif event['status'] == 'delete' and event['id'] == get_docker_id():
+                store.dispatch(
+                    DockerImageSetStatusAction(
+                        image=image_id,
+                        status=ImageStatus.NOT_AVAILABLE,
+                    ),
+                )
+        elif event['Type'] == 'container':
+            if event['status'] == 'start' and event['from'] == path:
+                container = find_container(docker_client, image=path)
+                if container:
+                    update_container(image_id, container)
+            elif event['status'] == 'die' and event['from'] == path:
+                store.dispatch(
+                    DockerImageSetStatusAction(
+                        image=image_id,
+                        status=ImageStatus.CREATED,
+                    ),
+                )
+            elif event['status'] == 'destroy' and event['from'] == path:
+                store.dispatch(
+                    DockerImageSetStatusAction(
+                        image=image_id,
+                        status=ImageStatus.AVAILABLE,
+                    ),
+                )
+
+
+def check_container(image_id: str) -> None:
+    """Check the container status."""
+    path = IMAGES[image_id].path
+
+    def act() -> None:
+        logger.info('Checking image', extra={'image': image_id, 'path': path})
+        docker_client = docker.from_env()
+        try:
+            image = docker_client.images.get(path)
+            if not isinstance(image, Image):
+                raise docker.errors.ImageNotFound(path)  # noqa: TRY301
+
+            if image.id:
+                store.dispatch(
+                    DockerImageSetDockerIdAction(
+                        image=image_id,
+                        docker_id=image.id,
+                    ),
+                )
+            logger.info('Image found', extra={'image': image_id, 'path': path})
+
+            container = find_container(docker_client, image=path)
+            if container:
+                update_container(image_id, container)
+                return
+
+            logger.info(
+                'Container running image not found',
+                extra={'image': image_id, 'path': path},
+            )
+            store.dispatch(
+                DockerImageSetStatusAction(
+                    image=image_id,
+                    status=ImageStatus.AVAILABLE,
+                ),
+            )
+        except docker.errors.ImageNotFound:
+            logger.exception(
+                'Image not found',
+                extra={'image': image_id, 'path': path},
+            )
+            store.dispatch(
+                DockerImageSetStatusAction(
+                    image=image_id,
+                    status=ImageStatus.NOT_AVAILABLE,
+                ),
+            )
+        except docker.errors.DockerException:
+            logger.exception(
+                'Image error',
+                extra={'image': image_id, 'path': path},
+            )
+            store.dispatch(
+                DockerImageSetStatusAction(
+                    image=image_id,
+                    status=ImageStatus.ERROR,
+                ),
+            )
+        finally:
+            docker_client.close()
+
+            @store.autorun(lambda state: getattr(state.docker, image_id).docker_id)
+            def get_docker_id(docker_id: str) -> str:
+                return docker_id
+
+            _monitor_events(image_id, get_docker_id)
+
+    to_thread(act)
+
+
+@store.autorun(lambda state: state.docker.service.usernames)
+def _reactive_fetch_image(usernames: dict[str, str]) -> Callable[[ImageState], None]:
+    def fetch_image(image: ImageState) -> None:
+        def act() -> None:
+            store.dispatch(
+                DockerImageSetStatusAction(
+                    image=image.id,
+                    status=ImageStatus.FETCHING,
+                ),
+            )
+            try:
+                logger.info('Fetching image', extra={'image': IMAGES[image.id].path})
+                docker_client = docker.from_env()
+                for registry, username in usernames.items():
+                    if IMAGES[image.id].registry == registry:
+                        docker_client.login(
+                            username=username,
+                            password=secrets.read_secret(
+                                DOCKER_CREDENTIALS_TEMPLATE.format(registry),
+                            ),
+                            registry=registry,
+                        )
+                docker_client.images.pull(IMAGES[image.id].path)
+                docker_client.close()
+            except docker.errors.DockerException:
+                logger.exception(
+                    'Image error',
+                    extra={'image': IMAGES[image.id].path},
+                )
+                store.dispatch(
+                    DockerImageSetStatusAction(
+                        image=image.id,
+                        status=ImageStatus.ERROR,
+                    ),
+                )
+
+        to_thread(act)
+
+    return fetch_image
+
+
+def _remove_image(image: ImageState) -> None:
+    def act() -> None:
+        docker_client = docker.from_env()
+        docker_client.images.remove(IMAGES[image.id].path, force=True)
+        docker_client.close()
+
+    to_thread(act)
+
+
+@overload
+async def _process_str(
+    value: str
+    | Callable[[], str | Coroutine[Any, Any, str]]
+    | Coroutine[Any, Any, str],
+) -> str: ...
+
+
+@overload
+async def _process_str(
+    value: str
+    | Callable[[], str | Coroutine[Any, Any, str | None] | None]
+    | Coroutine[Any, Any, str | None]
+    | None,
+) -> str | None: ...
+
+
+async def _process_str(
+    value: str
+    | Callable[[], str | Coroutine[Any, Any, str | None] | None]
+    | Coroutine[Any, Any, str | None]
+    | None,
+) -> str | None:
+    if callable(value):
+        value = value()
+    if iscoroutine(value):
+        value = cast(str, await value)
+    return value
+
+
+async def _process_environment_variables(image_id: str) -> dict[str, str]:
+    environment_variables = IMAGES[image_id].environment_vairables or {}
+    result: dict[str, str] = {}
+
+    for key in environment_variables:
+        result[key] = await _process_str(environment_variables[key])
+
+    return result
+
+
+@store.autorun(lambda state: state.docker)
+def _run_container_generator(docker_state: DockerState) -> Callable[[ImageState], None]:
+    def run_container(image: ImageState) -> None:
+        async def act() -> None:
+            docker_client = docker.from_env()
+            container = find_container(docker_client, image=IMAGES[image.id].path)
+            if container:
+                if container.status != 'running':
+                    container.start()
+            else:
+                hosts = {}
+                for key, value in IMAGES[image.id].hosts.items():
+                    if not hasattr(docker_state, value):
+                        store.dispatch(
+                            NotificationsAddAction(
+                                notification=Notification(
+                                    title='Dependency error',
+                                    content=f'Container "{value}" is not loaded',
+                                    importance=Importance.MEDIUM,
+                                ),
+                            ),
+                        )
+                        return
+                    if not getattr(docker_state, value).container_ip:
+                        store.dispatch(
+                            NotificationsAddAction(
+                                notification=Notification(
+                                    title='Dependency error',
+                                    content=f'Container "{value}" does not have an IP'
+                                    ' address',
+                                    importance=Importance.MEDIUM,
+                                ),
+                            ),
+                        )
+                        return
+                    if hasattr(docker_state, value):
+                        hosts[key] = getattr(docker_state, value).container_ip
+                    else:
+                        hosts[key] = value
+
+                docker_client.containers.run(
+                    IMAGES[image.id].path,
+                    hostname=image.id,
+                    publish_all_ports=True,
+                    detach=True,
+                    volumes=IMAGES[image.id].volumes,
+                    ports=IMAGES[image.id].ports,
+                    network_mode=IMAGES[image.id].network_mode,
+                    environment=await _process_environment_variables(image.id),
+                    extra_hosts=hosts,
+                    restart_policy={'Name': 'always'},
+                    command=await _process_str(IMAGES[image.id].command),
+                )
+            docker_client.close()
+
+        create_task(act())
+
+    return run_container
+
+
+def _stop_container(image: ImageState) -> None:
+    def act() -> None:
+        docker_client = docker.from_env()
+        container = find_container(docker_client, image=IMAGES[image.id].path)
+        if container and container.status != 'exited':
+            container.stop()
+        docker_client.close()
+
+    to_thread(act)
+
+
+def _remove_container(image: ImageState) -> None:
+    def act() -> None:
+        docker_client = docker.from_env()
+        container = find_container(docker_client, image=IMAGES[image.id].path)
+        if container:
+            container.remove(v=True, force=True)
+        docker_client.close()
+
+    to_thread(act)
+
+
+class DockerQRCodePage(PageWidget):
+    """QR code for the container's url (ip and port)."""
+
+    ips: list[str] = ListProperty()
+    port: str = StringProperty()
+    index: int = NumericProperty(0)
+
+    def go_down(self: DockerQRCodePage) -> None:
+        """Go down."""
+        self.index = (self.index + 1) % len(self.ips)
+        self.ids.slider.animated_value = len(self.ips) - 1 - self.index
+
+    def go_up(self: DockerQRCodePage) -> None:
+        """Go up."""
+        self.index = (self.index - 1) % len(self.ips)
+        self.ids.slider.animated_value = len(self.ips) - 1 - self.index
+
+
+@store.view(lambda state: state.ip.interfaces)
+def image_menu(
+    interfaces: Sequence[IpNetworkInterface],
+    image: ImageState,
+) -> HeadedMenu:
+    """Get the menu for the docker image."""
+    ip_addresses = [
+        ip for interface in interfaces or [] for ip in interface.ip_addresses
+    ]
+    items: list[Item] = []
+
+    def open_qrcode(port: str) -> Callable[[], PageWidget]:
+        def action() -> PageWidget:
+            return DockerQRCodePage(ips=ip_addresses, port=port)
+
+        return action
+
+    if image.status == ImageStatus.NOT_AVAILABLE:
+        items.append(
+            ActionItem(
+                label='Fetch',
+                icon='󰇚',
+                action=lambda: _reactive_fetch_image()(image),
+            ),
+        )
+    elif image.status == ImageStatus.FETCHING:
+        pass
+    elif image.status == ImageStatus.AVAILABLE:
+        items.extend(
+            [
+                ActionItem(
+                    label='Start',
+                    icon='󰐊',
+                    action=lambda: _run_container_generator()(image),
+                ),
+                ActionItem(
+                    label='Remove image',
+                    icon='󰆴',
+                    action=lambda: _remove_image(image),
+                ),
+            ],
+        )
+    elif image.status == ImageStatus.CREATED:
+        items.extend(
+            [
+                ActionItem(
+                    label='Start',
+                    icon='󰐊',
+                    action=lambda: _run_container_generator()(image),
+                ),
+                ActionItem(
+                    label='Remove container',
+                    icon='󰆴',
+                    action=lambda: _remove_container(image),
+                ),
+            ],
+        )
+    elif image.status == ImageStatus.RUNNING:
+        items.append(
+            ActionItem(
+                label='Stop',
+                key='stop',
+                icon='󰓛',
+                action=lambda: _stop_container(image),
+            ),
+        )
+        items.append(
+            SubMenuItem(
+                label='Ports',
+                key='ports',
+                icon='󰙜',
+                sub_menu=HeadlessMenu(
+                    title='Ports',
+                    items=[
+                        ActionItem(
+                            label=port,
+                            key=port,
+                            icon='󰙜',
+                            action=open_qrcode(port.split(':')[-1]),
+                        )
+                        if port.startswith('0.0.0.0')  # noqa: S104
+                        else Item(label=port, icon='󰙜')
+                        for port in image.ports
+                    ],
+                    placeholder='No ports',
+                ),
+            ),
+        )
+
+    return HeadedMenu(
+        title=f'Docker - {IMAGES[image.id].label}',
+        heading=IMAGES[image.id].label,
+        sub_heading={
+            ImageStatus.NOT_AVAILABLE: 'Image needs to be fetched',
+            ImageStatus.FETCHING: 'Image is being fetched',
+            ImageStatus.AVAILABLE: 'Image is ready but container is not running',
+            ImageStatus.CREATED: 'Container is created but not running',
+            ImageStatus.RUNNING: IMAGES[image.id].note or 'Container is running',
+            ImageStatus.ERROR: 'Image has an error, please check the logs',
+        }[image.status],
+        items=items,
+        placeholder='Waiting...',
+    )
+
+
+def image_menu_generator(image_id: str) -> Callable[[], Callable[[], HeadedMenu]]:
+    """Get the menu items for the Docker service."""
+    _image_menu = store.autorun(lambda state: getattr(state.docker, image_id))(
+        image_menu,
+    )
+
+    def open_image_menu() -> Callable[[], HeadedMenu]:
+        check_container(image_id)
+
+        return _image_menu
+
+    return open_image_menu
+
+
+IMAGE_MENUS = {image_id: image_menu_generator(image_id) for image_id in IMAGE_IDS}
+Builder.load_file(
+    pathlib.Path(__file__)
+    .parent.joinpath('docker_qrcode_page.kv')
+    .resolve()
+    .as_posix(),
+)
---
+++ packages/ubo-app/ubo_app-0.16.0.tar.gz/ubo_app-0.16.0/ubo_app/services/080-docker/reducer.py
@@ -0,0 +1,257 @@
+"""Docker reducer."""
+
+from __future__ import annotations
+
+from dataclasses import field, replace
+from typing import TYPE_CHECKING, Any
+
+from immutable import Immutable
+from redux import (
+    BaseEvent,
+    CombineReducerAction,
+    CombineReducerInitAction,
+    InitAction,
+    InitializationActionError,
+    ReducerResult,
+    combine_reducers,
+)
+
+from ubo_app.constants import DEBUG_MODE_DOCKER, DOCKER_PREFIX
+from ubo_app.store.services.docker import (
+    DockerAction,
+    DockerImageAction,
+    DockerImageEvent,
+    DockerImageSetDockerIdAction,
+    DockerImageSetStatusAction,
+    DockerRemoveUsernameAction,
+    DockerServiceState,
+    DockerSetStatusAction,
+    DockerState,
+    DockerStoreUsernameAction,
+    ImageState,
+)
+from ubo_app.store.services.notifications import NotificationExtraInformation
+from ubo_app.utils.qrcode import qrcode_input
+
+if TYPE_CHECKING:
+    from collections.abc import Callable, Coroutine
+
+    from ubo_app.store.services.ip import IpUpdateInterfacesAction
+
+Action = InitAction | DockerAction
+
+
+def service_reducer(
+    state: DockerServiceState | None,
+    action: Action,
+) -> ReducerResult[DockerServiceState, Action, BaseEvent]:
+    """Docker reducer."""
+    if state is None:
+        if isinstance(action, InitAction):
+            return DockerServiceState()
+        raise InitializationActionError(action)
+
+    if isinstance(action, DockerSetStatusAction):
+        return replace(state, status=action.status)
+
+    if isinstance(action, DockerStoreUsernameAction):
+        return replace(
+            state,
+            usernames={**state.usernames, action.registry: action.username},
+        )
+
+    if isinstance(action, DockerRemoveUsernameAction):
+        return replace(
+            state,
+            usernames={
+                registry: username
+                for registry, username in state.usernames.items()
+                if registry != action.registry
+            },
+        )
+
+    return state
+
+
+class ImageEntry(Immutable):
+    """An image to be used in a Docker container."""
+
+    id: str
+    label: str
+    icon: str
+    path: str
+    registry: str
+    dependencies: list[str] | None = None
+    ports: dict[str, int | list[int] | tuple[str, int] | None] = field(
+        default_factory=dict,
+    )
+    hosts: dict[str, str] = field(default_factory=dict)
+    note: str | None = None
+    environment_vairables: (
+        dict[
+            str,
+            str
+            | Coroutine[Any, Any, str]
+            | Callable[[], str | Coroutine[Any, Any, str]],
+        ]
+        | None
+    ) = None
+    network_mode: str = 'bridge'
+    volumes: list[str] | None = None
+    command: (
+        str
+        | Coroutine[Any, Any, str]
+        | Callable[[], str | Coroutine[Any, Any, str]]
+        | None
+    ) = None
+
+
+IMAGES = {
+    image.id: image
+    for image in [
+        ImageEntry(
+            id='home_assistant',
+            label='Home Assistant',
+            icon='󰟐',
+            path=DOCKER_PREFIX + 'homeassistant/home-assistant:stable',
+            registry='docker.io',
+            ports={'8123/tcp': 8123},
+        ),
+        ImageEntry(
+            id='home_bridge',
+            label='Home Bridge',
+            icon='󰘘',
+            path=DOCKER_PREFIX + 'homebridge/homebridge:latest',
+            registry='docker.io',
+        ),
+        ImageEntry(
+            id='portainer',
+            label='Portainer',
+            icon='',
+            path=DOCKER_PREFIX + 'portainer/portainer-ce:latest',
+            registry='docker.io',
+            volumes=['/var/run/docker.sock:/var/run/docker.sock'],
+        ),
+        ImageEntry(
+            id='pi_hole',
+            label='Pi-hole',
+            icon='󰇖',
+            environment_vairables={'WEBPASSWORD': 'admin'},
+            note='Password: admin',
+            path=DOCKER_PREFIX + 'pihole/pihole:latest',
+            registry='docker.io',
+        ),
+        ImageEntry(
+            id='ollama',
+            label='Ollama',
+            icon='󰳆',
+            path=DOCKER_PREFIX + 'ollama/ollama:latest',
+            registry='docker.io',
+            ports={'11434/tcp': 11434},
+        ),
+        ImageEntry(
+            id='open_webui',
+            label='Open WebUI',
+            icon='󰾔',
+            path=DOCKER_PREFIX + 'open-webui/open-webui:main',
+            registry='ghcr.io',
+            dependencies=['ollama'],
+            ports={'8080/tcp': 8080},
+            hosts={'host.docker.internal': 'ollama'},
+        ),
+        ImageEntry(
+            id='ngrok',
+            label='Ngrok',
+            icon='󰛶',
+            network_mode='host',
+            path=DOCKER_PREFIX + 'ngrok/ngrok:latest',
+            registry='docker.io',
+            environment_vairables={
+                'NGROK_AUTHTOKEN': lambda: qrcode_input(
+                    r'^[a-zA-Z0-9]{20,30}_[a-zA-Z0-9]{20,30}$',
+                    resolver=lambda code, _: code,
+                    prompt='Enter the Ngrok Auth Token',
+                    extra_information=NotificationExtraInformation(
+                        text="""\
+Follow these steps:
+
+1. Login to your ngrok account.
+2. Get the authentication token from the dashboard.
+3. Convert it to QR code.
+4. Scan QR code to input the token.""",
+                        picovoice_text="""\
+Follow these steps:
+
+1. Login to your {ngrok|EH N G EH R AA K} account
+2. Get the authentication token from the dashboard
+3. Convert it to {QR|K Y UW AA R} code
+4. Scan QR code to input the token""",
+                    ),
+                ),
+            },
+            command=lambda: qrcode_input(
+                '',
+                resolver=lambda code, _: code,
+                prompt='Enter the command, for example: `http 80` or `tcp 22`',
+                extra_information=NotificationExtraInformation(
+                    text='This is the command you would enter when running ngrok. '
+                    'Refer to ngrok documentation for further information',
+                    picovoice_text="""\
+This is the command you would enter when running {ngrok|EH N G EH R AA K}.
+Refer to {ngrok|EH N G EH R AA K} documentation for further information""",
+                ),
+            ),
+        ),
+        *(
+            [
+                ImageEntry(
+                    id='alpine',
+                    label='Alpine',
+                    icon='',
+                    path=DOCKER_PREFIX + 'alpine:latest',
+                    registry='docker.io',
+                ),
+            ]
+            if DEBUG_MODE_DOCKER
+            else []
+        ),
+    ]
+}
+IMAGE_IDS = list(IMAGES.keys())
+
+
+def image_reducer(
+    state: ImageState | None,
+    action: DockerImageAction | CombineReducerAction | IpUpdateInterfacesAction,
+) -> ImageState:
+    """Image reducer."""
+    if state is None:
+        if isinstance(action, CombineReducerInitAction):
+            image = IMAGES[action.key]
+            return ImageState(id=image.id)
+        raise InitializationActionError(action)
+
+    if not isinstance(action, DockerImageAction) or action.image != state.id:
+        return state
+
+    if isinstance(action, DockerImageSetStatusAction):
+        return replace(
+            state,
+            status=action.status,
+            ports=action.ports if action.ports else state.ports,
+            container_ip=action.ip,
+        )
+
+    if isinstance(action, DockerImageSetDockerIdAction):
+        return replace(state, docker_id=action.docker_id)
+
+    return state
+
+
+reducer, reducer_id = combine_reducers(
+    state_type=DockerState,
+    action_type=DockerImageAction,
+    event_type=DockerImageEvent,
+    service=service_reducer,
+    **{image: image_reducer for image in IMAGE_IDS},
+)
