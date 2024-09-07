ba5b600bbfe45d0151d8c02034c57a067d38f521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 03:06:01 +0000

    Add package pyinstrument/4.7.3/pyinstrument-4.7.3.tar.gz

---
+++ packages/pyinstrument/pyinstrument-4.7.3.tar.gz/pyinstrument-4.7.3/html_renderer/src/App.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+  import Header from './lib/Header.svelte'
+  import Frame from './lib/Frame.svelte'
+  import type Session from "./lib/model/Session";
+  export let session: Session
+  import faviconImage from './assets/favicon.png'
+  import { onDestroy, onMount } from 'svelte';
+
+  // add favicon
+  const favicon = document.createElement('link')
+  favicon.rel = 'shortcut icon'
+  favicon.href = faviconImage
+  document.head.appendChild(favicon)
+
+  // don't let the body scroll up due to lack of content (when a tree is closed)
+  // prevents the frames from jumping around when they are collapsed
+  function didScroll() {
+    document.body.style.minHeight = `${window.scrollY + window.innerHeight}px`;
+  }
+  onMount(() => {
+    window.addEventListener('scroll', didScroll);
+    didScroll();
+  });
+  onDestroy(() => {
+    window.removeEventListener('scroll', didScroll);
+  });
+
+  const rootFrame = session.rootFrame;
+  const duration = rootFrame?.time.toLocaleString(undefined, {maximumSignificantDigits: 3});
+  let name = rootFrame?.function;
+  if (name == '<module>') {
+    name = session.program;
+  }
+
+  document.title = `${duration}s - ${name} - pyinstrument`
+</script>
+
+<div class="app">
+  <Header session={session} />
+  <div class="spacer" style="height: 20px;"></div>
+  <div class="margins">
+
+  <div class="program"><span class="label">Program:&nbsp;</span>{name}</div>
+
+    {#if session.rootFrame}
+      <Frame frame={session.rootFrame} />
+    {:else}
+      <div class="error">
+        No samples recorded.
+      </div>
+    {/if}
+  </div>
+</div>
+
+
+<style lang="scss">
+  .app {
+    font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  .program {
+    font-size: 14px;
+    font-weight: 600;
+    margin-bottom: 16px;
+    color: #B4B4B4;
+    .label {
+      color: #EAEAEA;
+      text-transform: uppercase;
+    }
+  }
+</style>
---
+++ packages/pyinstrument/pyinstrument-4.7.3.tar.gz/pyinstrument-4.7.3/test/util.py
@@ -0,0 +1,85 @@
+import asyncio
+import os
+import time
+from typing import Generator, Generic, Iterable, Iterator, NoReturn, Optional, TypeVar
+
+from flaky import flaky
+
+from pyinstrument.frame import SYNTHETIC_LEAF_IDENTIFIERS, Frame
+from pyinstrument.profiler import Profiler
+
+if "CI" in os.environ:
+    # a decorator that allows some test flakiness in CI environments, presumably
+    # due to contention. Useful for tests that rely on real time measurements.
+    flaky_in_ci = flaky(max_runs=5, min_passes=1)
+else:
+    flaky_in_ci = lambda a: a
+
+
+def assert_never(x: NoReturn) -> NoReturn:
+    raise AssertionError(f"Invalid value: {x!r}")
+
+
+def do_nothing():
+    pass
+
+
+def busy_wait(duration):
+    end_time = time.time() + duration
+
+    while time.time() < end_time:
+        do_nothing()
+
+
+def walk_frames(frame: Frame) -> Generator[Frame, None, None]:
+    yield frame
+
+    for f in frame.children:
+        yield from walk_frames(f)
+
+
+T = TypeVar("T")
+
+
+def first(iterator: Iterator[T]) -> Optional[T]:
+    try:
+        return next(iterator)
+    except StopIteration:
+        return None
+
+
+def calculate_frame_tree_times(frame: Frame):
+    # assuming that the leaf nodes of a frame tree have correct, time values,
+    # calculate the times of all nodes in the frame tree
+
+    child_time_sum = 0.0
+
+    for child in frame.children:
+        if child.identifier not in SYNTHETIC_LEAF_IDENTIFIERS:
+            calculate_frame_tree_times(child)
+
+        child_time_sum += child.time
+
+    frame.time = child_time_sum + frame.absorbed_time
+
+
+BUSY_WAIT_SCRIPT = """
+import time, sys
+
+def do_nothing():
+    pass
+
+def busy_wait(duration):
+    end_time = time.time() + duration
+
+    while time.time() < end_time:
+        do_nothing()
+
+def main():
+    print('sys.argv: ', sys.argv)
+    busy_wait(0.1)
+
+
+if __name__ == '__main__':
+    main()
+"""
