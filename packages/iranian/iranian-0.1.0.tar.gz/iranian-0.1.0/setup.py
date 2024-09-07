8793594d812dc863c5457ee238a6313dcec98dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 05:06:36 +0000

    Add package iranian/0.1.0/iranian-0.1.0.tar.gz

---
+++ packages/iranian/iranian-0.1.0.tar.gz/iranian-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='iranian',
+version='0.1.0',
+author='Z3N',
+author_email='iliya.souri7890@example.com',
+description='This is Iran',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.7'
+)
