44c9928f10c4f5b6ad50e85310a4750cd51e879e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 14:05:26 +0000

    Add package envector/0.2.0/envector-0.2.0.tar.gz

---
+++ packages/envector/envector-0.2.0.tar.gz/envector-0.2.0/src/envector/_images.py
@@ -0,0 +1,16 @@
+r"""
+.. only:: html
+
+    .. |pkg_img| image:: https://badge.fury.io/py/envector.png
+       :target: https://pypi.python.org/pypi/envector/
+
+    .. |docs_img| image:: https://readthedocs.org/projects/pip/badge/?version=stable
+       :target: http://envector.readthedocs.org/en/stable/
+
+    .. |versions_img| image:: https://img.shields.io/pypi/pyversions/ENvector.svg
+       :target: https://github.com/mhogan-nwra/envector
+
+    .. |downloads_img| image:: https://pepy.tech/badge/envector/month
+       :target: https://pepy.tech/project/envector
+       :alt: PyPI - Downloads
+"""
