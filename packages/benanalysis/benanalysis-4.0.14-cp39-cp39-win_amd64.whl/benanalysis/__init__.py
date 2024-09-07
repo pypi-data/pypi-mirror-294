538e35a4b745940f55cef8348e0e3c1a96d4931d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 03:05:59 +0000

    Add package benanalysis/4.0.14/benanalysis-4.0.14-cp39-cp39-win_amd64.whl

---
+++ packages/benanalysis/benanalysis-4.0.14-cp39-cp39-win_amd64.whl/benanalysis/__init__.py
@@ -0,0 +1,42 @@
+#
+# @file __init__.py
+# @author Markus Führer
+# @date 5 May 2023
+# @copyright Copyright © 2023 Bentham Instruments Ltd. All Rights Reserved.
+#
+
+
+# start delvewheel patch
+def _delvewheel_patch_1_8_1():
+    import ctypes
+    import os
+    import platform
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'benanalysis.libs'))
+    is_conda_cpython = platform.python_implementation() == 'CPython' and (hasattr(ctypes.pythonapi, 'Anaconda_GetVersion') or 'packaged by conda-forge' in sys.version)
+    if sys.version_info[:2] >= (3, 8) and not is_conda_cpython or sys.version_info[:2] >= (3, 10):
+        if os.path.isdir(libs_dir):
+            os.add_dll_directory(libs_dir)
+    else:
+        load_order_filepath = os.path.join(libs_dir, '.load-order-benanalysis-4.0.14')
+        if os.path.isfile(load_order_filepath):
+            with open(os.path.join(libs_dir, '.load-order-benanalysis-4.0.14')) as file:
+                load_order = file.read().split()
+            kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+            kernel32.LoadLibraryExW.restype = ctypes.c_void_p
+            for lib in load_order:
+                lib_path = os.path.join(os.path.join(libs_dir, lib))
+                if os.path.isfile(lib_path) and not kernel32.LoadLibraryExW(ctypes.c_wchar_p(lib_path), None, 8):
+                    raise OSError('Error loading {}; {}'.format(lib, ctypes.FormatError(ctypes.get_last_error())))
+
+
+_delvewheel_patch_1_8_1()
+del _delvewheel_patch_1_8_1
+# end delvewheel patch
+
+from benanalysis._benpy_core import *
+from benanalysis._benpy_core import colorimetry
+from benanalysis._benpy_core import io
+from benanalysis._benpy_core import monochromator
+from benanalysis._benpy_core import physics
+from benanalysis._benpy_core import utils
