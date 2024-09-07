c140125499cf1f7fff8ef4a5bcf9d9ce0a435e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 04:05:07 +0000

    Add package pymium/0.3.0/pymium-0.3.0.tar.gz

---
+++ packages/pymium/pymium-0.3.0.tar.gz/pymium-0.3.0/pymium/core/base/prebuilt_types.py
@@ -0,0 +1,12 @@
+from .types import ElementType
+
+class Types:
+    div = ElementType("div")
+    p = ElementType("p")
+    h1 = ElementType("h1")
+    h2 = ElementType("h2")
+    h3 = ElementType("h3")
+    h4 = ElementType("h4")
+    h5 = ElementType("h5")
+    h6 = ElementType("h6")
+    button = ElementType("button")
