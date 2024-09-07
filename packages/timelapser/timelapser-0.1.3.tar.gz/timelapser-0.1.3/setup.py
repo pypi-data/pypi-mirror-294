dc92b82b1132c9f650b6e4b0585776a1d54b5630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 03:05:52 +0000

    Add package timelapser/0.1.3/timelapser-0.1.3.tar.gz

---
+++ packages/timelapser/timelapser-0.1.3.tar.gz/timelapser-0.1.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="timelapser",
+    version="0.1.3",
+    packages=find_packages(),
+    install_requires=[
+        "typer",
+        "numpy",
+        "opencv-python-headless",
+        "mss",
+        "pynput",
+        "rich",
+    ],
+    entry_points={"console_scripts": ["timelapser=timelapser.main:app"]},
+    author="Asib Hossen",
+    author_email="dev.asib@proton.me",
+    description="Record and save time-lapse videos while you work.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/asibhossen897/TimeLapser",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
---
+++ packages/timelapser/timelapser-0.1.3.tar.gz/timelapser-0.1.3/timelapser/recorder.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2 as cv
+import mss
+import os
+import time
+from .utils import find_time, result_format, result_format_codec
+
+file_path = None
+
+
+def mkdir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def create_video(fps: int, mp4: bool, screen_size, output_dir: str = "Outputs"):
+    mkdir(output_dir)
+    file_path = f"{output_dir}/TimeLapser_{find_time()}{result_format(mp4)}"
+
+    fourcc = cv.VideoWriter_fourcc(*result_format_codec(mp4))
+    out = cv.VideoWriter(
+        file_path,
+        fourcc,
+        fps,
+        screen_size,
+    )
+    return out, file_path
+
+
+def recorder(out, monitor):
+    with mss.mss() as sct:
+        screenshot = sct.grab(monitor)
+        frame = np.array(screenshot)
+        if frame.shape[2] == 4:
+            frame = frame[:, :, :3]
+        out.write(frame)
