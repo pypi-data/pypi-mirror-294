f9c12141b038198ca3884cdf2b5990d872f01e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 04:04:51 +0000

    Add package frieren/0.1.0/frieren-0.1.0-py3-none-any.whl

---
+++ packages/frieren/frieren-0.1.0-py3-none-any.whl/frieren/base/locator.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Generic, TypeVar
+
+
+from .repository import RepositoryT
+from .metadata import MetadataT
+
+
+@dataclass
+class BaseLocator(Generic[RepositoryT, MetadataT]):
+    e: RepositoryT
+
+    @property
+    @abstractmethod
+    def save_dir(self) -> Path:
+        pass
+
+    @abstractmethod
+    def get_path(self, metadata: MetadataT) -> Path:
+        pass
+
+
+LocatorT = TypeVar("LocatorT", bound=BaseLocator)
