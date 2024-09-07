1d9943b387925653d8b9a1b81234b12cb0da73e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 15:05:54 +0000

    Add package sbdletldemo/0.0.1/sbdletldemo-0.0.1-py3-none-any.whl

---
+++ packages/sbdletldemo/sbdletldemo-0.0.1-py3-none-any.whl/SBDL/__main__.py
@@ -0,0 +1,23 @@
+from SBDL.main_sbdl import main as sbdl_main
+from SBDL.lib.logger import logger
+
+import sys
+
+def main(argv):
+    sbdl_main(argv)
+
+def from_databricks():
+    print("--------------------------------------")
+    print("Running Wheel Atleast")
+    print(sys.argv)
+    print("---------------------------------------------")
+
+if __name__ == "__main__":
+    if(len(sys.argv)<2):
+        print("Usage: sbdl {entity_name}: Arguments are missing")
+        '''
+        "{\"entity\":\"acctload\"}"
+        '''
+        sys.exit(-1)
+    logger.info(sys.argv)
+    main(sys.argv)
