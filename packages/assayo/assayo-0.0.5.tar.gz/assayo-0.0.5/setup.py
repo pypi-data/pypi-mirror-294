a2a480f644021e3c59e68942008b8d838a57f124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 06:05:31 +0000

    Add package assayo/0.0.5/assayo-0.0.5.tar.gz

---
+++ packages/assayo/assayo-0.0.5.tar.gz/assayo-0.0.5/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import ast
+from setuptools import find_packages, setup
+
+setup(
+    name="assayo",
+    version="0.0.5",
+    author="Aleksei Bakhirev",
+    author_email="alexey-bakhirev@yandex.ru",
+    description="Visualization and analysis of git commit statistics. Team Lead performance tool.",
+    url="https://github.com/bakhirev/assayo-npx",
+    packages=find_packages(exclude=[]),
+    include_package_data=True,
+    keywords=["git","stat","statistic","statistics","log","report","audit","data-visualization","commit","commits","achivments"],
+    scripts=[],
+    entry_points={"console_scripts": ["assayo=bin.main:main"]},
+    zip_safe=False,
+    package_data = {"assayo":["assayo/**/*", "README.md"]},
+    python_requires=">=3.6",
+    license="License :: OSI Approved :: MIT License",
+    classifiers=[
+        "Programming Language :: Python",
+        # "Programming Language :: Python :: 3",
+        # "Operating System :: OS Independent",
+        # "Private :: Do Not Upload"
+    ],
+)
