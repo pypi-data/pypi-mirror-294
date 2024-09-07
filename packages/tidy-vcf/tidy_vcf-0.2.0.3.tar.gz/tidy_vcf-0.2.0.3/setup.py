0755207f50bfe326aad82d183da0517b14209a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 05:06:11 +0000

    Add package tidy-vcf/0.2.0.3/tidy_vcf-0.2.0.3.tar.gz

---
+++ packages/tidy-vcf/tidy_vcf-0.2.0.3.tar.gz/tidy_vcf-0.2.0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages 
+  
+long_description = 'Given a vcf file, produces a tidy versions of sites and genotypes data, in \
+                    efforts to make it easier to calculate summary statitics and visualizations.' 
+  
+setup( 
+        name ='tidy_vcf', 
+        version ='0.2.0.3', 
+        author ='Silas Tittes', 
+        author_email ='silas.tittes@gmail.com', 
+        url ='https://github.com/silastittes/tidy_vcf', 
+        description ='Make tidy VCF data.', 
+        long_description = long_description, 
+        long_description_content_type ="text/markdown", 
+        license ='MIT', 
+        packages = find_packages(), 
+        entry_points ={ 
+            'console_scripts': [ 
+                'tidy_vcf = tidy_vcf.tidy_vcf:main'
+            ] 
+        }, 
+        classifiers =( 
+            "Programming Language :: Python :: 3", 
+            "License :: OSI Approved :: MIT License", 
+            "Operating System :: OS Independent", 
+        ), 
+        keywords ='VCF genetics python package', 
+        zip_safe = False,
+        python_requires='>=3.7'
+) 
