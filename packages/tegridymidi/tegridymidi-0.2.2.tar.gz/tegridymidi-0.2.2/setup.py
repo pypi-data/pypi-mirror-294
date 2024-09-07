c27eeb2a24fe7ebae5153b2b93ea3fd944657359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 03:06:22 +0000

    Add package tegridymidi/0.2.2/tegridymidi-0.2.2.tar.gz

---
+++ packages/tegridymidi/tegridymidi-0.2.2.tar.gz/tegridymidi-0.2.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+# Install fluidsynth using apt
+os.system('apt-get update && apt-get install -y fluidsynth')
+
+# Read the contents of your README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="tegridymidi",
+    version="0.2.2",
+    description="Tools for reading, writing, and manipulating MIDIs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Alex Lev",
+    author_email="alexlev61@proton.me",
+    packages=find_packages(),  # Automatically find and include all packages
+    include_package_data=True,
+    package_data={
+        'tegridymidi': ['sample_midis/*'],
+    },
+    install_requires=[
+        'tqdm',
+        'numpy',
+        'matplotlib',
+        'scipy',
+        'networkx',
+        'scikit-learn',
+    ],
+    extras_require={
+        'cuda': [
+            'einops',
+            'torch',
+            'cupy-cuda12x',
+            'torch-summary',
+        ],
+    }
+)
