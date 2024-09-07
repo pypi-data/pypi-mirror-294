a76e9d21ca5b3d4acaabd4ccef3a7b840ad56c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 06:05:08 +0000

    Add package mkslides-reveal/0.1.2/mkslides_reveal-0.1.2-py3-none-any.whl

---
+++ packages/mkslides-reveal/mkslides_reveal-0.1.2-py3-none-any.whl/mkslides_reveal/constants.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+import re
+
+MD_IMAGE_REGEX = re.compile(
+    r"""
+    !                           # Start of the image
+    \[.*?\]                     # Alt text
+    \((?P<location>.*?)\)       # Image location
+    """,
+    re.VERBOSE,
+)
+HTML_IMAGE_REGEX = re.compile(
+    r"""
+    <img                        # Start of the image
+    .+?                         # Any attributes
+    src=                        # src attribute
+        (?P<delimiter>['\"])    # Delimiter
+        (?P<location>.+?)       # Image location
+        (?P=delimiter)          # Delimiter
+    .*?                         # Any attributes
+    >                           # End of the image
+    """,
+    re.VERBOSE,
+)
+
+HTML_BACKGROUND_IMAGE_REGEX = re.compile(
+    r"""
+    <!--                        # Start of the comment
+    .*?                         # Any content
+    data-background-image=      # data-background-image attribute
+        (?P<delimiter>['\"])    # Delimiter
+        (?P<location>.+?)       # Image location
+        (?P=delimiter)          # Delimiter
+    .*?                         # Any content
+    -->                         # End of the comment
+    """,
+    re.VERBOSE,
+)
+
+EXPECTED_CONFIG_LOCATION = "mkslides.yml"
+DEFAULT_CONFIG_PATH = Path("./assets/mkslides.default.yml").resolve(strict=True)
+DEFAULT_OUTPUT_DIR = "site"
+
+ASSETS_PATH = Path("assets").resolve(strict=True)
+REVEALJS_PATH = Path(ASSETS_PATH / "reveal.js").resolve(strict=True)
+HIGHLIGHTJS_THEMES_PATH = Path(ASSETS_PATH / "highlight.js" / "build").resolve(strict=True)
