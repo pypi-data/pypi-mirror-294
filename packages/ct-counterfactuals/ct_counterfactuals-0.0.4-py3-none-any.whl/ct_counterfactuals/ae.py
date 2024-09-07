2deac648186667942e69f0cc9c025303cb931561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 14:05:54 +0000

    Add package ct-counterfactuals/0.0.4/ct_counterfactuals-0.0.4-py3-none-any.whl

---
+++ packages/ct-counterfactuals/ct_counterfactuals-0.0.4-py3-none-any.whl/ct_counterfactuals/ae.py
@@ -0,0 +1,148 @@
+import torch
+import torch.nn as nn
+from omegaconf import OmegaConf
+import taming.models.vqgan
+import torchvision
+import numpy as np
+import latentshift
+import os
+
+baseurl = 'https://github.com/ieee8023/ct-counterfactuals/releases/download/init/'
+
+class VQGAN(nn.Module):
+    """
+    weights='2023-12-25T10-26-40_ct2_vqgan256_sddd' Trained on slices from TotalSegmenter, DeepLesion, and LUNA16 datasets.
+    """
+    def __init__(self, weights=None, ckpt_path=None, yaml_path=None, download=False, resize=None, rotate_flip=False, cache_dir=None):
+        super().__init__()
+        
+        if (weights is None) and ((ckpt_path is None) and (yaml_path is None)):
+            raise Exception('Must specify (weights) or (ckpt_path and yaml_path)')
+
+        if not weights is None:
+            if weights == '2023-12-25T10-26-40_ct2_vqgan256_sddd':
+                resize = 256
+                rotate_flip = True
+            
+            if cache_dir is None:
+                cache_dir = latentshift.utils.get_cache_folder()
+            ckpt_path = cache_dir + weights + '.ckpt'
+            yaml_path = cache_dir + weights + '.yaml'
+            if (not os.path.isfile(ckpt_path)) or (not os.path.isfile(yaml_path)):
+                if download:
+                    latentshift.utils.download(baseurl + weights + '.ckpt', ckpt_path)
+                    latentshift.utils.download(baseurl + weights + '.yaml', yaml_path)
+                else:
+                    print("No weights found, specify download=True to download them.")
+        
+        self.resize = resize
+        self.rotate_flip = rotate_flip
+        
+        c = OmegaConf.load(yaml_path)
+        
+        self.config = c['model']['params']
+        self.resolution = self.config['ddconfig']['resolution']
+        
+        vqmodel = taming.models.vqgan.VQModel(**self.config)
+        
+        if ckpt_path is not None:
+            a = torch.load(ckpt_path, map_location=torch.device('cpu'))
+            vqmodel.load_state_dict(a['state_dict'])
+        
+        self.model = vqmodel
+    
+    def encode(self, x):
+
+        # input range is 0,1 but this model expects [-1, 1]
+        x = ((x*2)-1)
+        
+        self.input_size = x.shape[3]
+        if not self.resize is None:
+            x = torchvision.transforms.Resize(self.resize)(x)
+
+        if self.rotate_flip:
+            # if the model was trained in a legacy orientation
+            # we rotate and flip into the standard view
+            x = torch.rot90(x, k=1, dims=[2,3])
+            x = torch.flip(x, dims=[3])
+            
+        return self.model.encode(x)[0]
+    
+    def decode(self, z, image_shape=None):
+        xp = self.model.decode(z)
+    
+        if self.rotate_flip:
+            xp = torch.flip(xp, dims=[3])
+            xp = torch.rot90(xp, k=3, dims=[2,3])
+        
+        if not self.resize is None:
+            xp = torchvision.transforms.Resize(self.input_size)(xp)
+
+        # the model should output between 0,1
+        xp = ((xp+2)/4)
+
+        return xp
+    
+    def forward(self, x):
+        return self.decode(self.encode(x))
+
+
+class SliceAECrop:
+    """Encodes just a subset of slices and replaced the rest with the input volume.
+    Prefer SliceAEFull over this class."""
+    def __init__(self, ae, start, end, limit_to_slices=False):
+        self.ae = ae
+        self.start = start
+        self.end = end
+        self.limit_to_slices = limit_to_slices
+        
+    def encode(self,x):
+        """Ignores the region outside of the slice.
+        Only encodes slices from start to end."""
+        self.x = x
+        zz = x[:,:,:,:,self.start:self.end].transpose(-1,0)[...,0]
+        return self.ae.encode(zz)
+
+    def decode(self,zz):
+        """Decodes the encoded slices and then concats the other slices from the volume."""
+        xxp = self.ae.decode(zz)
+        if self.limit_to_slices:
+            return  xxp[...,None].transpose(-1,0)
+        else:
+            return torch.cat([self.x[:,:,:,:,:self.start], xxp[...,None].transpose(-1,0), self.x[:,:,:,:,self.end:]], 4)
+
+
+class SliceAEFull:
+    """Encodes the entire volume but restricts the gradient to only decoding between some range"""
+    def __init__(self, ae, start, end, batch_size = 16):
+        self.ae = ae
+        self.start = start
+        self.end = end
+        self.batch_size = batch_size
+        
+    def encode(self, x):
+        """Encodes the entire volume in batches and concatenates the embeddings. Gradients blocked."""
+        zz = x.transpose(-1,0)[...,0]
+        with torch.no_grad():
+            zs = [self.ae.encode(zz[i:i+self.batch_size]) for i in np.arange(0,len(zz),self.batch_size)]
+        zs = torch.cat(zs)
+        return zs
+
+    def decode(self, zz):
+        """Decodes the embeddings in batches and concats them. Gradients blocked.
+        Then decodes a specific range with gradients enabled."""
+        with torch.no_grad():
+            xx = [self.ae.decode(zz[i:i+self.batch_size]) for i in np.arange(0,len(zz),self.batch_size)]
+            
+        xx = torch.cat(xx)
+        xx[self.start:self.end] = self.ae.decode(zz[self.start:self.end])
+        
+        return xx[...,None].transpose(-1,0)
+
+
+
+
+
+
+
+
---
+++ packages/ct-counterfactuals/ct_counterfactuals-0.0.4-py3-none-any.whl/ct_counterfactuals/classifiers/lungmask/__init__.py
@@ -0,0 +1,76 @@
+import argparse
+import numpy as np
+import pandas as pd
+import sys
+import os
+import torch
+import pathlib
+import torch.nn as nn
+import latentshift
+from .resunet import UNet
+
+baseurl = 'https://github.com/ieee8023/ct-counterfactuals/releases/download/init/'
+
+class LungMaskSegmenter(nn.Module):
+    """U-net(R231): This model was trained on a large and diverse dataset that covers a wide range of visual variabiliy. The model performs segmentation on individual slices, extracts right-left lung seperately includes airpockets, tumors and effusions. The trachea will not be included in the lung segmentation. https://doi.org/10.1186/s41747-020-00173-2
+
+    ```python
+    import ct_counterfactuals as ct_cf
+    model = ct_cf.classifiers.lungmask.LungMaskSegmenter()
+    x = torch.ones([1, 1, 224, 224, 174])
+    out = model(x)
+    out.shape # [1, 3, 224, 224, 1]
+    
+    # Channels
+    #0 = No lung
+    #1 = Right lung
+    #2 = Left lung
+    ```
+    
+    https://github.com/JoHof/lungmask
+
+    Hofmanninger, J., Prayer, F., Pan, J. et al. Automatic lung segmentation in routine imaging is primarily a data diversity problem, not a methodology problem. Eur Radiol Exp 4, 50 (2020). https://doi.org/10.1186/s41747-020-00173-2
+    """
+
+    def __init__(self, download=False, cache_dir=None):
+        super(LungMaskSegmenter, self).__init__()
+
+        self.resolution = 224
+
+        if cache_dir is None:
+            cache_dir = latentshift.utils.get_cache_folder()
+
+        weights = "lungmask-unet_r231-d5d2fc3d.pth"
+        weights_path = pathlib.Path(cache_dir) / weights
+
+        if not os.path.isfile(weights_path):
+            if download:
+                latentshift.utils.download(baseurl + weights, weights_path)
+            else:
+                raise Exception("No weights found, specify download=True to download them.")
+                
+        state_dict = torch.load(weights_path, map_location=torch.device('cpu'))
+
+        self.n_classes = len(list(state_dict.values())[-1])
+     
+        self.model = UNet(
+            n_classes=self.n_classes,
+            padding=True,
+            depth=5,
+            up_mode="upsample",
+            batch_norm=True,
+            residual=False,
+        )
+        self.model.load_state_dict(state_dict)
+        self.model = self.model.eval()
+    
+        self.targets = ['No Lung', 'Right Lung', 'Left Lung']
+
+    def forward(self, x):
+        if len(x.shape) == 5: # if a volume
+            output = torch.stack([self.model(x[...,i]) for i in range(x.shape[-1])], 4)
+            return torch.sigmoid(output)
+        else: #if a single slice
+            return torch.sigmoid(self.model(x))
+    
+
