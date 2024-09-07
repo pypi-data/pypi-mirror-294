631f6ab77e8eb2828a1e0a02dada7b87141d2b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 15:05:41 +0000

    Add package meds-torch/0.0.3/meds_torch-0.0.3.tar.gz

---
+++ packages/meds-torch/meds_torch-0.0.3.tar.gz/meds_torch-0.0.3/README.md
@@ -0,0 +1,123 @@
+# MEDS-torch
+
+<p align="center">
+  <a href="https://pytorch.org/get-started/locally/"><img alt="PyTorch" src="https://img.shields.io/badge/PyTorch-ee4c2c?logo=pytorch&logoColor=white"></a>
+  <a href="https://pytorchlightning.ai/"><img alt="Lightning" src="https://img.shields.io/badge/-Lightning-792ee5?logo=pytorchlightning&logoColor=white"></a>
+  <a href="https://hydra.cc/"><img alt="Config: Hydra" src="https://img.shields.io/badge/Config-Hydra-89b8cd"></a>
+  <a href="https://github.com/ashleve/lightning-hydra-template"><img alt="Template" src="https://img.shields.io/badge/-Lightning--Hydra--Template-017F2F?style=flat&logo=github&labelColor=gray"></a><br>
+  <a href="https://www.python.org/downloads/release/python-3100/"><img alt="Python" src="https://img.shields.io/badge/-Python_3.12+-blue?logo=python&logoColor=white"></a>
+  <a href="https://pypi.org/project/meds-torch/"><img alt="PyPI" src="https://img.shields.io/badge/PyPI-v0.0.1a1-blue?logoColor=blue"></a>
+  <a href="https://hydra.cc/"><img alt="Hydra" src="https://img.shields.io/badge/Config-Hydra_1.3-89b8cd"></a>
+  <a href="https://codecov.io/github/Oufattole/meds-torch"><img src="https://codecov.io/github/Oufattole/meds-torch/graph/badge.svg?token=BV119L5JQJ"/></a>
+  <a href="https://github.com/Oufattole/meds-torch/actions/workflows/tests.yaml"><img alt="Tests" src="https://github.com/Oufattole/meds-torch/actions/workflows/tests.yaml/badge.svg"></a>
+  <a href="https://github.com/Oufattole/meds-torch/actions/workflows/code-quality-main.yaml"><img alt="Code Quality" src="https://github.com/Oufattole/meds-torch/actions/workflows/code-quality-main.yaml/badge.svg"></a>
+  <a href="https://github.com/Oufattole/meds-torch/graphs/contributors"><img alt="Contributors" src="https://img.shields.io/github/contributors/oufattole/meds-torch.svg"></a>
+  <a href="https://github.com/Oufattole/meds-torch/pulls"><img alt="Pull Requests" src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg"></a>
+  <a href="https://github.com/Oufattole/meds-torch#license"><img alt="License" src="https://img.shields.io/badge/License-MIT-green.svg?labelColor=gray"></a>
+<a href='https://meds-torch.readthedocs.io/en/latest/?badge=latest'><img src='https://readthedocs.org/projects/meds-torch/badge/?version=latest' alt='Documentation Status' /></a>
+</p>
+
+## Description
+
+This repository provides a flexible suite for advanced machine learning over Electronic Health Records (EHR) using PyTorch, PyTorch Lightning, and Hydra for configuration management. The project ingests tensorized data from the [MEDS_transforms](<>) repository, a robust system for transforming EHR data into ML ready sequence data. By employing a variety of tokenization strategies and sequence model architectures, this framework facilitates the development and testing of models that can perform.
+
+Key features include:
+
+- **Configurable ML Pipeline**: Utilize Hydra to dynamically adjust configurations and seamlessly integrate with PyTorch Lightning for scalable training across multiple environments.
+- **Advanced Tokenization Techniques**: Explore different approaches to embedding EHR data in tokens that sequence model can reason over.
+- **Supervised Models**: Support for supervised training on arbitrary tasks defined on MEDS format data.
+- **Transfer Learning**: Pretrain via contrastive learning, forecasting, and other pre-training methods, and finetune to supervised tasks.
+
+The goal of this project is to push the boundaries of what's possible in healthcare machine learning by providing a flexible, robust, and scalable sequence model tools that accommodate a wide range of research and operational needs. Whether you're conducting academic research or developing clinical applications with MEDS format EHR data, this repository offers tools and flexibility to develop deep sequence models.
+
+## Installation
+
+#### Pip
+
+**PyPi**
+
+```bash
+pip install meds-torch
+```
+
+**git**
+
+```bash
+# clone project
+git clone git@github.com:Oufattole/meds-torch.git
+cd meds-torch
+
+# [OPTIONAL] create conda environment
+conda create -n meds-torch python=3.12
+conda activate meds-torch
+
+# install pytorch according to instructions
+# https://pytorch.org/get-started/
+
+# install requirements
+pip install -e .
+```
+
+## How to run
+
+Train model with default configuration
+
+```bash
+# train on CPU
+python -m meds_torch.train trainer=cpu
+
+# train on GPU
+python -m meds_torch.train trainer=gpu
+```
+
+Train model with chosen experiment configuration from [configs/experiment/](configs/experiment/)
+
+```bash
+python -m meds_torch.train experiment=experiment_name.yaml
+```
+
+You can override any parameter from command line like this
+
+```bash
+python -m meds_torch.train trainer.max_epochs=20 data.batch_size=64
+```
+
+## 📌  Introduction
+
+**Why you might want to use it:**
+
+✅ Support different tokenization methods for EHR data <br>
+
+- Triplet
+- Everything Is text
+- Everything Is a code
+
+✅ MEDS data Supervised Learning and Transfer Learning Support <br>
+
+- randomly initialize a model and train it in a supervised maner on your MEDS format medical data.
+- General Contrastive window Pretraining
+- Random [EBCL](https://arxiv.org/abs/2312.10308) Example
+- [OCP](https://arxiv.org/abs/2111.02599) Example
+- [STraTS](https://arxiv.org/abs/2107.14293) Value Forecasting
+
+✅ Ease of Use and Reusability <br>
+Collection of useful EHR sequence modeling tools, configs, and code snippets. You can use this repo as a reference for developing your own models. Additionally you can easily add new models, datasets, tasks, experiments, and train on different accelerators, like multi-GPU.
+
+## Loggers
+
+By default `wandb` logger is installed with the repo. Please install a different logger below if you wish to use it:
+
+```console
+pip install neptune-client
+pip install mlflow
+pip install comet-ml
+pip install aim>=3.16.2  # no lower than 3.16.2, see https://github.com/aimhubio/aim/issues/2550
+```
+
+## Development Help
+
+To run tests on 8 parallel workers run:
+
+```bash
+pytest -n 8
+```
---
+++ packages/meds-torch/meds_torch-0.0.3.tar.gz/meds_torch-0.0.3/tests/test_datamodules.py
@@ -0,0 +1,197 @@
+import rootutils
+
+root = rootutils.setup_root(__file__, dotenv=True, pythonpath=True, cwd=True)
+
+from pathlib import Path
+
+import pytest
+from torch.utils.data import DataLoader
+
+from meds_torch.data.components.multiwindow_pytorch_dataset import (
+    MultiWindowPytorchDataset,
+)
+from meds_torch.data.components.pytorch_dataset import PytorchDataset
+from meds_torch.data.datamodule import MEDSDataModule
+from tests.conftest import SUPERVISED_TASK_NAME, create_cfg
+
+
+@pytest.mark.parametrize(
+    "collate_type",
+    [
+        "triplet",
+        "event_stream",
+        "triplet_prompt",
+        "eic",
+        "text_code",
+    ],
+)
+def test_pytorch_dataset(meds_dir, collate_type):
+    cfg = create_cfg(overrides=[], meds_dir=meds_dir)
+    cfg.data.collate_type = collate_type
+    cfg.data.tokenizer = "emilyalsentzer/Bio_ClinicalBERT"
+    pyd = PytorchDataset(cfg.data, split="train")
+    assert not pyd.has_task
+    item = pyd[0]
+    assert item.keys() == {"static_indices", "static_values", "dynamic"}
+    batch = pyd.collate([pyd[i] for i in range(2)])
+    if collate_type == "event_stream":
+        assert batch.keys() == {
+            "event_mask",
+            "dynamic_values_mask",
+            "time_delta_days",
+            "dynamic_indices",
+            "dynamic_values",
+            "static_indices",
+            "static_values",
+        }
+    elif collate_type == "triplet":
+        assert batch.keys() == {
+            "mask",
+            "static_mask",
+            "code",
+            "numeric_value",
+            "time_delta_days",
+            "numeric_value_mask",
+        }
+    elif collate_type == "text_code":
+        assert set(batch.keys()) == {
+            "mask",
+            "static_mask",
+            "code",
+            "code_tokens",
+            "code_mask",
+            "numeric_value",
+            "time_delta_days",
+            "numeric_value_mask",
+        }
+    elif collate_type == "triplet_prompt":
+        assert batch.keys() == {
+            "mask",
+            "static_mask",
+            "code",
+            "numeric_value",
+            "time_delta_days",
+            "numeric_value_mask",
+        }
+    elif collate_type == "eic":
+        assert batch.keys() == {
+            "mask",
+            "static_mask",
+            "code",
+            "numeric_value",
+            "time_delta_days",
+            "numeric_value_mask",
+        }
+    else:
+        raise NotImplementedError(f"{collate_type} not implemented")
+
+
+@pytest.mark.parametrize("collate_type", ["triplet", "event_stream", "triplet_prompt", "text_code", "eic"])
+def test_pytorch_dataset_with_supervised_task(meds_dir, collate_type):
+    cfg = create_cfg(overrides=[], meds_dir=meds_dir, supervised=True)
+    cfg.data.collate_type = collate_type
+    assert Path(cfg.data.task_label_path).exists(), f"Path does not exist: {cfg.data.task_label_path}"
+
+    pyd = PytorchDataset(cfg.data, split="train")
+    assert pyd.has_task
+    item = pyd[0]
+    assert item.keys() == {"static_indices", "static_values", "dynamic", "supervised_task"}
+    batch = pyd.collate([pyd[i] for i in range(2)])
+    if collate_type == "event_stream":
+        assert batch.keys() == {
+            "event_mask",
+            "dynamic_values_mask",
+            "time_delta_days",
+            "dynamic_indices",
+            "dynamic_values",
+            "static_indices",
+            "static_values",
+            SUPERVISED_TASK_NAME,
+        }
+    elif collate_type in ["triplet", "triplet_prompt"]:
+        assert batch.keys() == {
+            "mask",
+            "static_mask",
+            "code",
+            "numeric_value",
+            "time_delta_days",
+            "numeric_value_mask",
+            SUPERVISED_TASK_NAME,
+        }
+    elif collate_type == "eic":
+        assert batch.keys() == {
+            "mask",
+            "static_mask",
+            "code",
+            "numeric_value",
+            "time_delta_days",
+            "numeric_value_mask",
+            SUPERVISED_TASK_NAME,
+        }
+    elif collate_type == "text_code":
+        assert set(batch.keys()) == {
+            "mask",
+            "static_mask",
+            "code",
+            "code_tokens",
+            "code_mask",
+            "numeric_value",
+            "time_delta_days",
+            "numeric_value_mask",
+            SUPERVISED_TASK_NAME,
+        }
+    else:
+        raise NotImplementedError(f"{collate_type} not implemented")
+
+
+@pytest.mark.parametrize("subject_level_sampling", [False, True])
+@pytest.mark.parametrize("collate_type", ["triplet"])
+def test_contrastive_windows(meds_dir, subject_level_sampling, collate_type):
+    cfg = create_cfg(overrides=["data=multiwindow_pytorch_dataset"], meds_dir=meds_dir)
+    cfg.data.collate_type = collate_type
+    cfg.data.subject_level_sampling = subject_level_sampling
+
+    assert cfg.data.cache_dir
+    assert Path(cfg.data.raw_windows_fp).exists()
+
+    pyd = MultiWindowPytorchDataset(cfg.data, split="train")
+    item = pyd[0]
+    assert item.keys() == {"pre", "post"}
+    assert item["pre"].keys() == {"static_indices", "static_values", "dynamic"}
+    assert item["post"].keys() == {"static_indices", "static_values", "dynamic"}
+
+    batch = pyd.collate([pyd[i] for i in range(2)])
+    assert batch.keys() == {"pre", "post"}
+    for window in ["pre", "post"]:
+        assert batch[window].keys() == {
+            "mask",
+            "static_mask",
+            "code",
+            "numeric_value",
+            "time_delta_days",
+            "numeric_value_mask",
+        }
+
+
+def test_full_datamodule(meds_dir):
+    cfg = create_cfg(overrides=[], meds_dir=meds_dir)
+    cfg.data.dataloader.batch_size = 1
+    dm = MEDSDataModule(cfg.data)
+    dm.prepare_data()
+
+    assert not dm.data_train and not dm.data_val and not dm.data_test
+
+    dm.setup()
+    assert (
+        isinstance(dm.data_train, PytorchDataset)
+        and isinstance(dm.data_val, PytorchDataset)
+        and isinstance(dm.data_test, PytorchDataset)
+    )
+    assert isinstance(dm.train_dataloader(), DataLoader) and isinstance(dm.val_dataloader(), DataLoader)
+
+    num_datapoints = len(dm.data_train)
+    assert num_datapoints == 70
+
+    batch = next(iter(dm.train_dataloader()))
+    for value in batch.values():
+        assert len(value) == cfg.data.dataloader.batch_size
