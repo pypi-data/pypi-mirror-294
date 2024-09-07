d2ab5b1d3685b454e7507b4c0a459458c6c804fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 04:05:32 +0000

    Add package adata/2.6.5/adata-2.6.5-py3-none-any.whl

---
+++ packages/adata/adata-2.6.5-py3-none-any.whl/adata/common/exception/handler.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+@desc: readme
+@author: 1nchaos
+@time: 2023/8/14
+@log: change log
+"""
+
+import pandas as pd
+
+
+def handler_null(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception:
+            print("data null:", func.__name__)
+            return pd.DataFrame(data=[], columns=[])
+
+    return wrapper
