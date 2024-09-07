550ada69a4000e1591a9fa9c2f59f4f72af688ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 06:05:40 +0000

    Add package example-package-chas-hooge7/0.0.5/example_package_chas_hooge7-0.0.5-py3-none-any.whl

---
+++ packages/example-package-chas-hooge7/example_package_chas_hooge7-0.0.5-py3-none-any.whl/example_package_chas_hooge7/main.py
@@ -0,0 +1,14 @@
+""" 
+following https://www.youtube.com/watch?v=Kz6IlDCyOUY
+
+This is a simple start to a shared package
+"""
+
+
+# main.py
+def hello():
+    print("Hello from example package chas hooge7.py")
+
+
+def goodbye():
+    print("Goodbye from example package chas hooge7.py")
