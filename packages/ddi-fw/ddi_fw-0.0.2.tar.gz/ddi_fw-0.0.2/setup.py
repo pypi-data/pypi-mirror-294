a4e8527f0e47731941c30c14751be64e06157e81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 03:05:47 +0000

    Add package ddi-fw/0.0.2/ddi_fw-0.0.2.tar.gz

---
+++ packages/ddi-fw/ddi_fw-0.0.2.tar.gz/ddi_fw-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+name='ddi_fw',
+version='0.0.2',
+author='Kıvanç Bayraktar',
+author_email='bayraktarkivanc@gmail.com',
+description='Do not use :)',
+# packages=find_packages(),
+packages=['ddi_fw/datasets','ddi_fw/drugbank','ddi_fw/experiments','ddi_fw/ner','ddi_fw/utils'],
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
