6ec3be72c97f3dbd4f6711d9c412b9dc9f1d3ed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 06:05:31 +0000

    Add package evidently/0.4.37/evidently-0.4.37-py3-none-any.whl

---
+++ packages/evidently/evidently-0.4.37-py3-none-any.whl/evidently/descriptors/sentiment_descriptor.py
@@ -0,0 +1,8 @@
+from evidently.features import sentiment_feature
+from evidently.features.generated_features import FeatureDescriptor
+from evidently.features.generated_features import GeneratedFeature
+
+
+class Sentiment(FeatureDescriptor):
+    def feature(self, column_name: str) -> GeneratedFeature:
+        return sentiment_feature.Sentiment(column_name, self.display_name)
