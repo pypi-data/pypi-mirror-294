a3388c17379bb0cf36c030a3e5f7f439eb44ad29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 15:05:49 +0000

    Add package dycw-utilities/0.52.9/dycw_utilities-0.52.9.tar.gz

---
+++ packages/dycw-utilities/dycw_utilities-0.52.9.tar.gz/dycw_utilities-0.52.9/tests/test_logging.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from logging import getLogger
+
+from pytest import mark, param, raises
+
+from utilities.logging import (
+    GetLoggingLevelError,
+    LogLevel,
+    basic_config,
+    get_logging_level,
+)
+
+
+class TestBasicConfig:
+    def test_main(self) -> None:
+        basic_config()
+        logger = getLogger(__name__)
+        logger.info("message")
+
+
+class TestGetLoggingLevel:
+    @mark.parametrize(
+        ("level", "expected"),
+        [
+            param(LogLevel.DEBUG, 10),
+            param(LogLevel.INFO, 20),
+            param(LogLevel.WARNING, 30),
+            param(LogLevel.ERROR, 40),
+            param(LogLevel.CRITICAL, 50),
+        ],
+    )
+    def test_main(self, *, level: str, expected: int) -> None:
+        assert get_logging_level(level) == expected
+
+    def test_error(self) -> None:
+        with raises(GetLoggingLevelError, match="Invalid logging level: 'invalid'"):
+            _ = get_logging_level("invalid")
+
+
+class TestLogLevel:
+    def test_main(self) -> None:
+        assert len(LogLevel) == 5
