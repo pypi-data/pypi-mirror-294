f8b22ec663a2e9d1fd514a826be08d43aaf71d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 06:05:08 +0000

    Add package dahood/0.0.1/dahood-0.0.1.tar.gz

---
+++ packages/dahood/dahood-0.0.1.tar.gz/dahood-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+  name="dahood",
+  version="0.0.1",
+  author="AntiDevelopment",
+  description="Use the DaHood Module to add LUA Implementation into python projects",
+  packages=["dahood"]
+)
