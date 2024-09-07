9f8347ba0368f76f45a9cdcd0c68d8447080a131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 03:06:22 +0000

    Add package lor-gdp-tools/0.1.6/lor_gdp_tools-0.1.6-py3-none-any.whl

---
+++ packages/lor-gdp-tools/lor_gdp_tools-0.1.6-py3-none-any.whl/gdp_tools/config.py
@@ -0,0 +1,5 @@
+SERVER = 'az-sql-gdp-prod-uks.database.windows.net' 
+DATABASE = 'az-syn-gdp-prod-uks' # Location of Synapse
+USERNAME = 'drumble_adm_sql' # load from config
+PASSWORD = 'X!2eLtGdRaCqdPaN9Rn94X' # load from config
+AUTH = 'ActiveDirectoryInteractive' #aka Azure Active Directory - Universal with MFA # using sbaadminuk and setting this auth method results in a connection timeout
