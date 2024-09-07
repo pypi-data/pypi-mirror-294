bd02199de8a2e90eb62ee2e646df2983b75b20b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 04:04:48 +0000

    Add package simple-rest-framework/0.1.5/simple_rest_framework-0.1.5.tar.gz

---
+++ packages/simple-rest-framework/simple_rest_framework-0.1.5.tar.gz/simple_rest_framework-0.1.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simple_rest_framework',
+    version='0.1.5',
+    packages=find_packages(include=['simple_rest_framework', 'simple_rest_framework.*']),
+    install_requires=[
+        'djangorestframework',
+    ],
+    entry_points={
+        'console_scripts': [
+            'simple-rest=simple_rest_framework.cli:main',
+        ],
+    },
+    author='Valentin Cabrera',
+    author_email='valentincabrera2003@gmail.com',
+    description='Esta libreria permite agilizar el desarrollo de APIs REST en Django. Manejando un CRUD básico de forma automática y manejando excepciones de forma sencilla.',
+    url='https://github.com/ValentinCabrera/simple_rest_framework',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
