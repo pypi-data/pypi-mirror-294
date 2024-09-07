da48d5109597ce20b8f09c4f4f2c9746499e18cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 03:06:20 +0000

    Add package fast-knifes/0.0.24/fast_knifes-0.0.24.tar.gz

---
+++ packages/fast-knifes/fast_knifes-0.0.24.tar.gz/fast_knifes-0.0.24/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="fast-knifes",
+    version="0.0.24",
+    author="knifes",
+    author_email="author@example.com",
+    description="Fast Swiss Army Knife",
+    long_description="`pip install fast-knifes --index-url https://pypi.python.org/simple -U`",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.10",
+    install_requires=[
+        "attrs",  #  required by knifes/jsons.py
+        "cattrs",  #  required by knifes/jsons.py
+        "anyio",  # required by knifes/shell.py
+        "click",  # required by knifes/deploy.py
+        # "python-decouple",
+        # "httpx[http2]",
+        # 'cryptography',  #  required by aes
+    ],
+    entry_points={
+        "console_scripts": [
+            "deploy = knifes.scripts.deploy:cli",
+        ],
+    },
+)
