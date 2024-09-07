d3837f970ad3832a0541bfcaff64c797851fd4cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 04:05:06 +0000

    Add package luminescent/0.1.60/luminescent-0.1.60.tar.gz

---
+++ packages/luminescent/luminescent-0.1.60.tar.gz/luminescent-0.1.60/src/luminescent/gplugins/luminescent/sparams.py
@@ -0,0 +1,66 @@
+from .setup import *
+from .constants import *
+from .layers import *
+from .utils import *
+import gdsfactory as gf
+from copy import deepcopy
+import matplotlib.pyplot as plt
+import numpy as np
+
+from gdsfactory.cross_section import Section
+from gdsfactory.generic_tech import LAYER_STACK, LAYER
+
+
+def sparams_problem(c: gf.Component,
+                    margin=None,  # zmargin=None,zlims=None,
+                    dx=.05, wavelengths=[1.55], center_wavelength=None, keys=None,
+                    approx_2D=False, layer_stack=LAYER_STACK,
+                    study="sparams",
+                    **kwargs):
+    wavelengths = sorted(wavelengths)
+    d = 2 if approx_2D else 3
+    prob = dict()
+
+    ports = [int(p.name[1])
+             for p in c.get_ports_list(prefix="o")]
+    if keys is None:
+        io = {i: ports for i in ports}
+    else:
+        io = SortedDict()
+        for k in keys:
+            (o, i) = longname(k).split(",")
+            if i not in io:
+                io[i] = SortedSet()
+            io[i].add(o)
+        for i in io:
+            if i not in io[i]:
+                io[i].add(i)
+
+    runs = [{
+        "d": d,
+        "sources": {
+            port_number(i): {
+                "center": (np.array(c.ports[f"o{port_number(i)}"].center)/1e3).tolist(),
+                "width": (np.array(c.ports[f"o{port_number(i)}"].width)/1e3).tolist(),
+                "normal": normal_from_orientation(c.ports[f"o{port_number(i)}"].orientation),
+                # "endpoints": extend(c.ports[f"o{port_number(i)}"]., margin),
+                "wavelength_mode_numbers": {w: [mode_number(i)] for w in wavelengths},
+                "port": port_number(i),
+            }},
+        "monitors": {
+            port_number(o): {
+                "port": port_number(o),
+                "normal": normal_from_orientation(c.ports[f"o{port_number(o)}"].orientation),
+                "center": (np.array(c.ports[f"o{port_number(o)}"].center)/1e3).tolist(),
+                "width": (np.array(c.ports[f"o{port_number(o)}"].width)/1e3).tolist(),
+                # "endpoints": extend(c.ports[f"o{port_number(o)}"].endpoints, margin),
+                "wavelength_mode_numbers": {w: [mode_number(o)] for w in wavelengths},
+            } for o in io[i]},
+    } for i in io]
+
+    prob = setup(c, study=study,  dx=dx,
+                 runs=runs, margin=margin,
+                 layer_stack=layer_stack, approx_2D=approx_2D, **kwargs)
+    prob["wavelengths"] = wavelengths
+
+    return prob
