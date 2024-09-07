a87f3ea7940eea4fabf60e30579d9fe1951a017a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 15:05:16 +0000

    Add package refact/0.9.8/refact-0.9.8.tar.gz

---
+++ packages/refact/refact-0.9.8.tar.gz/refact-0.9.8/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import re
+
+setup(
+    name="refact",
+    version="0.9.8",
+    packages=find_packages(),
+    install_requires=[
+        "aiohttp",
+        "termcolor",
+        "pydantic",
+    ],
+    author="Small Magellanic Cloud AI LTD",
+    author_email="info@smallcloud.tech",
+    description="A python client to refact-lsp server",
+    url="https://github.com/smallcloudai/refact",
+    classifiers=[
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            'refact=refact.refact_cmdline:cmdline_main',
+        ],
+    },
+)
+
+
+# XXX: move to automatic build
+#   sync version from Cargo.toml
+#   python setup.py sdist
+#   twine upload --repository pypi dist/refact-0.9.7.tar.gz
