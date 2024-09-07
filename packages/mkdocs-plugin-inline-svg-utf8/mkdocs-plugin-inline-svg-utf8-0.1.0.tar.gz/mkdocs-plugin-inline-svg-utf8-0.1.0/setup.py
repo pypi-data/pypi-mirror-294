034876765d106b9fa0163fddd6c150f6bf3ea66f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 03:05:41 +0000

    Add package mkdocs-plugin-inline-svg-utf8/0.1.0/mkdocs-plugin-inline-svg-utf8-0.1.0.tar.gz

---
+++ packages/mkdocs-plugin-inline-svg-utf8/mkdocs-plugin-inline-svg-utf8-0.1.0.tar.gz/mkdocs-plugin-inline-svg-utf8-0.1.0/setup.py
@@ -0,0 +1,29 @@
+# setup.py
+
+import os
+from setuptools import setup, find_packages
+
+def read_file(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='mkdocs-plugin-inline-svg-utf8',
+    version='0.1.0',
+    description='A MkDocs plugin that inlines SVG images into the output with utf-8 encoding.',
+	long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
+    keywords='mkdocs plugin inline svg utf8',
+    url='https://github.dev/Grufbert/mkdocs-plugin-inline-svg-utf8',
+    author='Nico Klinger',
+	license='MIT',
+	python_requires='>=3.5',
+    install_requires=[
+		'mkdocs'
+	],
+    packages=find_packages(),
+    entry_points={
+        'mkdocs.plugins': [
+            'inline-svg-utf8 = src:InlineSvgPluginUtf8',
+        ]
+    }
+)
