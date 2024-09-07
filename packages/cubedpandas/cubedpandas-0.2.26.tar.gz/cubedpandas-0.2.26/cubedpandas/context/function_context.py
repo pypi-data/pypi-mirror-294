5448841c5c12846f3f49f8ada010a2cbf3ac5fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 05:05:57 +0000

    Add package cubedpandas/0.2.26/cubedpandas-0.2.26.tar.gz

---
+++ packages/cubedpandas/cubedpandas-0.2.26.tar.gz/cubedpandas-0.2.26/cubedpandas/context/function_context.py
@@ -0,0 +1,34 @@
+# CubedPandas - Copyright (c)2024, Thomas Zeutschler, see LICENSE file
+
+from __future__ import annotations
+from typing import TYPE_CHECKING, Any
+
+from cubedpandas.context.enums import ContextFunction
+from cubedpandas.context.context import Context
+
+if TYPE_CHECKING:
+    from cubedpandas.cube import Cube
+    from cubedpandas.schema.measure import Measure
+    from cubedpandas.schema.dimension import Dimension
+
+
+class FunctionContext(Context):
+    """
+    A context representing a mathematical operation like SUM, MIN, MAX, AVG, etc.
+    """
+    KEYWORDS = {"SUM", "AVG", "MEDIAN", "MEAN", "MIN", "MAX", "STD", "VAR", "POF",
+                "COUNT", "NAN", "AN", "ZERO", "NZERO", "CUSTOM"}
+
+
+    def __init__(self, parent: Context | None = None, function: ContextFunction | str = ContextFunction.SUM):
+        super().__init__(cube=parent.cube, address=parent.address, parent=parent, row_mask=parent.row_mask,
+                         measure=parent.measure, dimension=parent.dimension, resolve=False, filtered=parent.is_filtered)
+
+        if isinstance(function, str):
+            try:
+                function = ContextFunction[function.upper()]
+            except KeyError:
+                raise ValueError(f"Unknown function {function}. Supported functions are {FunctionContext.KEYWORDS}")
+
+        self._function:ContextFunction = function
+
---
+++ packages/cubedpandas/cubedpandas-0.2.26.tar.gz/cubedpandas-0.2.26/setup.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+# setup.py for cubedpandas
+
+from setuptools import setup
+from setuptools import find_packages
+from cubedpandas import VERSION as CUBEDPANDAS_VERSION
+
+
+# ...to run the build and deploy process to pypi.org manually:
+# 1. delete folder 'build'
+# 1. empty folder 'dist'
+# 2. python3 setup.py sdist bdist_wheel   # note: Wheel need to be installed: pip install wheel
+# 3. twine upload -r  pypi dist/*         # note: Twine need to be installed: pip install twine
+
+# ... via Github actions
+# https://packaging.python.org/en/latest/guides/publishing-package-distribution-releases-using-github-actions-ci-cd-workflows/
+
+VERSION = CUBEDPANDAS_VERSION
+DESCRIPTION = "CubedPandas - Multi-dimensional data analysis for Pandas dataframes."
+LONG_DESCRIPTION = """
+CubedPandas provides an easy, intuitive, fast and fun approach to perform multi-dimensional 
+numerical data analysis & processing on Pandas dataframes. CubedPandas wraps almost any
+dataframe into a multi-dimensional cube, which can be aggregated, sliced, diced, filtered, 
+updated and much more. 
+
+CubedPandas is inspired by OLAP cubes (online analytical processing), which are typically used
+for reporting, business intelligence, data warehousing and financial analysis purposes.
+
+IMPORTANT NOTE: 
+CubedPandas is under active development, not yet feature complete and may be subject to breaking changes.
+ 
+Just give it a try...   
+"""
+
+setup(
+
+    name="cubedpandas",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        "Topic :: Utilities",
+        "Topic :: Database",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+    ],
+    author="Thomas Zeutschler",
+    keywords=['pandas', 'analysis', 'database', 'olap', 'planning', 'reporting', 'forecasting',
+              'multidimensional', 'cube', ],
+    author_email="cubedpandas@gmail.com",
+    url="https://github.com/Zeutschler/cubedpandas",
+    license='MIT',
+    platforms=['any'],
+    zip_safe=True,
+    python_requires='>= 3.10',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'python-dateutil',
+    ],
+    test_suite="cubedpandas.tests",
+    packages=['cubedpandas', 'cubedpandas.context', 'cubedpandas.schema', 'tests'],
+    project_urls={
+        'Homepage': 'https://zeutschler.github.io/cubedpandas/',
+        'Documentation': 'https://zeutschler.github.io/cubedpandas/',
+        'GitHub': 'https://github.com/Zeutschler/cubedpandas',
+    },
+)
