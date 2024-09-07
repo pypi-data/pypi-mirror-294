e4fd668545a2b5daf18c612e04bd875fd86fc451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 15:05:24 +0000

    Add package avplayer/1.8.1/avplayer-1.8.1-py3-none-any.whl

---
+++ packages/avplayer/avplayer-1.8.1-py3-none-any.whl/avplayer/apps/interface/av_interface.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+from abc import ABCMeta, abstractmethod
+from typing import Optional
+
+from numpy import uint8
+from numpy.typing import NDArray
+
+
+class AvInterface(metaclass=ABCMeta):
+    @abstractmethod
+    def on_open(self) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def on_close(self) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def on_image(self, image: NDArray[uint8]) -> Optional[NDArray[uint8]]:
+        raise NotImplementedError
+
+
+class AsyncAvInterface(metaclass=ABCMeta):
+    @abstractmethod
+    async def on_open(self) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def on_close(self) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def on_image(self, image: NDArray[uint8]) -> Optional[NDArray[uint8]]:
+        raise NotImplementedError
+
+
+class AsyncAvTckInterface(AsyncAvInterface):
+    @abstractmethod
+    async def on_resize(self, width: int, height: int) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def on_key(self, keysym: str) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def on_grap(self, image: NDArray[uint8]) -> NDArray[uint8]:
+        raise NotImplementedError
