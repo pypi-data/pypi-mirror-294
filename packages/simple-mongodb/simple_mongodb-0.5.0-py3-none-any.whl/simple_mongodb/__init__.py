7ae503dcd71e0bd08f4b198be5e5664738157fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 04:05:29 +0000

    Add package simple-mongodb/0.5.0/simple_mongodb-0.5.0-py3-none-any.whl

---
+++ packages/simple-mongodb/simple_mongodb-0.5.0-py3-none-any.whl/simple_mongodb/__init__.py
@@ -0,0 +1,9 @@
+from .base_collection import BaseCollection
+from .index import Index
+from .mongodb_client import MongoDBClient
+
+__all__ = [
+    'BaseCollection',
+    'Index',
+    'MongoDBClient',
+]
