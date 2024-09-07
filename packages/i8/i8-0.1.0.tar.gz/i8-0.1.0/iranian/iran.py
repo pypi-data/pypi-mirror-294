8d823737cb6d1fefb130535ee98c33e8663accbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 05:07:13 +0000

    Add package i8/0.1.0/i8-0.1.0.tar.gz

---
+++ packages/i8/i8-0.1.0.tar.gz/i8-0.1.0/iranian/iran.py
@@ -0,0 +1,2 @@
+# import webview
+print("this is i8")
---
+++ packages/i8/i8-0.1.0.tar.gz/i8-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='i8',
+version='0.1.0',
+author='Z3N',
+author_email='iliya.souri7890@example.com',
+description='This is Iran',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.7'
+)
