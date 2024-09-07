cf5297f9deb511aac64910b01c88fe30f303bc1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-07 05:05:27 +0000

    Add package aigyminsper/0.2.1/aigyminsper-0.2.1.tar.gz

---
+++ packages/aigyminsper/aigyminsper-0.2.1.tar.gz/aigyminsper-0.2.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aigyminsper",
+    version="0.2.1",
+    packages=['aigyminsper','aigyminsper.search'],
+    author='Fabricio Barth',
+    author_email='fabriciojb@insper.edu.br',
+    description='A libray that helps you to learn Artificial Intelligence.',
+    long_description=open("ABOUT.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/insper/ai_gym/",
+)
