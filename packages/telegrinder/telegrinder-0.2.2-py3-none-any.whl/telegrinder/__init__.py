1adb2ac9f3af357bac516a4e43b59653315cb8e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 03:07:11 +0000

    Add package telegrinder/0.2.2/telegrinder-0.2.2-py3-none-any.whl

---
+++ packages/telegrinder/telegrinder-0.2.2-py3-none-any.whl/telegrinder/__init__.py
@@ -0,0 +1,229 @@
+"""Telegrinder
+
+Modern visionary telegram bot framework.
+
+* Type hinted
+* Customizable and extensible
+* Ready to use scenarios and rules
+* Fast models built on msgspec
+* Both low-level and high-level API
+* Support [optional dependecies](https://github.com/timoniq/telegrinder/blob/dev/docs/guide/optional_dependencies.md)
+
+Basic example:
+
+```python
+from telegrinder import API, Message, Telegrinder, Token
+from telegrinder.modules import logger
+from telegrinder.rules import Text
+
+api = API(token=Token("123:token"))
+bot = Telegrinder(api)
+logger.set_level("INFO")
+
+
+@bot.on.message(Text("/start"))
+async def start(message: Message):
+    me = (await api.get_me()).unwrap()
+    await message.answer(f"Hello, {message.from_user.full_name}! I'm {me.full_name}.")
+
+
+bot.run_forever()
+```
+"""
+
+import typing
+
+from .api import API, APIError, APIResponse, Token
+from .bot import (
+    ABCDispatch,
+    ABCHandler,
+    ABCMiddleware,
+    ABCPolling,
+    ABCReturnManager,
+    ABCRule,
+    ABCScenario,
+    ABCStateView,
+    ABCView,
+    AudioReplyHandler,
+    BaseCute,
+    BaseReturnManager,
+    BaseStateView,
+    BaseView,
+    CallbackQueryCute,
+    CallbackQueryReturnManager,
+    CallbackQueryRule,
+    CallbackQueryView,
+    ChatJoinRequestCute,
+    ChatJoinRequestRule,
+    ChatJoinRequestView,
+    ChatMemberUpdatedCute,
+    ChatMemberView,
+    Checkbox,
+    Choice,
+    Context,
+    Dispatch,
+    DocumentReplyHandler,
+    FuncHandler,
+    InlineQueryCute,
+    InlineQueryReturnManager,
+    InlineQueryRule,
+    MediaGroupReplyHandler,
+    MessageCute,
+    MessageReplyHandler,
+    MessageReturnManager,
+    MessageRule,
+    MessageView,
+    PhotoReplyHandler,
+    Polling,
+    RawEventView,
+    ShortState,
+    StickerReplyHandler,
+    Telegrinder,
+    UpdateCute,
+    VideoReplyHandler,
+    ViewBox,
+    WaiterMachine,
+    register_manager,
+)
+from .bot.rules import StateMeta
+from .client import ABCClient, AiohttpClient
+from .model import Model
+from .modules import logger
+from .tools import (
+    ABCErrorHandler,
+    ABCGlobalContext,
+    ABCLoopWrapper,
+    ABCStateStorage,
+    ABCTranslator,
+    ABCTranslatorMiddleware,
+    AnyMarkup,
+    Button,
+    CtxVar,
+    DelayedTask,
+    ErrorHandler,
+    FormatString,
+    GlobalContext,
+    HTMLFormatter,
+    I18nEnum,
+    InlineButton,
+    InlineKeyboard,
+    Keyboard,
+    KeyboardSetBase,
+    KeyboardSetYAML,
+    Lifespan,
+    LoopWrapper,
+    MemoryStateStorage,
+    ParseMode,
+    RowButtons,
+    SimpleI18n,
+    SimpleTranslator,
+    StateData,
+    ctx_var,
+    magic_bundle,
+)
+
+Update: typing.TypeAlias = UpdateCute
+Message: typing.TypeAlias = MessageCute
+ChatJoinRequest: typing.TypeAlias = ChatJoinRequestCute
+ChatMemberUpdated: typing.TypeAlias = ChatMemberUpdatedCute
+CallbackQuery: typing.TypeAlias = CallbackQueryCute
+InlineQuery: typing.TypeAlias = InlineQueryCute
+Bot: typing.TypeAlias = Telegrinder
+
+
+__all__ = (
+    "ABCClient",
+    "ABCDispatch",
+    "ABCErrorHandler",
+    "ABCGlobalContext",
+    "ABCHandler",
+    "ABCLoopWrapper",
+    "ABCMiddleware",
+    "ABCPolling",
+    "ABCReturnManager",
+    "ABCRule",
+    "ABCScenario",
+    "ABCStateStorage",
+    "ABCStateView",
+    "ABCTranslator",
+    "ABCTranslatorMiddleware",
+    "ABCView",
+    "API",
+    "APIError",
+    "APIResponse",
+    "AiohttpClient",
+    "AnyMarkup",
+    "BaseCute",
+    "BaseReturnManager",
+    "BaseStateView",
+    "BaseView",
+    "Bot",
+    "Button",
+    "CallbackQuery",
+    "CallbackQueryCute",
+    "CallbackQueryReturnManager",
+    "CallbackQueryRule",
+    "CallbackQueryView",
+    "ChatJoinRequest",
+    "ChatJoinRequestCute",
+    "ChatJoinRequestRule",
+    "ChatJoinRequestView",
+    "ChatMemberUpdated",
+    "ChatMemberUpdatedCute",
+    "ChatMemberView",
+    "Checkbox",
+    "Choice",
+    "CtxVar",
+    "DelayedTask",
+    "Dispatch",
+    "ErrorHandler",
+    "FormatString",
+    "FuncHandler",
+    "GlobalContext",
+    "HTMLFormatter",
+    "I18nEnum",
+    "InlineButton",
+    "InlineKeyboard",
+    "InlineQuery",
+    "InlineQueryCute",
+    "InlineQueryReturnManager",
+    "InlineQueryRule",
+    "Keyboard",
+    "KeyboardSetBase",
+    "KeyboardSetYAML",
+    "Lifespan",
+    "LoopWrapper",
+    "MediaGroupReplyHandler",
+    "MemoryStateStorage",
+    "Message",
+    "MessageCute",
+    "MessageReplyHandler",
+    "MessageReplyHandler",
+    "MessageReturnManager",
+    "MessageRule",
+    "MessageView",
+    "Model",
+    "ParseMode",
+    "PhotoReplyHandler",
+    "Polling",
+    "RawEventView",
+    "RowButtons",
+    "ShortState",
+    "SimpleI18n",
+    "SimpleTranslator",
+    "StateData",
+    "StateMeta",
+    "StickerReplyHandler",
+    "Telegrinder",
+    "Token",
+    "Update",
+    "UpdateCute",
+    "VideoReplyHandler",
+    "ViewBox",
+    "WaiterMachine",
+    "ctx_var",
+    "logger",
+    "magic_bundle",
+    "register_manager",
+    "Context",
+)
