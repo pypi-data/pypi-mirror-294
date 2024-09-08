213102c4fc7e7ac74eb47456df0319695140f027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 03:07:04 +0000

    Add package starstream/0.0.3/starstream-0.0.3-py3-none-any.whl

---
+++ packages/starstream/starstream-0.0.3-py3-none-any.whl/starstream/proba_2.py
@@ -0,0 +1,80 @@
+from typing import Callable, Coroutine, List, Tuple
+from .utils import datetime_interval, handle_client_connection_error
+from astropy.io import fits
+from io import BytesIO
+import numpy as np
+import aiofiles
+import os
+import asyncio
+from datetime import datetime, timedelta
+
+__all__ = ["PROBA_2"]
+
+
+class PROBA_2:
+    class LYRA:
+        def __init__(self, sequence_length: timedelta):
+            self.url: Callable[[str], str] = (
+                lambda date: f"http://proba2.oma.be/lyra/data/bsd/{date[:4]}/{date[4:6]}/{date[6:]}/lyra_{date}-000000_lev3_std.fits"
+            )
+            self.lyra_folder_path: str = "./data/LYRA/"
+            self.lyra_fits_path: Callable[[str], str] = (
+                lambda date: f"./data/LYRA/{date}.fits"
+            )
+            self.lyra_csv_path: Callable[[str], str] = (
+                lambda date: f"./data/LYRA/{date}.csv"
+            )
+            self.sl: timedelta = sequence_length
+            os.makedirs(self.lyra_folder_path, exist_ok=True)
+
+        def get_check_tasks(self, scrap_date: Tuple[datetime, datetime]) -> None:
+            new_scrap_date: List[str] = datetime_interval(
+                *scrap_date, timedelta(days=1)
+            )
+            self.new_scrap_date_list: List[str] = [
+                date
+                for date in new_scrap_date
+                if not os.path.exists(self.lyra_csv_path(date))
+            ]
+
+        def get_download_tasks(self, session) -> List[Coroutine]:
+            return [
+                self.download_url(session, date) for date in self.new_scrap_date_list
+            ]
+
+        @handle_client_connection_error(
+            max_retries=3, default_cooldown=5, increment="exp"
+        )
+        async def download_url(self, session, date: str) -> None:
+            async with session.get(self.url(date), ssl=False) as response:
+                data = await response.read()
+                async with aiofiles.open(self.lyra_fits_path(date), "wb") as f:
+                    await f.write(data)
+
+        def get_preprocessing_tasks(self) -> List[Coroutine]:
+            return [self.preprocessing(date) for date in self.new_scrap_date_list]
+
+        async def preprocessing(self, date: str) -> None:
+            async with aiofiles.open(self.lyra_fits_path(date), "rb") as f:
+                data = await f.read()
+                with fits.open(BytesIO(data)) as hdul:
+                    np.savetxt(
+                        self.lyra_csv_path(date),
+                        np.array(list(hdul[1].data), dtype=np.float32),
+                        delimiter=",",
+                    )
+            os.remove(self.lyra_fits_path(date))
+
+        async def downloader_pipeline(
+            self, scrap_date: Tuple[datetime, datetime], session
+        ) -> None:
+            self.get_check_tasks(scrap_date)
+            if len(self.new_scrap_date_list) == 0:
+                print("Already downloaded!")
+            else:
+                await asyncio.gather(*self.get_download_tasks(session))
+                await asyncio.gather(*self.get_preprocessing_tasks())
+
+
+def save_npy(file, array) -> None:
+    np.savetxt(file, array, delimiter=",")
---
+++ packages/starstream/starstream-0.0.3-py3-none-any.whl/starstream/secchi.py
@@ -0,0 +1,94 @@
+import asyncio
+from typing import Coroutine, List
+from .utils import datetime_interval, handle_client_connection_error
+from datetime import timedelta
+from io import BytesIO
+from bs4 import BeautifulSoup
+import glob
+from itertools import chain
+import os
+from PIL import Image
+
+__all__ = ["STEREO"]
+
+
+class STEREO:
+    class SECCHI:
+        class EUVI:
+            def __init__(self) -> None:
+                self.url: Callable[[str, str, str], str] = (
+                    lambda date, wavelength, name: f"https://stereo-ssc.nascom.nasa.gov/data/ins_data/secchi/wavelets/pngs/{date[:6]}/{date[6:]}/{wavelength}_A/{name}"
+                )
+                self.euvi_png_path: Callable[[str], str] = (
+                    lambda name: f'./data/SECCHI/EUVI/{name.split("_")[-2][:3]}/{name}'
+                )
+                self.wavelengths: List[str] = ["171", "195", "284", "304"]
+
+                for wavelength in self.wavelengths:
+                    os.makedirs(f"./data/SECCHI/EUVI/{wavelength}", exist_ok=True)
+
+            async def scrap_date_names(self, session, date: str) -> List[str]:
+                async with session.get(self.url(date, ""), ssl=False) as response:
+                    html = await response.text()
+                    soup = BeautifulSoup(html, "html.parser")
+                    sizes = [
+                        size.text.strip()
+                        for size in soup.find_all(
+                            "td",
+                            align="right",
+                            text=lambda text: text.endswith("M") or text.endswith("K"),
+                        )
+                    ]
+                    names = [
+                        name["href"]
+                        for name in soup.find_all(
+                            "a", href=lambda href: href.endswith("R.png")
+                        )
+                    ]
+                    return [
+                        name for size, name in zip(sizes, names) if size.endswith("M")
+                    ]
+
+            @handle_client_connection_error(
+                increment="exp", default_cooldown=5, max_retries=3
+            )
+            async def download_url(self, session, name: str) -> None:
+                async with session.get(
+                    self.url(name.split("_")[0], name), ssl=False
+                ) as response:
+                    data = await response.read()
+                    img = await asyncio.get_event_loop().run_in_executor(
+                        None, Image.open, BytesIO(data)
+                    )
+                    await asyncio.get_event_loop().run_in_executor(
+                        None, img.save, self.euvi_png_path(name), "PNG"
+                    )
+
+            def get_scrap_names_tasks(self, scrap_date, session) -> List[Coroutine]:
+                return [self.scrap_date_names(session, date) for date in scrap_date]
+
+            def check_tasks(self, scrap_date) -> None:
+                self.new_scrap_date_list = [
+                    date
+                    for date in scrap_date
+                    if len(glob.glob(self.euvi_png_path(date, date + "*"))) == 0
+                ]
+
+            def get_days(self, scrap_date):
+                return [
+                    *chain.from_iterable(
+                        [
+                            glob.glob(self.euvi_png_path(date, "*"))
+                            for date in scrap_date
+                        ]
+                    )
+                ]
+
+            def data_prep(self, scrap_date):
+                scrap_date = datetime_interval(
+                    scrap_date[0], scrap_date[-1], timedelta(days=1)
+                )
+                return self.get_days(scrap_date)
+
+            def get_slice(self, idx: slice):
+                pass
