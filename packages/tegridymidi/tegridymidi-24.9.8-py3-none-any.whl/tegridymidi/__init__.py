841eaaad73f3444717c816432e83f51ed7411fd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 03:06:33 +0000

    Add package tegridymidi/24.9.8/tegridymidi-24.9.8-py3-none-any.whl

---
+++ packages/tegridymidi/tegridymidi-24.9.8-py3-none-any.whl/tegridymidi/__init__.py
@@ -0,0 +1,42 @@
+#=======================================================================================================
+# Tegridy MIDI __init__ module
+#=======================================================================================================
+
+#=======================================================================================================
+# CPU Imports
+#=======================================================================================================
+
+from .bits_and_ints import *
+from .chords import *
+from .constants import *
+from .files_helpers import *
+from .haystack_search import *
+from .helpers import *
+from .karaoke import *
+from .matrixes import *
+from .melodies import *
+from .misc import *
+from .plots import *
+from .processors import *
+from .sample_midis_helpers import *
+from .tokenizers import *
+
+#=======================================================================================================
+# GPU Imports
+#=======================================================================================================
+
+try:
+  
+  import torch
+  
+  if torch.cuda.is_available():
+    
+    from .x_transformer import *
+    import random
+      
+except ImportError:
+  pass
+
+#=======================================================================================================
+# This is the end of __init__ module
+#=======================================================================================================
