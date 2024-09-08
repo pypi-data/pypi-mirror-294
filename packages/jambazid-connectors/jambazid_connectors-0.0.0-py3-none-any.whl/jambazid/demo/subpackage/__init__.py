88af45a9b54eee32d12f079706af09d5505dab87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 04:06:31 +0000

    Add package jambazid-connectors/0.0.0/jambazid_connectors-0.0.0-py3-none-any.whl

---
+++ packages/jambazid-connectors/jambazid_connectors-0.0.0-py3-none-any.whl/jambazid/demo/subpackage/__init__.py
@@ -0,0 +1,30 @@
+"""
+## Heading 1
+
+This will also recognise markdown.
+
+```sh
+!/bin/bash
+
+echo "markdown syntax highlighting."
+```
+
+## Heading 2
+
+...
+
+
+## Heading 3
+
+...
+
+## Mermaid Support
+
+```mermaid
+graph TD;
+    A-->B;
+    A-->C;
+    B-->D;
+    C-->D;
+```
+"""
