57745bce5833d88e4ce3bacb3ad198322f0498e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 04:06:04 +0000

    Add package pytest-takeltest/0.12.7/pytest-takeltest-0.12.7.tar.gz

---
+++ packages/pytest-takeltest/pytest-takeltest-0.12.7.tar.gz/pytest-takeltest-0.12.7/takeltest/__init__.py
@@ -0,0 +1,7 @@
+from takeltest.boilerplate import hosts  # noqa F401
+import takeltest.moleculebook
+import takeltest.moleculeenv
+import takeltest.plugin
+import takeltest.multitestvars  # noqa F401
+
+name = "takeltest"
