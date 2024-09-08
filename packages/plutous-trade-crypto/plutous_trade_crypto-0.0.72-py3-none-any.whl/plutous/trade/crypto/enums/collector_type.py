0d488ebfef9bd5752b1753576f91adeb9d3bd5af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 04:07:28 +0000

    Add package plutous-trade-crypto/0.0.72/plutous_trade_crypto-0.0.72-py3-none-any.whl

---
+++ packages/plutous-trade-crypto/plutous_trade_crypto-0.0.72-py3-none-any.whl/plutous/trade/crypto/enums/collector_type.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class CollectorType(Enum):
+    FUNDING_RATE = "funding_rate"
+    LONG_SHORT_RATIO = "long_short_ratio"
+    OHLCV = "ohlcv"
+    OPEN_INTEREST = "open_interest"
+    ORDERBOOK = "orderbook"
