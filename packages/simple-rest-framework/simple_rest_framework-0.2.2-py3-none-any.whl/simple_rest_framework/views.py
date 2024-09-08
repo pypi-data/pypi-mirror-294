a8bce7e4bf573814b25ba7c25dc71b44ea7330dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 03:06:42 +0000

    Add package simple-rest-framework/0.2.2/simple_rest_framework-0.2.2-py3-none-any.whl

---
+++ packages/simple-rest-framework/simple_rest_framework-0.2.2-py3-none-any.whl/simple_rest_framework/views.py
@@ -0,0 +1,41 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .services import BaseService
+from .exceptions import HandleExceptionsMixin
+
+class BaseView(HandleExceptionsMixin, APIView, BaseService): pass
+
+class BaseABMView(HandleExceptionsMixin, APIView, BaseService):
+    def get(self, request, id):
+        self.set_objeto(id)
+
+        return Response(self.get_serializado())
+
+    def post(self, request):
+        self.crear(**request.data)
+
+        return Response({
+            "mensaje": f"{self.modelo.MODELO_SINGULAR} creado con éxito.", 
+            "data": self.get_serializado()
+            })
+
+    def put(self, request):
+        objeto_id = request.data.get('id')
+        self.set_objeto(objeto_id)
+        self.actualizar(**request.data)
+
+        return Response({
+            "mensaje": f"{self.modelo.MODELO_SINGULAR} actualizado con éxito.", 
+            "data": self.get_serializado()
+            })
+
+    def delete(self, request):
+        objeto_id = request.data.get('id')
+        self.set_objeto(objeto_id)
+        self.eliminar()
+
+        return Response({
+            "mensaje": f"{self.modelo.MODELO_SINGULAR} eliminado con éxito."
+            })
+    
