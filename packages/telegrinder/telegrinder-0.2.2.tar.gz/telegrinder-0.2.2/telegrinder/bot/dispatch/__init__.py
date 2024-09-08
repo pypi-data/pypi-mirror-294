3969d98313bffc8b1d73061ee4b718bba59a0158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 03:07:11 +0000

    Add package telegrinder/0.2.2/telegrinder-0.2.2.tar.gz

---
+++ packages/telegrinder/telegrinder-0.2.2.tar.gz/telegrinder-0.2.2/telegrinder/bot/dispatch/__init__.py
@@ -0,0 +1,79 @@
+from telegrinder.bot.dispatch.abc import ABCDispatch
+from telegrinder.bot.dispatch.context import Context
+from telegrinder.bot.dispatch.dispatch import Dispatch, TelegrinderContext
+from telegrinder.bot.dispatch.handler import (
+    ABCHandler,
+    AudioReplyHandler,
+    DocumentReplyHandler,
+    FuncHandler,
+    MediaGroupReplyHandler,
+    MessageReplyHandler,
+    PhotoReplyHandler,
+    StickerReplyHandler,
+    VideoReplyHandler,
+)
+from telegrinder.bot.dispatch.middleware import ABCMiddleware
+from telegrinder.bot.dispatch.process import check_rule, process_inner
+from telegrinder.bot.dispatch.return_manager import (
+    ABCReturnManager,
+    BaseReturnManager,
+    CallbackQueryReturnManager,
+    InlineQueryReturnManager,
+    Manager,
+    MessageReturnManager,
+    register_manager,
+)
+from telegrinder.bot.dispatch.view import (
+    ABCStateView,
+    ABCView,
+    BaseStateView,
+    BaseView,
+    CallbackQueryView,
+    ChatJoinRequestView,
+    ChatMemberView,
+    InlineQueryView,
+    MessageView,
+    RawEventView,
+    ViewBox,
+)
+from telegrinder.bot.dispatch.waiter_machine import ShortState, WaiterMachine, clear_wm_storage_worker
+
+__all__ = (
+    "ABCDispatch",
+    "ABCHandler",
+    "ABCMiddleware",
+    "ABCReturnManager",
+    "ABCStateView",
+    "ABCView",
+    "AudioReplyHandler",
+    "BaseReturnManager",
+    "BaseStateView",
+    "BaseView",
+    "CallbackQueryReturnManager",
+    "CallbackQueryView",
+    "ChatJoinRequestView",
+    "ChatMemberView",
+    "Context",
+    "Dispatch",
+    "DocumentReplyHandler",
+    "FuncHandler",
+    "InlineQueryReturnManager",
+    "InlineQueryView",
+    "Manager",
+    "MediaGroupReplyHandler",
+    "MessageReplyHandler",
+    "MessageReturnManager",
+    "MessageView",
+    "PhotoReplyHandler",
+    "RawEventView",
+    "ShortState",
+    "StickerReplyHandler",
+    "TelegrinderContext",
+    "VideoReplyHandler",
+    "ViewBox",
+    "WaiterMachine",
+    "check_rule",
+    "clear_wm_storage_worker",
+    "process_inner",
+    "register_manager",
+)
