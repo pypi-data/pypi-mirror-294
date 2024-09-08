e131f09fd525d514f289bd8358cc6b3ac9c82bee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 04:06:55 +0000

    Add package starstream/0.0.4/starstream-0.0.4-py3-none-any.whl

---
+++ packages/starstream/starstream-0.0.4-py3-none-any.whl/starstream/_base.py
@@ -0,0 +1,93 @@
+from .utils import datetime_interval, timedelta_to_freq, handle_client_connection_error
+from datetime import timedelta, datetime
+from spacepy import pycdf
+import pandas as pd
+import numpy as np
+import aiofiles
+import asyncio
+import os
+from typing import Callable, List, Tuple
+
+
+class CDAWeb:
+    phy_obs: List[str]
+    batch_size = 8
+    variables: List[str]
+    csv_path: Callable[[str], str]
+    cdf_path: Callable[[str], str]
+    url: Callable[[str], str]
+    root_path: str
+
+    def default_cda_processing(self, cdf_file, date):
+        epoch = cdf_file["Epoch"][:].reshape(-1)
+        data_columns = [
+            (
+                cdf_file[var][:].reshape(-1, 1)
+                if len(cdf_file[var][:].shape) == 1
+                else cdf_file[var][:].reshape(epoch.shape[0], -1)
+            )
+            for var in self.phy_obs
+        ]
+        data = np.concatenate(data_columns, axis=1).astype(np.float32)
+        pd.DataFrame(
+            data,
+            columns=self.variables,
+            index=pd.to_datetime(epoch),
+        ).resample("1T").mean().to_csv(self.csv_path(date))
+
+    def check_tasks(self, scrap_date: Tuple[datetime, datetime]):
+        new_scrap_date: List[str] = datetime_interval(*scrap_date, timedelta(days=1))
+        self.new_scrap_date_list = [
+            date for date in new_scrap_date if not os.path.exists(self.csv_path(date))
+        ]
+        os.makedirs(self.root_path, exist_ok=True)
+
+    def get_download_tasks(self, session):
+        return [self.download_url(session, date) for date in self.new_scrap_date_list]
+
+    @handle_client_connection_error(default_cooldown=5, increment="exp", max_retries=5)
+    async def download_url(self, session, date: str):
+        url = self.url(date)
+        async with session.get(url, ssl=False) as response:
+            cdf_data = await response.read()
+            async with aiofiles.open(self.cdf_path(date), "wb") as f:
+                await f.write(cdf_data)
+
+    async def preprocessing(self, date):
+        with pycdf.CDF(self.cdf_path(date)) as cdf_file:
+            self.default_cda_processing(cdf_file, date)
+        os.remove(self.cdf_path(date))
+
+    def get_preprocessing_tasks(self):
+        return [self.preprocessing(date) for date in self.new_scrap_date_list]
+
+    async def downloader_pipeline(self, scrap_date, session):
+        self.check_tasks(scrap_date)
+        tasks = self.get_download_tasks(session)
+
+        for i in range(0, len(self.new_scrap_date_list), self.batch_size):
+            await asyncio.gather(*tasks[i : i + self.batch_size])
+
+        await asyncio.gather(*self.get_preprocessing_tasks())
+
+    async def get_df_unit(self, date: str) -> pd.DataFrame:
+        df = await asyncio.get_event_loop().run_in_executor(
+            None, pd.read_csv, self.csv_path(date)
+        )
+        return df
+
+    async def get_df(self, scrap_date: Tuple[datetime, datetime]) -> List[pd.DataFrame]:
+        new_scrap_date: List[str] = datetime_interval(*scrap_date, timedelta(days=1))
+        dfs = await asyncio.gather(*[self.get_df_unit(date) for date in new_scrap_date])
+        return dfs
+
+    def data_prep(
+        self, scrap_date: Tuple[datetime, datetime], step_size: timedelta
+    ) -> pd.DataFrame:
+        dfs = asyncio.run(self.get_df(scrap_date))
+        df = pd.concat(dfs)
+        return (
+            df[(df.index >= scrap_date[0]) & (df.index <= scrap_date[-1])]
+            .resample(timedelta_to_freq(step_size))
+            .mean()
+        )
---
+++ packages/starstream/starstream-0.0.4-py3-none-any.whl/starstream/omni.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+from typing import Coroutine, List, Tuple
+from .utils import asyncCDF, datetime_interval, handle_client_connection_error
+from ._base import CDAWeb
+import aiofiles
+import asyncio
+import os
+from dateutil.relativedelta import relativedelta
+
+__all__ = ["OMNI"]
+
+
+class OMNI(CDAWeb):
+    def __init__(self) -> None:
+        super().__init__()
+        self.url = (
+            lambda date: f"https://cdaweb.gsfc.nasa.gov/sp_phys/data/omni/hro2_5min/{date[:4]}/omni_hro2_5min_{date}01_v01.cdf"
+        )
+        self.phy_obs = [
+            "BX_GSE",
+            "BY_GSE",
+            "BZ_GSE",
+            "Mach_num",
+            "Mgs_mach_num",
+            "PR-FLX_10",
+            "PR-FLX_30",
+            "PR-FLX_60",
+            "proton_density",
+            "flow_speed",
+            "Vx",
+            "Vy",
+            "Vz",
+        ]
+        self.variables = self.phy_obs
+        self.csv_path = lambda date: f"./data/OMNI/HRO2/{date}.csv"
+        self.cdf_path = lambda date: f"./data/OMNI/HRO2/{date}.cdf"
+        os.makedirs("./data/OMNI/HRO2/", exist_ok=True)
+
+    def check_tasks(self, scrap_date: Tuple[datetime, datetime]):
+        new_scrap_date: List[str] = datetime_interval(
+            *scrap_date, relativedelta(months=1), "%Y%m"
+        )
+        self.new_scrap_date_list: List[str] = [
+            date for date in new_scrap_date if not os.path.exists(self.csv_path(date))
+        ]
+
+    @handle_client_connection_error(max_retries=3, default_cooldown=5, increment="exp")
+    async def download_url(self, session, date: str):
+        async with session.get(self.url(date), ssl=False) as response:
+            cdf_data = await response.read()
+            async with aiofiles.open(self.cdf_path(date), mode="wb") as f:
+                await f.write(cdf_data)
+
+    def get_download_tasks(self, session) -> List[Coroutine]:
+        return [self.download_url(session, date) for date in self.new_scrap_date_list]
+
+    async def preprocessing(self, date: str) -> None:
+        await asyncCDF(self.cdf_path(date), self.default_cda_processing, date)
+        os.remove(self.cdf_path(date))
+
+    def get_preprocessing_tasks(self) -> List[Coroutine]:
+        return [self.preprocessing(date) for date in self.new_scrap_date_list]
+
+    async def downloader_pipeline(self, scrap_date: Tuple[datetime, datetime], session):
+        self.check_tasks(scrap_date)
+        await asyncio.gather(*self.get_download_tasks(session))
+        await asyncio.gather(*self.get_preprocessing_tasks())
