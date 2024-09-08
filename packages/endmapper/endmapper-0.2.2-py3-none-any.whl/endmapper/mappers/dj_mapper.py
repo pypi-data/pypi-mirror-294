3c21c4057204e36b3ef2d1696e92b4fb895a54b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 04:06:56 +0000

    Add package endmapper/0.2.2/endmapper-0.2.2-py3-none-any.whl

---
+++ packages/endmapper/endmapper-0.2.2-py3-none-any.whl/endmapper/mappers/dj_mapper.py
@@ -0,0 +1,23 @@
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from endmapper.mappers import base_mapper
+from endmapper import endpoint_handlers
+
+
+class DjangoEndpointMapper(APIView, base_mapper.BaseEndpointMapper):
+    """
+    ATTENTION: add "path("", include("endmapper.urls"))" to main urls
+
+    This will add new endpoint "api/endpoints/" to your project
+    """
+    @staticmethod
+    def get(response):
+        config = super().config()
+        result = endpoint_handlers.DjangoEndpointHandler(**config).result
+        return Response(result, status=status.HTTP_200_OK)
+
+# path_white_list=['api'], name_black_list=['schema'],
+#                                 microservices={
+#                                     "communicator": "http://communicator-app-1:8000/api/search/endpoints/"}
