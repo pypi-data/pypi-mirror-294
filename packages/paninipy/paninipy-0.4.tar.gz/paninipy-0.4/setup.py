b9097a8223f131107abbc9862cc0cef64cb5129c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 03:06:36 +0000

    Add package paninipy/0.4/paninipy-0.4.tar.gz

---
+++ packages/paninipy/paninipy-0.4.tar.gz/paninipy-0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages, Extension
+from Cython.Build import cythonize
+import numpy
+
+setup(
+    name='paninipy',
+    version='0.4',
+    description='Packageof Algorithms for Nonparametric Inference on Networks in Python',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://paninipy.readthedocs.io/en/latest/index.html',
+    author='Baiyue He',
+    author_email='baiyueh@hku.hk',
+    license='The Unlicense',
+    projects_urls={
+        "Documentation": "https://paninipy.readthedocs.io/en/latest/index.html",
+        "Source": "https://paninipy.readthedocs.io/en/latest/index.html"
+    },
+    python_requires=">=3.9, <3.12",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={"console_scripts": ["paper = paper.cli:main"]},
+)
