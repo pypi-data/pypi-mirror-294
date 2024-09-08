bfcffad69d60b6b4e217810791d9a910326638cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 04:06:31 +0000

    Add package xia-module-terraform-gcs/0.0.16/xia-module-terraform-gcs-0.0.16.tar.gz

---
+++ packages/xia-module-terraform-gcs/xia-module-terraform-gcs-0.0.16.tar.gz/xia-module-terraform-gcs-0.0.16/xia_module_terraform_gcs/__init__.py
@@ -0,0 +1,20 @@
+from xia_module_terraform_gcs.foundation_gcs import FoundationGcsState
+from xia_module_terraform_gcs.foundation_backend_gcs import FoundationGcsBackend
+from xia_module_terraform_gcs.application_gcs import ApplicationGcsState
+from xia_module_terraform_gcs.application_backend_gcs import ApplicationGcsBackend
+
+modules = {
+    "module-foundation-state-gcs": "FoundationGcsState",
+    "module-foundation-backend-gcs": "FoundationGcsBackend",
+    "module-application-state-gcs": "ApplicationGcsState",
+    "module-application-backend-gcs": "ApplicationGcsBackend",
+}
+
+__all__ = [
+    "FoundationGcsState",
+    "FoundationGcsBackend",
+    "ApplicationGcsState",
+    "ApplicationGcsBackend"
+]
+
+__version__ = "0.0.16"
