d26c9f7d4472edb893fa830b5c31675b71420f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 03:06:48 +0000

    Add package mangokit/1.0.1/mangokit-1.0.1-py3-none-any.whl

---
+++ packages/mangokit/mangokit-1.0.1-py3-none-any.whl/src/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Project: MangoServer
+# @Description: 
+# @Time   : 2024-09-07 22:15
+# @Author : 毛鹏
+__version__ = '1.0.1'
+
+from src.data_processor import DataProcessor, DataClean, ObtainRandomData
+from src.mail_send import mail_send
