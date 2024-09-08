bdc364356d95fe315913c606da525733512bab89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 03:06:52 +0000

    Add package mkdocstrings-python-legacy/0.2.4/mkdocstrings_python_legacy-0.2.4.tar.gz

---
+++ packages/mkdocstrings-python-legacy/mkdocstrings_python_legacy-0.2.4.tar.gz/mkdocstrings_python_legacy-0.2.4/tests/test_collector.py
@@ -0,0 +1,31 @@
+"""Tests for the `collector` module."""
+
+from unittest import mock
+
+import pytest
+from mkdocstrings.handlers.base import CollectionError
+
+from mkdocstrings_handlers.python import get_handler
+
+
+@pytest.mark.parametrize(
+    ("retval", "exp_res"),
+    [
+        ({"error": "error1", "traceback": "hello"}, "error1\nhello"),
+        ({"error": "error1"}, "error1"),
+        ({"error": "", "traceback": "hello"}, "\nhello"),
+    ],
+)
+def test_collect_result_error(retval: dict, exp_res: str) -> None:
+    """Test handling of errors when collecting an object.
+
+    Args:
+        retval: Return value to mock `json.loads` with.
+        exp_res: Expected result.
+    """
+    with mock.patch("mkdocstrings_handlers.python.handler.json.loads") as m_loads:  # noqa: SIM117
+        with pytest.raises(CollectionError) as excinfo:  # noqa: PT012
+            m_loads.return_value = retval
+            handler = get_handler("material")
+            assert handler.collect("", {})
+            assert str(excinfo.value) == exp_res
