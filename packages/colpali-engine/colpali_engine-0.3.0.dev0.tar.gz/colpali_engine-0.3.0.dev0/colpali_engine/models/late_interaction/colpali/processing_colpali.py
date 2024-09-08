9a6bca2aced3cefe9a70f17c32613d0db0ad9948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 04:06:16 +0000

    Add package colpali-engine/0.3.0.dev0/colpali_engine-0.3.0.dev0.tar.gz

---
+++ packages/colpali-engine/colpali_engine-0.3.0.dev0.tar.gz/colpali_engine-0.3.0.dev0/colpali_engine/models/late_interaction/colpali/processing_colpali.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+
+from typing import List, Optional, cast
+
+from PIL import Image
+from transformers import BatchEncoding, BatchFeature, LlamaTokenizerFast, PaliGemmaProcessor
+
+from colpali_engine.utils.processing_utils import BaseVisualRetrieverProcessor
+
+
+class ColPaliProcessor(BaseVisualRetrieverProcessor):
+    """
+    Processor for ColPali.
+    """
+
+    def __init__(
+        self,
+        vlm_backbone_model_name_or_path: str = "google/paligemma-3b-mix-448",
+    ):
+        super().__init__()
+
+        # TODO: After ColPali integration in transformers where ColPaliProcessor will copy
+        # PaligemmaProcessor, remove `self.processor` and use `self` directly.
+        self.processor = cast(PaliGemmaProcessor, PaliGemmaProcessor.from_pretrained(vlm_backbone_model_name_or_path))
+        self.tokenizer = cast(LlamaTokenizerFast, self.processor.tokenizer)  # type: ignore
+
+    def process_images(
+        self,
+        images: List[Image.Image],
+    ) -> BatchFeature:
+        """
+        Process images for ColPali.
+        """
+        texts_doc = ["Describe the image."] * len(images)
+        images = [image.convert("RGB") for image in images]
+
+        batch_doc = self.processor(
+            text=texts_doc,
+            images=images,
+            return_tensors="pt",
+            padding="longest",
+        )
+        return batch_doc
+
+    def process_queries(
+        self,
+        queries: List[str],
+        max_length: int = 50,
+        suffix: Optional[str] = None,
+    ) -> BatchEncoding:
+        """
+        Process queries for ColPali.
+
+        NOTE: This method uses the PaliGemma tokenizer instead of the processor because
+        the processor cannot handle text without images.
+        """
+        suffix = suffix or "<pad>" * 10
+        texts_query: List[str] = []
+
+        for query in queries:
+            query = f"Question: {query}"
+            query += suffix  # add suffix (pad tokens)
+            texts_query.append(query)
+
+        batch_query = self.tokenizer(
+            texts_query,
+            return_tensors="pt",
+            padding="longest",
+            max_length=max_length,
+        )
+        return batch_query
