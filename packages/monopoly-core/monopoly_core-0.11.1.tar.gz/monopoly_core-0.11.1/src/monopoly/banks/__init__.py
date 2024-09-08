40254be9dbe202f41f9a4388af57a1a03c199111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 03:07:01 +0000

    Add package monopoly-core/0.11.1/monopoly_core-0.11.1.tar.gz

---
+++ packages/monopoly-core/monopoly_core-0.11.1.tar.gz/monopoly_core-0.11.1/src/monopoly/banks/__init__.py
@@ -0,0 +1,26 @@
+import logging
+from typing import Type
+
+from .base import BankBase
+from .citibank import Citibank
+from .dbs import Dbs
+from .detector import BankDetector
+from .example_bank import ExampleBank
+from .hsbc import Hsbc
+from .maybank import Maybank
+from .ocbc import Ocbc
+from .standard_chartered import StandardChartered
+
+banks: list[Type["BankBase"]] = [
+    Citibank,
+    Dbs,
+    ExampleBank,
+    Hsbc,
+    Maybank,
+    Ocbc,
+    StandardChartered,
+]
+
+logger = logging.getLogger(__name__)
+
+__all__ = ["BankDetector", "BankBase", *[bank.__name__ for bank in banks]]
