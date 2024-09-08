73e6cfba3000fbe83b7ba6241352f6c0d7300cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 04:06:11 +0000

    Add package endmapper/0.1.8/endmapper-0.1.8.tar.gz

---
+++ packages/endmapper/endmapper-0.1.8.tar.gz/endmapper-0.1.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='endmapper',
+    version='0.1.8',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        # добавьте зависимости, если есть
+    ],
+)
