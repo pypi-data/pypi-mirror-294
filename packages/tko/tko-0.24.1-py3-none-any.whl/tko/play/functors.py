420113af6012b61744df9f864ccf152b9bea055f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 03:06:20 +0000

    Add package tko/0.24.1/tko-0.24.1-py3-none-any.whl

---
+++ packages/tko/tko-0.24.1-py3-none-any.whl/tko/play/functors.py
@@ -0,0 +1,19 @@
+from .floating_manager import FloatingManager
+from .flags import Flag, Flags
+from .floating import Floating
+from ..util.sentence import Sentence
+
+class FlagFunctor:
+    def __init__(self, flag: Flag):
+        self.flag = flag
+
+    def __call__(self):
+        self.flag.toggle()
+
+class GradeFunctor:
+    def __init__(self, grade: int, fn):
+        self.grade = grade
+        self.fn = fn
+
+    def __call__(self):
+        self.fn(self.grade)
