344cd1c7efa449152f9ee36f597ef5fd0995e186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 03:07:04 +0000

    Add package endmapper/0.1.4/endmapper-0.1.4.tar.gz

---
+++ packages/endmapper/endmapper-0.1.4.tar.gz/endmapper-0.1.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import glob
+import os
+
+scripts = [f for f in glob.glob(os.path.join('bin', '*')) if os.path.isfile(f)]
+
+setup(
+    name='endmapper',
+    version='0.1.4',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'endmapper': ['bin/*']
+    },
+    scripts=scripts,
+    install_requires=[
+        # добавьте зависимости, если есть
+    ],
+    test_suite='tests',
+)
